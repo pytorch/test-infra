8c117528734be0813d6066972f158eff88f0bd47
Elton Pinto <eltonpinto@fb.com>
2021-05-25 08:19:52 -0700

    Address PR comments Summary: - Quote bash vars - Fix spelling errors - Turn optimizations on for llvm build - Fix cloning+patch code - Make script to stop on first error (using set -e)

--- clang-tidy-checks/setup.sh
+++ clang-tidy-checks/setup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+set -e
+
 function info() {
-  echo "[info]: " $1
+  echo "[info]:" "$1"
 }
 
 function success() {
@@ -10,26 +12,24 @@ function success() {
 
 function check_requirements() {
   info "checking requirements"
-  cmake --version &&
-  gcc --version &&
-  python3 --version &&
-  ninja --version &&
-  lld --version &&
+  cmake --version
+  gcc --version
+  python3 --version
+  ninja --version
+  ld.lld --version
   success
 }
 
 function clone_llvm() {
   info "cloning llvm"
-  git clone https://github.com/llvm/llvm-project.git &&
-  git fetch --all --tags &&
-  git checkout -b tags/llvmorg-11.0.0 &&
+  git clone -b llvmorg-11.0.0 https://github.com/llvm/llvm-project.git --depth=1
   success
 }
 
 function apply_patches() {
   info "applying patches"
   cd llvm-project
-  patch potential-unbounded-loop-check.diff &&
+  cat ../potential-unbounded-loop-check.diff | patch -p1 -N -d .
   success
 }
 
@@ -38,7 +38,7 @@ function build() {
   cd build
   cmake -DCMAKE_C_COMPILER=clang \
         -DCMAKE_CXX_COMPILER=clang++ \
-        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_BUILD_TYPE=RelWithDebInfo \
         -DLLVM_ENABLE_PROJECTS=clang,clang-tools-extra \
         -DLLVM_USE_LINKER=lld \
         -DLLVM_TARGETS_TO_BUILD="X86" \
@@ -46,15 +46,15 @@ function build() {
         -DCLANG_ENABLE_ARCMT=OFF \
         -DLLVM_BUILD_TOOLS=OFF \
         -DLLVM_BUILD_UTILS=OFF \
-        -DCMAKE_CXX_FLAGS_RELEASE="-O0" \
-        -GNinja ../llvm  &&
-  ninja
+        -GNinja ../llvm
+  cmake --build build
 }
 
 function setup() {
-  clone_llvm &&
-  apply_patches &&
+  clone_llvm
+  apply_patches
   build
 }
 
-check_requirements && setup
+check_requirements
+setup
