bdaf1ff0bd974557e43245950e4abba4e474db5f
Eli Uriegas <eliuriegas@fb.com>
2021-06-01 18:11:32 -0700

    runners: Remove choco install from user data script
    Has been added tob ase AMI instead
    
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

--- modules/runners/templates/user-data.ps1
+++ modules/runners/templates/user-data.ps1
@@ -5,33 +5,6 @@ Start-Transcript -Path "C:\UserData.log" -Append
 
 ${pre_install}
 
-# Install Chocolatey
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
-$env:chocolateyUseWindowsCompression = 'true'
-Invoke-WebRequest https://community.chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
-
-# Add Chocolatey to powershell profile
-$ChocoProfileValue = @'
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
-Remove-Item Alias:curl
-Remove-Item Alias:wget
-refreshenv
-'@
-# Write it to the $profile location
-Set-Content -Path "$PsHome\Microsoft.PowerShell_profile.ps1" -Value $ChocoProfileValue -Force
-# Source it
-. "$PsHome\Microsoft.PowerShell_profile.ps1"
-
-Write-Host "Installing curl..."
-choco install curl -y
-
-refreshenv
-
-Get-Command curl
-
 # %{~ if enable_cloudwatch_agent ~}
 ## --- cloudwatch-agent ----
 Write-Host "Setting up cloudwatch agent..."
@@ -42,12 +15,6 @@ Remove-Item C:\amazon-cloudwatch-agent.msi
 & 'C:\Program Files\Amazon\AmazonCloudWatchAgent\amazon-cloudwatch-agent-ctl.ps1' -a fetch-config -m ec2 -s -c ssm:${ssm_key_cloudwatch_agent_config}
 # %{~ endif ~}
 ## --- cloudwatch-agent ----
-
-# Install dependent tools
-Write-Host "Installing additional development tools"
-choco install jq awscli archiver 7zip.install -y
-choco install git --params "/GitAndUnixToolsOnPath" -y
-refreshenv
 
 ${install_config_runner}
 ${post_install}
