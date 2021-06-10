4b2e52135eb8499745351b75050bf36b9222ec3d
Elton Leander Pinto <eltonpinto@fb.com>
2021-06-10 15:31:45 -0400

    Update Dockerfile to use custom clang-tidy build (#23)
    * Update Dockerfile to use custom clang-tidy build
    
    * Add verification script for clang-tidy custom install
    
    * Fix clang-tidy list checks

---
+++ clang-tidy-checks/verify.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -xe
+
+checks=$(ls *.diff)
+found_checks=$(/bin/clang-tidy -checks=* --list-checks)
+for check in $checks; do
+  name=${check%-check.*}
+  echo $name
+  grep "$name" <<< "$found_checks"
+done
+
+echo "verified!"
