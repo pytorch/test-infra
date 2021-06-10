b06fa58aea1d5b9bdad1f05475722bc401f3c22e
Elton Pinto <eltonpinto@fb.com>
2021-06-10 19:29:06 +0000

    Fix clang-tidy list checks

--- clang-tidy-checks/verify.sh
+++ clang-tidy-checks/verify.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-set -e
+set -xe
 
 checks=$(ls *.diff)
-found_checks=$(/bin/clang-tidy --list-checks)
+found_checks=$(/bin/clang-tidy -checks=* --list-checks)
 for check in $checks; do
   name=${check%-check.*}
+  echo $name
   grep "$name" <<< "$found_checks"
 done
 
