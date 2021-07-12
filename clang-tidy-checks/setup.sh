b84d7ec2359b6ea12ec4428dceea52ad8eff7fb6
Elton Pinto <eltonp3103@gmail.com>
2021-07-12 11:29:04 -0400

    Fix clang header file conflicts
    The docker image has two versions of clang header files: v11.1.0 and v11.0.0.
    This leads to issues when running clang-tidy on pytorch because there is a
    header file conflict. To fix this problem, we opt into using v11.1.0.

--- clang-tidy-checks/setup.sh
+++ clang-tidy-checks/setup.sh
@@ -2,16 +2,6 @@
 
 set -e
 
-function unreachable() {
-  echo "[fatal]: Hit unreachable code!"
-  exit 1
-}
-
-function error() {
-  echo "[error]:" "$1"
-  exit 1
-}
-
 function info() {
   echo "[info]:" "$1"
 }
@@ -22,15 +12,6 @@ function success() {
 
 function check_requirements() {
   info "checking requirements"
-
-  case $(uname) in
-    Linux|Darwin)
-      ;;
-    *)
-      error "Unsupported OS $(uname)"
-      ;;
-  esac
-
   cmake --version
   gcc --version
   python3 --version
@@ -58,44 +39,22 @@ function apply_patches() {
 }
 
 function build() {
-  local cmake_common_args=(
-    -DCMAKE_C_COMPILER=clang
-    -DCMAKE_CXX_COMPILER=clang++
-    -DCMAKE_BUILD_TYPE=Release
-    -DCLANG_ENABLE_STATIC_ANALYZER=OFF
-    -DCLANG_ENABLE_ARCMT=OFF
-    -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra"
-    -DLLVM_BUILD_TOOLS=OFF
-    -DLLVM_BUILD_UTILS=OFF
-    -GNinja
-  )
-
-  local cmake_os_args
-
-  case $(uname) in
-    Linux)
-      cmake_os_args=(
-        -DLLVM_USE_LINKER=lld
-        -DLLVM_BUILD_STATIC=ON
-        -DCMAKE_CXX_FLAGS="-static"
-      )
-      ;;
-    Darwin)
-      cmake_os_args=(
-        -DCMAKE_CXX_FLAGS="-static-libgcc -static-libstdc++"
-        -DCMAKE_OSX_DEPLOYMENT_TARGET="10.15"
-      )
-      ;;
-    *)
-      unreachable
-      ;;
-  esac
-
-
   mkdir build
   cd build
-
-  cmake "${cmake_common_args[@] cmake_os_args[@]}" ../llvm
+  cmake -DCMAKE_C_COMPILER=clang \
+        -DCMAKE_CXX_COMPILER=clang++ \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_EXE_LINKER_FLAGS="-static" \
+        -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" \
+        -DLLVM_ENABLE_LIBCXX=ON \
+        -DLLVM_STATIC_LINK_CXX_STDLIB=ON \
+        -DLLVM_USE_LINKER=lld \
+        -DLLVM_TARGETS_TO_BUILD="X86" \
+        -DCLANG_ENABLE_STATIC_ANALYZER=OFF \
+        -DCLANG_ENABLE_ARCMT=OFF \
+        -DLLVM_BUILD_TOOLS=OFF \
+        -DLLVM_BUILD_UTILS=OFF \
+        -GNinja ../llvm
   cmake --build . --target clang-tidy
   success
 }
@@ -106,27 +65,9 @@ function setup() {
   build
 }
 
-function check_if_static() {
-  case $(uname) in
-    Linux)
-      ldd ./bin/clang-tidy 2>&1 | grep -q "not a dynamic executable"
-      ;;
-    Darwin)
-      # No static link check for MacOS
-      #
-      # Apple makes it a little hard to build a fully statically linked binary.
-      # It involves performing some hacks that move shared dynamic libraries to
-      # particular directories. This adds a bit of complexity to the build
-      # step. To keep things simple, we ignore this check.
-      ;;
-    *)
-      unreachable
-      ;;
-  esac
-}
-
 function verify() {
-  [[ -e ./bin/clang-tidy ]] && check_if_static
+  [[ -e ./bin/clang-tidy ]] &&
+  ldd ./bin/clang-tidy 2>&1 | grep -q "not a dynamic executable"
 }
 
 check_requirements
