7fe29e03b54bc543f0784932e88e1f8d37ece06c
zhouzhuojie <zhouzhuojie@gmail.com>
2021-07-15 17:25:38 -0700

    fix filenames

--- fbossci-aws-lambas/us-east-1/scribe-proxy/test.py
+++ fbossci-aws-lambas/us-east-1/scribe-proxy/test.py
@@ -6,26 +6,26 @@ import os
 import unittest
 from unittest.mock import ANY, Mock, patch
 
-import scribe_proxy
+import lambda_function
 
 
 class TestScribeProxy(unittest.TestCase):
     @patch.dict(os.environ, {'SCRIBE_GRAPHQL_ACCESS_TOKEN': '123'}, clear=True)
-    @patch('scribe_proxy.urlopen')
+    @patch('lambda_function.urlopen')
     def test_logs(self, mock_urlopen):
         event = {'logs': 'logs'}
-        scribe_proxy.lambda_handler(event, Mock())
+        lambda_function.lambda_handler(event, Mock())
         mock_urlopen.assert_called_once_with(
             ANY,
             data=b'{"access_token": "123", "logs": "logs"}',
         )
 
     @patch.dict(os.environ, {'SCRIBE_GRAPHQL_ACCESS_TOKEN': '123'}, clear=True)
-    @patch('scribe_proxy.urlopen')
+    @patch('lambda_function.urlopen')
     def test_base64_bz2_logs(self, mock_urlopen):
         event = {'base64_bz2_logs': base64.b64encode(
             bz2.compress('logs'.encode())).decode()}
-        scribe_proxy.lambda_handler(event, Mock())
+        lambda_function.lambda_handler(event, Mock())
         mock_urlopen.assert_called_once_with(
             ANY,
             data=b'{"access_token": "123", "logs": "logs"}',
@@ -34,4 +34,4 @@ class TestScribeProxy(unittest.TestCase):
     def test_invalid_events(self):
         with self.assertRaises(Exception):
             event = {'invalid': '123'}
-            scribe_proxy.lambda_handler(event, Mock())
+            lambda_function.lambda_handler(event, Mock())
