84888785965810ba77b7e12e6594b1a94d75dbf1
Eli Uriegas <eliuriegas@fb.com>
2021-08-23 10:29:21 -0700

    add ImageHelpers, install windows SDK from MSFT
    Adds ImageHelpers which were taken from actions/virtual-environments
    scripts directly and use their script to install the correct windows SDK
    
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

---
+++ aws/ami/windows/scripts/Installers/Install-WDK.ps1
@@ -0,0 +1,34 @@
+################################################################################
+##  File:  Install-WDK.ps1
+##  Desc:  Install the Windows Driver Kit
+################################################################################
+
+# Requires Windows SDK with the same version number as the WDK
+if (Test-IsWin19)
+{
+    $winSdkUrl = "https://go.microsoft.com/fwlink/p/?linkid=2120843"
+    $wdkUrl = "https://go.microsoft.com/fwlink/?linkid=2128854"
+    $FilePath = "C:\Program Files (x86)\Windows Kits\10\Vsix\VS2019\WDK.vsix"
+    $VSver = "2019"
+}
+else
+{
+    $winSdkUrl = "https://go.microsoft.com/fwlink/p/?LinkID=2023014"
+    $wdkUrl = "https://go.microsoft.com/fwlink/?linkid=2026156"
+    $FilePath = "C:\Program Files (x86)\Windows Kits\10\Vsix\WDK.vsix"
+    $VSver = "2017"
+}
+
+$argumentList = ("/features", "+", "/quiet")
+
+# `winsdksetup.exe /features + /quiet` installs all features without showing the GUI
+Install-Binary -Url $winSdkUrl -Name "winsdksetup.exe" -ArgumentList $argumentList
+
+# `wdksetup.exe /features + /quiet` installs all features without showing the GUI
+Install-Binary -Url $wdkUrl -Name "wdksetup.exe" -ArgumentList $argumentList
+
+# Need to install the VSIX to get the build targets when running VSBuild
+Install-VsixExtension -FilePath $FilePath -Name "WDK.vsix" -VSversion $VSver -InstallOnly
+
+# Test that the glags executable is available
+Test-Path "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\gflags.exe"
