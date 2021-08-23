91347246062359185cc7973d07212b65548c9a58
Eli Uriegas <eliuriegas@fb.com>
2021-08-23 11:09:56 -0700

    Pick change from internal repository for windows ami
    Gets all the changes from our internal repository onto this public
    repository specifically for Window SDK issues
    
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

--- aws/ami/windows/scripts/Installers/Install-Tools.ps1
+++ aws/ami/windows/scripts/Installers/Install-Tools.ps1
@@ -1,7 +1,7 @@
 Write-Host "Installing additional development tools"
 choco install jq awscli archiver 7zip.install curl vswhere -y
 choco install git --params "/GitAndUnixToolsOnPath" -y
-choco install windows-sdk-10.0 -y
+choco install windows-sdk-10-version-2004-all --version=10.0.19041.0 -y
 
 refreshenv
 
