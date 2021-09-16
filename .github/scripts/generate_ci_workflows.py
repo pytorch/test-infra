e86180a852a534a9fffd064606d9dd44b8bc0a14
David Riazati <driazati@users.noreply.github.com>
2021-09-16 09:59:39 -0700

    Update dashboards before deploying
    Without this step we have the risk of re-deploying dashboards before they get updated to the repo if they were updated in the last 15 minutes and a deploy is triggered. This also consolidates these down to templates

--- .github/scripts/generate_ci_workflows.py
+++ .github/scripts/generate_ci_workflows.py
@@ -1,57 +1,60 @@
 #!/usr/bin/env python3
 
-from dataclasses import asdict, dataclass
 from pathlib import Path
 
 import jinja2
 import os
-from typing import Dict
+from typing import Any
 
 
-DOCKER_REGISTRY = "308535385114.dkr.ecr.us-east-1.amazonaws.com"
 REPO_ROOT = Path(__file__).resolve().parent.parent.parent
 GITHUB_DIR = REPO_ROOT / ".github"
 
 
-@dataclass
 class CIWorkflow:
     name: str
+    template: str
+
+    def __init__(self, name: str, template: str, **kwargs: Any) -> None:
+        self.name = name
+        self.template = template
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def generate_workflow_file(self, workflow_template: jinja2.Template) -> None:
-        output_file_path = (
-            GITHUB_DIR / f"workflows/generated-{self.name}.yml"
-        )
+        output_file_path = GITHUB_DIR / f"workflows/generated-{self.name}.yml"
         with open(output_file_path, "w") as output_file:
-            generated = "generated"  # Note that please keep the variable generated otherwise phabricator will hide the whole file
-
             filename = Path(workflow_template.filename).relative_to(REPO_ROOT)
-            output_file.write(f"# @{generated} DO NOT EDIT MANUALLY\n")
+            output_file.write("# @generated DO NOT EDIT MANUALLY\n")
             output_file.write(f"# Generated from {filename}\n")
-            output_file.write(workflow_template.render(asdict(self)))
+            output_file.write(workflow_template.render(self.__dict__))
             output_file.write("\n")
-        print(output_file_path)
+        print("Wrote", output_file_path.relative_to(REPO_ROOT))
 
 
-@dataclass
-class ZipLambda(CIWorkflow):
-    timeout: int
-
-
-ZIP_LAMBDAS = [
-    ZipLambda(name="github_webhook_rds_sync", timeout=3),
-    ZipLambda(name="checks-cron", timeout=3),
-    ZipLambda(name="rds-proxy", timeout=3),
+WORKFLOWS = [
+    CIWorkflow(
+        template="deploy_lambda.yml.j2", name="github_webhook_rds_sync", timeout=3
+    ),
+    CIWorkflow(template="deploy_lambda.yml.j2", name="checks-cron", timeout=3),
+    CIWorkflow(template="deploy_lambda.yml.j2", name="rds-proxy", timeout=3),
+    CIWorkflow(
+        template="metrics_pytorch_org.yml.j2", name="metrics-pytorch-org", timeout=3
+    ),
+    CIWorkflow(
+        template="update_grafana_dashboards.yml.j2",
+        name="update-grafana-dashboards",
+        timeout=3,
+    ),
 ]
 
 
 if __name__ == "__main__":
     jinja_env = jinja2.Environment(
         variable_start_string="!{{",
-        loader=jinja2.FileSystemLoader(str(GITHUB_DIR.joinpath("templates"))),
+        loader=jinja2.FileSystemLoader(str(GITHUB_DIR / "templates")),
     )
-    template_and_workflows = [
-        (jinja_env.get_template("deploy_lambda.yml.j2"), ZIP_LAMBDAS),
-    ]
+
     # Delete the existing generated files first, this should align with .gitattributes file description.
     existing_workflows = GITHUB_DIR.glob("workflows/generated-*")
     for w in existing_workflows:
@@ -60,7 +63,6 @@ if __name__ == "__main__":
         except Exception as e:
             print(f"Error occurred when deleting file {w}: {e}")
 
-    for template, workflows in template_and_workflows:
-        for workflow in workflows:
-            workflow.generate_workflow_file(workflow_template=template)
-
+    for workflow in WORKFLOWS:
+        template = jinja_env.get_template(workflow.template)
+        workflow.generate_workflow_file(workflow_template=template)
