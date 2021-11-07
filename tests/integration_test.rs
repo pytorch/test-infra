ae8a2c306070390b65ad2945255fe0dbb717f3f9
Michael Suo <suo@fb.com>
2021-11-06 19:44:26 -0700

    consolidate test snapshots into one string

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -8,9 +8,16 @@ use std::io::Write;
 fn assert_output_snapshot(cmd: &mut Command) -> Result<()> {
     let re = Regex::new("test-lintrunner-config(.*)toml").unwrap();
     let output = cmd.output()?;
-    assert_yaml_snapshot!(std::str::from_utf8(&output.stderr)?
-        .lines()
-        .collect::<Vec<_>>(),
+
+    let output_string = format!(
+        "STDOUT:\n{}\n\nSTDERR:\n{}",
+        std::str::from_utf8(&output.stdout)?,
+        std::str::from_utf8(&output.stderr)?,
+    );
+    let output_lines = output_string.lines().collect::<Vec<_>>();
+
+    assert_yaml_snapshot!(
+        output_lines,
         // Define a dynamic redaction on all lines. This will replace the config
         // name (which is a tempfile that changes from run to run) with a fixed value.
         // Everything else is passed through normally.
@@ -22,9 +29,6 @@ fn assert_output_snapshot(cmd: &mut Command) -> Result<()> {
             ),
         }
     );
-    assert_yaml_snapshot!(std::str::from_utf8(&output.stdout)?
-        .lines()
-        .collect::<Vec<_>>());
     Ok(())
 }
 
