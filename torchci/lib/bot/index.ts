2424cc58c88009f90f973c384ca5a051332a09b2
Michael Suo <suo@fb.com>
2022-01-20 16:30:22 -0800

    migrate mergebot as well

--- torchci/lib/bot/index.ts
+++ torchci/lib/bot/index.ts
@@ -5,6 +5,7 @@ import CIFlowBot from "./ciflowBot";
 import ciflowPushTrigger from "./ciflowPushTrigger";
 import webhookToDynamo from "./webhookToDynamo";
 import verifyDisableTestIssueBot from "./verifyDisableTestIssueBot";
+import mergeBot from "./mergeBot";
 
 export default function bot(app: Probot) {
   autoCcBot(app);
@@ -13,4 +14,5 @@ export default function bot(app: Probot) {
   CIFlowBot.main(app);
   ciflowPushTrigger(app);
   webhookToDynamo(app);
+  mergeBot(app);
 }
