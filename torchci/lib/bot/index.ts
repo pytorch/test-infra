6de39eda495989d2bf670e78df176f60a2e63f0b
Michael Suo <suo@fb.com>
2022-01-20 19:40:23 -0800

    Simplify ciflow bot
    Now that the push trigger is rolled out, we can take the opportunity to significantly simplify the ciflow bot logic. In particular:
    - We do not need the dispatch or signaling stuff anymore, as the dispatch is handled through the push trigger.
    - We do not need ciflow command parsing or handling anymore, since adding a label is sufficient to trigger workflow runs.
    
    So what is retained is:
    - User configuration handling; ciflow will populate initial labels based on configuration. Honestly, we could get rid of this; the tracking issue has only a single config. But keeping it for now.
    - CIFlow comment is nice because it tells you what labels you need to add, so that stays.
    - Now, if you try to give ciflow a command, it'll respond telling you not to bother.

--- torchci/lib/bot/index.ts
+++ torchci/lib/bot/index.ts
@@ -1,7 +1,7 @@
 import { Probot } from "probot";
 import autoCcBot from "./autoCcBot";
 import autoLabelBot from "./autoLabelBot";
-import CIFlowBot from "./ciflowBot";
+import ciflowBot from "./ciflowBot";
 import ciflowPushTrigger from "./ciflowPushTrigger";
 import webhookToDynamo from "./webhookToDynamo";
 import verifyDisableTestIssueBot from "./verifyDisableTestIssueBot";
@@ -11,7 +11,7 @@ export default function bot(app: Probot) {
   autoCcBot(app);
   autoLabelBot(app);
   verifyDisableTestIssueBot(app);
-  CIFlowBot.main(app);
+  ciflowBot(app);
   ciflowPushTrigger(app);
   webhookToDynamo(app);
   mergeBot(app);
