eb96a7c77df58123686cd5fd6b0e80af66ac1bb4
Nikita Shulga <nshulga@fb.com>
2022-01-22 12:01:58 -0800

    Add `@pytorchmergebot merge this` as alias
    Rename `cmdPat` to `mergeCmdPath`

--- torchci/lib/bot/mergeBot.ts
+++ torchci/lib/bot/mergeBot.ts
@@ -1,14 +1,14 @@
 import { Probot } from "probot";
 
 function mergeBot(app: Probot): void {
-  const cmdPat = new RegExp("@pytorchbot\\s+merge\\s+this");
+  const mergeCmdPat = new RegExp("@pytorch(merge|)bot\\s+merge\\s+this");
   app.on("issue_comment.created", async (ctx) => {
     const commentBody = ctx.payload.comment.body;
     const owner = ctx.payload.repository.owner.login;
     const repo = ctx.payload.repository.name;
     const commentId = ctx.payload.comment.id;
     const prNum = ctx.payload.issue.number;
-    if (commentBody.match(cmdPat)) {
+    if (commentBody.match(mergeCmdPat)) {
       await ctx.octokit.repos.createDispatchEvent({
         owner,
         repo,
