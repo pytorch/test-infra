a8eefd01fff85d5c8a7c316d7cab0e3c5af8a7bd
Jane Xu <janeyx@fb.com>
2022-02-09 14:44:22 -0500

    Tested code locally with commits

--- torchci/pages/api/flaky-tests/disable.ts
+++ torchci/pages/api/flaky-tests/disable.ts
@@ -1,82 +1,102 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import urllib from "urllib";
+import * as urllib from "urllib";
 
-import { Octokit } from "@octokit/rest";
+import { getOctokit } from "lib/github";
 import fetchFlakyTests from "lib/fetchFlakyTests";
 import { FlakyTestData, IssueData } from "lib/types";
 import { supportedPlatforms } from "lib/bot/verifyDisableTestIssueBot";
 import fetchIssuesByLabel from "lib/fetchIssuesByLabel";
 
-const NUM_HOURS = 6
-const octokit = new Octokit();
-const owner = "pytorch";
-const repo = "pytorch";
+
+const NUM_HOURS = 240;
+const owner: string = "pytorch";
+const repo: string = "pytorch";
+
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<void>
+    req: NextApiRequest,
+    res: NextApiResponse<void>
 ) {
-  const flaky_tests = await fetchFlakyTests(`${NUM_HOURS}`);
-  const issues: IssueData[] = await fetchIssuesByLabel("skipped");
-
-  flaky_tests.forEach((test: FlakyTestData) => {
-    const issueTitle = `DISABLED ${test.name} (${test.suite})`;
-    const matchingIssues = issues.filter((issue) => issue.title === issueTitle);
-
-    if (matchingIssues.length !== 0) {
-        // There is a matching issue
-        const matchingIssue = matchingIssues[0];
-        if (matchingIssue.state === "open") {
-            const body = `Another case of flakiness has been found [here](${getLatestWorkflowURL(test.workflow_ids)}).
-            Please verify the issue was opened after this instance, that the platforms list includes all of
-            [${getPlatformsAffected(test.workflow_names).join(", ")}], or disable bot might not be working as expected.`
-
-            octokit.rest.issues.createComment({
-                owner,
-                repo,
-                issue_number: matchingIssue.number,
-                body,
-            });
-        } else {
-            // Re-open the issue
-            octokit.rest.issues.update({
-                owner,
-                repo,
-                issue_number: matchingIssue.number,
-                state: "open",
-            });
-
-            const body = `Another case of flakiness has been found [here](${getLatestWorkflowURL(test.workflow_ids)}).
-            Reopening the issue to disable. Please verify that the platforms list includes all of
-            [${getPlatformsAffected(test.workflow_names).join(", ")}].`
-
-            octokit.rest.issues.createComment({
-                owner,
-                repo,
-                issue_number: matchingIssue.number,
-                body,
-              });
-        }
-    } else {
-        createIssueFromFlakyTest(test);
-    }
-  });
-
-
-
-  res.status(200);
+    await disableFlakyTests();
+    res.status(200);
 }
 
+
+async function disableFlakyTests() {
+    const octokit = await getOctokit(owner, repo);
+    const flaky_tests = await fetchFlakyTests(`${NUM_HOURS}`);
+    const issues: IssueData[] = await fetchIssuesByLabel("skipped");
+    flaky_tests.forEach(async function(test: FlakyTestData) {
+        const issueTitle = getIssueTitle(test.name, test.suite);
+        // console.log(issueTitle);
+        const matchingIssues = issues.filter((issue) => issue.title === issueTitle);
+        // console.log(matchingIssues)
+        if (matchingIssues.length !== 0) {
+            // There is a matching issue
+            // console.log("there is a matching issue.");
+            const matchingIssue = matchingIssues[0];
+            if (matchingIssue.state === "open") {
+                // console.log("The issue is open.");
+                const body = `Another case of flakiness has been found [here](${getLatestWorkflowURL(test.workflow_ids)}).
+                Please verify the issue was opened after this instance, that the platforms list includes all of
+                [${getPlatformsAffected(test.workflow_names).join(", ")}], or disable bot might not be working as expected.`;
+                // console.log(body);
+                // console.log(`Would create a comment for issue ${matchingIssue.number}.`);
+                octokit.rest.issues.createComment({
+                    owner,
+                    repo,
+                    issue_number: matchingIssue.number,
+                    body,
+                });
+            } else {
+                // Re-open the issue
+                console.log("The issue is closed!");
+                octokit.rest.issues.update({
+                    owner,
+                    repo,
+                    issue_number: matchingIssue.number,
+                    state: "open",
+                });
+
+                const body = `Another case of flakiness has been found [here](${getLatestWorkflowURL(test.workflow_ids)}).
+                Reopening the issue to disable. Please verify that the platforms list includes all of
+                [${getPlatformsAffected(test.workflow_names).join(", ")}].`;
+                // console.log(body);
+                // console.log(`Would add a comment for issue num ${matchingIssue.number}.`);
+                octokit.rest.issues.createComment({
+                    owner,
+                    repo,
+                    issue_number: matchingIssue.number,
+                    body,
+                });
+            }
+        } else {
+            await createIssueFromFlakyTest(test);
+        }
+    });
+}
+
+
 function getLatestWorkflowURL(workflow_ids: string[]): string {
-    return `https://github.com/pytorch/pytorch/actions/runs/${workflow_ids[workflow_ids.length - 1]}`
+    return `https://github.com/pytorch/pytorch/actions/runs/${workflow_ids[workflow_ids.length - 1]}`;
+}
+
+
+function getIssueTitle(test_name: string, test_suite: string) {
+    let suite = test_suite;
+    // If the test class is not a subclass, it belongs to __main__
+    if (test_suite.indexOf(".") < 0) {
+        suite = "__main__." + suite;
+    }
+    return `DISABLED ${test_name} (${suite})`;
 }
 
 
 function getPlatformsAffected(workflow_names: string[]): string[] {
-    let platformsToSkip: string[] = []
+    let platformsToSkip: string[] = [];
     supportedPlatforms.forEach(function(platform: string) {
         workflow_names.forEach(workflow_name => {
-            if (workflow_name.includes(platform)) {
+            if (workflow_name.includes(platform) && !platformsToSkip.includes(platform)) {
                 platformsToSkip.push(platform);
             }
         });
@@ -86,51 +106,57 @@ function getPlatformsAffected(workflow_names: string[]): string[] {
 
 
 function getIssueBodyForFlakyTest(test: FlakyTestData): string {
-    const examplesURL = `http://torch-ci.com/failure/${encodeURIComponent(`${test.name}, ${test.suite}`)}`
+    const examplesURL = `http://torch-ci.com/failure/${encodeURIComponent(`${test.name}, ${test.suite}`)}`;
     const message = `Platforms: ${getPlatformsAffected(test.workflow_names).join(", ")}
 
     This test was disabled because it is failing on trunk. See [recent examples](${examplesURL}) and the most recent
     [workflow logs](${getLatestWorkflowURL(test.workflow_ids)}).
 
     Over the past ${NUM_HOURS} hours, it has been determined flaky in ${test.workflow_ids.length} workflows with
-    ${test.num_red} red and ${test.num_green} green.`
+    ${test.num_red} red and ${test.num_green} green.`;
 
     return encodeURIComponent(message);
 }
 
 
-function getTestOwnerLabels(test_file: string) : string[] {
+async function getTestOwnerLabels(test_file: string) : Promise<string[]> {
     const urlkey = "https://raw.githubusercontent.com/pytorch/pytorch/master/test/";
 
-    urllib.request(`${urlkey}${test_file}`, function (err, data, res) {
-        if (err) {
-            console.warn(`Error thrown when attempting to read test file of flaky test: ${err.message}`)
-            return ["module: unknown"]
+    return await urllib.request(`${urlkey}${test_file}.py`).then(function (result): string[] {
+        const status_code = result.res.statusCode;
+        if (status_code !== 200) {
+            console.warn(`Error retrieving test file of flaky test: ${status_code}`);
+            return ["module: unknown"];
         }
-        // TODO: remove the following
-        console.log(res.statusCode);
-        console.log(res.headers);
-        const file_contents = data.toString();  // data is a Buffer
+        const file_contents = result.data.toString();  // data is a Buffer
         const lines = file_contents.split(/[\r\n]+/);
-        const prefix = "# Owner(s): "
-        lines.array.forEach((line: string) => {
+        const prefix = "# Owner(s): ";
+        for (const line of lines) {
             if (line.startsWith(prefix)) {
                 const labels: string[] = JSON.parse(line.substring(prefix.length));
                 if (labels.length === 0 || (labels.length === 1 && labels[0] === "module: unknown")) {
                     return ["module: unknown"];
                 }
+                if (labels.length === 1 && labels[0] === "high priority") {
+                    return ["high priority"];
+                }
                 return labels.concat(["triaged"]);
             }
-        });
+        }
+        return ["module: unknown"];
+    }).catch(function (err) : string[] {
+        console.warn(`Error retrieving test file of flaky test: ${err}`);
+        return ["module: unknown"];
     });
-    return ["module: unknown"]
 }
 
 
-function createIssueFromFlakyTest(test: FlakyTestData): void {
-    const title = `DISABLED ${test.name} (${test.suite})`;
+async function createIssueFromFlakyTest(test: FlakyTestData): Promise<void> {
+    const title = getIssueTitle(test.name, test.suite);
     const body = getIssueBodyForFlakyTest(test);
-    const labels = ["skipped", "module: flaky-tests"].concat(getTestOwnerLabels(test.file));
+    const labels = ["skipped", "module: flaky-tests"].concat(await getTestOwnerLabels(test.file));
+    // console.log(`For test ${JSON.stringify(test)}!!\nWould create an issue for ${title} and labels ${labels} and body: ${body}\n\n`)
+    const octokit = await getOctokit(owner, repo);
     octokit.rest.issues.create({
         owner,
         repo,
