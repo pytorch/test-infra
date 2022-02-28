5f7d74905543ead2fc01f0118fc40d6dbde5fa61
zengk95 <34172846+zengk95@users.noreply.github.com>
2022-02-28 11:26:46 -0600

    [torch ci] Add additional paths (#211)
    * added additional paths
    
    * use next.config.js

--- torchci/next.config.js
+++ torchci/next.config.js
@@ -7,6 +7,22 @@ module.exports = {
         source: "/",
         destination: "/hud/pytorch/pytorch/master/0",
       },
+      {
+        source: "/commit/:repoOwner/:repoName/:sha",
+        destination: "/:repoOwner/:repoName/commit/:sha",
+      },
+      {
+        source: "/pr/:repoOwner/:repoName/:prNumber",
+        destination: "/:repoOwner/:repoName/pull/:prNumber",
+      },
+      {
+        source: "/pr/:prNumber",
+        destination: "/pytorch/pytorch/pull/:prNumber",
+      },
+      {
+        source: "/commit/:sha",
+        destination: "/pytorch/pytorch/commit/:sha",
+      },
     ];
   },
 };
