a5bba88b2b6d9e8dfb2ee6b1750b3e55fff6457e
Jane Xu <janeyx@fb.com>
2022-03-01 11:39:10 -0500

    Use upgraded query to allow for a view

--- torchci/lib/fetchFlakyTests.ts
+++ torchci/lib/fetchFlakyTests.ts
@@ -2,22 +2,38 @@ import getRocksetClient from "./rockset";
 
 import { FlakyTestData } from "./types";
 
-export default async function fetchFlakyTests(num_hours: string): Promise<FlakyTestData[]> {
+export default async function fetchFlakyTests(numHours: string = "3",
+  testName: string = "%", testSuite: string = "%", testFile: string = "%"): Promise<FlakyTestData[]> {
   const rocksetClient = getRocksetClient();
   const flakyTestQuery = await
     rocksetClient.queryLambdas.executeQueryLambda(
       "commons",
       "flaky_test_query",
-      "7e7c838ec2592c60",
+      "2bbca6ed782f660d",
       {
         parameters: [
           {
             name: "num_hours",
             type: "int",
-            value: num_hours,
+            value: numHours,
+          },
+          {
+            name: "name",
+            type: "string",
+            value: `%${testName}%`,
+          },
+          {
+            name: "suite",
+            type: "string",
+            value: `%${testSuite}%`,
+          },
+          {
+            name: "file",
+            type: "string",
+            value: `%${testFile}%`,
           },
         ],
       }
     );
-   return flakyTestQuery.results ?? [];
+  return flakyTestQuery.results ?? [];
 }
