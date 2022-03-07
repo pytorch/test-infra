5063388d0eab05ab4daf405354037307bb9a5b50
zengk95 <34172846+zengk95@users.noreply.github.com>
2022-03-07 13:49:46 -0600

    small fix

--- torchci/components/AnnouncementBanner.tsx
+++ torchci/components/AnnouncementBanner.tsx
@@ -8,7 +8,7 @@ function AnnouncementBanner() {
         HUD Migration:
       </a>
       HUD is being migrated. If you would like to use the old HUD, go here to
-      use the <a href={"https://hud.pytorch.org/"}>old HUD</a> or{" "}
+      use the <a href={"https://hud2.pytorch.org/"}>old HUD</a> or{" "}
       <a href="https://github.com/pytorch/test-infra/issues/new?assignees=&labels=&template=feature_request.yaml&title=%5Bfeature%5D%3A+">
         file an issue
       </a>
