2b0ac768c7c2d42b6a1992d77ff920998681d789
zengk95 <34172846+zengk95@users.noreply.github.com>
2022-03-07 19:00:29 -0600

    debug

--- torchci/scripts/checkRockset.mjs
+++ torchci/scripts/checkRockset.mjs
@@ -23,47 +23,51 @@ function printDiff(first, second) {
 }
 
 async function checkQuery(client, queryName, version) {
-  console.log(
-    `Checking that commons.${queryName}:${version} is matches your local checkout`
-  );
-  const qLambda = await client.queryLambdas.getQueryLambdaVersion(
-    "commons",
-    queryName,
-    version
-  );
-  let passesCheck = true;
-
-  // Check that the query SQL matches the local checkout.
-  const remoteQuery = qLambda.data.sql.query;
-  const localQuery = await fs.readFile(
-    `./rockset/commons/__sql/${queryName}.sql`,
-    "utf8"
-  );
-  if (remoteQuery !== localQuery) {
+  try {
     console.log(
-      `::error::commons.${queryName}:${version} SQL does not match your local checkout. Run 'yarn node scripts/downloadQueryLambda.mjs commons ${queryName} ${version}' to overwrite your local checkout.`
+      `Checking that commons.${queryName}:${version} is matches your local checkout`
     );
-    printDiff(remoteQuery, localQuery);
-    passesCheck = false;
-  }
-
-  // Check that the query config matches the local checkout.
-  const localConfig = await readJSON(
-    `./rockset/commons/${queryName}.lambda.json`
-  );
-
-  const remoteParams = JSON.stringify(
-    qLambda.data.sql.default_parameters,
-    null,
-    2
-  );
-  const localParams = JSON.stringify(localConfig.default_parameters, null, 2);
-  if (remoteParams !== localParams) {
-    console.log(
-      `::error::commons.${queryName}:${version} config does not match your local checkout. Run 'yarn node scripts/downloadQueryLambda.mjs commons ${queryName} ${version}' to overwrite your local checkout.`
+    const qLambda = await client.queryLambdas.getQueryLambdaVersion(
+      "commons",
+      queryName,
+      version
     );
-    printDiff(remoteParams, localParams);
-    passesCheck = false;
+    let passesCheck = true;
+
+    // Check that the query SQL matches the local checkout.
+    const remoteQuery = qLambda.data.sql.query;
+    const localQuery = await fs.readFile(
+      `./rockset/commons/__sql/${queryName}.sql`,
+      "utf8"
+    );
+    if (remoteQuery !== localQuery) {
+      console.log(
+        `::error::commons.${queryName}:${version} SQL does not match your local checkout. Run 'yarn node scripts/downloadQueryLambda.mjs commons ${queryName} ${version}' to overwrite your local checkout.`
+      );
+      printDiff(remoteQuery, localQuery);
+      passesCheck = false;
+    }
+
+    // Check that the query config matches the local checkout.
+    const localConfig = await readJSON(
+      `./rockset/commons/${queryName}.lambda.json`
+    );
+
+    const remoteParams = JSON.stringify(
+      qLambda.data.sql.default_parameters,
+      null,
+      2
+    );
+    const localParams = JSON.stringify(localConfig.default_parameters, null, 2);
+    if (remoteParams !== localParams) {
+      console.log(
+        `::error::commons.${queryName}:${version} config does not match your local checkout. Run 'yarn node scripts/downloadQueryLambda.mjs commons ${queryName} ${version}' to overwrite your local checkout.`
+      );
+      printDiff(remoteParams, localParams);
+      passesCheck = false;
+    }
+  } catch (e) {
+    console.log("EXCEPTION", console.trace(), client, queryName, version);
   }
   return passesCheck;
 }
@@ -78,5 +82,6 @@ const checks = Object.entries(prodVersions).map(([queryName, version]) =>
 const checkStatuses = await Promise.all(checks);
 
 if (checkStatuses.some((status) => status === false)) {
+  console.log("CHECK STATUS", checkStatuses);
   process.exit(1);
 }
