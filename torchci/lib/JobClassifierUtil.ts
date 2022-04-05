5de1c5beda8c55beb322477e05af5b0d69ab481c
Michael Suo <suo@fb.com>
2022-04-05 11:21:41 -0700

    [hud] revert naming change (#282)
    During the transition to the consolidated workflow setup, we mapped, e.g. `pull / mytest` to `mytest` so that naming would be consistent across the transition. Now that we are fully migrated over, revert to reporting workflow and name normally.

--- torchci/lib/JobClassifierUtil.ts
+++ torchci/lib/JobClassifierUtil.ts
@@ -7,16 +7,28 @@ const groups = [
     name: "Lint Jobs",
   },
   {
-    regex: /(\(periodic-pytorch)|(ci\/circleci: periodic_pytorch)|(^periodic-)/,
-    name: "Periodic Jobs",
+    regex: /android/,
+    name: "Android",
   },
   {
-    regex: /(Linux CI \(pytorch-linux-)|(^linux-)/,
-    name: "Linux GitHub Actions",
+    regex: /\slinux-/,
+    name: "Linux",
+  },
+  {
+    regex: /linux-binary/,
+    name: "Binary Linux",
+  },
+  {
+    regex: /windows-binary/,
+    name: "Binary Windows",
+  },
+  {
+    regex: /pytorch-xla/,
+    name: "XLA",
   },
   {
     regex:
-      /(Add annotations )|(Close stale pull requests)|(Label PRs & Issues)|(Triage )|(Update S3 HTML indices)|(codecov\/project)|(Facebook CLA Check)|(auto-label-rocm)/,
+      /(Add annotations )|(Close stale pull requests)|(Label PRs & Issues)|(Triage )|(Update S3 HTML indices)|(is-properly-labeled)|(Facebook CLA Check)|(auto-label-rocm)/,
     name: "Annotations and labeling",
   },
   {
@@ -25,8 +37,8 @@ const groups = [
     name: "Docker",
   },
   {
-    regex: /(Windows CI \(pytorch-)|(^win-)/,
-    name: "Windows GitHub Actions",
+    regex: /\swin-/,
+    name: "Windows",
   },
   {
     regex: / \/ calculate-docker-image/,
@@ -41,12 +53,12 @@ const groups = [
     name: "ci/circleci: pytorch_ios",
   },
   {
-    regex: /^ios-/,
-    name: "iOS Github Actions",
+    regex: /\sios-/,
+    name: "iOS",
   },
   {
-    regex: /^macos-/,
-    name: "macOS Github Actions",
+    regex: /\smacos-/,
+    name: "Mac",
   },
   {
     regex:
@@ -54,70 +66,16 @@ const groups = [
     name: "Parallel",
   },
   {
-    regex:
-      /(ci\/circleci: pytorch_cpp_doc_build)|(ci\/circleci: pytorch_cpp_doc_test)|(pytorch_python_doc_build)|(pytorch_doc_test)/,
+    regex: /(docs push)|(docs build)/,
     name: "Docs",
-  },
-  {
-    regex: /ci\/circleci: pytorch_linux_bionic_cuda10_2_cudnn7_py3_9_gcc7_/,
-    name: "ci/circleci: pytorch_linux_bionic_cuda10_2_cudnn7_py3_9_gcc7",
-  },
-  {
-    regex: /ci\/circleci: pytorch_linux_xenial_cuda10_2_cudnn7_py3_/,
-    name: "ci/circleci: pytorch_linux_xenial_cuda10_2_cudnn7_py3",
-  },
-  {
-    regex: /ci\/circleci: pytorch_linux_xenial_cuda11_1_cudnn8_py3_gcc7_/,
-    name: "ci/circleci: pytorch_linux_xenial_cuda11_1_cudnn8_py3_gcc7",
-  },
-  {
-    regex:
-      /(ci\/circleci: pytorch_linux_xenial_py3_clang5_android_ndk_r19c_)|(ci\/circleci: pytorch-linux-xenial-py3-clang5-android-ndk-r19c-)/,
-    name: "ci/circleci: pytorch_linux_xenial_py3_clang5_android_ndk",
-  },
-  {
-    regex: /ci\/circleci: pytorch_linux_xenial_py3_6_gcc7_build/,
-    name: "ci/circleci: pytorch_linux_xenial_py3_clang5_asan_build",
-  },
-  {
-    regex: /ci\/circleci: pytorch_linux_xenial_py3_clang5_mobile_/,
-    name: "ci/circleci: pytorch_linux_xenial_py3_clang5_mobile",
-  },
-  {
-    regex: /ci\/circleci: pytorch_linux_xenial_py3_clang7_onnx_/,
-    name: "ci/circleci: pytorch_linux_xenial_py3_clang7_onnx",
-  },
-  {
-    regex: /ci\/circleci: pytorch_linux_xenial_py3_clang5_asan_/,
-    name: "ci/circleci: pytorch_linux_xenial_py3_clang5_asan",
-  },
-  {
-    regex: /ci\/circleci: pytorch_linux_xenial_py3_6_gcc7_/,
-    name: "ci/circleci: pytorch_linux_xenial_py3_6_gcc7",
-  },
-  {
-    regex: /ci\/circleci: pytorch_macos_10_13_py3_/,
-    name: "ci/circleci: pytorch_macos_10_13_py3",
-  },
-  {
-    regex: /ci\/circleci: pytorch_linux_xenial_py3_6_gcc5_4_/,
-    name: "ci/circleci: pytorch_linux_xenial_py3_6_gcc5_4",
-  },
-  {
-    regex: /ci\/circleci: binary_linux_/,
-    name: "ci/circleci: binary_linux",
-  },
-  {
-    regex: /ci\/circleci: binary_macos_/,
-    name: "ci/circleci: binary_macos",
-  },
-  {
-    regex: /ci\/circleci: binary_windows_/,
-    name: "ci/circleci: binary_windows",
   },
   {
     regex: /(pytorch-linux-bionic-rocm)|(pytorch_linux_bionic_rocm)/,
     name: "ROCm",
+  },
+  {
+    regex: /libtorch/,
+    name: "libtorch",
   },
 ];
 
