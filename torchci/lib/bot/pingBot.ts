1d7b888d22e5e7b853330b1cbc06ad93218c2571
zengk95 <34172846+zengk95@users.noreply.github.com>
2022-04-19 09:27:01 -0500

    finish on label

--- torchci/lib/bot/pingBot.ts
+++ torchci/lib/bot/pingBot.ts
@@ -1,9 +1,11 @@
-import { Probot } from "probot";
-import { reactOnComment } from "./botUtils";
+import { Context, Probot } from "probot";
+import { reactOnComment, addLabels } from "./botUtils";
 
 export default function pingBot(app: Probot): void {
-  console.log();
-  const openOnGreen = new RegExp("^s*@pytorchbots+?openOnGreen.*");
+  const openOnGreen = new RegExp("^s*@pytorchbots+?mergeOnGreen.*");
+  const LAND_PENDING = "land-pending";
+  const LANDED = "landed";
+  const LAND_FAILED = "land-failed";
 
   app.on("issue_comment.created", async (ctx) => {
     const commentBody = ctx.payload.comment.body;
@@ -14,11 +16,58 @@ export default function pingBot(app: Probot): void {
         return;
       } else {
         await reactOnComment(ctx, "+1");
+        await addLabels(ctx, [LAND_PENDING]);
       }
     }
   });
 
-  app.on("workflow_run.completed", async (ctx) => {
-    ctx.payload.workflow_run.
+  app.on(["pull_request.labeled"], async (ctx) => {
+    const labelNames = ctx.payload["pull_request"].labels.map(
+      (label) => label.name
+    );
+
+    if (labelNames.includes(LAND_PENDING)) {
+      // Check pull request's head ref and check runs to see if it has all green and merge if it's good
+      // TODO: Add pagination if checks go above 100
+      const payloadData = getPayloadDataForMerge(ctx);
+      const ref = ctx.payload.pull_request.head.sha;
+      const prNum = ctx.payload.pull_request.number;
+      const checks = await ctx.octokit.checks.listForRef({
+        ...payloadData,
+        ref,
+        per_page: 100,
+      });
+
+      const notSuccessfulJobs = checks?.data.check_runs.filter(
+        (check) =>
+          check.conclusion !== "success" && check.conclusion !== "neutral"
+      );
+      // If there's any jobs that aren't successful yet
+      if (notSuccessfulJobs.length === 0) {
+        await ctx.octokit.repos.createDispatchEvent({
+          ...payloadData,
+          event_type: "try_merge",
+          client_payload: {
+            pr_num: prNum,
+          },
+        });
+      }
+    }
   });
+
+  app.on("check_run.completed", async (ctx) => {
+    const conclusion = ctx.payload["check_run"].conclusion;
+    if (conclusion === "success") {
+      // Check pull requests and see if it has all green and merge
+    } else if (conclusion === "failure") {
+      // @ the author that it's failed to land and add land-failed
+    }
+  });
+}
+
+function getPayloadDataForMerge(ctx: any) {
+  return {
+    owner: ctx.payload.repository.owner.login,
+    repo: ctx.payload.repository.name,
+  };
 }
