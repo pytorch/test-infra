0c4505376274b7e3cc1e2db59a6bc1d14019c36c
Jane Xu <janeyx@fb.com>
2022-05-03 18:25:43 -0400

    Use a better viewer for singular flaky tests

--- torchci/pages/api/flaky-tests/flakytest.ts
+++ torchci/pages/api/flaky-tests/flakytest.ts
@@ -12,7 +12,7 @@ export default async function handler(
   const suite = req.query.suite;
   const file = req.query.file;
 
-  let numHours = 14 * 24 + "";
+  let numHours = 30 * 24 + "";
 
   const flakyTests: FlakyTestData[] = await fetchFlakyTests(numHours, name as string, suite as string, file as string);
 
