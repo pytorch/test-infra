db20356389a28679cea1301d67e17497c2753679
zengk95 <34172846+zengk95@users.noreply.github.com>
2022-05-03 10:52:43 -0500

    updated merge bot

--- torchci/lib/bot/mergeBot.ts
+++ torchci/lib/bot/mergeBot.ts
@@ -2,123 +2,124 @@ import { Probot } from "probot";
 import { addComment, reactOnComment } from "./botUtils";
 
 function mergeBot(app: Probot): void {
-  const mergeCmdPat = new RegExp(
-    "^\\s*@pytorch(merge|)bot\\s+(force\\s+)?merge\\s+this"
-  );
-  const revertCmdPat = new RegExp("^\\s*@pytorch(merge|)bot\\s+revert\\s+this");
-  const rebaseCmdPat = new RegExp(
-    "^\\s*@pytorch(merge|)bot\\s+rebase\\s+(me|this)"
-  );
-  const mergeOnGreenCmdPat = new RegExp(
-    "^\\s*@pytorch(merge|)bot\\s+land\\s+(me|this)"
-  );
-  app.on("issue_comment.created", async (ctx) => {
-    const commentBody = ctx.payload.comment.body;
-    const owner = ctx.payload.repository.owner.login;
-    const repo = ctx.payload.repository.name;
-    const prNum = ctx.payload.issue.number;
+    const mergeCmdPat = new RegExp(
+        "^\\s*@pytorch(merge|)bot\\s+(force\\s+)?merge\\s+this\\s*(on\\s*green)?"
+    );
 
-    async function dispatchEvent(
-      event_type: string,
-      force: boolean = false,
-      onGreen: boolean = false
-    ) {
-      let payload: any = {
-        pr_num: prNum,
-        comment_id: ctx.payload.comment.id,
-      };
+    const revertCmdPat = new RegExp(
+        "^\\s*@pytorch(merge|)bot\\s+revert\\s+this"
+    );
+    const rebaseCmdPat = new RegExp(
+        "^\\s*@pytorch(merge|)bot\\s+rebase\\s+(me|this)"
+    );
+    app.on("issue_comment.created", async (ctx) => {
+        const commentBody = ctx.payload.comment.body;
+        const owner = ctx.payload.repository.owner.login;
+        const repo = ctx.payload.repository.name;
+        const prNum = ctx.payload.issue.number;
 
-      if (onGreen) {
-        payload.on_green = true;
-      } else if (force) {
-        payload.force = true;
-      }
+        async function dispatchEvent(
+            event_type: string,
+            force: boolean = false,
+            onGreen: boolean = false
+        ) {
+            let payload: any = {
+                pr_num: prNum,
+                comment_id: ctx.payload.comment.id,
+            };
 
-      await ctx.octokit.repos.createDispatchEvent({
-        owner,
-        repo,
-        event_type: event_type,
-        client_payload: payload,
-      });
-    }
+            if (force) {
+                payload.force = true;
+            } else if (onGreen) {
+                payload.force = true;
+            }
 
-    const match = commentBody.match(mergeCmdPat);
-    if (match) {
-      if (!ctx.payload.issue.pull_request) {
-        // Issue, not pull request.
-        await reactOnComment(ctx, "confused");
-        return;
-      }
-      await dispatchEvent("try-merge", typeof match[2] === "string");
-      await reactOnComment(ctx, "+1");
-    } else if (commentBody.match(revertCmdPat)) {
-      if (!ctx.payload.issue.pull_request) {
-        // Issue, not pull request.
-        await reactOnComment(ctx, "confused");
-        return;
-      }
-      const revertWithReasonCmdPat = new RegExp(
-        "^\\s*@pytorch(merge|)bot\\s+revert\\s+this.*(\\s+\\w+){3,}"
-      );
-      if (commentBody.match(revertWithReasonCmdPat) === null) {
-        // revert reason of 3+ words not given
-        await addComment(
-          ctx,
-          "Revert unsuccessful: please retry the command explaining why the revert is necessary, " +
-            "e.g. @pytorchbot revert this as it breaks mac tests on trunk, see <url to logs>."
-        );
-        return;
-      }
-      await dispatchEvent("try-revert");
-      await reactOnComment(ctx, "+1");
-    } else if (
-      commentBody.match(rebaseCmdPat) &&
-      ctx.payload.comment.user.login == "clee2000"
-    ) {
-      if (!ctx.payload.issue.pull_request) {
-        // Issue, not pull request.
-        await reactOnComment(ctx, "confused");
-        return;
-      }
-      await dispatchEvent("try-rebase");
-      await reactOnComment(ctx, "+1");
-    } else if (commentBody.match(mergeOnGreenCmdPat)) {
-      await dispatchEvent("try-merge", false, true);
-      await reactOnComment(ctx, "+1");
-    }
-  });
-  app.on(
-    ["pull_request_review.submitted", "pull_request_review.edited"],
-    async (ctx) => {
-      const reviewBody = ctx.payload.review.body;
-      const owner = ctx.payload.repository.owner.login;
-      const repo = ctx.payload.repository.name;
-      const prNum = ctx.payload.pull_request.number;
-      async function addComment(comment: string) {
-        await ctx.octokit.issues.createComment({
-          issue_number: prNum,
-          body: comment,
-          owner,
-          repo,
-        });
-      }
-      async function dispatchEvent(event_type: string) {
-        await ctx.octokit.repos.createDispatchEvent({
-          owner,
-          repo,
-          event_type: event_type,
-          client_payload: {
-            pr_num: prNum,
-          },
-        });
-      }
+            await ctx.octokit.repos.createDispatchEvent({
+                owner,
+                repo,
+                event_type: event_type,
+                client_payload: payload,
+            });
+        }
 
-      if (reviewBody?.match(mergeCmdPat)) {
-        await dispatchEvent("try-merge");
-        await addComment("+1"); // REST API doesn't support reactions for code reviews.
-      }
-    }
-  );
+        const match = commentBody.match(mergeCmdPat);
+        if (match) {
+            if (!ctx.payload.issue.pull_request) {
+                // Issue, not pull request.
+                await reactOnComment(ctx, "confused");
+                return;
+            }
+            await dispatchEvent(
+                "try-merge",
+                typeof match[2] === "string",
+                typeof match[3] == "string"
+            );
+            await reactOnComment(ctx, "+1");
+        } else if (commentBody.match(revertCmdPat)) {
+            if (!ctx.payload.issue.pull_request) {
+                // Issue, not pull request.
+                await reactOnComment(ctx, "confused");
+                return;
+            }
+            const revertWithReasonCmdPat = new RegExp(
+                "^\\s*@pytorch(merge|)bot\\s+revert\\s+this.*(\\s+\\w+){3,}"
+            );
+            if (commentBody.match(revertWithReasonCmdPat) === null) {
+                // revert reason of 3+ words not given
+                await addComment(
+                    ctx,
+                    "Revert unsuccessful: please retry the command explaining why the revert is necessary, " +
+                        "e.g. @pytorchbot revert this as it breaks mac tests on trunk, see <url to logs>."
+                );
+                return;
+            }
+            await dispatchEvent("try-revert");
+            await reactOnComment(ctx, "+1");
+        } else if (
+            commentBody.match(rebaseCmdPat) &&
+            ctx.payload.comment.user.login == "clee2000"
+        ) {
+            if (!ctx.payload.issue.pull_request) {
+                // Issue, not pull request.
+                await reactOnComment(ctx, "confused");
+                return;
+            }
+            await dispatchEvent("try-rebase");
+            await reactOnComment(ctx, "+1");
+        }
+    });
+    app.on(
+        ["pull_request_review.submitted", "pull_request_review.edited"],
+        async (ctx) => {
+            const reviewBody = ctx.payload.review.body;
+            const owner = ctx.payload.repository.owner.login;
+            const repo = ctx.payload.repository.name;
+            const prNum = ctx.payload.pull_request.number;
+            async function addComment(comment: string) {
+                await ctx.octokit.issues.createComment({
+                    issue_number: prNum,
+                    body: comment,
+                    owner,
+                    repo,
+                });
+            }
+            async function dispatchEvent(event_type: string) {
+                await ctx.octokit.repos.createDispatchEvent({
+                    owner,
+                    repo,
+                    event_type: event_type,
+                    client_payload: {
+                        pr_num: prNum,
+                    },
+                });
+            }
+
+            if (reviewBody?.match(mergeCmdPat)) {
+                await dispatchEvent("try-merge");
+                await addComment("+1"); // REST API doesn't support reactions for code reviews.
+            }
+        }
+    );
 }
 
 export default mergeBot;
