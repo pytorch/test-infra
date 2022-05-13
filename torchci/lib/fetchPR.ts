11fc16c69b26cf87fcfdfd5a7357879f1583b35e
zengk95 <34172846+zengk95@users.noreply.github.com>
2022-05-13 17:46:33 -0500

    update formatting

--- torchci/lib/fetchPR.ts
+++ torchci/lib/fetchPR.ts
@@ -3,31 +3,31 @@ import getRocksetClient from "./rockset";
 import { PRData } from "./types";
 
 export default async function fetchPR(
-    owner: string,
-    repo: string,
-    prNumber: string
+  owner: string,
+  repo: string,
+  prNumber: string
 ): Promise<PRData> {
-    const octokit = await getOctokit(owner, repo);
-    const [pull, commits] = await Promise.all([
-        octokit.rest.pulls.get({
-            owner,
-            repo,
-            pull_number: parseInt(prNumber),
-        }),
-        octokit.paginate(octokit.rest.pulls.listCommits, {
-            owner,
-            repo,
-            pull_number: parseInt(prNumber),
-            per_page: 100,
-            endpoint: {
-                url: "/repos/{owner}/{repo}/commits",
-            },
-        }),
-    ]);
-    const title = pull.data.title;
-    const shas = commits.map((commit) => {
-        return { sha: commit.sha, title: commit.commit.message.split("\n")[0] };
-    });
+  const octokit = await getOctokit(owner, repo);
+  const [pull, commits] = await Promise.all([
+    octokit.rest.pulls.get({
+      owner,
+      repo,
+      pull_number: parseInt(prNumber),
+    }),
+    octokit.paginate(octokit.rest.pulls.listCommits, {
+      owner,
+      repo,
+      pull_number: parseInt(prNumber),
+      per_page: 100,
+      endpoint: {
+        url: "/repos/{owner}/{repo}/commits",
+      },
+    }),
+  ]);
+  const title = pull.data.title;
+  const shas = commits.map((commit) => {
+    return { sha: commit.sha, title: commit.commit.message.split("\n")[0] };
+  });
 
-    return { title, shas };
+  return { title, shas };
 }
