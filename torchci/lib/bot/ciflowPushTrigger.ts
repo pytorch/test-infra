f37127c6031395f34c57d721034ff1d0023e6d00
Michael Suo <suo@fb.com>
2022-05-25 13:31:20 -0700

    [ciflow] remove ciflow/all as an option
    Remove this from the list of supported labels, and provide a message
    explaining why.

--- torchci/lib/bot/ciflowPushTrigger.ts
+++ torchci/lib/bot/ciflowPushTrigger.ts
@@ -137,7 +137,6 @@ async function handleLabelEvent(context: Context<"pull_request.labeled">) {
   const valid_labels = [
     "ciflow/trunk",
     "ciflow/periodic",
-    "ciflow/all",
     "ciflow/android",
     "ciflow/binaries",
     "ciflow/nightly",
@@ -146,13 +145,22 @@ async function handleLabelEvent(context: Context<"pull_request.labeled">) {
     "ciflow/binaries_wheel",
   ];
   if (!valid_labels.includes(label)) {
-    let body = `We have recently simplified the CIFlow labels and \`${label}\` is no longer in use.\n`;
+    let body;
+    if (label === "ciflow/all") {
+      body =
+        "The `ciflow/all` label was recently removed. It ran very expensive periodic CI jobs when most contributors ";
+      body +=
+        "did not need them. If you just want to check that you won't be reverted, use `ciflow/trunk`. ";
+      body +=
+        "If you *really* want the old `ciflow/all` behavior, add `ciflow/trunk` and `ciflow/periodic`.";
+    } else {
+      body = `We have recently simplified the CIFlow labels and \`${label}\` is no longer in use.\n`;
+    }
     body += "You can use any of the following\n";
     body +=
       "- `ciflow/trunk` (`.github/workflows/trunk.yml`): all jobs we run per-commit on master\n";
     body +=
       "- `ciflow/periodic` (`.github/workflows/periodic.yml`): all jobs we run periodically on master\n";
-    body += "- `ciflow/all`: trunk + periodic; all jobs we run in master CI\n";
     body +=
       "- `ciflow/android` (`.github/workflows/run_android_tests.yml`): android build and test\n";
     body +=
