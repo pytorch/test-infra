f99277277e77b840fde899c32c04df3ca22d7e36
Michael Suo <suo@fb.com>
2022-05-26 09:22:35 -0700

    delete old ciflow bot (#352)
    This has been unused for a while, just cleaning up the old code.

--- torchci/lib/bot/index.ts
+++ torchci/lib/bot/index.ts
@@ -1,7 +1,6 @@
 import { Probot } from "probot";
 import autoCcBot from "./autoCcBot";
 import autoLabelBot from "./autoLabelBot";
-import ciflowBot from "./ciflowBot";
 import ciflowPushTrigger from "./ciflowPushTrigger";
 import webhookToDynamo from "./webhookToDynamo";
 import verifyDisableTestIssueBot from "./verifyDisableTestIssueBot";
@@ -13,7 +12,6 @@ export default function bot(app: Probot) {
   autoCcBot(app);
   autoLabelBot(app);
   verifyDisableTestIssueBot(app);
-  // ciflowBot(app);
   ciflowPushTrigger(app);
   webhookToDynamo(app);
   triggerCircleCIWorkflows(app);
