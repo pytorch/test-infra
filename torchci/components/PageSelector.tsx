427360901be7d6bab559193e8389f5e57a72159f
Michael Suo <suo@fb.com>
2022-06-13 14:29:18 -0700

    eliminate more prefetches

--- torchci/components/PageSelector.tsx
+++ torchci/components/PageSelector.tsx
@@ -15,6 +15,7 @@ export default function PageSelector({
       {params.page > 1 ? (
         <span>
           <Link
+            prefetch={false}
             href={formatHudUrlForRoute(baseUrl, {
               ...params,
               page: params.page - 1,
@@ -26,6 +27,7 @@ export default function PageSelector({
         </span>
       ) : null}
       <Link
+        prefetch={false}
         href={formatHudUrlForRoute(baseUrl, {
           ...params,
           page: params.page + 1,
