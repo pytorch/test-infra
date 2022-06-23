c22f064cf5195fb450c57cf90fb2d92df366a48c
Sarah Wang <sarah.wang2009@gmail.com>
2022-06-23 14:09:48 -0400

    made Dr. CI bot that adds blank comment to swang392's PRs (#429)
    * made Dr. CI bot that adds blank comment to swang392's PRs
    
    * added list of possible users instead of checking string equality

---
+++ torchci/test/drciBot.test.ts
@@ -0,0 +1,56 @@
+import { Probot } from "probot";
+import * as utils from "./utils";
+import nock from "nock";
+import myProbotApp, * as botUtils from "../lib/bot/drciBot";
+
+describe("verify-drci-functionality", () => {
+  let probot: Probot;
+
+  beforeEach(() => {
+    probot = utils.testProbot();
+    probot.load(myProbotApp);
+  });
+
+  afterEach(() => {
+    nock.cleanAll();
+    jest.restoreAllMocks();
+  });
+  
+  test("Dr. CI comment if user of PR is swang392", async () => {
+    nock("https://api.github.com")
+      .post("/app/installations/2/access_tokens")
+      .reply(200, { token: "test" });
+
+    const payload = require("./fixtures/pull_request.opened")["payload"];
+    payload["pull_request"]["user"]["login"] = "swang392"
+
+    const scope = nock("https://api.github.com")
+      .get("/repos/zhouzhuojie/gha-ci-playground/issues/31/comments", (body) => {
+        return true;
+      })
+      .reply(200)
+      .post("/repos/zhouzhuojie/gha-ci-playground/issues/31/comments", (body) => {
+        expect(body).toMatchObject({ body: "<!-- drci-comment-start -->hello there!<!-- drci-comment-end -->" });
+        return true;
+      })
+      .reply(200);
+
+    await probot.receive({ name: "pull_request", payload: payload, id: "2" });
+
+    if (!nock.isDone()) {
+      console.error("pending mocks: %j", scope.pendingMocks());
+    }
+  });
+
+  test("Dr. CI does not comment when user of PR is swang392", async () => {
+    const payload = require("./fixtures/pull_request.opened")["payload"];
+    payload["pull_request"]["user"]["login"] = "not_swang392"
+
+    const mock = jest.spyOn(botUtils, 'formDrciComment')
+    mock.mockImplementation();
+
+    await probot.receive({ name: "pull_request", payload: payload, id: "2" });
+    expect(mock).not.toHaveBeenCalled();
+  });
+
+});
