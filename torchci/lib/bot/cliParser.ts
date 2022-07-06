b34497faf6368dcba3fcf51642cddac1bb3cb4ae
Kerry Zeng <34172846+zengk95@users.noreply.github.com>
2022-07-06 09:45:34 -0500

    [mergebot] update land check message  (#443)
    * update land check messages
    
    * update again
    
    * Update torchci/lib/bot/cliParser.ts
    
    Co-authored-by: Jane (Yuan) Xu <31798555+janeyx99@users.noreply.github.com>
    
    Co-authored-by: Jane (Yuan) Xu <31798555+janeyx99@users.noreply.github.com>

--- torchci/lib/bot/cliParser.ts
+++ torchci/lib/bot/cliParser.ts
@@ -49,7 +49,7 @@ mergeOption.add_argument("-f", "--force", {
 });
 mergeOption.add_argument("-l", "--land-checks", {
   action: "store_true",
-  help: "Merge with land time checks. (EXPERIMENTAL! DO NOT USE)",
+  help: "Merge with land time checks. This will create a new branch with your changes rebased on viable/strict and run additional tests (EXPERIMENTAL)",
 });
 
 // Revert
