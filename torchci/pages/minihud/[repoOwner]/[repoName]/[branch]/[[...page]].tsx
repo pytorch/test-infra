39b7c70c8e1be4c0d33cd679f1129711c4cb060c
Catherine Lee <csl@fb.com>
2022-07-12 14:51:04 -0700

    moinihdu

--- torchci/pages/minihud/[repoOwner]/[repoName]/[branch]/[[...page]].tsx
+++ torchci/pages/minihud/[repoOwner]/[repoName]/[branch]/[[...page]].tsx
@@ -6,11 +6,12 @@ import JobLinks from "components/JobLinks";
 import LogViewer from "components/LogViewer";
 import styles from "components/minihud.module.css";
 import PageSelector from "components/PageSelector";
-import { LocalTimeHuman } from "components/TimeUtils";
+import { durationHuman, LocalTimeHuman } from "components/TimeUtils";
 import { isFailedJob } from "lib/jobUtils";
 import { HudParams, JobData, packHudParams, RowData } from "lib/types";
 import useHudData from "lib/useHudData";
 import useScrollTo from "lib/useScrollTo";
+import _ from "lodash";
 import { useRouter } from "next/router";
 import {
   createContext,
@@ -187,10 +188,12 @@ function CommitSummaryLine({
   row,
   numPending,
   showRevert,
+  ttsAlert,
 }: {
   row: RowData;
   numPending: number;
   showRevert: boolean;
+  ttsAlert: boolean;
 }) {
   const router = useRouter();
   useScrollTo();
@@ -231,12 +234,161 @@ function CommitSummaryLine({
           </a>
         </span>
       )}
+      {ttsAlert && (
+        <span style={{ float: "right" }}>
+          <b>tts alert</b>
+        </span>
+      )}
       <CommitLinks row={row} />
     </div>
   );
 }
 
-function CommitSummary({ row }: { row: RowData }) {
+function DurationInfo({
+  jobs,
+  prevRow,
+  expandAllDurationInfo,
+}: {
+  jobs: JobData[];
+  prevRow: RowData | undefined;
+  expandAllDurationInfo: boolean;
+}) {
+  function getAggregateTestTimes(jobs: JobData[] | undefined) {
+    return _.reduce(
+      jobs,
+      (
+        prev: {
+          [key: string]: {
+            duration: number;
+            validData: boolean;
+          };
+        },
+        cur
+      ) => {
+        // filter out rocm and macos because they tend to be variable
+        if (
+          cur.name != undefined &&
+          cur.name.includes(",") &&
+          !cur.name.includes("rocm") &&
+          !cur.name.includes("macos")
+        ) {
+          let name = cur.name.substring(0, cur.name.indexOf(","));
+          if (!(name in prev)) {
+            prev[name] = { duration: 0, validData: true };
+          }
+          if (cur.conclusion != "success" || cur.durationS === undefined) {
+            prev[name].validData = false;
+          } else {
+            prev[name].duration += cur.durationS;
+          }
+        }
+        return prev;
+      },
+      {}
+    );
+  }
+
+  const prevRowJobsAggregate = _.pickBy(
+    getAggregateTestTimes(prevRow?.jobs),
+    (value) => value.validData
+  );
+
+  function getDurationInfo(name: string, duration: number, validData: boolean) {
+    const durationString = validData ? durationHuman(duration) : "invalid data";
+    var color = "black";
+    if (
+      !validData ||
+      prevRow === undefined ||
+      prevRowJobsAggregate[name] === undefined
+    ) {
+      return {
+        concerningChange: false,
+        name,
+        color,
+        duration: durationString,
+        percentChangeString: "",
+        absoluteChangeString: "",
+      };
+    }
+    const prevDuration = prevRowJobsAggregate[name].duration;
+
+    const percentChange = duration / prevDuration;
+    const percentChangeString =
+      percentChange >= 1
+        ? `+ ${((percentChange - 1) * 100).toFixed(2)}%`
+        : `- ${((1 - percentChange) * 100).toFixed(2)}%`;
+    const absoluteChange = Math.round(duration - prevDuration);
+    const absoluteChangeString =
+      absoluteChange >= 0
+        ? `+ ${durationHuman(absoluteChange)}`
+        : `- ${durationHuman(Math.abs(absoluteChange))}`;
+    const concerningChange = Math.abs(absoluteChange) > 60 * 30;
+    if (concerningChange) {
+      color = absoluteChange > 0 ? "red" : "purple";
+    }
+    return {
+      concerningChange,
+      color,
+      name,
+      duration: durationString,
+      percentChangeString,
+      absoluteChangeString,
+    };
+  }
+
+  const [concerning, notConcerning] = _.partition(
+    _.map(getAggregateTestTimes(jobs), (value, key) => {
+      return getDurationInfo(key, value.duration, value.validData);
+    }),
+    (e) => e.concerningChange
+  );
+
+  function getRow(val: {
+    concerningChange: boolean;
+    name: string | undefined;
+    duration: string;
+    color: string;
+    percentChangeString: string;
+    absoluteChangeString: string;
+  }) {
+    return (
+      <tr key={`duration-row-${val.name}`} style={{ color: val.color }}>
+        <td style={{ width: "750px" }}>{val.name}</td>
+        <td style={{ width: "100px" }}>{val.duration}</td>
+        <td style={{ width: "100px" }}>{val.percentChangeString}</td>
+        <td style={{ width: "100px" }}>{val.absoluteChangeString}</td>
+      </tr>
+    );
+  }
+  return {
+    ttsAlert: concerning.length > 0,
+    durationJsxElement: (
+      <div style={{ padding: "10px" }}>
+        <table>
+          <tbody>{concerning.map((val) => getRow(val))}</tbody>
+        </table>
+        <details open={expandAllDurationInfo}>
+          <summary>See all jobs</summary>
+          <table>
+            <tbody>{notConcerning.map((val) => getRow(val))}</tbody>
+          </table>
+        </details>
+      </div>
+    ),
+  };
+}
+
+function CommitSummary({
+  row,
+  prevRow,
+  showDurationInfo,
+  expandAllDurationInfo,
+}: {
+  row: RowData;
+  prevRow: RowData | undefined;
+  showDurationInfo: boolean;
+  expandAllDurationInfo: boolean;
+}) {
   const [jobFilter, _setJobFilter] = useContext(JobFilterContext);
   const [highlighted, setHighlighted] = useState(false);
 
@@ -266,6 +418,12 @@ function CommitSummary({ row }: { row: RowData }) {
     className += " " + styles.workflowBoxHighlight;
   }
 
+  const { ttsAlert, durationJsxElement } = DurationInfo({
+    jobs,
+    prevRow,
+    expandAllDurationInfo,
+  });
+
   useEffect(() => {
     const onHashChanged = () => {
       if (window.location.hash === "") {
@@ -289,14 +447,24 @@ function CommitSummary({ row }: { row: RowData }) {
         row={row}
         numPending={pendingJobs.length}
         showRevert={failedJobs.length !== 0}
+        ttsAlert={ttsAlert}
       />
-      <FailedJobs failedJobs={failedJobs} />
+      {!showDurationInfo && <FailedJobs failedJobs={failedJobs} />}
+      {showDurationInfo && durationJsxElement}
     </div>
   );
 }
 
 function MiniHud({ params }: { params: HudParams }) {
   const data = useHudData(params);
+
+  var paramsNextPage = { ...params };
+  paramsNextPage.page = params.page + 1;
+  const extraRow = useHudData(paramsNextPage);
+
+  const [showDurationInfo, setShowDurationInfo] = useState(false);
+  const [expandAllDurationInfo, setExpandAllDurationInfo] = useState(false);
+
   if (data === undefined) {
     return <div>Loading...</div>;
   }
@@ -305,8 +473,39 @@ function MiniHud({ params }: { params: HudParams }) {
 
   return (
     <>
-      {shaGrid.map((row: RowData) => (
-        <CommitSummary row={row} key={row.sha} />
+      <div>
+        <input
+          type="checkbox"
+          id={"showDurationInfo"}
+          onChange={() => setShowDurationInfo(!showDurationInfo)}
+        />
+        <label htmlFor={"showDurationInfo"}>show duration info</label>
+        {showDurationInfo && (
+          <>
+            <input
+              type="checkbox"
+              id={"expandAllDurationInfo"}
+              checked={expandAllDurationInfo}
+              onChange={() => setExpandAllDurationInfo(!expandAllDurationInfo)}
+            />
+            <label htmlFor={"expandAllDurationInfo"}>
+              expand all duration info
+            </label>
+          </>
+        )}
+      </div>
+      {shaGrid.map((row: RowData, index: number, array: RowData[]) => (
+        <CommitSummary
+          row={row}
+          prevRow={
+            index + 1 >= array.length
+              ? extraRow?.shaGrid[0]
+              : array.at(index + 1)
+          }
+          key={row.sha}
+          showDurationInfo={showDurationInfo}
+          expandAllDurationInfo={expandAllDurationInfo}
+        />
       ))}
     </>
   );
