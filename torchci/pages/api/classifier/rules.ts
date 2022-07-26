4e26b6c17a0ee241789075a4fdb43b4c47a08334
Catherine Lee <csl@fb.com>
2022-07-26 14:47:17 -0700

    jump to stack traces

--- torchci/pages/api/classifier/rules.ts
+++ torchci/pages/api/classifier/rules.ts
@@ -106,7 +106,7 @@ export default async function handler(
       },
       {
         name: "Python pytest failure",
-        pattern: r`^FAILED test.*`,
+        pattern: r`=== FAILURES ===`,
         priority: 997,
       },
       {
