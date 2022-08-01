a1377342b7fe0f0c465da4ff21d5430ab0f7c5bb
Huy Do <huydhn@gmail.com>
2022-08-01 15:01:04 -0700

    Minor wording fix

--- torchci/lib/bot/pytorchBotHandler.ts
+++ torchci/lib/bot/pytorchBotHandler.ts
@@ -138,7 +138,7 @@ class PytorchBotHandler {
         "You need to provide a reason for using force merge, in the format `@pytorchbot merge -f 'Explanation'`. " +
           "The explanation needs to be clear on why this is needed. Here are some good examples:\n" +
           "* Bypass checks due to unrelated upstream failures from ...\n" +
-          "* This is a minor fix to clean up unused variables, which shouldn't break anything\n" +
+          "* This is a minor fix to ..., which shouldn't break anything\n" +
           "* This is pre-tested in a previous CI run\n" +
           "* Bypass flaky ... check"
       );
