d975e6020bf12b8e284de2921a94aa03cba054fe
Huy Do <huydhn@gmail.com>
2022-08-08 10:35:11 -0700

    Add more conda and pip dependencies

--- aws/ami/windows/scripts/Installers/Install-Miniconda3.ps1
+++ aws/ami/windows/scripts/Installers/Install-Miniconda3.ps1
@@ -24,18 +24,25 @@ $argsList = "/InstallationType=AllUsers /RegisterPython=0 /S /D=$installationDir
 Write-Output "Installing Miniconda to $installationDir"
 Start-Process -FilePath "$downloadDir\$condaFileName" -ArgumentList "$argsList" -Wait -NoNewWindow -PassThru
 
-$condaExe = "$installationDir\Scripts\conda.exe"
-if (-Not (Test-Path -Path $condaExe -PathType Leaf)) {
-  Write-Error "Miniconda installation failed, no executable found at $condaExe"
+$condaHook = "$installationDir\shell\condabin\conda-hook.ps1"
+if (-Not (Test-Path -Path $condaHook -PathType Leaf)) {
+  Write-Error "Miniconda installation failed, no hook found at $condaHook"
   exit 1
 }
 
-Write-Output "Installing some common conda packages"
+# Load conda into powershell
+& $condaHook
+# and activate it (without this, python and pip commands won't be recognized)
+conda activate base
+
+Write-Output "Installing conda and pip packages for building and testing PyTorch"
 # The list of dependencies is copied from the current PyTorch miniconda installation script
-$pythonDeps = 'numpy"<1.23" ninja pyyaml setuptools cmake cffi typing_extensions future six requests dataclasses boto3 libuv'
-Start-Process -FilePath $condaExe -ArgumentList "install -y $pythonDeps" -Wait -NoNewWindow -PassThru
-$cmakeDep = "-c conda-forge cmake=3.22.3"
-Start-Process -FilePath $condaExe -ArgumentList "install -y $cmakeDep" -Wait -NoNewWindow -PassThru
+conda install -y numpy"<1.23" ninja pyyaml setuptools cmake cffi typing_extensions future six requests dataclasses boto3 libuv
+conda install -y -c conda-forge cmake=3.22.3
+# and setup_pytorch_env script
+conda install -y mkl protobuf numba scipy=1.6.2 typing_extensions dataclasses
+# Some dependencies are installed by pip, copying them exactly
+pip install "ninja==1.10.0.post1" future "hypothesis==5.35.1" "expecttest==0.1.3" "librosa>=0.6.2" "scipy==1.6.3" "psutil==5.9.1" "pynvml==11.4.1" pillow "unittest-xml-reporting<=3.2.0,>=2.0.0" pytest pytest-xdist pytest-rerunfailures
 
 # Clean up the temp file
 Remove-Item -Path "$downloadDir\*" -Recurse -Force -ErrorAction SilentlyContinue
