86deb9026ea2ea4bce51f386731629913c595bdf
Zain Rizvi <zainr@fb.com>
2022-08-09 11:39:37 -0500

    Redownload query

--- torchci/rockset/pytorch_dev_infra_kpis/__sql/num_reverts.sql
+++ torchci/rockset/pytorch_dev_infra_kpis/__sql/num_reverts.sql
@@ -1,5 +1,5 @@
 SELECT
-    FORMAT_TIMESTAMP('%m-%d-%y', DATE_TRUNC('week', push._event_time)) AS week_bucket,
+    FORMAT_TIMESTAMP('%m-%d-%y', DATE_TRUNC(:granularity, push._event_time)) AS bucket,
     COUNT(*) as num
 FROM
     push
@@ -8,11 +8,12 @@ WHERE
     AND push.repository.owner.name = 'pytorch'
     AND push.repository.name = 'pytorch'
     AND (
-        push.head_commit.message LIKE 'Revert D%'
+        push.head_commit.message LIKE 'Revert %'
         OR push.head_commit.message LIKE 'Back out%'
     )
     AND push._event_time >= PARSE_DATETIME_ISO8601(:startTime)
+    AND push._event_time <= PARSE_DATETIME_ISO8601(:stopTime)
 group by
-    week_bucket
+    bucket
 order by
-    week_bucket
+    bucket
