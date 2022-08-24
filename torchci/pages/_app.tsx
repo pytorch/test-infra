0e0f942da8d8558a7391f17104025920706c2b17
zengk95 <34172846+zengk95@users.noreply.github.com>
2022-08-24 12:09:39 -0500

    add some web vitals

--- torchci/pages/_app.tsx
+++ torchci/pages/_app.tsx
@@ -1,19 +1,31 @@
 import AnnouncementBanner from "components/AnnouncementBanner";
 import NavBar from "components/NavBar";
 import SevReport from "components/SevReport";
-import type { AppProps } from "next/app";
+import type { AppProps, NextWebVitalsMetric } from "next/app";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import "styles/globals.css";
-import { pageview } from "../lib/googleAnalytics";
+import { pageview, event } from "../lib/googleAnalytics";
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  // @ts-ignore
+  window.gtag("event", metric.name, {
+    event_category:
+      metric.label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
+    value: Math.round(
+      metric.name === "CLS" ? metric.value * 1000 : metric.value
+    ), // values must be integers
+    event_label: metric.id, // id unique to current page load
+    non_interaction: true, // avoids affecting bounce rate.
+  });
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   useEffect(() => {
     const handleRouteChange = (url: string) => {
-      console.log("GOING INTO PAGE VIEW");
       pageview(url);
     };
     //When the component is mounted, subscribe to router changes
