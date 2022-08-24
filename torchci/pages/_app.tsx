a6447dbada6314c2494118a3919e1c4ecb8a690a
zengk95 <34172846+zengk95@users.noreply.github.com>
2022-08-24 11:45:09 -0500

    add google analytics

--- torchci/pages/_app.tsx
+++ torchci/pages/_app.tsx
@@ -3,9 +3,30 @@ import NavBar from "components/NavBar";
 import SevReport from "components/SevReport";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import "styles/globals.css";
+import { pageview } from "../lib/googleAnalytics";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      console.log("GOING INTO PAGE VIEW");
+      pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
