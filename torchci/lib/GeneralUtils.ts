dc814c4b53cd9486dac98e69662fedff3dade5cf
zengk95 <34172846+zengk95@users.noreply.github.com>
2022-08-25 17:17:49 -0500

    update some tsc

--- torchci/lib/GeneralUtils.ts
+++ torchci/lib/GeneralUtils.ts
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 import { isFailure } from "./JobClassifierUtil";
-=======
->>>>>>> e93c8910 (fix up some stuff)
 import { CommitData, JobData } from "./types";
 
 export function includesCaseInsensitive(
@@ -32,19 +29,6 @@ export function getFailureMessage(
     return "";
   }
   const failedJobs = jobData.filter((job) => isFailure(job.conclusion));
-<<<<<<< HEAD
-  const failedJobsString = failedJobs
-    .map((failedJob) => `- [${failedJob.name}](${failedJob.htmlUrl})`)
-    .join("\n");
-  const hudLink = `https://hud.pytorch.org/pytorch/pytorch/commit/${commitData.sha}`;
-  return `
-  ### Additional Information
-
-  @${commitData.author} This PR is being reverted. The following jobs failed on this PR: 
-  ${failedJobsString}
-
-  Debug these failures on [HUD](${hudLink}).
-=======
 
   const hudLink = `https://hud.pytorch.org/pytorch/pytorch/commit/${commitData.sha}`;
   return `
@@ -56,6 +40,5 @@ export function getFailureMessage(
   ${failedJobs.map((failedJob) => `- ${failedJob.name}`)}
   
   For more information, check the [HUD](${hudLink}).
->>>>>>> e93c8910 (fix up some stuff)
   `;
 }
