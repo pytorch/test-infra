619ca730a4fa022ff1f2eb673caef32e0237458c
zengk95 <34172846+zengk95@users.noreply.github.com>
2022-08-30 16:50:36 -0500

    fx up bootstrap and make modal

--- torchci/lib/githubFunctions.ts
+++ torchci/lib/githubFunctions.ts
@@ -6,20 +6,20 @@ export async function commentOnPR(
   accessToken: string,
   onComplete: Function
 ) {
-  const commentUrl = `https://api.github.com/repos/${owner}/${repo}/issues/${issue_number}/comments`;
   // Prevent devs from accidentally reverting something in DEV
   if (process.env.NODE_ENV !== "production") {
     repo = "test-infra";
     issue_number = "582";
   }
+  const commentUrl = `https://api.github.com/repos/${owner}/${repo}/issues/${issue_number}/comments`;
 
-  // const response = await fetch(commentUrl, {
-  //   method: "POST",
-  //   body: JSON.stringify({ body: message }),
-  //   headers: {
-  //     Accept: "application/vnd.github+json",
-  //     Authorization: `token ${accessToken}`,
-  //   },
-  // });
-  // onComplete(JSON.stringify(await response.json(), undefined, 2));
+  const response = await fetch(commentUrl, {
+    method: "POST",
+    body: JSON.stringify({ body: message }),
+    headers: {
+      Accept: "application/vnd.github+json",
+      Authorization: `token ${accessToken}`,
+    },
+  });
+  onComplete(JSON.stringify(await response.json(), undefined, 2));
 }
