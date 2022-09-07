36c40d66b593097929ad4ba319a157a77e3c797c
Zain Rizvi <zainr@fb.com>
2022-09-07 18:48:52 -0500

    Fix formatting

--- torchci/rockset/pytorch_dev_infra_kpis/__sql/time_to_signal.sql
+++ torchci/rockset/pytorch_dev_infra_kpis/__sql/time_to_signal.sql
@@ -33,7 +33,7 @@ completed_workflows as (
         AND w.repository.full_name = 'pytorch/pytorch'
         and PARSE_TIMESTAMP_ISO8601(w.created_at) > PARSE_TIMESTAMP_ISO8601(:startTime)
         and w.run_attempt = 1 
-  		and w.head_sha in (select sha from successful_commits)
+        and w.head_sha in (select sha from successful_commits)
 ),
 tts_by_sha as (
     select
