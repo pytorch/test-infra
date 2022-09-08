860f516f13bf2db0c28e295e55d3ce0e865f491b
DanilBaibak <danil.baibak@gmail.com>
2022-09-08 16:25:00 +0200

    Added build conda for MacOS

--- tools/scripts/generate_binary_build_matrix.py
+++ tools/scripts/generate_binary_build_matrix.py
@@ -66,7 +66,8 @@ def validation_runner(arch_type: str, os: str) -> str:
             return WIN_CPU_RUNNER
     elif os == "macos-arm64":
         return MACOS_M1_RUNNER
-    elif os == "macos-x86_64":
+    # elif os == "macos-x86_64":
+    elif os == "macos":
         return MACOS_X86_RUNNER
     else: # default to linux cpu runner
         return LINUX_CPU_RUNNER
