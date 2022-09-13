b7d40f820e2612d620569e2f748f55353c342dff
Kerry Zeng <34172846+zengk95@users.noreply.github.com>
2022-09-13 18:00:22 -0500

    Add revert button (#598)
    This adds a revert button to HUD so that people can revert and give some
    meangingful comments more easily. Basically the user just has to type in
    a reason, select a classification and press revert and it should leave a
    comment on the PR to revert. There's a message preview on the right for
    people to get a preview of the message they're leaving.
    
    
    
    https://user-images.githubusercontent.com/34172846/187551431-39ed54dc-4f10-4b2e-8325-f09714513a3c.mov
    
    
    
    
    This also introduces bootstrap into the project. If we don't like the
    CSS (modifies other parts of HUD a little), we can just move the css
    into just the revert page.
    
    Test Plan:
    Tested it by testing the post here:
    https://github.com/pytorch/test-infra/pull/582
    Also added a test case for this in the mergebot commands to make sure
    that the command is valid.

---
+++ torchci/lib/githubFunctions.ts
@@ -0,0 +1,25 @@
+export async function commentOnPR(
+  owner: string,
+  repo: string,
+  issue_number: string,
+  message: string,
+  accessToken: string,
+  onComplete: Function
+) {
+  // Prevent devs from accidentally reverting something in DEV
+  if (process.env.NODE_ENV !== "production") {
+    repo = "test-infra";
+    issue_number = "582";
+  }
+  const commentUrl = `https://api.github.com/repos/${owner}/${repo}/issues/${issue_number}/comments`;
+
+  const response = await fetch(commentUrl, {
+    method: "POST",
+    body: JSON.stringify({ body: message }),
+    headers: {
+      Accept: "application/vnd.github+json",
+      Authorization: `token ${accessToken}`,
+    },
+  });
+  onComplete(JSON.stringify(await response.json(), undefined, 2));
+}
