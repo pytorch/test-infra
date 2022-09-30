785db4c7b975f86aef73a98b0a71df09d1e57faf
Zain Rizvi <zainr@fb.com>
2022-09-30 14:13:46 -0500

    Add duration

--- torchci/components/JobSummary.tsx
+++ torchci/components/JobSummary.tsx
@@ -2,10 +2,22 @@ import { JobData } from "lib/types";
 import JobConclusion from "./JobConclusion";
 
 export default function JobSummary({ job }: { job: JobData }) {
+  var queueTimeInfo = null
+  if (job.queueTimeS != null) {
+    queueTimeInfo = <><i>Queued:</i> {Math.max(Math.round(job.queueTimeS / 60), 0)} mins</>
+  }
+
+  var durationInfo = null
+  if (job.durationS != null) {
+    durationInfo = <><i>Duration:</i> {Math.round((job.durationS / 60))} mins</>
+  }
+
+  var separator = (queueTimeInfo && durationInfo) ? ", ": ""
+
   return (
     <>
       <JobConclusion conclusion={job.conclusion} />
-      <a href={job.htmlUrl}> {job.jobName} </a>
+      <a href={job.htmlUrl}> {job.jobName}</a> {queueTimeInfo}{separator} {durationInfo}
     </>
   );
 }
