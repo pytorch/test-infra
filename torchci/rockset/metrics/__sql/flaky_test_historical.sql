d391ef3db22bb78529ee4c8279df536e122d7270
Huy Do <huydhn@gmail.com>
2022-10-17 12:42:17 -0700

    Add flaky tests chart and count

---
+++ torchci/rockset/metrics/__sql/flaky_test_historical.sql
@@ -0,0 +1,35 @@
+WITH
+flaky_tests AS (
+    SELECT
+        FORMAT_ISO8601(
+            DATE_TRUNC(
+                :granularity,
+                issues._event_time AT TIME ZONE :timezone
+            )
+        ) AS granularity_bucket,
+        COUNT(issues.title) as number_of_new_flaky_tests,
+    FROM
+        commons.issues
+    WHERE
+        issues.title LIKE '%DISABLED%'
+        AND issues.state = :state
+    GROUP BY
+        granularity_bucket
+),
+total_flaky_tests AS (
+    SELECT
+        granularity_bucket,
+        number_of_new_flaky_tests,
+        SUM(number_of_new_flaky_tests) OVER (ORDER BY granularity_bucket) AS number_of_open_flaky_tests
+    FROM
+        flaky_tests
+)
+SELECT
+    *
+FROM
+    total_flaky_tests
+WHERE
+    PARSE_DATETIME_ISO8601(granularity_bucket) >= PARSE_DATETIME_ISO8601(:startTime)
+    AND PARSE_DATETIME_ISO8601(granularity_bucket) < PARSE_DATETIME_ISO8601(:stopTime)
+ORDER BY
+    granularity_bucket DESC
