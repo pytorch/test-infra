4fa58831f1fb91b32c74bd5d3253765e9e105e35
David Berard <dberard@fb.com>
2022-10-20 21:38:03 -0700

    lint

--- torchci/pages/userbenchmark_view.tsx
+++ torchci/pages/userbenchmark_view.tsx
@@ -6,9 +6,10 @@ import React, { useState } from "react";
 import styles from "components/hud.module.css";
 
 const ROW_HEIGHT = 240;
-
-class UserbenchmarkResults extends React.Component {
-    constructor(props) {
+type UserbenchmarkProps = {};
+type UserbenchmarkState = {url: string | null, content: any};
+class UserbenchmarkResults extends React.Component<UserbenchmarkProps, UserbenchmarkState> {
+    constructor(props: UserbenchmarkProps) {
         super(props);
 
         this.state = {
@@ -24,29 +25,31 @@ class UserbenchmarkResults extends React.Component {
     //   1. Base revision latency
     //   2. PR revision latency
     //   3. Speedup from the PR revision
-    parseCsv(msg) {
-        let as_array = msg.split("\n").map(x => x.split(";"));
-        as_array = as_array.filter(subarray => subarray.length == 3);
+    parseCsv(msg: string) {
+        let as_array = msg.split("\n").map((x: any) => x.split(";"));
+        as_array = as_array.filter((subarray: any[]) => subarray.length == 3);
         as_array[0].push("Speedup");
         for (let i=1; i<as_array.length; ++i) {
             for (let j=1; j<=2; ++j) {
                 as_array[i][j] = parseFloat(as_array[i][j]);
             }
-            as_array[i].push(as_array[i][1] / as_array[i][2] - 1);
+            const base_value = as_array[i][1] as number;
+            const pr_value = as_array[i][2] as number;
+            as_array[i].push(base_value / pr_value - 1);
         }
         return as_array;
     }
 
-    getHeader(row) {
-        const metric_header = row[0];
-        const base_revision = row[1] + " (base)";
-        const change_revision = row[2] + " (change)";
-        const speedup_header = row[3];
+    getHeader(row: any[]) {
+        const metric_header = row[0] as string;
+        const base_revision = row[1] + " (base)" as string;
+        const change_revision = row[2] + " (change)" as string;
+        const speedup_header = row[3] as string;
         const headers = [metric_header, base_revision, change_revision, speedup_header]
         return (
             <thead>
                 <tr className={styles.userbenchmarkTable}>
-                    {headers.map((val, idx) => (
+                    {headers.map((val: string, idx: number) => (
                         <th className={styles.userbenchmarkTable} key={idx}>{val}</th>
                     ))}
                 </tr>
@@ -54,19 +57,19 @@ class UserbenchmarkResults extends React.Component {
         );
     }
 
-    getRow(row, minSpeedup, maxSpeedup) {
-        const convertSpeedup = (speedupRaw) => {
+    getRow(row: any[], minSpeedup: number, maxSpeedup: number) {
+        const convertSpeedup = (speedupRaw: number) => {
             if (typeof speedupRaw === 'number') {
                 const text = (speedupRaw * 100).toFixed(2) + '%';
                 // assign a color
                 let color = "rgb(255, 255, 255)";
                 if (speedupRaw >= 0) {
                     const ratio = speedupRaw / maxSpeedup;
-                    const otherColor = parseInt(255 * (1 - ratio)).toString();
+                    const otherColor = Math.floor(255 * (1 - ratio)).toString();
                     color = "rgb(" + otherColor + ", 255, " + otherColor + ")";
                 } else {
                     const ratio = speedupRaw / minSpeedup;
-                    const otherColor = parseInt(255 * (1 - ratio)).toString();
+                    const otherColor = Math.floor(255 * (1 - ratio)).toString();
                     color = "rgb(255, " + otherColor + ", " + otherColor + ")";
                 }
                 return <td style={{backgroundColor: color}} className={styles.userbenchmarkTable}>{text}</td>;
@@ -78,20 +81,20 @@ class UserbenchmarkResults extends React.Component {
         return (
             <tbody>
                 <tr className={styles.userbenchmarkTable}>
-                    {row.slice(0, 3).map((val, idx) => (
+                    {row.slice(0, 3).map((val: any, idx: number) => (
                         <td className={styles.userbenchmarkTable} key={idx}>{val}</td>
                     ))}
-                    {convertSpeedup(row[3])}
+                    {convertSpeedup(parseFloat(row[3]))}
                 </tr>
             </tbody>
         );
     }
 
-    csvToTable(csvString) {
+    csvToTable(csvString: string) {
         const data = this.parseCsv(csvString);
 
-        const minSpeedup = Math.min(...data.slice(1).map(subarr => subarr[3]), 0.0)
-        const maxSpeedup = Math.max(...data.slice(1).map(subarr => subarr[3]), 0.0)
+        const minSpeedup = Math.min(...data.slice(1).map((subarr: any[]) => subarr[3]), 0.0)
+        const maxSpeedup = Math.max(...data.slice(1).map((subarr: any[]) => subarr[3]), 0.0)
         return (
             <table className={styles.userbenchmarkTable}>
                 {this.getHeader(data[0])}
@@ -101,9 +104,9 @@ class UserbenchmarkResults extends React.Component {
     }
 
     componentDidMount() {
-        let url = null;
+        let url: string | null = null;
         if (typeof document !== 'undefined') {
-            const searchParams = (new URL(document.location)).searchParams;
+            const searchParams = (new URL(document.location.toString())).searchParams;
             if (!searchParams.has("url")) {
                 console.log("!has_url");
                 // setDataMessage("No URL provided (e.g. ?url=[...])")
