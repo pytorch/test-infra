6e99f8546c59a521eb994a8d2b5b4524ad4e9430
Eli Uriegas <eliuriegas@meta.com>
2022-11-01 12:09:56 -0700

    torchci: Add ciflow/trunk when attempting to merge
    Adds the ciflow/trunk label when the `@pytorchbot merge` command is run.
    
    A follow up to ensure that the land validations only run when people are
    ready to merge and not just when someone else approves their PR
    
    Signed-off-by: Eli Uriegas <eliuriegas@meta.com>

--- torchci/lib/bot/autoLabelBot.ts
+++ torchci/lib/bot/autoLabelBot.ts
@@ -2,7 +2,6 @@ import { Context, Probot } from "probot";
 import { addLabels, hasWritePermissions } from "./botUtils";
 import { isPyTorchPyTorch } from "./utils";
 
-export const CIFLOW_TRUNK_LABEL = "ciflow/trunk";
 
 const titleRegexToLabel: [RegExp, string][] = [
   [/rocm/gi, "module: rocm"],
@@ -345,17 +344,6 @@ function myBot(app: Probot): void {
     const owner = context.payload.repository.owner.login;
     const repo = context.payload.repository.name;
     if (!isPyTorchPyTorch(owner, repo)) {
-      return;
-    }
-    const labels: string[] = context.payload.pull_request.labels.map(
-      (e) => e["name"]
-    );
-    if (labels.find( x => x === CIFLOW_TRUNK_LABEL)) {
-      return;
-    }
-    const reviewer = context.payload.review.user.login;
-    // Ignore reviews from users without write permissions
-    if (!await hasWritePermissions(context, reviewer)) {
       return;
     }
     // TEMP disable for 24 hours to see if it affects number of reverts / queueing
