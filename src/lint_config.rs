fa8d7b32641e58c041e9f3bf15a4b26e1afff915
Justin Chu <justinchuby@users.noreply.github.com>
2022-11-15 09:51:35 -0800

    fix: typo in init_command doc (#17)
    * Fix typo in init_command doc
    
    * Update lint_config.rs

--- src/lint_config.rs
+++ src/lint_config.rs
@@ -86,7 +86,7 @@ pub struct LintConfig {
     /// # Examples
     /// - Calling a Python script:
     /// ```toml
-    /// command = ['python3', 'my_linter.py', -- '@{{PATHSFILE}}']
+    /// command = ['python3', 'my_linter.py', '--', '@{{PATHSFILE}}']
     /// ```
     pub command: Vec<String>,
 
@@ -105,7 +105,8 @@ pub struct LintConfig {
     /// # Examples
     /// - Calling a Python script:
     /// ```toml
-    /// command = ['python3', 'my_linter_init.py', '--dry-run={{DRYRUN}}']
+    /// init_command = ['python3', 'my_linter_init.py', '--dry-run={{DRYRUN}}']
+    /// ```
     pub init_command: Option<Vec<String>>,
 
     /// If true, this linter will be considered a formatter, and will invoked by
