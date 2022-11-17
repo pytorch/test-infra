4f10858255d320a678179d95adb8ecf0ded4ae8e
dagitses <dagitses@users.noreply.github.com>
2022-11-17 16:45:01 -0500

    extract the git_repo pytest fixture to the shared conftest (#1114)

---
+++ tools/stronghold/tests/api/conftest.py
@@ -0,0 +1,16 @@
+import pathlib
+
+import pytest
+
+import api.git
+
+
+@pytest.fixture
+def git_repo(tmp_path: pathlib.Path) -> api.git.Repository:
+    """pytest fixture providing an empty initialized git repository."""
+    repo = api.git.Repository(tmp_path)
+    repo.run(['init'], check=True)
+    # Set the user for this repository only.
+    repo.run(['config', 'user.email', 'user@mcuserface.test'], check=True)
+    repo.run(['config', 'user.name', 'User McUserface'], check=True)
+    return repo
