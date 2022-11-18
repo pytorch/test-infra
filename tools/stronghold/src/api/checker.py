22755f14c876584c791dcb1333fcca75bba034fb
mikey dagitses <mikeyd@meta.com>
2022-11-18 15:53:56 -0500

    test stronghold workflow
    Summary:
    
    Test Plan:

--- tools/stronghold/src/api/checker.py
+++ tools/stronghold/src/api/checker.py
@@ -1,6 +1,7 @@
 """Runs the API backward compatibility on the head commit."""
 
 import argparse
+import os
 import pathlib
 import sys
 
@@ -13,4 +14,9 @@ def run() -> None:
     parser.parse_args(sys.argv[1:])
 
     repo = api.git.Repository(pathlib.Path('.'))
-    api.compatibility.check_commit(repo, commit_id='HEAD')
+    import pprint
+
+    pprint.pp(os.environ)
+    api.compatibility.check_range(
+        repo, head=os.environ['GITHUB_HEAD_REF'], base=os.environ['GITHUB_BASE_REF']
+    )
