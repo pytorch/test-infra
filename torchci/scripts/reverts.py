9a5b65f2bd7011df5cd69ef75318929344294aaf
Catherine Lee <csl@fb.com>
2022-11-28 13:09:14 -0800

    get rid of short printer

--- torchci/scripts/reverts.py
+++ torchci/scripts/reverts.py
@@ -29,20 +29,6 @@ def find_corresponding_gitlog_commit(
     return None
 
 
-def format_string_for_markdown_short(
-    commit: Optional[GitCommit], rockset_result: Optional[Dict[str, str]] = None
-) -> str:
-    s = ""
-    if commit is None:
-        s += "- cannot find commit corresponding to @pytorchbot revert comment"
-    else:
-        s += f"- [{commit.title}](https://github.com/pytorch/pytorch/commit/{commit.commit_hash})"
-    if rockset_result is not None:
-        s += f' by [comment]({rockset_result["comment_url"]})'
-    s += "\n"
-    return s
-
-
 def format_string_for_markdown_long(
     commit: GitCommit, rockset_result: Optional[Dict[str, str]] = None
 ) -> str:
@@ -127,11 +113,6 @@ def main():
 
     filename = f"{start_time.split('T')[0]}.md"
     with open(filename, "w") as f:
-        f.write(f"# Week of {start_time.split('T')[0]} to {end_time.split('T')[0]}\n")
-        for classification, reverts in classification_dict.items():
-            f.write(f"\n### {CLASSIFICATIONS[classification]}\n\n")
-            for commit, rockset_result in reverts:
-                f.write(format_string_for_markdown_short(commit, rockset_result))
         f.write(f"# Week of {start_time.split('T')[0]} to {end_time.split('T')[0]}\n")
         for classification, reverts in classification_dict.items():
             f.write(f"\n### {CLASSIFICATIONS[classification]}\n\n")
