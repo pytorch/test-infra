76627cf7a245172eb2707b8441e14c8235a45ebe
mikey dagitses <mikeyd@meta.com>
2022-12-01 09:28:37 -0500

    add a function to render a violation as a github warning
    Summary:
    When printed to the log, this will render a warning in the GitHub UI.
    
    Test Plan: Visually inspect with downstream test PRs.

---
+++ tools/stronghold/src/api/github.py
@@ -0,0 +1,9 @@
+"""GitHub integration for the API compatibility checker."""
+
+import pathlib
+
+import api.compatibility
+
+
+def render_violation(file: pathlib.Path, violation: api.compatibility.Violation) -> str:
+    return f'::warning file={file},line={violation.line}::Function {violation.func}: {violation.message}'
