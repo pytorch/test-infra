777bdc026ef2ff6ab048446a4a8392e5a80fd4e1
mikey dagitses <mikeyd@meta.com>
2022-12-05 06:05:09 -0500

    add functions to test API compatibility changes
    Summary: DO NOT SUBMIT

---
+++ tools/stronghold/api_compatibility_test.py
@@ -0,0 +1,27 @@
+"""Contains functions to test API compatibility changes."""
+
+
+def removed_function() -> None:
+    pass
+
+
+def remove_positional_parameter(
+    a: int,
+    /,
+    b: int,
+    *args: int,
+    c: int,
+    **kwds: int,
+) -> None:
+    pass
+
+
+def complex(
+    a: int,
+    /,
+    b: int,
+    *args: int,
+    c: int,
+    **kwds: int,
+) -> None:
+    pass
