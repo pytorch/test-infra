d74387f273b600c787f78206a146a47374eea3b7
mikey dagitses <mikeyd@meta.com>
2022-12-05 07:43:40 -0500

    add functions to test API compatibility changes
    Summary: DO NOT MERGE

---
+++ tools/stronghold/example.py
@@ -0,0 +1,73 @@
+"""Contains functions to test API compatibility changes."""
+
+
+def removed_function() -> None:
+    pass
+
+
+def remove_positional_parameter(
+    a: int,
+    /,
+    b: int,
+    c = 0: int
+    *args: int,
+    d: int,
+    e: int = 0,
+    **kwds: int,
+) -> None:
+    pass
+
+
+def remove_flexible_parameter(
+    a: int,
+    /,
+    b: int,
+    *args: int,
+    c: int,
+    **kwds: int,
+) -> None:
+    pass
+
+
+def remove_keyword_parameter(
+    a: int,
+    /,
+    b: int,
+    *args: int,
+    c: int,
+    **kwds: int,
+) -> None:
+    pass
+
+
+def remove_varargs(
+    a: int,
+    /,
+    b: int,
+    *args: int,
+    c: int,
+    **kwds: int,
+) -> None:
+    pass
+
+
+def remove_kwds(
+    a: int,
+    /,
+    b: int,
+    *args: int,
+    c: int,
+    **kwds: int,
+) -> None:
+    pass
+
+
+def complex(
+    a: int,
+    /,
+    b: int,
+    *args: int,
+    c: int,
+    **kwds: int,
+) -> None:
+    pass
