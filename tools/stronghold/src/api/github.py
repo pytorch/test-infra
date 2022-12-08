3e954eb02b965432ff50235e8f4c303cb2cfa33e
mikey dagitses <mikeyd@meta.com>
2022-12-08 10:36:28 -0500

    downgrade severity from warning to info if the compat check is supressed
    Summary:
    We wish to still see violations when suppressing, but they should be
    at info level, rather than warning.
    
    Test Plan: Verify with a sample PR.

--- tools/stronghold/src/api/github.py
+++ tools/stronghold/src/api/github.py
@@ -5,8 +5,8 @@ import pathlib
 import api.compatibility
 
 
-def render_violation(file: pathlib.Path, violation: api.compatibility.Violation) -> str:
+def render_violation(level: str, file: pathlib.Path, violation: api.compatibility.Violation) -> str:
     return (
-        f'::warning file={file},line={violation.line}::'
+        f'::{level} file={file},line={violation.line}::'
         f'Function {violation.func}: {violation.message}'
     )
