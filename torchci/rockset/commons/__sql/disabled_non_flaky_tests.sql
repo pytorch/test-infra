26f31dc1c5728e8fb2895f904e3f4f63c70942ba
Huy Do <huydhn@gmail.com>
2022-12-12 18:32:21 -0800

    Update rockset query

--- torchci/rockset/commons/__sql/disabled_non_flaky_tests.sql
+++ torchci/rockset/commons/__sql/disabled_non_flaky_tests.sql
@@ -1,18 +1,25 @@
+WITH aggregated_weekly_data AS (
+  SELECT
+    name,
+    classname,
+    filename,
+    SUM(CASE WHEN flaky THEN 1 ELSE 0 END) > 0 AS flaky,
+    SUM(num_green) AS num_green,
+    SUM(num_red) as num_red
+  FROM
+    commons.rerun_disabled_tests
+  WHERE
+    _event_time > CURRENT_TIMESTAMP() - INTERVAL 7 DAY
+  GROUP BY
+    name,
+    classname,
+    filename
+)
 SELECT
-  name,
-  classname,
-  filename,
-  SUM(CASE WHEN NOT flaky THEN 1 ELSE 0 END) = 0 AS flaky,
-  SUM(num_green) AS num_green,
-  SUM(num_red) as num_red
+  *
 FROM
-  commons.rerun_disabled_tests
+  aggregated_weekly_data
 WHERE
-  _event_time > CURRENT_TIMESTAMP() - INTERVAL 7 DAY
-  AND flaky = false
+  flaky = false
   AND num_green >= :min_num_green
   AND num_red <= :max_num_red
-GROUP BY
-  name,
-  classname,
-  filename
