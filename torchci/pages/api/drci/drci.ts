461fdc2c102832d49f29163359b7ff7e5aabf25c
Catherine Lee <csl@fb.com>
2022-12-13 16:19:55 -0800

    update

--- torchci/pages/api/drci/drci.ts
+++ torchci/pages/api/drci/drci.ts
@@ -83,7 +83,6 @@ export async function updateDrciComments(octokit: Octokit, prNumber?: string) {
         await updateCommentWithWorkflow(octokit, pr_info, comment);
       })
     );
-    console.log("done")
 }
 
 async function addMergeBaseCommits(
