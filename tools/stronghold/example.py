6adf39b9f6c5d6a4deab8d05d935fb761b310b99
mikey dagitses <mikeyd@meta.com>
2022-12-21 11:57:18 -0500

    violate API compatibility checks

--- tools/stronghold/example.py
+++ tools/stronghold/example.py
@@ -2,6 +2,7 @@
 
 
 def parameter_added_with_lots_of_parameters(
+    a: str,
     b: int,
     c: int,
     d: int,
@@ -20,13 +21,7 @@ def parameter_added_with_lots_of_parameters(
     pass
 
 
-def removed_function() -> None:
-    pass
-
-
 def remove_positional_parameter(
-    a: int,
-    /,
     b: int,
     c: int = 0,
     *args: int,
@@ -83,6 +78,7 @@ def remove_kwds(
 
 def add_positional_parameter(
     a: int,
+    new_param: int,
     /,
     b: int,
     c: int = 0,
@@ -96,6 +92,7 @@ def add_positional_parameter(
 
 def add_positional_parameter_with_default(
     a: int,
+    new_param: int = 0,
     /,
     b: int = 0,
     c: int = 0,
@@ -111,6 +108,7 @@ def add_flexible_parameter(
     a: int,
     /,
     b: int,
+    new_param: int,
     c: int = 0,
     *args: int,
     d: int,
@@ -196,7 +194,6 @@ def complex(
 
 
 def remove_positional_parameter_with_varargs(
-    a: int,
     /,
     *args: int,
 ) -> None:
@@ -205,14 +202,12 @@ def remove_positional_parameter_with_varargs(
 
 def remove_keyword_parameter_with_kwargs(
     *,
-    a: int,
     **kwds: int,
 ) -> None:
     pass
 
 
 def remove_flexible_parameter_with_varargs_and_kwargs(
-    a: int,
     *args: int,
     **kwds: int,
 ) -> None:
