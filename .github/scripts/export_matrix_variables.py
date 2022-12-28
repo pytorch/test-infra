f3ce6d0537c40763f20b75f0c5732cf71e266bad
atalman <atalman@fb.com>
2022-12-28 07:15:00 -0800

    Cleanup

--- .github/scripts/export_matrix_variables.py
+++ .github/scripts/export_matrix_variables.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import argparse
 import json
 import sys
@@ -12,27 +11,15 @@ def main(args) -> None:
         help="Input file to use, must be JSON",
         type=str,
     )
-    parser.add_argument(
-        "--operating-system",
-        help="Operating system to generate for",
-        type=str,
-        default="linux",
-    )
     options = parser.parse_args(args)
 
     with open(options.input_file) as fp:
         variables_to_export: Dict[str, str] = json.loads(fp.read())
 
-    if options.operating_system == "linux":
-        for key, value in variables_to_export.items():
-            print(
-                f"MATRIX_{key.upper().replace('-', '_')}={value.replace('-', '_')}"
-            )
-    elif options.operating_system == "windows":
-        import subprocess
-        for key, value in variables_to_export.items():
-            subprocess.call(['setx', f"MATRIX_{key.upper().replace('-', '_')}", f"{value.replace('-', '_')}"], shell=True)
-
+    for key, value in variables_to_export.items():
+        print(
+            f"MATRIX_{key.upper().replace('-', '_')}={value.replace('-', '_')}"
+        )
 
 if __name__ == "__main__":
     main(sys.argv[1:])
