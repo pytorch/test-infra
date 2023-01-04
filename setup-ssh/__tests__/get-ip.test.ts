beebcbcdbe0cf68d11c6d744c2fb0296cfad0515
Nikita Shulga <nshulga@meta.com>
2023-01-04 15:19:02 -0800

    Run yarn-all
    After all the dependabot updates

--- setup-ssh/__tests__/get-ip.test.ts
+++ setup-ssh/__tests__/get-ip.test.ts
@@ -1,10 +1,10 @@
-import { getIPs } from "../src/get-ip";
-import { expect, test } from "@jest/globals";
+import {getIPs} from '../src/get-ip'
+import {expect, test} from '@jest/globals'
 
-test("ip found", async () => {
-  const ips = await getIPs();
-  console.debug(`IPv4 is: ${ips.ipv4}`);
-  expect(ips.ipv4).not.toBe("");
-  console.debug(`IPv6 is: ${ips.ipv6}`);
-  expect(ips.ipv6).not.toBe("");
-});
+test('ip found', async () => {
+  const ips = await getIPs()
+  console.debug(`IPv4 is: ${ips.ipv4}`)
+  expect(ips.ipv4).not.toBe('')
+  console.debug(`IPv6 is: ${ips.ipv6}`)
+  expect(ips.ipv6).not.toBe('')
+})
