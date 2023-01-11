8b380f59ae68a907bdf29f9995f6bf80e2729edc
Andrey Talman <atalman@fb.com>
2023-01-11 08:10:55 -0500

    Fixes setting INSTALL command for env vars (#1378)
    Fixes an issue when using install command with MATRIX env var. Without
    this change only first word of install command is assigned to env var.
    For example if install is:
    ```
    conda install pytorch torchvision torchaudio cpuonly -c pytorch
    ```
    the MATRIX_INSTALL would be:
    ```
    conda
    ```
    With this fix, it should be resolved.

--- .github/scripts/export_matrix_variables.py
+++ .github/scripts/export_matrix_variables.py
@@ -11,15 +11,18 @@ def main(args) -> None:
         help="Input file to use, must be JSON",
         type=str,
     )
+    parser.add_argument('--add-quotes-around-value', dest='add_quotes_around_value', action='store_true')
+    parser.set_defaults(add_quotes_around_value=False)
     options = parser.parse_args(args)
 
     with open(options.input_file) as fp:
         variables_to_export: Dict[str, str] = json.loads(fp.read())
 
     for key, value in variables_to_export.items():
-        print(
-            f"MATRIX_{key.upper().replace('-', '_')}={value}"
-        )
+        if options.add_quotes_around_value:
+            value = f"\"{value}\""
+
+        print(f"MATRIX_{key.upper().replace('-', '_')}={value}")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
