944182fa76ba1c4c43217224f24ab699762d8a32
Michael Suo <suo@fb.com>
2023-01-12 17:53:12 -0800

    chore: add cargo update to release script

--- do_release.sh
+++ do_release.sh
@@ -1,6 +1,7 @@
 set -eux
 git cliff --tag "$1"
+sed -i 's/^version.*/version = "$1"/' Cargo.toml
 git commit -am "chore(release): prep for $1"
-git tag "$1"
+git tag "v$1"
 git push
-git push origin "$1"
+git push origin "v$1"
