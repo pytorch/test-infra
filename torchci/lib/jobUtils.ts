160be9590820bd7d90306db79750291024cbc9ec
Huy Do <huydhn@gmail.com>
2023-01-23 10:57:05 -0800

    Typo

--- torchci/lib/jobUtils.ts
+++ torchci/lib/jobUtils.ts
@@ -13,9 +13,3 @@ export function isRerunDisabledTestsJob(job: JobData) {
   // on the nature of the disabled tests, so we don't want to count them sometimes
   return job.name !== undefined && job.name.includes("rerun_disabled_tests");
 }
-
-export function isUnstableJobs(job: JobData) {
-  // Rerunning disabled tests are expected to fail from time to time depending
-  // on the nature of the disabled tests, so we don't want to count them sometimes
-  return job.name !== undefined && job.name.includes("unstable");
-}
