pytorch/test-infra

commit 66bd3b462db85febc603529f5043cda42080aba9

Support an optional job name filter (regex) in the OSS alerting (#2109)
As a prerequisite for pytorch/pytorch#93991 adding the possibility to
specify a job name filter for the OSS alert generation mechanism.

The idea is to be able to specify `JOB_NAME_REGEX` in
https://github.com/pytorch/test-infra/blob/e194edeb74080faf454ad302139a85ef91de2752/.github/workflows/check-alerts.yml#L28
to restrict alerting only to the desired set of jobs.

---------

Co-authored-by: Nikita Shulga <nshulga@fb.com>

torchci/scripts/check_alerts.py
@@ -444,8 +444,16 @@ def handle_flaky_tests_alert(existing_alerts: List[Dict]) -> Dict:
     return None
 
 
-def check_for_recurrently_failing_jobs_alert(repo: str, branch: str, dry_run: bool):
+# filter job names that don't match the regex
+def filter_job_names(job_names: List[str], job_name_regex: str) -> List[str]:
+    if job_name_regex:
+        return [job_name for job_name in job_names if re.match(job_name_regex, job_name)]
+    return job_names
+
+
+def check_for_recurrently_failing_jobs_alert(repo: str, branch: str, job_name_regex: str, dry_run: bool):
     job_names, sha_grid = fetch_hud_data(repo=repo, branch=branch)
+    job_names = filter_job_names(job_names, job_name_regex)
     (jobs_to_alert_on, flaky_jobs) = classify_jobs(job_names, sha_grid)
 
     # Fetch alerts
@@ -498,6 +506,12 @@ def parse_args() -> argparse.Namespace:
         default=os.getenv("BRANCH_TO_CHECK", "master")
     )
     parser.add_argument(
+        "--job-name-regex",
+        help="Consider only job names matching given regex (if omitted, all jobs are matched)",
+        type=str,
+        default=os.getenv("JOB_NAME_REGEX", "")
+    )
+    parser.add_argument(
         "--with-flaky-test-alert",
         help="Run this script with the flaky test alerting",
         type=distutils.util.strtobool,
@@ -514,7 +528,7 @@ def parse_args() -> argparse.Namespace:
 
 def main():
     args = parse_args()
-    check_for_recurrently_failing_jobs_alert(args.repo, args.branch, args.dry_run)
+    check_for_recurrently_failing_jobs_alert(args.repo, args.branch, args.job_name_regex, args.dry_run)
     # TODO: Fill out dry run for flaky test alerting, not going to do in one PR
     if args.with_flaky_test_alert:
         check_for_no_flaky_tests_alert()

torchci/scripts/test_check_alerts.py
@@ -3,6 +3,7 @@ from unittest import main, TestCase
 from unittest.mock import patch
 
 from check_alerts import (
+    filter_job_names,
     gen_update_comment,
     generate_no_flaky_tests_issue,
     handle_flaky_tests_alert,
@@ -125,6 +126,36 @@ class TestGitHubPR(TestCase):
         mock_get_num_issues_with_label.return_value = 0
         res = handle_flaky_tests_alert(existing_alerts)
         self.assertDictEqual(res, mock_issue)
+
+    # test filter job names
+    def test_job_filter(self):
+        job_names = ["pytorch_linux_xenial_py3_6_gcc5_4_test", "pytorch_linux_xenial_py3_6_gcc5_4_test2"]
+        self.assertListEqual(
+            filter_job_names(job_names, ""),
+            job_names,
+            "empty regex should match all jobs"
+        )
+        self.assertListEqual(
+            filter_job_names(job_names, ".*"),
+            job_names
+        )
+        self.assertListEqual(
+            filter_job_names(job_names, ".*xenial.*"),
+            job_names
+        )
+        self.assertListEqual(
+            filter_job_names(job_names, ".*xenial.*test2"),
+            ["pytorch_linux_xenial_py3_6_gcc5_4_test2"]
+        )
+        self.assertListEqual(
+            filter_job_names(job_names, ".*xenial.*test3"),
+            []
+        )
+        self.assertRaises(
+            Exception,
+            lambda: filter_job_names(job_names, "["),
+            msg="malformed regex should throw exception"
+        )
 
 
 if __name__ == "__main__":

commit 174ce433600777aa5600c399912ec10e3b58f253

Wheel size validation using https://download.pytorch.org/whl/ index (#2681)
Brunt of the work for pytorch/pytorch#93991

Changes:
1. Adds a script (`binary_size_validation.py `) to perform wheel size
fetching and validation using given rules.
2. Includes the specific job pattern from the builder repo to create
alerts.
3. Fixes a minor bug in `check_alerts.py` (regression from #2109)

A separate PR will schedule the script periodic execution in the
pytorch/builder repo (TODO). The builder repo is chosen as compromise,
as `test-infra` doesn't have HUD enabled as of now (alerts do not work).


----

### Brief docs for `binary_size_validation.py ` (separate readme is
included):

#### Installation

```bash
pip install -r requirements.txt
```

#### Usage
    
```bash
# print help
python binary_size_validation.py --help

# print sizes of the all items in the index
python binary_size_validation.py --url https://download.pytorch.org/whl/nightly/torch/

# fail if any of the torch2.0 wheels are larger than 900MB
python binary_size_validation.py --url https://download.pytorch.org/whl/nightly/torch/ --include "torch-2\.0"  --threshold 900

# fail if any of the latest nightly pypi wheels are larger than 750MB
python binary_size_validation.py --include "pypi" --only-latest-version --threshold 750
```

.github/workflows/check-alerts.yml
@@ -19,13 +19,20 @@ jobs:
           - repo: pytorch/pytorch
             branch: master
             with_flaky_test_alerting: YES
+            job_filter_regex: ""
           - repo: pytorch/pytorch
             branch: nightly
             with_flaky_test_alerting: NO
+            job_filter_regex: ""
+          - repo: pytorch/builder
+            branch: main
+            with_flaky_test_alerting: NO
+            job_filter_regex: "nightly.pypi.binary.size.validation"
     env:
       REPO_TO_CHECK: ${{ matrix.repo }}
       BRANCH_TO_CHECK: ${{ matrix.branch }}
       WITH_FLAKY_TEST_ALERT: ${{ matrix.with_flaky_test_alerting }}
+      JOB_NAME_REGEX: ${{ matrix.job_filter_regex }}
       # Don't do actual work on pull request
       DRY_RUN: ${{ github.event_name == 'pull_request'}}
     runs-on: ubuntu-18.04

tools/binary_size_validation/binary_size_validation.py
@@ -1,0 +1,91 @@
+# Script that parses wheel index (e.g. https://download.pytorch.org/whl/test/torch/),
+# fetches and validates binary size for the files that match the given regex.
+
+import requests
+import re
+from collections import namedtuple
+import click
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+Wheel = namedtuple("Wheel", ["name", "url"])
+
+
+def parse_index(html: str,
+                base_url: str,
+                include_regex: str = "",
+                exclude_regex: str = "",
+                latest_version_only=False) -> list[Wheel]:
+    """
+    parse the html page and return a list of wheels
+    :param html: html page
+    :param base_url: base url of the page
+    :param include_regex: regex to filter the wheel names. If empty, all wheels are included
+    :param exclude_regex: regex to exclude the matching wheel names. If empty, no wheels are excluded
+    :param latest_version_only: if True, return the wheels of the latest version only
+    :return: list of wheels
+    """
+    soup = BeautifulSoup(html, "html.parser")
+
+    wheels = []
+    for a in soup.find_all("a"):
+        wheel_name = a.text
+        wheel_url = urljoin(base_url, a.get("href"))
+        if (not include_regex or re.search(include_regex, wheel_name)) \
+                and (not exclude_regex or not re.search(exclude_regex, wheel_name)):
+            wheels.append(Wheel(name=wheel_name, url=wheel_url))
+
+    # filter out the wheels that are not the latest version
+    if len(wheels) > 0 and latest_version_only:
+        # get the prefixes (up to the second '+'/'-' sign) of the wheels
+        prefixes = set()
+        for wheel in wheels:
+            prefix = re.search(r"^([^-+]+[-+][^-+]+)[-+]", wheel.name).group(1)
+            if not prefix:
+                raise RuntimeError(f"Failed to get version prefix of {wheel.name}"
+                                   "Please check the regex_filter or don't use --latest-version-only")
+            prefixes.add(prefix)
+        latest_version = max(prefixes)
+        print(f"Latest version prefix: {latest_version}")
+
+        # filter out the wheels that are not the latest version
+        wheels = [wheel for wheel in wheels if wheel.name.startswith(latest_version)]
+
+    return wheels
+
+
+def get_binary_size(file_url: str) -> int:
+    """
+    get the binary size of the given file
+    :param file_url: url of the file
+    :return: binary size in bytes
+    """
+    return int(requests.head(file_url).headers['Content-Length'])
+
+
+@click.command(
+    help="Validate the binary sizes of the given wheel index."
+)
+@click.option("--url", help="url of the wheel index",
+              default="https://download.pytorch.org/whl/nightly/torch/")
+@click.option("--include", help="regex to filter the wheel names. Only the matching wheel names will be checked.",
+              default="")
+@click.option("--exclude", help="regex to exclude wheel names. Matching wheel names will NOT be checked.",
+              default="")
+@click.option("--threshold", help="threshold in MB, optional", default=0)
+@click.option("--only-latest-version", help="only validate the latest version",
+              is_flag=True, show_default=True, default=False)
+def main(url, include, exclude, threshold, only_latest_version):
+    page = requests.get(url)
+    wheels = parse_index(page.text, url, include, exclude, only_latest_version)
+    for wheel in wheels:
+        print(f"Validating {wheel.url}...")
+        size = get_binary_size(wheel.url)
+        print(f"{wheel.name}: {int(size) / 1024 / 1024:.2f} MB")
+        if threshold and int(size) > threshold * 1024 * 1024:
+            raise RuntimeError(
+                f"Binary size of {wheel.name} {int(size) / 1024 / 1024:.2f} MB exceeds the threshold {threshold} MB")
+
+
+if __name__ == "__main__":
+    main()

torchci/scripts/check_alerts.py
@@ -4,11 +4,9 @@ import os
 import re
 import urllib.parse
 from collections import defaultdict
-from curses.ascii import CAN
 from datetime import datetime, timedelta
 from difflib import SequenceMatcher
-from email.policy import default
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict, List, Set, Tuple
 from setuptools import distutils  # type: ignore[import]
 
 import requests
@@ -400,11 +398,18 @@ def trunk_is_green(sha_grid: Any):
     return first_green_sha_ind < first_red_sha_ind
 
 
-# Creates Job Statuses which has the logic for if need to alert or if there's flaky jobs
 def classify_jobs(
-    job_names: List[str], sha_grid: Any
+    all_job_names: List[str], sha_grid: Any, filtered_jobs_names: Set[str]
 ) -> Tuple[List[JobStatus], List[Any]]:
-    job_data = map_job_data(job_names, sha_grid)
+    """
+    Creates Job Statuses which has the logic for if need to alert or if there's flaky jobs.
+    Classifies jobs into jobs to alert on and flaky jobs.
+    :param all_job_names: list of all job names as returned by the HUD
+    :param sha_grid: list of all job data as returned by the HUD (parallel index to all_job_names)
+    :param filtered_jobs_names: set of job names to actually consider
+    :return:
+    """
+    job_data = map_job_data(all_job_names, sha_grid)
     job_statuses: list[JobStatus] = []
     for job in job_data:
         job_statuses.append(JobStatus(job, job_data[job]))
@@ -413,10 +418,13 @@ def classify_jobs(
     flaky_jobs = []
 
     for job_status in job_statuses:
+        if job_status.job_name not in filtered_jobs_names:
+            continue
         if job_status.should_alert():
             jobs_to_alert_on.append(job_status)
         flaky_jobs.extend(job_status.flaky_jobs)
-    return (jobs_to_alert_on, flaky_jobs)
+
+    return jobs_to_alert_on, flaky_jobs
 
 
 def handle_flaky_tests_alert(existing_alerts: List[Dict]) -> Dict:
@@ -453,8 +461,8 @@ def filter_job_names(job_names: List[str], job_name_regex: str) -> List[str]:
 
 def check_for_recurrently_failing_jobs_alert(repo: str, branch: str, job_name_regex: str, dry_run: bool):
     job_names, sha_grid = fetch_hud_data(repo=repo, branch=branch)
-    job_names = filter_job_names(job_names, job_name_regex)
-    (jobs_to_alert_on, flaky_jobs) = classify_jobs(job_names, sha_grid)
+    filtered_job_names = set(filter_job_names(job_names, job_name_regex))
+    (jobs_to_alert_on, flaky_jobs) = classify_jobs(job_names, sha_grid, filtered_job_names)
 
     # Fetch alerts
     existing_alerts = fetch_alerts(TEST_INFRA_REPO_NAME, PYTORCH_ALERT_LABEL)

.github/workflows/test-binary-size-validation.yml
@@ -1,0 +1,21 @@
+name: Test binary size validation script
+on:
+  pull_request:
+    paths:
+      - .github/workflows/binary-size-validation.yml
+      - tools/binary_size_validation/test_binary_size_validation.py
+      - tools/binary_size_validation/binary_size_validation.py
+  workflow_dispatch:
+
+jobs:
+  test-binary-size-validation:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v3
+      - name: Install requirements
+        run: |
+          pip3 install -r tools/binary_size_validation/requirements.txt
+      - name: Run pytest
+        run: |
+          pytest tools/binary_size_validation/test_binary_size_validation.py

tools/binary_size_validation/test_binary_size_validation.py
@@ -1,0 +1,47 @@
+from binary_size_validation import parse_index
+
+# ignore long lines in this file
+# flake8: noqa: E501
+test_html = """
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1>Links for torch</h1>
+    <a href="/whl/nightly/cpu/torch-1.13.0.dev20220728%2Bcpu-cp310-cp310-linux_x86_64.whl">torch-1.13.0.dev20220728+cpu-cp310-cp310-linux_x86_64.whl</a><br/>
+    <a href="/whl/nightly/cpu/torch-1.13.0.dev20220728%2Bcpu-cp310-cp310-win_amd64.whl">torch-1.13.0.dev20220728+cpu-cp310-cp310-win_amd64.whl</a><br/>
+    <a href="/whl/nightly/cpu/torch-1.13.0.dev20220728%2Bcpu-cp37-cp37m-linux_x86_64.whl">torch-1.13.0.dev20220728+cpu-cp37-cp37m-linux_x86_64.whl</a><br/>
+    <a href="/whl/nightly/cpu/torch-1.13.0.dev20220728%2Bcpu-cp37-cp37m-win_amd64.whl">torch-1.13.0.dev20220728+cpu-cp37-cp37m-win_amd64.whl</a><br/>
+    <a href="/whl/nightly/rocm5.3/torch-2.0.0.dev20230206%2Brocm5.3-cp39-cp39-linux_x86_64.whl">torch-2.0.0.dev20230206+rocm5.3-cp39-cp39-linux_x86_64.whl</a><br/>
+    <a href="/whl/nightly/rocm5.3/torch-2.0.0.dev20230207%2Brocm5.3-cp310-cp310-linux_x86_64.whl">torch-2.0.0.dev20230207+rocm5.3-cp310-cp310-linux_x86_64.whl</a><br/>
+    <a href="/whl/nightly/rocm5.3/torch-2.0.0.dev20230207%2Brocm5.3-cp38-cp38-linux_x86_64.whl">torch-2.0.0.dev20230207+rocm5.3-cp38-cp38-linux_x86_64.whl</a><br/>
+    <a href="/whl/nightly/rocm5.3/torch-2.0.0.dev20230207%2Brocm5.3-cp39-cp39-linux_x86_64.whl">torch-2.0.0.dev20230207+rocm5.3-cp39-cp39-linux_x86_64.whl</a><br/>
+  </body>
+</html>
+<!--TIMESTAMP 1675892605-->
+"""
+
+base_url = "https://download.pytorch.org/whl/nightly/torch/"
+
+
+def test_get_whl_links():
+    wheels = parse_index(test_html, base_url)
+    assert len(wheels) == 8
+    assert wheels[0].url == \
+        "https://download.pytorch.org/whl/nightly/cpu/torch-1.13.0.dev20220728%2Bcpu-cp310-cp310-linux_x86_64.whl"
+
+
+def test_include_exclude():
+    wheels = parse_index(test_html, base_url, "amd6\\d")
+    assert len(wheels) == 2
+    assert wheels[0].name == "torch-1.13.0.dev20220728+cpu-cp310-cp310-win_amd64.whl"
+    assert wheels[1].name == "torch-1.13.0.dev20220728+cpu-cp37-cp37m-win_amd64.whl"
+
+    wheels = parse_index(test_html, base_url, "amd6\\d", "cp37")
+    assert len(wheels) == 1
+    assert wheels[0].name == "torch-1.13.0.dev20220728+cpu-cp310-cp310-win_amd64.whl"
+
+
+def test_latest_version_only():
+    wheels = parse_index(test_html, base_url, latest_version_only=True)
+    assert len(wheels) == 3
+    assert all(w.name.startswith("torch-2.0.0.dev20230207") for w in wheels)