22fcdeeec33fbd085171e672630741324b4c766c
DanilBaibak <danil.baibak@gmail.com>
2023-02-14 11:22:34 +0100

    Added a workflow for updating the viablestrict branch
    Adde scripts for fetching the latest green commit
    
    Fixed linter
    
    Updated trigger for test-update-viablestrict workflow
    
    Fixed workflow path
    
    Commented out concurrency part for now
    
    Fixed python script issue
    
    Added setup ssh step
    
    Added teardown linux step
    
    Added delay
    
    Added Setup Linux step
    
    Debug
    
    Debug
    
    Small fix
    
    Debug
    
    Cleanup
    
    Fixed path
    
    Fixed secrets
    
    Adjusted required workflows
    
    Try to push the latest green commit to the viable/strict branch
    
    Added gihub token
    
    Adjusted github token
    
    Addjusted secret name
    
    Added another credentials
    
    Adjusted credentials
    
    Trigger workflow
    
    Try to get more debugging info
    
    Trigger workflow
    
    Set bot credential
    
    Try another branch
    
    Set my credentials
    
    Cleanup
    
    Configurate gitconfig in a separate step
    
    Cleanup
    
    Try to use my repo
    
    Fixed linter issue
    
    Fixed linter issue
    
    Trigger workflow
    
    A bit of cleanup
    
    Changed credentials
    
    Check the token
    
    Checked credentials
    
    Minimize logic
    
    Fix path
    
    Checkout test-infra repo
    
    Don't checkout test-infra repo
    
    Cleanup
    
    Cleanup
    
    Cleanup
    
    Don't push
    
    Updated tocken
    
    Cleanup
    
    Try checkout V2
    
    Try without token
    
    Cleanup
    
    Try another token
    
    Try another configuration
    
    Check git config
    
    Cleanup
    
    Try to use bot credentials
    
    Try to checkout test-infra
    
    Check both repos
    
    Checkout both repos into different folders
    
    Go to the vision repo
    
    Check another token
    
    Updated commit hash
    
    Use the repo name from the inputs
    
    Try to use pytorch/vision repo
    
    Check the latest commit for the domain repo
    
    Check paths
    
    Check paths
    
    Fixed path
    
    Combine two steps
    
    Cleaning up the debugging part
    
    Make the list of workflows a parameter
    
    Fixed parameter
    
    Fixed parameter
    
    Fixed parameter
    
    Fixed parameter
    
    Fixed parameter
    
    Fixed parameter
    
    Fixed parameter
    
    Fixed parameter
    
    Test required_checks param
    
    Cleanup
    
    Improved scripts structure
    
    Added tests
    
    Added 'Test tools' job to the lint workflow
    
    Fixed lint workflow
    
    Fixed lint workflow
    
    Fixed lint workflow
    
    Fixed lint workflow
    
    Fixed lint workflow
    
    Fixed lint workflow
    
    Fixed problem with project path

---
+++ tools/scripts/fetch_latest_green_commit.py
@@ -0,0 +1,147 @@
+import sys
+import os
+import re
+
+from typing import Any, Dict, List, NamedTuple, Tuple, cast
+from argparse import ArgumentParser
+
+import rockset  # type: ignore[import]
+from tools.scripts.gitutils import check_output
+
+
+def eprint(msg: str) -> None:
+    print(msg, file=sys.stderr)
+
+
+class WorkflowCheck(NamedTuple):
+    workflowName: str
+    name: str
+    jobName: str
+    conclusion: str
+
+
+# git log -n 1 --pretty=format:%H origin/viable/strict
+# git rev-list cb5f7835ebd88c501a258a2888732e439c8bffeb^..HEAD --remotes=origin/master
+# git rev-list ^..HEAD --remotes=origin/master
+
+# git rev-parse HEAD
+# git commit-tree -p FETCH_HEAD HEAD^{tree} -m "2023-02-20 nightly release (a192c95)"
+
+def get_latest_commits() -> List[str]:
+    latest_viable_commit = check_output(
+        [
+            "git",
+            "log",
+            "-n",
+            "1",
+            "--pretty=format:%H",
+            "origin/viable/strict",
+        ],
+        encoding="ascii",
+    )
+    commits = check_output(
+        [
+            "git",
+            "rev-list",
+            f"{latest_viable_commit}^..HEAD",
+            "--remotes=*origin/master",
+        ],
+        encoding="ascii",
+    ).splitlines()
+
+    return commits
+
+
+def query_commits(commits: List[str]) -> List[Dict[str, Any]]:
+    rs = rockset.RocksetClient(
+        host="api.usw2a1.rockset.com", api_key=os.environ["ROCKSET_API_KEY"]
+    )
+    params = [{
+        "name": "shas",
+        "type": "string",
+        "value": ",".join(commits)
+    }]
+    res = rs.QueryLambdas.execute_query_lambda(
+        query_lambda='commit_jobs_batch_query',
+        version='8003fdfd18b64696',
+        workspace='commons',
+        parameters=params
+    )
+
+    return cast(List[Dict[str, Any]], res.results)
+
+
+def print_commit_status(commit: str, results: Dict[str, Any]) -> None:
+    for check in results['results']:
+        if check['sha'] == commit:
+            print(f"\t{check['conclusion']:>10}: {check['name']}")
+
+
+def get_commit_results(commit: str, results: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    workflow_checks = []
+    for check in results:
+        if check['sha'] == commit:
+            workflow_checks.append(WorkflowCheck(
+                workflowName=check['workflowName'],
+                name=check['name'],
+                jobName=check['jobName'],
+                conclusion=check['conclusion'],
+            )._asdict())
+    return workflow_checks
+
+
+def is_green(commit: str, results: List[Dict[str, Any]], required_checks: List[str]) -> Tuple[bool, str]:
+    workflow_checks = get_commit_results(commit, results)
+    regex = {check: False for check in required_checks}
+
+    for check in workflow_checks:
+        workflow_name = check["workflowName"]
+        conclusion = check["conclusion"]
+        for required_check in regex:
+            if re.match(required_check, workflow_name, flags=re.IGNORECASE):
+                if conclusion not in ["success", "skipped"]:
+                    return False, workflow_name + " checks were not successful"
+                else:
+                    regex[required_check] = True
+
+    missing_workflows = [x for x in regex.keys() if not regex[x]]
+    if len(missing_workflows) > 0:
+        return False, "missing required workflows: " + ", ".join(missing_workflows)
+
+    return True, ""
+
+
+def get_latest_green_commit(commits: List[str], results: List[Dict[str, Any]], required_checks: str) -> Any:
+    required_checks = required_checks.split(',')
+
+    for commit in commits:
+        eprint(f"Checking {commit}")
+        is_green_status, msg = is_green(commit, results, required_checks)
+
+        if is_green_status:
+            eprint("GREEN")
+            return commit
+        else:
+            eprint("RED: " + msg)
+    return None
+
+
+def _arg_parser() -> Any:
+    parser = ArgumentParser()
+    parser.add_argument("required_checks", type=str)
+
+    return parser.parse_args()
+
+
+def main() -> None:
+    args = _arg_parser()
+
+    commits = get_latest_commits()
+    results = query_commits(commits)
+
+    latest_viable_commit = get_latest_green_commit(commits, results, args.required_checks)
+    print(latest_viable_commit)
+
+
+if __name__ == "__main__":
+    main()
