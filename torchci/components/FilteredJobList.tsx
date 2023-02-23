093e5bf3b630949dde50aaf347c1bd4c9459904e
Catherine Lee <csl@fb.com>
2023-02-22 17:06:22 -0800

    scroll

--- torchci/components/FilteredJobList.tsx
+++ torchci/components/FilteredJobList.tsx
@@ -7,6 +7,37 @@ import JobLinks from "./JobLinks";
 import JobSummary from "./JobSummary";
 import LogViewer from "./LogViewer";
 import { JobAnnotation } from "lib/types";
+import useScrollTo from "lib/useScrollTo";
+
+function FailedJobInfo({
+  job,
+  showClassification,
+  annotation,
+}: {
+  job: JobData;
+  showClassification: boolean;
+  annotation: JobAnnotation;
+}) {
+  const router = useRouter();
+  useScrollTo();
+  const { repoOwner, repoName } = router.query;
+  return (
+    <li key={job.id} id={job.id}>
+      <JobSummary job={job} />
+      <div>
+        <JobLinks job={job} />
+      </div>
+      <LogViewer job={job} />
+      {showClassification && (
+        <JobAnnotationToggle
+          job={job}
+          annotation={annotation ?? JobAnnotation.NULL}
+          repo={`${repoOwner}/${repoName}`}
+        />
+      )}
+    </li>
+  );
+}
 
 export default function FilteredJobList({
   filterName,
@@ -43,22 +74,16 @@ export default function FilteredJobList({
       <h2>{filterName}</h2>
       <ul>
         {filteredJobs.map((job) => (
-          <li key={job.id}>
-            <JobSummary job={job} />
-            <div>
-              <JobLinks job={job} />
-            </div>
-            <LogViewer job={job} />
-            {showClassification && (
-              <JobAnnotationToggle
-                job={job}
-                annotation={
-                  (data[job?.id ?? ""] && data[job?.id ?? ""]["annotation"]) ?? JobAnnotation.NULL
-                }
-                repo={`${repoOwner}/${repoName}`}
-              />
-            )}
-          </li>
+          <FailedJobInfo
+            job={job}
+            showClassification={showClassification}
+            annotation={
+              (data &&
+                data[job?.id ?? ""] &&
+                data[job?.id ?? ""]["annotation"]) ??
+              JobAnnotation.NULL
+            }
+          />
         ))}
       </ul>
     </div>
