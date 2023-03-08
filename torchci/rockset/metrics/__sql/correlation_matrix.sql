45da766265907a30d661aad1eee27de6d281f887
Huy Do <huydhn@gmail.com>
2023-03-08 10:50:49 -0800

    Update correlation script to use hamming distance and ignore flaky results (#3852)
    Update the correlation script to use hamming distance to calculate the
    similarity between jobs instead of the default Peason correlation
    provided by pandas `df.corr()`. The latter doesn't work for binary data
    like what we have `is_green: boolean`, so its correlation value didn't
    make no sense.
    
    I also update the Rockset query to:
    
    * Use the latest retry result to remove flakiness
    * Remove untracked jobs like `rerun_disabled_tests`
    * Sort by event time
    
    Finally, I detect the GREEN RED GREEN pattern in the job results, and
    approximate the RED one as flaky.
    
    The result shows a **~91%** correlation between MacOS x86-64 and arm64
    `python scripts/compute_correlation.py --ignore-flaky` in the past 21
    days
    
    ```
    print(
        correlation_matrix["macos-12-py3-x86-64 / test (default)"][
            "macos-12-py3-arm64 / test (default)"
        ]
    )
    0.9132569558101473
    
    print(
        correlation_matrix["macos-12-py3-x86-64 / test (functorch)"][
            "macos-12-py3-arm64 / test (functorch)"
        ]
    )
    0.9116202945990179
    ```
    
    If I look at only one week results, the correlation is higher at
    **95%**. After a manual check, the differences were due to:
    
    * https://github.com/pytorch/pytorch/issues/95922 GitHub outage. x86_64
    jobs failed while arm64 succeeded
    ([hud](https://hud.pytorch.org/hud/pytorch/pytorch/master/4?per_page=50&name_filter=macos))
    * https://github.com/pytorch/pytorch/issues/95510, M1 queuing issue
    where M1 jobs were disabled. x86_64 job succeeded while arm64 was
    disabled (missing result, default to failed)
    
    Using this number, we have high confidence that we can tone down x86_64
    jobs.
    
    ### Correlation results
    
    
    https://torchci-git-fork-huydhn-improve-correlation-9b4670-fbopensource.vercel.app/correlation

--- torchci/rockset/metrics/__sql/correlation_matrix.sql
+++ torchci/rockset/metrics/__sql/correlation_matrix.sql
@@ -19,21 +19,37 @@ WITH all_jobs AS (
       WHEN job.conclusion = 'skipped' THEN NULL
       ELSE 1
     END AS is_green,
+    workflow._event_time AS event_time,
+    ROW_NUMBER() OVER(PARTITION BY job.name, push.head_commit.id ORDER BY job.run_attempt DESC) AS attempt,
 FROM
   workflow_run workflow
   INNER JOIN commons.workflow_job job ON workflow.id = job.run_id
+  INNER JOIN push on workflow.head_commit.id = push.head_commit.id
 WHERE
   job._event_time > CURRENT_TIMESTAMP() - INTERVAL 21 DAY
   AND ARRAY_CONTAINS(SPLIT(:workflowNames, ','), LOWER(workflow.name))
+  AND workflow.event != 'workflow_run'
+  AND push.ref = 'refs/heads/master'
+  AND push.repository.owner.name = 'pytorch'
+  AND push.repository.name = 'pytorch'
   AND job.name LIKE '%test%'
+  AND job.name NOT LIKE '%filter%'
+  AND job.name NOT LIKE '%rerun_disabled_tests%'
 )
 SELECT
   IF (name LIKE '%(%' AND name NOT LIKE '%)%', CONCAT(name, ')'), name) AS name,
   head_sha,
   is_green,
+  event_time,
 FROM
   all_jobs
+WHERE
+  is_green IS NOT NULL
+  AND attempt = 1
 GROUP BY
   name,
   head_sha,
-  is_green
+  is_green,
+  event_time
+ORDER BY
+  event_time DESC
