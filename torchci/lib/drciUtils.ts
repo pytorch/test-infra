6bdb67270ce500a4c940b2c97916353ca75e4cd4
Danylo Baibak <baibak@meta.com>
2023-03-09 09:39:39 +0100

    Enabled DR. CI for vision repo (#3842)
    Enabled DR. CI for vision repo.
    
    ---------
    
    Co-authored-by: Zain Rizvi <ZainRizvi@users.noreply.github.com>

--- torchci/lib/drciUtils.ts
+++ torchci/lib/drciUtils.ts
@@ -2,7 +2,7 @@ import _ from "lodash";
 import { Octokit } from "octokit";
 import { IssueData } from "./types";
 import fetchIssuesByLabel from "lib/fetchIssuesByLabel";
-import { isPyTorchPyTorch } from "./bot/utils";
+import { isDrCIEnabled } from "./bot/utils";
 
 export const NUM_MINUTES = 30;
 export const REPO: string = "pytorch";
@@ -116,8 +116,8 @@ ${sev_list}\n
 // The context here is the context from probot.
 // Today we only use probot for upserts, but this could later be split into logger
 export async function upsertDrCiComment(owner: string, repo: string, prNum: number, context: any, prUrl: string) {
-  // Dr.CI only supports pytorch/pytorch at the moment
-  if (!isPyTorchPyTorch(owner,repo)) {
+  // Dr.CI only supports [pytorch/pytorch, pytorch/vision] at the moment
+  if (!isDrCIEnabled(owner, repo)) {
     context.log(`Pull request to ${owner}/${repo} is not supported by Dr.CI bot, no comment is made`);
     return;
   }
