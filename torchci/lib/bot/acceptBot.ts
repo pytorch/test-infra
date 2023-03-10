ffb9897d2f4652ef2d84c2142b49f7d47211a0c7
Sahan Paliskara <sahancpal@gmail.com>
2023-03-10 12:04:03 -0800

    Remove accept2ship label (#3858)

--- torchci/lib/bot/acceptBot.ts
+++ torchci/lib/bot/acceptBot.ts
@@ -6,43 +6,9 @@ function containsLabel(labels: Label[], labelName: string) {
   return labels.filter((label) => label.name === labelName).length > 0;
 }
 
-export const ACCEPT_2_SHIP = "accept2ship";
 export const CIFLOW_TRUNK_LABEL = "ciflow/trunk";
 
-export const ACCEPT_MESSAGE_PREFIX =
-  "This PR has been accepted with the accept2ship label. Attempting to merge now.";
-
-export const ACCEPT_MESSAGE = `${ACCEPT_MESSAGE_PREFIX}
-
-@pytorchbot merge
-`;
-
 function acceptBot(app: Probot): void {
-  app.on(["pull_request_review.submitted"], async (ctx) => {
-    if (ctx.payload.review.state === "approved") {
-      const labels = ctx.payload.pull_request.labels;
-      const owner = ctx.payload.repository.owner.login;
-      const repo = ctx.payload.repository.name;
-      const issue_number = ctx.payload.pull_request.number;
-
-      const hasAcceptToShip = containsLabel(labels, ACCEPT_2_SHIP);
-
-      if (hasAcceptToShip) {
-        await ctx.octokit.issues.createComment({
-          owner,
-          repo,
-          issue_number,
-          body: ACCEPT_MESSAGE,
-        });
-        await ctx.octokit.issues.removeLabel({
-          owner,
-          repo,
-          issue_number,
-          name: ACCEPT_2_SHIP,
-        });
-      }
-    }
-  });
 
   app.on("pull_request.labeled", async (ctx) => {
     const owner = ctx.payload.repository.owner.login;
@@ -59,23 +25,6 @@ function acceptBot(app: Probot): void {
       const isApproved =
         reviews.data.filter((review) => review.state == "approved").length > 0;
       return isApproved;
-    }
-
-    if (ctx.payload.label.name === ACCEPT_2_SHIP) {
-      if (await isApproved()) {
-        await ctx.octokit.issues.createComment({
-          owner,
-          repo,
-          issue_number,
-          body: ACCEPT_MESSAGE,
-        });
-        await ctx.octokit.issues.removeLabel({
-          owner,
-          repo,
-          issue_number,
-          name: ACCEPT_2_SHIP,
-        });
-      }
     }
   });
 }
