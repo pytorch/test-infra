cdef32ef0bdea9622d0ffc1fd394050bb92304f4
atalman <atalman@fb.com>
2023-03-16 14:02:12 -0700

    Remove mkl constriant

--- tools/pkg-helpers/pytorch_pkg_helpers/macos.py
+++ tools/pkg-helpers/pytorch_pkg_helpers/macos.py
@@ -5,7 +5,4 @@ def get_macos_variables(arch_name: str, python_version: str = "3.8") -> list:
         "export CXX=clang++",
     ]
 
-    if arch_name != "arm64" and python_version != "3.11":
-        variables.append("export CONDA_EXTRA_BUILD_CONSTRAINT='- mkl<=2023.0.0'")
-
     return variables
