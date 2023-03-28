789dd3ddd47bcb7227b601c19d7e5dbeafdbc67b
DanilBaibak <baibak@meta.com>
2023-03-28 19:47:42 +0200

    Updated Rockset query

--- torchci/rockset/commons/__sql/recent_pr_workflows_query.sql
+++ torchci/rockset/commons/__sql/recent_pr_workflows_query.sql
@@ -1,20 +1,23 @@
-WITH recent_shas as (
-    SELECT
-        p.head.sha as sha,
-        p.number as number
-    FROM
-        workflow_job j
-        JOIN commons.pull_request p ON j.head_sha = p.head.sha
-    where
-        (
+WITH
+    recent_shas as (
+        SELECT
+            p.head.sha as sha,
+            p.number as number,
+            PARSE_TIMESTAMP_ISO8601(push.head_commit.timestamp) as sha_time,
+        FROM
+            workflow_job j
+            JOIN commons.pull_request p ON j.head_sha = p.head.sha
+            JOIN commons.push push on push.head_commit.id = j.head_sha
+        where
             (
-                PARSE_TIMESTAMP_ISO8601(j.completed_at) > (CURRENT_TIMESTAMP() - MINUTES(:numMinutes))
-                and :prNumber = 0
+                (
+                    PARSE_TIMESTAMP_ISO8601(j.completed_at) > (CURRENT_TIMESTAMP() - MINUTES(:numMinutes))
+                    and :prNumber = 0
+                )
+                or :prNumber = p.number
             )
-            or :prNumber = p.number
-        )
-        and p.base.repo.full_name = :repo
-)
+            and p.base.repo.full_name = :repo
+    )
 SELECT
     w.id as workflow_id,
     j.id,
@@ -25,6 +28,7 @@ SELECT
     recent_shas.number AS pr_number,
     recent_shas.sha as head_sha,
     j.torchci_classification.captures as failure_captures,
+    recent_shas.sha_time
 FROM
     recent_shas
     join commons.workflow_job j ON j.head_sha = recent_shas.sha
@@ -39,7 +43,8 @@ SELECT
     w.html_url,
     recent_shas.number AS pr_number,
     w.head_sha,
-    null as failure_line
+    null as failure_line,
+    recent_shas.sha_time
 FROM
     recent_shas
     join commons.workflow_run w ON w.head_sha = recent_shas.sha
