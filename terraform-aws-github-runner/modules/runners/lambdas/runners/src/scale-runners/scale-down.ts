9a10f45eb1ab99f6d3e267cf2331ea529b5ec492
Jean Schmidt <4520845+jeanschmidt@users.noreply.github.com>
2023-03-30 12:59:54 +0200

    Randomizes the sequence of scaleDown runners to improve resiliency in a overload scenario (#3962)

--- terraform-aws-github-runner/modules/runners/lambdas/runners/src/scale-runners/scale-down.ts
+++ terraform-aws-github-runner/modules/runners/lambdas/runners/src/scale-runners/scale-down.ts
@@ -14,7 +14,7 @@ import {
 } from './gh-runners';
 import { ScaleDownMetrics, sendMetricsAtTimeout, sendMetricsTimeoutVars } from './metrics';
 import { listRunners, resetRunnersCaches, terminateRunner } from './runners';
-import { getRepo, groupBy, Repo, RunnerInfo, isGHRateLimitError } from './utils';
+import { getRepo, groupBy, Repo, RunnerInfo, isGHRateLimitError, shuffleArrayInPlace } from './utils';
 
 export async function scaleDown(): Promise<void> {
   const metrics = new ScaleDownMetrics();
@@ -47,7 +47,7 @@ export async function scaleDown(): Promise<void> {
       return;
     }
 
-    for (const [runnerType, runners] of runnersDict.entries()) {
+    for (const [runnerType, runners] of shuffleArrayInPlace(Array.from(runnersDict.entries()))) {
       if (runners.length < 1 || runners[0].runnerType === undefined || runnerType === undefined) continue;
 
       const ghRunnersRemovable: Array<[RunnerInfo, GhRunner | undefined]> = [];
