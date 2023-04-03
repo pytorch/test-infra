8110f38b4d5c16ebdb4520446db3352b464e7201
Huy Do <huydhn@gmail.com>
2023-04-03 15:33:06 -0700

    Add TorchDynamo performance Dashboard on HUD (#3865)
    Influenced by https://github.com/pytorch/pytorch/issues/93794. This is
    the initial version of what the dashboard looks like
    https://torchci-git-fork-huydhn-add-compilers-bench-74abf8-fbopensource.vercel.app/benchmark/compilers
    
    Related issue https://github.com/pytorch/test-infra/issues/3783

---
+++ torchci/rockset/inductor/__sql/compilers_benchmark_performance.sql
@@ -0,0 +1,116 @@
+WITH performance_results AS (
+  SELECT
+    name,
+    speedup,
+    REPLACE(
+      filename,
+      CONCAT(
+        '_', : dtypes, '_', : mode, '_', : device,
+        '_performance'
+      )
+    ) AS filename,
+    compilation_latency,
+    compression_ratio,
+    workflow_id,    
+  FROM
+    inductor.torch_dynamo_perf_stats
+  WHERE
+    filename LIKE '%_performance'
+    AND filename LIKE CONCAT(
+      '%_', : dtypes, '_', : mode, '_', : device,
+      '_%'
+    )
+    AND _event_time >= PARSE_DATETIME_ISO8601(:startTime)
+    AND _event_time < PARSE_DATETIME_ISO8601(:stopTime)
+    AND (workflow_id = :workflowId OR :workflowId = 0)    
+),
+accuracy_results AS (
+  SELECT
+    name,
+    accuracy,
+    REPLACE(
+      filename,
+      CONCAT(
+        '_', : dtypes, '_', : mode, '_', : device,
+        '_accuracy'
+      )
+    ) AS filename,
+    workflow_id,
+  FROM
+    inductor.torch_dynamo_perf_stats
+  WHERE
+    filename LIKE '%_accuracy'
+    AND filename LIKE CONCAT(
+      '%_', : dtypes, '_', : mode, '_', : device,
+      '_%'
+    )
+    AND _event_time >= PARSE_DATETIME_ISO8601(:startTime)
+    AND _event_time < PARSE_DATETIME_ISO8601(:stopTime)
+    AND (workflow_id = :workflowId OR :workflowId = 0)
+),
+results AS (
+  SELECT
+    accuracy_results.workflow_id AS workflow_id,
+    CASE
+      WHEN accuracy_results.filename LIKE '%_torchbench' THEN 'torchbench'
+      WHEN accuracy_results.filename LIKE '%_timm_models' THEN 'timm_models'
+      WHEN accuracy_results.filename LIKE '%_huggingface' THEN 'huggingface'
+      ELSE NULL
+    END AS suite,
+    CASE
+      WHEN accuracy_results.filename LIKE '%_torchbench' THEN REPLACE(
+        accuracy_results.filename, '_torchbench'
+      )
+      WHEN accuracy_results.filename LIKE '%_timm_models' THEN REPLACE(
+        accuracy_results.filename, '_timm_models'
+      )
+      WHEN accuracy_results.filename LIKE '%_huggingface' THEN REPLACE(
+        accuracy_results.filename, '_huggingface'
+      )
+      ELSE NULL
+    END AS compiler,
+    accuracy_results.name,
+    CAST(IF(
+      speedup IS NULL, '0.0000',
+      speedup
+    ) AS FLOAT) AS speedup,
+    accuracy,
+    CAST(IF(
+      compilation_latency IS NULL, '0.0000',
+      compilation_latency
+    ) AS FLOAT) AS compilation_latency,
+    CAST(IF(
+      compression_ratio IS NULL, '0.0000',
+      compression_ratio
+    ) AS FLOAT) AS compression_ratio,
+  FROM
+    accuracy_results
+    LEFT JOIN performance_results ON performance_results.name = accuracy_results.name
+    AND performance_results.filename = accuracy_results.filename
+    AND performance_results.workflow_id = accuracy_results.workflow_id
+)
+SELECT DISTINCT
+  results.workflow_id,
+  results.suite,
+  results.compiler,
+  results.name,
+  results.speedup,
+  results.accuracy,
+  results.compilation_latency,
+  results.compression_ratio,
+  FORMAT_ISO8601(
+    DATE_TRUNC(: granularity, w._event_time)
+  ) AS granularity_bucket,
+FROM
+  results LEFT JOIN commons.workflow_run w ON results.workflow_id = w.id
+WHERE
+  ARRAY_CONTAINS(SPLIT(:suites, ','), LOWER(results.suite))
+  AND (ARRAY_CONTAINS(SPLIT(:compilers, ','), LOWER(results.compiler)) OR :compilers = '')
+  AND head_branch LIKE :branch
+  AND (ARRAY_CONTAINS(SPLIT(:commits, ','), head_sha) OR :commits = '')  
+ORDER BY
+  granularity_bucket DESC,
+  workflow_id DESC,
+  suite ASC,
+  compiler ASC,
+  name ASC
