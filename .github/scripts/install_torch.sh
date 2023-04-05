aa5e3035d6cefc843371b639996355846bb46834
Andrey Talman <atalman@fb.com>
2023-04-05 13:18:54 -0400

    Python 3.11 remove temporary channel (#3991)
    Python 3.11 remove temporary channel

--- .github/scripts/install_torch.sh
+++ .github/scripts/install_torch.sh
@@ -4,9 +4,6 @@ export MATRIX_INSTALLATION="${MATRIX_INSTALLATION/torchvision}"
 export MATRIX_INSTALLATION="${MATRIX_INSTALLATION/torchaudio}"
 if [[ ${MATRIX_PACKAGE_TYPE} = "conda" ]]; then
     export MATRIX_INSTALLATION=${MATRIX_INSTALLATION/"conda install"/"conda install --yes --quiet"}
-    if [[ ${MATRIX_PYTHON_VERSION} = "3.11" ]]; then
-        export MATRIX_INSTALLATION=${MATRIX_INSTALLATION/"-c pytorch"/" -c malfet -c pytorch"}
-    fi
 fi
 eval $MATRIX_INSTALLATION
 
