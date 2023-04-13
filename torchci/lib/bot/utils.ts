233ba155f9bec1c7798e2ea3b5c87b132241903b
DanilBaibak <baibak@meta.com>
2023-04-13 16:00:56 +0200

    Enable Dr. CI for torchtext

--- torchci/lib/bot/utils.ts
+++ torchci/lib/bot/utils.ts
@@ -17,7 +17,7 @@ export function isPyTorchPyTorch(owner: string, repo: string): boolean {
 }
 
 export function isDrCIEnabled(owner: string, repo: string): boolean {
-  return isPyTorchOrg(owner) && ["pytorch", "vision"].includes(repo);
+  return isPyTorchOrg(owner) && ["pytorch", "vision", "text"].includes(repo);
 }
 
 export class CachedConfigTracker {
