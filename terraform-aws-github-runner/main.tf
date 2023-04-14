c2e6be7275a20402ca0e00ec599d610c97459094
Jean Schmidt <contato@jschmidt.me>
2023-04-14 16:59:09 +0200

    more fixes..

--- terraform-aws-github-runner/main.tf
+++ terraform-aws-github-runner/main.tf
@@ -104,24 +104,11 @@ module "runners" {
     encrypt    = var.encrypt_secrets
   }
 
-  s3_bucket_runner_binaries           = module.runner_binaries.bucket
-  s3_location_runner_binaries_linux   = local.s3_action_runner_url_linux
-  s3_location_runner_binaries_windows = local.s3_action_runner_url_windows
-
   must_have_issues_labels = var.must_have_issues_labels
   cant_have_issues_labels = var.cant_have_issues_labels
 
   redis_endpoint = aws_elasticache_replication_group.es.primary_endpoint_address
   redis_login    = "${aws_elasticache_user.scale_lambda.user_name}:${random_password.es_password.result}"
-
-  instance_type         = var.instance_type
-  block_device_mappings = var.block_device_mappings
-
-  runner_architecture = local.runner_architecture
-  ami_owners_linux    = var.ami_owners_linux
-  ami_owners_windows  = var.ami_owners_windows
-  ami_filter_linux    = var.ami_filter_linux
-  ami_filter_windows  = var.ami_filter_windows
 
   sqs_build_queue                      = aws_sqs_queue.queued_builds
   sqs_build_queue_retry                = aws_sqs_queue.queued_builds_retry
@@ -130,9 +117,7 @@ module "runners" {
   scale_down_schedule_expression       = var.scale_down_schedule_expression
   minimum_running_time_in_minutes      = var.minimum_running_time_in_minutes
   runner_extra_labels                  = var.runner_extra_labels
-  runner_as_root                       = var.runner_as_root
   idle_config                          = var.idle_config
-  enable_ssm_on_runners                = var.enable_ssm_on_runners
   secretsmanager_secrets_id            = var.secretsmanager_secrets_id
 
   lambda_s3_bucket                 = var.lambda_s3_bucket
@@ -145,36 +130,27 @@ module "runners" {
 
   lambda_security_group_ids        = concat(
     var.lambda_security_group_ids,
-    [module.runner_instances.output.security_groups_ids_vpcs[0]]
+    [module.runners_instances.security_groups_ids_vpcs[0]]
   )
-  github_app_key_base64            = module.runners_instances.output.github_app_key_base64
-  github_app_client_secret         = module.runners_instances.output.github_app_client_secret
-  role_runner_arn                  = module.runners_instances.output.role_runner_arn
+  github_app_key_base64            = module.runners_instances.github_app_key_base64
+  github_app_client_secret         = module.runners_instances.github_app_client_secret
+  role_runner_arn                  = module.runners_instances.role_runner_arn
 
-  launch_template_name_linux             = module.runners_instances.output.launch_template_name_linux
-  launch_template_name_linux_nvidia      = module.runners_instances.output.launch_template_name_linux_nvidia
-  launch_template_name_windows           = module.runners_instances.output.launch_template_name_windows
-  launch_template_version_linux          = module.runners_instances.output.launch_template_version_linux
-  launch_template_version_windows        = module.runners_instances.output.launch_template_version_windows
-  launch_template_version_linux_nvidia   = module.runners_instances.output.launch_template_version_linux_nvidia
+  launch_template_name_linux             = module.runners_instances.launch_template_name_linux
+  launch_template_name_linux_nvidia      = module.runners_instances.launch_template_name_linux_nvidia
+  launch_template_name_windows           = module.runners_instances.launch_template_name_windows
+  launch_template_version_linux          = module.runners_instances.launch_template_version_linux
+  launch_template_version_windows        = module.runners_instances.launch_template_version_windows
+  launch_template_version_linux_nvidia   = module.runners_instances.launch_template_version_linux_nvidia
 
   logging_retention_in_days        = var.logging_retention_in_days
-  enable_cloudwatch_agent          = var.enable_cloudwatch_agent
   scale_up_lambda_concurrency      = var.scale_up_lambda_concurrency
   scale_up_provisioned_concurrent_executions = var.scale_up_provisioned_concurrent_executions
 
-  instance_profile_path     = var.instance_profile_path
   role_path                 = var.role_path
   role_permissions_boundary = var.role_permissions_boundary
 
-  userdata_template     = var.userdata_template
-  userdata_pre_install  = var.userdata_pre_install
-  userdata_post_install = var.userdata_post_install
-  key_name              = var.key_name
-
   create_service_linked_role_spot = var.create_service_linked_role_spot
-
-  runner_iam_role_managed_policy_arns = var.runner_iam_role_managed_policy_arns
 
   ghes_url = var.ghes_url
 }
@@ -183,10 +159,7 @@ module "runners_instances" {
   source = "./modules/runners-instances"
 
   aws_region           = var.aws_region
-  aws_region_instances = var.aws_region_instances
   vpc_ids              = var.vpc_ids
-  vpc_sgs              = var.vpc_sgs
-  subnet_vpc_ids       = var.subnet_vpc_ids
   environment          = var.environment
   tags                 = local.tags
 
@@ -199,12 +172,6 @@ module "runners_instances" {
   s3_location_runner_binaries_linux   = local.s3_action_runner_url_linux
   s3_location_runner_binaries_windows = local.s3_action_runner_url_windows
 
-  must_have_issues_labels = var.must_have_issues_labels
-  cant_have_issues_labels = var.cant_have_issues_labels
-
-  redis_endpoint = aws_elasticache_replication_group.es.primary_endpoint_address
-  redis_login    = "${aws_elasticache_user.scale_lambda.user_name}:${random_password.es_password.result}"
-
   instance_type         = var.instance_type
   block_device_mappings = var.block_device_mappings
 
@@ -214,30 +181,12 @@ module "runners_instances" {
   ami_filter_linux    = var.ami_filter_linux
   ami_filter_windows  = var.ami_filter_windows
 
-  sqs_build_queue                      = aws_sqs_queue.queued_builds
-  sqs_build_queue_retry                = aws_sqs_queue.queued_builds_retry
   github_app                           = var.github_app
-  enable_organization_runners          = var.enable_organization_runners
-  scale_down_schedule_expression       = var.scale_down_schedule_expression
-  minimum_running_time_in_minutes      = var.minimum_running_time_in_minutes
-  runner_extra_labels                  = var.runner_extra_labels
   runner_as_root                       = var.runner_as_root
-  idle_config                          = var.idle_config
   enable_ssm_on_runners                = var.enable_ssm_on_runners
-  secretsmanager_secrets_id            = var.secretsmanager_secrets_id
 
-  lambda_s3_bucket                 = var.lambda_s3_bucket
-  runners_lambda_s3_key            = var.runners_lambda_s3_key
-  runners_lambda_s3_object_version = var.runners_lambda_s3_object_version
-  lambda_zip                       = var.runners_lambda_zip
-  lambda_timeout_scale_up          = var.runners_scale_up_lambda_timeout
-  lambda_timeout_scale_down        = var.runners_scale_down_lambda_timeout
-  lambda_subnet_ids                = var.lambda_subnet_ids
-  lambda_security_group_ids        = var.lambda_security_group_ids
   logging_retention_in_days        = var.logging_retention_in_days
   enable_cloudwatch_agent          = var.enable_cloudwatch_agent
-  scale_up_lambda_concurrency      = var.scale_up_lambda_concurrency
-  scale_up_provisioned_concurrent_executions = var.scale_up_provisioned_concurrent_executions
 
   instance_profile_path     = var.instance_profile_path
   role_path                 = var.role_path
@@ -247,8 +196,6 @@ module "runners_instances" {
   userdata_pre_install  = var.userdata_pre_install
   userdata_post_install = var.userdata_post_install
   key_name              = var.key_name
-
-  create_service_linked_role_spot = var.create_service_linked_role_spot
 
   runner_iam_role_managed_policy_arns = var.runner_iam_role_managed_policy_arns
 
