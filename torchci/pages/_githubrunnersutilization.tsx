2276a28c6e4c45a4961d9c57e08f53de38c5ec3a
Nikita Shulga <nikita.shulga@gmail.com>
2023-04-25 16:54:21 -0700

    [HUD] Add page for tracking GitHub runners utilization

---
+++ torchci/pages/_githubrunnersutilization.tsx
@@ -0,0 +1,57 @@
+import TimeSeriesPanel from "components/metrics/panels/TimeSeriesPanel";
+import TablePanel from "components/metrics/panels/TablePanel";
+import { RocksetParam } from "lib/rockset";
+import { durationDisplay } from "components/TimeUtils";
+import { TimeRangePicker } from "./metrics";
+import { useState } from "react";
+import dayjs from "dayjs";
+import {
+  Grid,
+} from "@mui/material";
+
+const ROW_HEIGHT = 340;
+
+export default function Page() {
+  const [startTime, setStartTime] = useState(dayjs().subtract(1, "week"));
+  const [stopTime, setStopTime] = useState(dayjs());
+  const [timeRange, setTimeRange] = useState<number>(30);
+
+  const timeParams: RocksetParam[] = [
+    {
+      name: "startTime",
+      type: "string",
+      value: startTime,
+    },
+    {
+      name: "stopTime",
+      type: "string",
+      value: stopTime,
+    },
+  ];
+
+  return (
+    <div>
+        <TimeRangePicker
+          startTime={startTime}
+          setStartTime={setStartTime}
+          stopTime={stopTime}
+          setStopTime={setStopTime}
+          timeRange={timeRange}
+          setTimeRange={setTimeRange}
+        />
+    <Grid item xs={6} height={ROW_HEIGHT}>
+      <TimeSeriesPanel
+        title={"Runners utilization daily"}
+        queryCollection={"utilization"}
+        queryName={"runner_utilization"}
+        groupByFieldName={"label"}
+        granularity={"day"}
+        timeFieldName={"started_date"}
+        yAxisFieldName={"duration"}
+        yAxisRenderer={durationDisplay}
+        queryParams={timeParams}
+      />
+    </Grid>
+    </div>
+  );
+}
---
+++ torchci/rockset/utilization/__sql/runner_utilization.sql
@@ -0,0 +1,24 @@
+SELECT
+      FORMAT_ISO8601(
+        DATE_TRUNC(
+            :granularity,
+            started_at AT TIME ZONE :timezone
+        )
+    ) AS started_date,
+  label,
+  SUM(DATE_DIFF('SECOND',started_at,completed_at)) AS duration,
+FROM (SELECT 
+  PARSE_TIMESTAMP_ISO8601(started_at) as started_at,
+  PARSE_TIMESTAMP_ISO8601(completed_at) as completed_at,
+  ELEMENT_AT(labels, 1) AS label
+  FROM commons.workflow_job
+  WHERE
+      status = 'completed' AND
+      runner_group_name = 'GitHub Actions'
+  ) AS gha_jobs
+WHERE
+    started_at >= PARSE_DATETIME_ISO8601(:startTime) AND
+    started_at < PARSE_DATETIME_ISO8601(:stopTime)
+GROUP BY started_date, label
+ORDER BY started_date DESC, label
+LIMIT 500;
