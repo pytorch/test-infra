6d96d71733c39c60e219e54ce47c4059a8eb1fe0
Nikita Shulga <nikita.shulga@gmail.com>
2023-04-25 17:34:21 -0700

    Add runner utilization by repo breakdown

--- torchci/pages/_githubrunnersutilization.tsx
+++ torchci/pages/_githubrunnersutilization.tsx
@@ -7,9 +7,53 @@ import { useState } from "react";
 import dayjs from "dayjs";
 import {
   Grid,
+  Stack,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  SelectChangeEvent,
 } from "@mui/material";
 
 const ROW_HEIGHT = 340;
+
+/**
+ * Allows the user to pick the TTS metrics.
+ */
+export function RunnerTypePicker({
+  runnerType,
+  setRunnerType,
+}: {
+  runnerType: string;
+  setRunnerType: any;
+}) {
+  function handleChange(e: SelectChangeEvent<number>) {
+    setRunnerType(e.target.value as string);
+  }
+
+  return (
+    <>
+      <FormControl>
+        <InputLabel id="github-runner-type-picker-select-label">
+          Runner Type
+        </InputLabel>
+        <Select
+          defaultValue={runnerType}
+          label="Runner Type"
+          labelId="github-runner-type-picker-select-label"
+          onChange={handleChange}
+        >
+          <MenuItem value={"macos-12"}>MacOS 12</MenuItem>
+          <MenuItem value={"macos-12-xl"}>MacOS 12 XL</MenuItem>
+          <MenuItem value={"ubuntu-20.04"}>Ubuntu 20.04</MenuItem>
+          <MenuItem value={"ubuntu-22.04"}>Ubuntu 22.04</MenuItem>
+          <MenuItem value={"ubuntu-latest"}>Ubuntu Latest</MenuItem>
+          <MenuItem value={"windows-2019"}>Windows 2019</MenuItem>
+        </Select>
+      </FormControl>
+    </>
+  );
+}
 
 export default function Page() {
   const [startTime, setStartTime] = useState(dayjs().subtract(30, "day"));
@@ -28,9 +72,11 @@ export default function Page() {
       value: stopTime,
     },
   ];
+  const [runnerType, setRunnerType] = useState<string>("macos-12-xl");
 
   return (
     <div>
+      <Stack direction="row" spacing={2} sx={{ mb: 2 }}>
         <TimeRangePicker
           startTime={startTime}
           setStartTime={setStartTime}
@@ -39,6 +85,11 @@ export default function Page() {
           timeRange={timeRange}
           setTimeRange={setTimeRange}
         />
+        <RunnerTypePicker
+          runnerType={runnerType}
+          setRunnerType={setRunnerType}
+        />
+      </Stack>
     <Grid item xs={6} height={ROW_HEIGHT}>
       <TimeSeriesPanel
         title={"Runners utilization daily"}
@@ -49,7 +100,39 @@ export default function Page() {
         timeFieldName={"started_date"}
         yAxisFieldName={"duration"}
         yAxisRenderer={durationDisplay}
-        queryParams={timeParams}
+        queryParams={[
+          {
+            name: "timezone",
+            type: "string",
+            value: Intl.DateTimeFormat().resolvedOptions().timeZone,
+          },
+          ...timeParams,
+        ]}
+      />
+    </Grid>
+    <Grid item xs={6} height={ROW_HEIGHT}>
+      <TimeSeriesPanel
+        title={"Runners utilization daily by repo"}
+        queryCollection={"utilization"}
+        queryName={"runner_utilization_by_repo"}
+        groupByFieldName={"project"}
+        granularity={"day"}
+        timeFieldName={"started_date"}
+        yAxisFieldName={"duration"}
+        yAxisRenderer={durationDisplay}
+        queryParams={[
+          {
+            name: "timezone",
+            type: "string",
+            value: Intl.DateTimeFormat().resolvedOptions().timeZone,
+          },
+          {
+            name: "label",
+            type: "string",
+            value: runnerType,
+          },
+          ...timeParams,
+        ]}
       />
     </Grid>
     </div>
