c9aea81902360604e954da8e7a350c553c0f17fa
Nikita Shulga <nshulga@meta.com>
2023-04-28 14:48:57 -0700

    Add CUDA-12.1 to nightly builds (#4090)
    Update `cuda.py` to remove Keplers and add support for Hopper for CUDA-12.0+ builds

--- tools/pkg-helpers/pytorch_pkg_helpers/cuda.py
+++ tools/pkg-helpers/pytorch_pkg_helpers/cuda.py
@@ -8,10 +8,12 @@ WINDOWS_PATH_PREFIX = "C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v"
 
 
 def get_cuda_arch_list(sanitized_version: str) -> str:
+    if float(sanitized_version) >= 12.0:
+        return "5.0+PTX;6.0;7.0;7.5;8.0;8.6;9.0"
     if float(sanitized_version) > 11.3:
         return "3.5;5.0+PTX;6.0;7.0;7.5;8.0;8.6"
-    else:  # mainly for cuda 10.2
-        return "3.5;5.0+PTX;6.0;7.0;7.5"
+    # mainly for cuda 10.2
+    return "3.5;5.0+PTX;6.0;7.0;7.5"
 
 
 def get_cuda_variables(
