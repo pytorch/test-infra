238fadc0c2ca06ee2785eeb714918fd3a5ad0e5f
PaliC <sahancpal@gmail.com>
2023-05-01 14:26:53 -0700

    update sql

--- torchci/rockset/commons/__sql/individual_test_stats_per_workflow_per_oncall.sql
+++ torchci/rockset/commons/__sql/individual_test_stats_per_workflow_per_oncall.sql
@@ -1,4 +1,4 @@
-WITH t AS (
+WITH filtered_table AS (
   SELECT 
     AVG(
       sum_duration_in_second / occurences
@@ -11,12 +11,22 @@ WITH t AS (
     test_class, 
     test_file, 
     MAX(max_failures) as max_failures, 
-    MAX(max_errors) AS max_errors 
+    MAX(max_errors) AS max_errors, 
+    RTRIM(
+      LTRIM(
+        SPLIT(job_name, '/') [1]
+      )
+    ) as config_job_name, 
+    SUBSTR(
+      job_name, 
+      STRPOS(job_name, '(') + 1, 
+      STRPOS(job_name, ',') - STRPOS(job_name, '(') -1
+    ) AS config_shard_name 
   FROM 
     metrics.aggregated_test_metrics 
-  WHERE
-  workflow_run_attempt = 1
-  AND workflow_id IS NOT NULL
+  WHERE 
+    workflow_run_attempt = 1 
+    AND workflow_id IS NOT NULL 
   GROUP BY 
     workflow_name, 
     job_name, 
@@ -24,6 +34,26 @@ WITH t AS (
     test_file, 
     date, 
     oncalls
+), 
+filtered_with_costs AS (
+  SELECT 
+    t.avg_duration_in_second as avg_duration_in_second, 
+    t.avg_skipped, 
+    t.oncalls, 
+    t.date, 
+    t.workflow_name, 
+    t.job_name, 
+    t.test_class, 
+    t.test_file, 
+    t.max_failures AS max_failures, 
+    t.max_errors AS max_errors, 
+    CASE WHEN p.price IS NULL THEN 0 ELSE CAST(p.price AS float) * t.avg_duration_in_second / 60 / 60 END as estimated_price 
+  FROM 
+    filtered_table as t 
+    LEFT JOIN commons.price_per_config p ON (
+      t.config_job_name = p.job_name 
+      AND t.config_shard_name = p.shard_name
+    )
 ), 
 test_runs AS (
   SELECT 
@@ -35,13 +65,17 @@ test_runs AS (
     t.workflow_name as workflow_type, 
     SUM(t.avg_duration_in_second) AS avg_duration_in_second, 
     SUM(t.avg_skipped) AS avg_skipped, 
+    TRUNC(
+      SUM(t.estimated_price), 
+      2
+    ) AS estimated_price_per_run_in_dollars, 
     MAX(t.max_errors) AS max_errors, 
     MAX(t.max_failures) AS max_failures, 
     t.date as granularity_bucket, 
     t.test_class as test_class, 
     t.test_file as test_file 
   FROM 
-    t, 
+    filtered_with_costs as t, 
     UNNEST(t.oncalls AS oncall) AS t2 
   WHERE 
     DATE_TRUNC(
--- torchci/rockset/commons/__sql/test_time_per_oncall.sql
+++ torchci/rockset/commons/__sql/test_time_per_oncall.sql
@@ -1,4 +1,4 @@
-With avg_table as (
+With filtered_table as (
   SELECT 
     AVG(
       sum_duration_in_second / occurences
@@ -8,7 +8,17 @@ With avg_table as (
     workflow_name, 
     job_name, 
     test_class, 
-    test_file 
+    test_file, 
+    RTRIM(
+      LTRIM(
+        SPLIT(job_name, '/') [1]
+      )
+    ) as config_job_name, 
+    SUBSTR(
+      job_name, 
+      STRPOS(job_name, '(') + 1, 
+      STRPOS(job_name, ',') - STRPOS(job_name, '(') -1
+    ) AS config_shard_name, 
   FROM 
     metrics.aggregated_test_metrics 
   WHERE 
@@ -33,9 +43,9 @@ With avg_table as (
     ) < DATE_TRUNC(
       'DAY', 
       PARSE_DATETIME_ISO8601(: endDate)
-    )
-  AND workflow_id IS NOT NULL
-  AND workflow_run_attempt = 1
+    ) 
+    AND workflow_id IS NOT NULL 
+    AND workflow_run_attempt = 1 
   GROUP BY 
     workflow_name, 
     job_name, 
@@ -44,23 +54,45 @@ With avg_table as (
     date, 
     oncalls
 ), 
+filtered_with_costs as (
+  SELECT 
+    t.avg_duration_in_second as avg_duration_in_second, 
+    t.oncalls as oncalls, 
+    t.date as date, 
+    t.workflow_name as workflow_name, 
+    t.job_name as job_name, 
+    t.test_class as test_class, 
+    t.test_file as test_file, 
+    t.config_job_name as config_job_name, 
+    t.config_shard_name as config_shard_name, 
+    CASE WHEN p.price IS NULL THEN 0 ELSE CAST(p.price AS float) END as price_per_hour 
+  FROM 
+    filtered_table t 
+    LEFT JOIN commons.price_per_config p ON (
+      t.config_job_name = p.job_name 
+      AND t.config_shard_name = p.shard_name
+    )
+), 
 total_table as (
   SELECT 
     date, 
     workflow_name, 
-    SUM(avg_duration_in_second) as total_duration_per_workflow_per_day 
+    SUM(avg_duration_in_second) as total_duration_per_workflow_per_day, 
+    SUM(
+      price_per_hour * avg_duration_in_second / 60 / 60
+    ) as total_price_per_workflow_per_day 
   FROM 
-    avg_table 
+    filtered_with_costs 
   GROUP BY 
     date, 
     workflow_name
 ), 
-avg_and_oncalls as (
+filtered_with_oncalls as (
   SELECT 
     * 
   FROM 
     (
-      avg_table CROSS 
+      filtered_with_costs CROSS 
       JOIN UNNEST(oncalls AS oncall)
     ) 
   WHERE 
@@ -70,49 +102,72 @@ avg_and_oncalls as (
       ''
     ) LIKE : oncall
 ), 
-t as (
+filtered_with_oncalls_and_totals as (
   SELECT 
     avg_duration_in_second, 
     oncall, 
-    avg_and_oncalls.date as date, 
-    avg_and_oncalls.workflow_name as workflow_name, 
+    filtered_with_oncalls.date as date, 
+    filtered_with_oncalls.workflow_name as workflow_name, 
     job_name, 
     test_class, 
     test_file, 
-    total_duration_per_workflow_per_day 
+    total_duration_per_workflow_per_day, 
+    total_price_per_workflow_per_day, 
+    price_per_hour, 
   FROM 
-    avg_and_oncalls 
-    INNER JOIN total_table ON avg_and_oncalls.date = total_table.date 
-    AND avg_and_oncalls.workflow_name = total_table.workflow_name
+    filtered_with_oncalls 
+    INNER JOIN total_table ON filtered_with_oncalls.date = total_table.date 
+    AND filtered_with_oncalls.workflow_name = total_table.workflow_name
+), 
+t as (
+  SELECT 
+    REPLACE(
+      REPLACE(oncall, 'module: ', ''), 
+      'oncall: ', 
+      ''
+    ) as oncall, 
+    workflow_name as workflow_type, 
+    SUM(avg_duration_in_second) as time_in_seconds, 
+    TRUNC(
+      SUM(
+        price_per_hour * avg_duration_in_second / 60 / 60
+      ), 
+      2
+    ) as estimated_price_per_run_in_dollars, 
+    date as granularity_bucket, 
+    TRUNC(
+      SUM(avg_duration_in_second) / ARBITRARY(
+        -- add noise to avoid divide by 0
+        total_duration_per_workflow_per_day + 0.000001
+      )* 100, 
+      2
+    ) as percentage_of_time, 
+    TRUNC(
+      SUM(
+        price_per_hour * avg_duration_in_second / 60 / 60
+      ) / ARBITRARY(
+        -- add noise to avoid divide by 0
+        total_price_per_workflow_per_day + 0.000001
+      )* 100, 
+      2
+    ) as percentage_of_cost, 
+  FROM 
+    filtered_with_oncalls_and_totals as t 
+  WHERE 
+    REPLACE(
+      REPLACE(t.oncall, 'module: ', ''), 
+      'oncall: ', 
+      ''
+    ) LIKE : oncall 
+    AND t.workflow_name LIKE : workflow_type 
+  GROUP BY 
+    t.oncall, 
+    t.date, 
+    t.workflow_name
 ) 
 SELECT 
-  REPLACE(
-    REPLACE(oncall, 'module: ', ''), 
-    'oncall: ', 
-    ''
-  ) as oncall, 
-  workflow_name as workflow_type, 
-  SUM(avg_duration_in_second) as time_in_seconds, 
-  date as granularity_bucket, 
-  TRUNC(
-    SUM(avg_duration_in_second) / ARBITRARY(
-      total_duration_per_workflow_per_day
-    )* 100, 
-    2
-  ) as percentage 
+  *, 
 FROM 
   t 
-WHERE 
-  -- CAST(PARSE_DATETIME_ISO8601(t.date) as date) < PARSE_DATETIME_ISO8601(:endDate) AND
-  REPLACE(
-    REPLACE(t.oncall, 'module: ', ''), 
-    'oncall: ', 
-    ''
-  ) LIKE : oncall 
-  AND t.workflow_name LIKE : workflow_type 
-GROUP BY 
-  t.oncall, 
-  t.date, 
-  t.workflow_name 
 ORDER BY 
   time_in_seconds DESC
