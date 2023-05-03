5456b6ab354ac951b8a0957ce63cff68e582bfbe
atalman <atalman@fb.com>
2023-05-03 15:59:40 -0700

    Pin older MKL version - fix torchaudio x86 compilation

--- tools/pkg-helpers/pytorch_pkg_helpers/macos.py
+++ tools/pkg-helpers/pytorch_pkg_helpers/macos.py
@@ -5,4 +5,7 @@ def get_macos_variables(arch_name: str, python_version: str = "3.8") -> list:
         "export CXX=clang++",
     ]
 
+    if arch_name != "arm64"
+        variables.append("export CONDA_EXTRA_BUILD_CONSTRAINT='- mkl<=2021.4.0'")
+
     return variables
