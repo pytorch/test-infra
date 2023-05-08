f6ba70688d3fcfd6aa47c437c3f21d0d4d4c801c
Jean Schmidt <contato@jschmidt.me>
2023-05-08 21:01:18 +0200

    enables IPV6

--- terraform-aws-github-runner/modules/runners-instances/security-groups.tf
+++ terraform-aws-github-runner/modules/runners-instances/security-groups.tf
@@ -9,10 +9,11 @@ resource "aws_security_group" "runners_sg" {
   vpc_id      = element(var.vpc_ids, count.index).vpc
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
   tags = merge(
     local.tags,
