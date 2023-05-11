f53e3ca7d044486e37c703d4b6eca3a70c2b1b05
Sergii Dymchenko <sdym@fb.com>
2023-05-11 16:58:36 -0700

    Initial flake8-torch version

---
+++ tools/flake8-torch/tests/test_flake8_torch.py
@@ -0,0 +1,32 @@
+import ast
+from pathlib import Path
+from flake8_torch.checker import TorchChecker
+
+FIXTURES_PATH = Path(__file__).absolute().parent / 'fixtures'
+
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+def _fixture_path(filename):
+    return Path(__file__).absolute().parent / 'fixtures' / filename
+
+def _results(s):
+    tree = ast.parse(s)
+    checker = TorchChecker(tree)
+    return [f'{line}:{col} {msg}' for line, col, msg, _ in checker.run()]
+
+def test_empty():
+    assert _results('') == []
+
+def test_fixtures():
+    for source_path in FIXTURES_PATH.glob("*.py"):
+        LOGGER.info(f'Testing {source_path}')
+        expected_path = str(source_path)[:-2] + "txt"
+        expected_results = []
+        with open(expected_path) as expected:
+            for line in expected:
+                expected_results.append(line.rstrip())
+
+        with open(source_path) as source:
+            assert _results(source.read()) == expected_results
