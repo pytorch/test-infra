834da6665833769b16c4e4476e920a0da78c14bb
Sergii Dymchenko <sdym@fb.com>
2023-05-23 13:16:44 -0700

    Replace manual AST traversal with libcst

--- tools/flake8-torch/flake8_torch/checker.py
+++ tools/flake8-torch/flake8_torch/checker.py
@@ -1,89 +1,59 @@
 from pathlib import Path
-import ast
 import yaml
+import libcst as cst
 
 __version__ = "0.0.1"
 
 
-class TorchVisitor(ast.NodeVisitor):
+class TorchVisitor(cst.CSTVisitor):
+
+    METADATA_DEPENDENCIES = (
+        cst.metadata.QualifiedNameProvider,
+        cst.metadata.WhitespaceInclusivePositionProvider,
+    )
+
     def __init__(self):
-        # Map from import alias (may be the same as name) to import name
-        self._import_info = {}
-        self._call_info = []
+        self.call_info = []
 
     def visit_Call(self, node):
-        func_name = ""
-        curr = node.func
-        while isinstance(curr, ast.Attribute):
-            func_name = "." + curr.attr + func_name
-            curr = curr.value
-        if isinstance(curr, ast.Name):
-            func_name = curr.id + func_name
+        name_metadata = list(
+            self.get_metadata(cst.metadata.QualifiedNameProvider, node)
+        )
+        if not name_metadata:
+            return
+        qualified_name = name_metadata[0].name
 
-        self._call_info.append(
-            {"name": func_name, "lineno": node.lineno, "col_offset": node.col_offset}
+        position_metadata = self.get_metadata(
+            cst.metadata.WhitespaceInclusivePositionProvider, node
         )
 
-        self.generic_visit(node)
-
-    def visit_Import(self, node):
-        for alias in node.names:
-            if alias.asname is None:
-                self._import_info[alias.name] = alias.name
-            else:
-                self._import_info[alias.asname] = alias.name
-
-        self.generic_visit(node)
-
-    def visit_ImportFrom(self, node):
-        module = "" if node.module is None else node.module
-        module = "." * node.level + module
-        for alias in node.names:
-            if alias.asname is None:
-                self._import_info[alias.name] = f"{module}.{alias.name}"
-            else:
-                self._import_info[alias.asname] = f"{module}.{alias.name}"
-        self.generic_visit(node)
+        self.call_info.append(
+            (
+                qualified_name,
+                position_metadata.start.line,
+                position_metadata.start.column,
+            )
+        )
 
 
 class TorchChecker:
     name = "flake8-torch"
     version = __version__
 
-    def __init__(self, tree):
-        self.tree = tree
+    # The parameters need to have these exact names.
+    # See https://flake8.pycqa.org/en/latest/plugin-development/plugin-parameters.html
+    # tree is unused but the plugin doesn't work without it.
+    def __init__(self, tree, lines):
         self.deprecated_config = {}
         with open(Path(__file__).absolute().parent / "deprecated_symbols.yaml") as f:
             for item in yaml.load(f, yaml.SafeLoader):
                 self.deprecated_config[item["name"]] = item
 
-        self.func_calls = []
-        self._get_qualified_func_calls()
-
-    def _get_qualified_func_calls(self):
-        """Return list of function calls in the tree.
-
-        Each item in the list is a tuple
-        (qualified_name, lineno, col_offset).
-        """
+        module = cst.parse_module("".join(lines))
         visitor = TorchVisitor()
-        visitor.visit(self.tree)
-
-        func_calls = visitor._call_info
-
-        self.func_calls = []
-        for call_info in func_calls:
-            name = call_info["name"]
-            dotted_parts = name.split(".")
-            if dotted_parts[0] in visitor._import_info:
-                qualified_name = ".".join(
-                    [visitor._import_info[dotted_parts[0]]] + dotted_parts[1:]
-                )
-            else:
-                qualified_name = name
-            self.func_calls.append(
-                (qualified_name, call_info["lineno"], call_info["col_offset"])
-            )
+        self.module = cst.MetadataWrapper(module)
+        self.module.visit(visitor)
+        self.func_calls = visitor.call_info
 
     def _check_deprecated_functions(self):
         for (name, lineno, col_offset) in self.func_calls:
