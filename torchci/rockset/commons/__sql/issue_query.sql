c629d5e9bf7eea89e45d50ad7f866b14fd26242f
Catherine Lee <csl@fb.com>
2023-05-24 15:43:15 -0700

    update

--- torchci/rockset/commons/__sql/issue_query.sql
+++ torchci/rockset/commons/__sql/issue_query.sql
@@ -1,20 +1,13 @@
 SELECT
-  issue.number,
-  issue.title,
-  issue.html_url,
-  issue.state,
-  issue.body,
-  issue.updated_at,
-  issue.author_association,
+    issue.number,
+    issue.title,
+    issue.html_url,
+    issue.state,
+    issue.body,
+    issue.updated_at,
+    issue.author_association,
 FROM
-  issues AS issue CROSS
-  JOIN UNNEST(issue.labels AS label) AS labels
+    issues AS issue
+    CROSS JOIN UNNEST(issue.labels AS label) AS labels
 WHERE
-  labels.label.name = : label
-  AND (
-    ARRAY_CONTAINS(
-      SPLIT(: associations, ','),
-      LOWER(issue.author_association)
-    )
-    OR : associations = ''
-  )
+    labels.label.name =: label
