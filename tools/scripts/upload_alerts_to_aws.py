04dafe1e0ed06bafc33ac65ca0d6976b92346a4d
PaliC <sahancpal@gmail.com>
2023-05-23 20:39:30 -0700

    add upload script

--- tools/scripts/upload_alerts_to_aws.py
+++ tools/scripts/upload_alerts_to_aws.py
@@ -37,6 +37,46 @@ def upload_to_s3(
     )
     print("Done!")
 
+def get_recent_alerts(orgname, reponame):
+    rockset_api_key = os.environ["ROCKSET_API_KEY"]
+    rockset_api_server = "api.rs2.usw2.rockset.com"
+    iso_date = datetime.date.isoformat()
+    rs = rockset.RocksetClient(
+        host="api.usw2a1.rockset.com", api_key=os.environ["ROCKSET_API_KEY"]
+    )
+
+    # Define the name of the Rockset collection and lambda function
+    collection_name = "commons"
+    lambda_function_name = "get_recent_alerts"
+    query_parameters = [
+        rockset.models.QueryParameter(name="repo", type="string", value=reponame),
+        rockset.models.QueryParameter(name="org", type="string", value=orgname),
+    ]
+    api_response = rs.QueryLambdas.execute_query_lambda(
+        query_lambda=lambda_function_name,
+        version="692684fa5b37177f",
+        parameters=query_parameters,
+    )
+    for i in range(len(api_response["results"])):
+        oncalls = get_oncall_from_testfile(api_response["results"][i]["test_file"])
+        api_response["results"][i]["oncalls"] = oncalls
+    return json.loads(
+        json.dumps(api_response["results"], indent=4, sort_keys=True, default=str)
+    )
+
+def merge_alerts(current_alerts, new_alerts):
+    current_alert_keys = set()
+    for alert in current_alerts:
+        key = (alert["AlertObject"], alert["AlertType"])
+        current_alert_keys.add(key)
+    for alert in new_alerts:
+        key = (alert["AlertObject"], alert["AlertType"])
+        if key not in current_alert_keys:
+            alert["closed"] = True
+            current_alerts.append(alert)
+    return current_alerts
+
+
 def append_metadata(json_string, org_name, repo_name, timestamp):
     # Load the JSON string into a Python object
     data = json.loads(json_string)
@@ -57,7 +97,9 @@ if __name__ == '__main__':
     parser.add_argument('--repo', type=str, required=True, help="Repository for alerts")
     args = parser.parse_args()
     timestamp = datetime.datetime.now().isoformat()
-    data = append_metadata(args.alerts, args.org, args.repo, timestamp)
+    new_alerts = append_metadata(args.alerts, args.org, args.repo, timestamp)
+    current_alerts = get_recent_alerts(args.org, args.repo)
+    data = merge_alerts(current_alerts, new_alerts)
     upload_to_s3(        
         bucket_name="torchci-alerts",
         key=f"test_alerts/{str(timestamp)}",
