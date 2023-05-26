baa54621017a890436c0e771d949d88f79e60cfb
Sergii Dymchenko <sdym@fb.com>
2023-05-25 17:59:28 -0700

    Refactor

--- tools/flake8-torch/flake8_torch/checker.py
+++ tools/flake8-torch/flake8_torch/checker.py
@@ -1,19 +1,36 @@
 from pathlib import Path
 import yaml
 import libcst as cst
+from dataclasses import dataclass
+from typing import Optional, List
+
 
 __version__ = "0.0.1"
 
 
-class TorchVisitor(cst.CSTVisitor):
+@dataclass
+class LintViolation:
+    code: str
+    message: str
+    line: int
+    column: int
+    node: cst.CSTNode
+    replacement: Optional[cst.CSTNode]
 
+    def flake8_result(self):
+        full_message = f"{self.code} {self.message}"
+        return (self.line, 1 + self.column, full_message, "TorchChecker")
+
+
+class TorchVisitor(cst.CSTVisitor):
     METADATA_DEPENDENCIES = (
         cst.metadata.QualifiedNameProvider,
         cst.metadata.WhitespaceInclusivePositionProvider,
     )
 
-    def __init__(self):
-        self.call_info = []
+    def __init__(self, deprecated_config=None):
+        self.deprecated_config = {} if deprecated_config is None else deprecated_config
+        self.violations: List[LintViolation] = []
 
     def visit_Call(self, node):
         name_metadata = list(
@@ -23,17 +40,26 @@ class TorchVisitor(cst.CSTVisitor):
             return
         qualified_name = name_metadata[0].name
 
-        position_metadata = self.get_metadata(
-            cst.metadata.WhitespaceInclusivePositionProvider, node
-        )
-
-        self.call_info.append(
-            (
-                qualified_name,
-                position_metadata.start.line,
-                position_metadata.start.column,
+        if qualified_name in self.deprecated_config:
+            position_metadata = self.get_metadata(
+                cst.metadata.WhitespaceInclusivePositionProvider, node
             )
-        )
+            if self.deprecated_config[qualified_name]["remove_pr"] is None:
+                code = "TOR101"
+                message = f"Use of deprecated function {qualified_name}"
+            else:
+                code = "TOR201"
+                message = f"Use of removed function {qualified_name}"
+            self.violations.append(
+                LintViolation(
+                    code=code,
+                    message=message,
+                    line=position_metadata.start.line,
+                    column=position_metadata.start.column,
+                    node=node,
+                    replacement=None,
+                )
+            )
 
 
 class TorchChecker:
@@ -42,27 +68,19 @@ class TorchChecker:
 
     # The parameters need to have these exact names.
     # See https://flake8.pycqa.org/en/latest/plugin-development/plugin-parameters.html
-    # tree is unused but the plugin doesn't work without it.
+    # tree is unused, but the plugin doesn't work without it.
     def __init__(self, tree, lines):
-        self.deprecated_config = {}
+        deprecated_config = {}
         with open(Path(__file__).absolute().parent / "deprecated_symbols.yaml") as f:
             for item in yaml.load(f, yaml.SafeLoader):
-                self.deprecated_config[item["name"]] = item
+                deprecated_config[item["name"]] = item
 
         module = cst.parse_module("".join(lines))
-        visitor = TorchVisitor()
+        visitor = TorchVisitor(deprecated_config)
         self.module = cst.MetadataWrapper(module)
         self.module.visit(visitor)
-        self.func_calls = visitor.call_info
-
-    def _check_deprecated_functions(self):
-        for (name, lineno, col_offset) in self.func_calls:
-            if name in self.deprecated_config:
-                if self.deprecated_config[name]["remove_pr"] is None:
-                    message = f"TOR101 Use of deprecated function {name}"
-                else:
-                    message = f"TOR201 Use of removed function {name}"
-                yield ((lineno, 1 + col_offset, message, TorchChecker))
+        self.violations = visitor.violations
 
     def run(self):
-        yield from self._check_deprecated_functions()
+        for violation in self.violations:
+            yield violation.flake8_result()
