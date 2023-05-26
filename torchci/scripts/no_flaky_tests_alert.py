40faf201a7b5ff8290fecefb5685f3a80f7fe5fc
Catherine Lee <csl@fb.com>
2023-05-26 14:34:33 -0700

    uncommit_later

--- torchci/scripts/no_flaky_tests_alert.py
+++ torchci/scripts/no_flaky_tests_alert.py
@@ -2,14 +2,14 @@ import argparse
 import json
 import os
 from datetime import datetime, timedelta
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 import requests
 from check_alerts import (
     GRAPHQL_URL,
     PYTORCH_REPO_NAME,
-    REPO_OWNER,
     TEST_INFRA_REPO_NAME,
+    REPO_OWNER,
     create_issue,
     fetch_alerts,
     headers,
@@ -18,7 +18,8 @@ from setuptools import distutils  # type: ignore[import]
 
 FLAKY_TESTS_LABEL = "module: flaky-tests"
 NO_FLAKY_TESTS_LABEL = "no-flaky-tests-alert"
-FLAKY_TESTS_SEARCH_PERIOD_DAYS = 14
+FLAKY_TESTS_SEARCH_PERIOD_DAYS = 7
+UPPER_THRESHOLD_PER_DAY = 10
 
 NUM_ISSUES_QUERY = """
 query ($query: String!) {
@@ -29,8 +30,12 @@ query ($query: String!) {
 """
 
 
-def get_num_issues_with_label(owner: str, repo: str, label: str, from_date: str) -> int:
-    query = f'repo:{owner}/{repo} label:"{label}" created:>={from_date} is:issue'
+def num_open_issues_with_label(
+    owner: str, repo: str, label: str, from_date: Optional[str] = None
+) -> int:
+    query = f'repo:{owner}/{repo} label:"{label}" is:issue is:open'
+    if from_date:
+        query += f" created:>={from_date}"
     try:
         r = requests.post(
             GRAPHQL_URL,
@@ -51,7 +56,7 @@ def generate_no_flaky_tests_issue() -> Any:
     ] = f"[Pytorch][Warning] No flaky test issues have been detected in the past {FLAKY_TESTS_SEARCH_PERIOD_DAYS} days!"
     issue["body"] = (
         f"No issues have been filed in the past {FLAKY_TESTS_SEARCH_PERIOD_DAYS} days for "
-        f"the repository {REPO_OWNER}/{TEST_INFRA_REPO_NAME}.\n"
+        f"the repository {REPO_OWNER}/{PYTORCH_REPO_NAME}.\n"
         "This can be an indication that the flaky test bot has stopped filing tests."
     )
     issue["labels"] = [NO_FLAKY_TESTS_LABEL]
@@ -59,36 +64,27 @@ def generate_no_flaky_tests_issue() -> Any:
     return issue
 
 
-def handle_flaky_tests_alert(existing_alerts: List[Dict]) -> Dict:
-    if (
-        not existing_alerts
-        or datetime.fromisoformat(
-            existing_alerts[0]["createdAt"].replace("Z", "+00:00")
-        ).date()
-        != datetime.today().date()
-    ):
-        from_date = (
-            datetime.today() - timedelta(days=FLAKY_TESTS_SEARCH_PERIOD_DAYS)
-        ).strftime("%Y-%m-%d")
-        num_issues_with_flaky_tests_lables = get_num_issues_with_label(
-            REPO_OWNER, PYTORCH_REPO_NAME, FLAKY_TESTS_LABEL, from_date
-        )
-        print(
-            f"Num issues with `{FLAKY_TESTS_LABEL}` label: ",
-            num_issues_with_flaky_tests_lables,
-        )
-        if num_issues_with_flaky_tests_lables == 0:
-            return create_issue(generate_no_flaky_tests_issue(), False)
-
-    print("No new alert for flaky tests bots.")
-    return None
+def handle_flaky_tests_alert(dry_run: bool) -> Dict:
+    from_date = (
+        datetime.today() - timedelta(days=FLAKY_TESTS_SEARCH_PERIOD_DAYS)
+    ).strftime("%Y-%m-%d")
+    num_issues_with_flaky_tests_label = num_open_issues_with_label(
+        REPO_OWNER, PYTORCH_REPO_NAME, FLAKY_TESTS_LABEL, from_date
+    )
+    print(
+        f"Num issues with `{FLAKY_TESTS_LABEL}` label: ",
+        num_issues_with_flaky_tests_label,
+    )
+    if num_issues_with_flaky_tests_label == 0:
+        return create_issue(generate_no_flaky_tests_issue(), dry_run)
 
 
 def check_for_no_flaky_tests_alert(dry_run: bool):
-    existing_no_flaky_tests_alerts = fetch_alerts(
-        labels=[NO_FLAKY_TESTS_LABEL],
+    num_existing_alerts = num_open_issues_with_label(
+        REPO_OWNER, TEST_INFRA_REPO_NAME, NO_FLAKY_TESTS_LABEL
     )
-    handle_flaky_tests_alert(existing_no_flaky_tests_alerts)
+    if num_existing_alerts == 0:
+        handle_flaky_tests_alert(dry_run)
 
 
 def parse_args() -> argparse.Namespace:
