ef087645a1f50509e9033b02b80a827ddf97f797
Sergii Dymchenko <sdym@fb.com>
2023-05-26 18:30:47 -0700

    Replace torch.ger

--- tools/flake8-torch/flake8_torch/checker.py
+++ tools/flake8-torch/flake8_torch/checker.py
@@ -1,16 +1,17 @@
 from pathlib import Path
 import yaml
 import libcst as cst
+import libcst.codemod as codemod
 from dataclasses import dataclass
 from typing import Optional, List
-
+import argparse
 
 __version__ = "0.0.1"
 
 
 @dataclass
 class LintViolation:
-    code: str
+    error_code: str
     message: str
     line: int
     column: int
@@ -18,7 +19,7 @@ class LintViolation:
     replacement: Optional[cst.CSTNode]
 
     def flake8_result(self):
-        full_message = f"{self.code} {self.message}"
+        full_message = f"{self.error_code} {self.message}"
         return (self.line, 1 + self.column, full_message, "TorchChecker")
 
 
@@ -45,21 +46,47 @@ class TorchVisitor(cst.CSTVisitor):
                 cst.metadata.WhitespaceInclusivePositionProvider, node
             )
             if self.deprecated_config[qualified_name]["remove_pr"] is None:
-                code = "TOR101"
+                error_code = "TOR101"
                 message = f"Use of deprecated function {qualified_name}"
             else:
-                code = "TOR201"
+                error_code = "TOR201"
                 message = f"Use of removed function {qualified_name}"
+
+            replacement = None
+            if qualified_name == "torch.ger":
+                replacement = node.with_deep_changes(
+                    old_node=node.func.attr, value="outer"
+                )
+
             self.violations.append(
                 LintViolation(
-                    code=code,
+                    error_code=error_code,
                     message=message,
                     line=position_metadata.start.line,
                     column=position_metadata.start.column,
                     node=node,
-                    replacement=None,
+                    replacement=replacement,
                 )
             )
+
+
+def _read_deprecated_config(path=None):
+    if path is None:
+        path = Path(__file__).absolute().parent / "deprecated_symbols.yaml"
+
+    deprecated_config = {}
+    with open(path) as f:
+        for item in yaml.load(f, yaml.SafeLoader):
+            deprecated_config[item["name"]] = item
+    return deprecated_config
+
+
+def _get_violations(code: str):
+    module = cst.parse_module(code)
+    visitor = TorchVisitor(_read_deprecated_config())
+    module = cst.MetadataWrapper(module)
+    module.visit(visitor)
+    return visitor.violations
 
 
 class TorchChecker:
@@ -70,17 +97,34 @@ class TorchChecker:
     # See https://flake8.pycqa.org/en/latest/plugin-development/plugin-parameters.html
     # tree is unused, but the plugin doesn't work without it.
     def __init__(self, tree, lines):
-        deprecated_config = {}
-        with open(Path(__file__).absolute().parent / "deprecated_symbols.yaml") as f:
-            for item in yaml.load(f, yaml.SafeLoader):
-                deprecated_config[item["name"]] = item
-
-        module = cst.parse_module("".join(lines))
-        visitor = TorchVisitor(deprecated_config)
-        self.module = cst.MetadataWrapper(module)
-        self.module.visit(visitor)
-        self.violations = visitor.violations
+        self.violations = _get_violations("".join(lines))
 
     def run(self):
         for violation in self.violations:
             yield violation.flake8_result()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename")
+    args = parser.parse_args()
+    with open(args.filename) as source:
+        module = cst.parse_module(source.read())
+        visitor = TorchVisitor(_read_deprecated_config())
+        wrapped_module = cst.MetadataWrapper(module)
+        wrapped_module.visit(visitor)
+        violations = visitor.violations
+
+    for violation in violations:
+        if violation.replacement is not None:
+            print(
+                codemod.diff_code(
+                    module.code_for_node(violation.node),
+                    module.code_for_node(violation.replacement),
+                    3,
+                )
+            )
+
+
+if __name__ == "__main__":
+    main()
