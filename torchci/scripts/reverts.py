84a308fd38724b7ea0d4177a880c946882b5209e
clee2000 <44682903+clee2000@users.noreply.github.com>
2023-05-30 14:09:04 -0700

    Fix revert printer (#4041)
    Attempt to make revert printer slightly more robust against weird
    comments by re-parsing the comment. This is probably a bad idea
    
    * also change mention on master to main
    * change sql query to be more general, also put sql query directly into
    the script so its easier to change
    * sort the categories 
    * added comment url to the revert body in trymerge so use that to
    determine which comment caused the revert. This also means you can get
    rid of the "comment but no corresponding commit" section, but it also
    means that it this script won't work on commits before the change to
    trymerge
    
    test plan:
    ran it against the past three weeks and compared against old versions. I
    actually found that the old printer was unable to find some comments and
    incorrectly put the revert into the "not through pytorchbot section"

--- torchci/scripts/reverts.py
+++ torchci/scripts/reverts.py
@@ -1,12 +1,19 @@
+import argparse
 import datetime
-import json
 import os
 import re
+import shlex
+import sys
 from collections import defaultdict
-from typing import Any, Dict, List, Optional, Tuple
+from typing import Dict, List, Optional, Tuple
 
 from rockset import RocksetClient  # type: ignore[import]
-from torchci.scripts.github_analyze import GitCommit, GitRepo  # type: ignore[import]
+
+from torchci.scripts.github_analyze import GitCommit  # type: ignore[import]
+from torchci.scripts.github_analyze import GitRepo
+
+# Should match the contents produced by trymerge on revert
+RE_REVERT_COMMIT_BODY = r"Reverted .* on behalf of .* due to .* \(\[comment\]\((.*)\)\)"
 
 CLASSIFICATIONS = {
     "nosignal": "No Signal",
@@ -15,17 +22,34 @@ CLASSIFICATIONS = {
     "weird": "Weird",
     "ghfirst": "GHFirst",
     "manual": "Not through pytorchbot",
-    "unknown": "Got @pytorchbot revert command, but no corresponding commit",
 }
 
+ROCKSET_REVERT_QUERY = """
+SELECT
+    ic._event_time revert_time,
+    ic.user.login as reverter,
+    ic.body,
+    ic.html_url as comment_url
+FROM
+    commons.issue_comment AS ic
+WHERE
+    REGEXP_LIKE(
+        ic.body,
+        '^ *@pytorch(merge|)bot revert'
+    )
+    AND ic._event_time >= PARSE_TIMESTAMP_ISO8601(:startTime)
+    AND ic._event_time < PARSE_TIMESTAMP_ISO8601(:stopTime)
+    AND ic.user.login != 'pytorch-bot[bot]'
+"""
 
-def find_corresponding_gitlog_commit(
-    pr_num: str, list_of_commits: List[GitCommit]
-) -> Optional[GitCommit]:
-    for i, revert in enumerate(list_of_commits):
-        if revert.title.endswith(f'(#{pr_num})"'):
-            return list_of_commits.pop(i)
-    return None
+
+def parse_body(body: str) -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-c", "--classification")
+    parser.add_argument("-m", "--message")
+
+    return parser.parse_args(shlex.split(" ".join(body.split(" ")[2:])))
 
 
 def format_string_for_markdown_long(
@@ -52,29 +76,42 @@ def get_start_stop_times() -> Tuple[str, str]:
 
 
 def get_rockset_reverts(start_time: str, end_time: str) -> List[Dict[str, str]]:
-    params = [
-        {"name": "startTime", "type": "string", "value": start_time},
-        {"name": "stopTime", "type": "string", "value": end_time},
-    ]
+    params = {
+        "startTime": start_time,
+        "stopTime": end_time,
+    }
     ROCKSET_API_KEY = os.environ.get("ROCKSET_API_KEY")
     if ROCKSET_API_KEY is None:
         raise RuntimeError("ROCKSET_API_KEY not set")
-
-    with open("torchci/rockset/prodVersions.json") as f:
-        prod_versions = json.load(f)
 
     client = RocksetClient(
         api_key=ROCKSET_API_KEY,
         host="https://api.usw2a1.rockset.com",
     )
-    response = client.QueryLambdas.execute_query_lambda(
-        query_lambda="reverted_prs_with_reason",
-        version=prod_versions["commons"]["reverted_prs_with_reason"],
-        workspace="commons",
-        parameters=params,
-    )
+    response = client.sql(ROCKSET_REVERT_QUERY, params=params)
     res: List[Dict[str, str]] = response.results
-    return res
+
+    cleaned_up: List[Dict[str, str]] = []
+    # re-parse message to reduce errors, should really just upload to rockset from pytorchbot or trymerge
+    for revert in res:
+        body = revert["body"].splitlines()[0].strip()
+        try:
+            parse = parse_body(body)
+            if parse.classification is None or parse.message is None:
+                # comment is badly formed
+                continue
+            revert["code"] = parse.classification
+            revert["message"] = parse.message
+            cleaned_up.append(revert)
+        except (Exception, SystemExit) as e:
+            print(
+                f"failed to parse {revert['comment_url']}: {revert['body']}",
+                file=sys.stderr,
+            )
+            print(e, file=sys.stderr)
+            continue
+
+    return cleaned_up
 
 
 def get_gitlog_reverts(start_time: str, end_time: str) -> List[GitCommit]:
@@ -82,7 +119,7 @@ def get_gitlog_reverts(start_time: str, end_time: str) -> List[GitCommit]:
     remote = "origin"
     repo = GitRepo(repo_path, remote)
     all_commits = repo._run_git_log(
-        f"{remote}/master", [f"--since={start_time}", f"--before={end_time}"]
+        f"{remote}/main", [f"--since={start_time}", f"--before={end_time}"]
     )
 
     return [
@@ -101,19 +138,20 @@ def main() -> None:
         str, List[Tuple[Optional[str], Optional[Dict[str, str]]]]
     ] = defaultdict(lambda: [])
 
-    for rockset_revert in rockset_reverts:
-        pr_num_match = re.search(r"/(\d+)\#", rockset_revert["comment_url"])
-        if pr_num_match is None:
-            continue
-        pr_num = pr_num_match.group(1)
-        commit = find_corresponding_gitlog_commit(pr_num, gitlog_reverts)
-        if commit is not None:
-            classification_dict[rockset_revert["code"]].append((commit, rockset_revert))
-        else:
-            classification_dict["unknown"].append((None, rockset_revert))
-
     for gitlog_revert in gitlog_reverts:
-        classification_dict["manual"].append((gitlog_revert, None))
+        comment_match = re.search(
+            RE_REVERT_COMMIT_BODY,
+            gitlog_revert.body,
+        )
+        if comment_match is None:
+            classification_dict["manual"].append((gitlog_revert, None))
+            continue
+        comment_url = comment_match[1]
+        for rockset_revert in rockset_reverts:
+            if rockset_revert["comment_url"] == comment_url:
+                classification_dict[rockset_revert["code"]].append(
+                    (gitlog_revert, rockset_revert)
+                )
 
     filename = f"{start_time.split('T')[0]}.md"
     with open(filename, "w") as f:
@@ -121,7 +159,9 @@ def main() -> None:
         f.write(
             f"# Week of {start_time.split('T')[0]} to {end_time.split('T')[0]} ({num_reverts})\n"
         )
-        for classification, reverts in classification_dict.items():
+        for classification, reverts in sorted(
+            classification_dict.items(), key=lambda x: x[0]
+        ):
             f.write(f"\n### {CLASSIFICATIONS[classification]} ({len(reverts)})\n\n")
             for commit, rockset_result in reverts:
                 f.write(format_string_for_markdown_long(commit, rockset_result))
