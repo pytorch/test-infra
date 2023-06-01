5c56c4a537bbe3f8ec58d84bb18830dc012b5630
Sergii Dymchenko <sdym@fb.com>
2023-05-31 18:11:16 -0700

    UI and tests for codemod

--- tools/flake8-torch/flake8_torch/checker.py
+++ tools/flake8-torch/flake8_torch/checker.py
@@ -5,8 +5,16 @@ import libcst.codemod as codemod
 from dataclasses import dataclass
 from typing import Optional, List
 import argparse
+import contextlib
+import io
+import sys
 
 __version__ = "0.0.1"
+
+CYAN = "\033[96m"
+RED = "\033[31m"
+BOLD = "\033[1m"
+ENDC = "\033[0m"
 
 
 @dataclass
@@ -21,6 +29,13 @@ class LintViolation:
     def flake8_result(self):
         full_message = f"{self.error_code} {self.message}"
         return (self.line, 1 + self.column, full_message, "TorchChecker")
+
+    def codemod_result(self) -> str:
+        fixable = f" [{CYAN}*{ENDC}]" if self.replacement is not None else ""
+        colon = f"{CYAN}:{ENDC}"
+        position = f"{colon}{self.line}{colon}{1 + self.column}{colon}"
+        error_code = f"{RED}{BOLD}{self.error_code}{ENDC}"
+        return f"{position} {error_code}{fixable} {self.message}"
 
 
 class TorchVisitor(cst.CSTVisitor):
@@ -81,49 +96,115 @@ def _read_deprecated_config(path=None):
     return deprecated_config
 
 
-def _get_violations(code: str):
-    module = cst.parse_module(code)
-    visitor = TorchVisitor(_read_deprecated_config())
-    module = cst.MetadataWrapper(module)
-    module.visit(visitor)
-    return visitor.violations
-
-
 class TorchChecker:
     name = "flake8-torch"
     version = __version__
 
     # The parameters need to have these exact names.
     # See https://flake8.pycqa.org/en/latest/plugin-development/plugin-parameters.html
-    # tree is unused, but the plugin doesn't work without it.
+    # `tree` is unused, but the plugin doesn't work without it.
     def __init__(self, tree, lines):
-        self.violations = _get_violations("".join(lines))
+        module = cst.parse_module("".join(lines))
+        visitor = TorchVisitor(_read_deprecated_config())
+        module = cst.MetadataWrapper(module, unsafe_skip_copy=True)
+        module.visit(visitor)
+        self.violations = visitor.violations
 
     def run(self):
         for violation in self.violations:
             yield violation.flake8_result()
 
 
+class TorchCodemod(codemod.Codemod):
+    def transform_module_impl(self, module: cst.Module) -> cst.Module:
+        visitor = TorchVisitor(_read_deprecated_config())
+        fixes_count = 0
+
+        # We use `unsafe_skip_copy`` here not only to save some time, but
+        # because `deep_replace`` is identity-based and will not work on
+        # the original module if the wrapper does a deep copy:
+        # in that case we would need to use `wrapped_module.module`
+        # instead of `module`.
+        wrapped_module = cst.MetadataWrapper(module, unsafe_skip_copy=True)
+        wrapped_module.visit(visitor)
+        new_module = module
+
+        violations = visitor.violations
+        for violation in violations:
+            if violation.replacement is not None:
+                new_module = new_module.deep_replace(
+                    violation.node, violation.replacement
+                )
+                fixes_count += 1
+            try:
+                path = Path(self.context.filename).relative_to(Path.cwd())
+            except ValueError:
+                # Not a subpath of a current dir, use absolute path
+                path = self.context.filename
+            print(f"{path}{violation.codemod_result()}")
+        if fixes_count == 0:
+            raise codemod.SkipFile("No changes")
+        return new_module
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("filename")
-    args = parser.parse_args()
-    with open(args.filename) as source:
-        module = cst.parse_module(source.read())
-        visitor = TorchVisitor(_read_deprecated_config())
-        wrapped_module = cst.MetadataWrapper(module)
-        wrapped_module.visit(visitor)
-        violations = visitor.violations
 
-    for violation in violations:
-        if violation.replacement is not None:
-            print(
-                codemod.diff_code(
-                    module.code_for_node(violation.node),
-                    module.code_for_node(violation.replacement),
-                    3,
-                )
+    parser.add_argument(
+        "path",
+        nargs="+",
+        help=("Path to check/fix. Can be a directory, a file, or multiple of either."),
+    )
+    parser.add_argument(
+        "--fix",
+        action="store_true",
+        help="Fix fixable violations.",
+    )
+    parser.add_argument(
+        "-j",
+        "--jobs",
+        help="Number of jobs to use when processing files. Defaults to number of cores",
+        type=int,
+        default=None,
+    )
+
+    args = parser.parse_args()
+
+    files = codemod.gather_files(args.path)
+    command_instance = TorchCodemod(codemod.CodemodContext())
+    DIFF_CONTEXT = 5
+    try:
+        # Silence "Failed to determine module name"
+        # https://github.com/Instagram/LibCST/issues/944
+        with contextlib.redirect_stderr(io.StringIO()):
+            result = codemod.parallel_exec_transform_with_prettyprint(
+                command_instance,
+                files,
+                jobs=args.jobs,
+                unified_diff=(None if args.fix else DIFF_CONTEXT),
+                hide_progress=True,
+                format_code=False,
+                repo_root=None,
             )
+    except KeyboardInterrupt:
+        print("Interrupted!", file=sys.stderr)
+        return 2
+
+    print(
+        f"Finished checking {result.successes + result.skips + result.failures} files.",
+        file=sys.stderr,
+    )
+
+    if result.successes > 0:
+        if args.fix:
+            print(f"Transformed {result.successes} files successfully.")
+        else:
+            print(
+                f"[{CYAN}*{ENDC}] {result.successes} "
+                "potentially fixable with the --fix option"
+            )
+
+    return 1 if result.failures > 0 else 0
 
 
 if __name__ == "__main__":
