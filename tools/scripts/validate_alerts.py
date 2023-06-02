74bede79c34990cd3efd714408c5dd2fc25a658a
PaliC <sahancpal@gmail.com>
2023-06-02 15:43:03 -0400

    patch

--- tools/scripts/validate_alerts.py
+++ tools/scripts/validate_alerts.py
@@ -21,7 +21,7 @@ def validate_json(json_string):
         json_object = json.loads(json_string)
         print("The input string is a valid JSON.")
     except ValueError as e:
-        raise ValueError(f"The input string is not a valid JSON: Error: {e}\n\n The input string is {json_string}")
+        raise ValueError(f"The input string is not a valid JSON: Error: {e}")
 def validate_schema(json_string):
     json_object = json.loads(json_string)
     jsonschema.validate(instance=json_object, schema=ALERT_SCHEMA)
@@ -31,7 +31,7 @@ def main():
     parser = argparse.ArgumentParser(description="Validate json string containing alerts")
     parser.add_argument('--alerts', type=str, required=True, help="JSON string to validate.")
     args = parser.parse_args()
-    print(f"the alerts are {args.alerts}")
+    print(args.alerts)
     validate_json(args.alerts)
     validate_schema(args.alerts)
 
