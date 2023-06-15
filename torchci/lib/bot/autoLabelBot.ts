4d8ce2b5a836e6e31f1df83b44deb40c5f9f3aae
Jane (Yuan) Xu <31798555+janeyx99@users.noreply.github.com>
2023-06-15 09:26:28 -0400

    [Autolabel] Add optim release notes label
    I just created the optim release notes label for all things optimizer related. Have the bot use it.

--- torchci/lib/bot/autoLabelBot.ts
+++ torchci/lib/bot/autoLabelBot.ts
@@ -58,7 +58,7 @@ const filenameRegexToReleaseCategory: [RegExp, string][] = [
   // nn_frontend => also did not exist
   [/test\/test_nn.py/gi, "release notes: nn"],
   [/test\/test_module.py/gi, "release notes: nn"],
-  [/torch\/optim/gi, "release notes: nn"],
+  [/torch\/optim/gi, "release notes: optim"],
   [/tools\/nn\/modules/gi, "release notes: nn"],
   [/tools\/nn\/functional.py/gi, "release notes: nn"],
   // jit
