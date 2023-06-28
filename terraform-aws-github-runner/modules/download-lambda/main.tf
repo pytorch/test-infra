af50196d08a1e64a6af5fdedf227170c4fdee02c
Jean Schmidt <contato@jschmidt.me>
2023-06-28 15:24:46 +0200

    upgrading terrafor version

--- terraform-aws-github-runner/modules/download-lambda/main.tf
+++ terraform-aws-github-runner/modules/download-lambda/main.tf
@@ -1,7 +1,10 @@
 terraform {
   required_version = ">= 1.5"
   required_providers {
-    null = "~> 3.2.1"
+    null = {
+      source  = "hashicorp/terraform-provider-null"
+      version = "~> 3.2.1"
+    }
   }
 }
 
