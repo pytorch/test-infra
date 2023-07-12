49e0aed401a64e9d910763373901986b74905484
Omkar Salpekar <osalpekar@fb.com>
2023-07-12 16:08:23 -0700

    linting

--- tools/tests/test_generate_binary_build_matrix.py
+++ tools/tests/test_generate_binary_build_matrix.py
@@ -7,23 +7,20 @@ from tools.scripts.generate_binary_build_matrix import generate_build_matrix
 
 ASSETS_DIR = "tools/tests/assets"
 
+
 class GenerateBuildMatrixTest(TestCase):
     def matrix_compare_helper(
-            self,
+        self, package_type, operating_system, cuda, reference_output_file
+    ):
+        out = generate_build_matrix(
             package_type,
             operating_system,
-            cuda,
-            reference_output_file):
-        out = generate_build_matrix(
-                package_type,
-                operating_system,
-                "nightly",
-                "enable" if cuda else "disable",
-                "false",
+            "nightly",
+            "enable" if cuda else "disable",
+            "false",
         )
 
-        expected_json_filename = os.path.join(
-            ASSETS_DIR, reference_output_file)
+        expected_json_filename = os.path.join(ASSETS_DIR, reference_output_file)
 
         with open(expected_json_filename) as f:
             expected = json.load(f)
@@ -36,39 +33,49 @@ class GenerateBuildMatrixTest(TestCase):
             package_type="wheel",
             operating_system="linux",
             cuda=True,
-            reference_output_file="build_matrix_linux_wheel_cuda.json")
+            reference_output_file="build_matrix_linux_wheel_cuda.json",
+        )
 
     def test_linux_conda_cuda(self):
         self.matrix_compare_helper(
             package_type="conda",
             operating_system="linux",
             cuda=True,
-            reference_output_file="build_matrix_linux_conda_cuda.json")
+            reference_output_file="build_matrix_linux_conda_cuda.json",
+        )
 
     def test_macos_wheel(self):
         self.matrix_compare_helper(
             package_type="wheel",
             operating_system="macos",
             cuda=False,
-            reference_output_file="build_matrix_macos_wheel.json")
+            reference_output_file="build_matrix_macos_wheel.json",
+        )
 
     def test_macos_conda(self):
         self.matrix_compare_helper(
             package_type="conda",
             operating_system="macos",
             cuda=False,
-            reference_output_file="build_matrix_macos_conda.json")
+            reference_output_file="build_matrix_macos_conda.json",
+        )
 
     def test_windows_wheel_cuda(self):
         self.matrix_compare_helper(
             package_type="wheel",
             operating_system="windows",
             cuda=True,
-            reference_output_file="build_matrix_windows_wheel_cuda.json")
+            reference_output_file="build_matrix_windows_wheel_cuda.json",
+        )
 
     def test_windows_conda_cuda(self):
         self.matrix_compare_helper(
             package_type="conda",
             operating_system="windows",
             cuda=True,
-            reference_output_file="build_matrix_windows_conda_cuda.json")
+            reference_output_file="build_matrix_windows_conda_cuda.json",
+        )
+
+
+if __name__ == "__main__":
+    main()
