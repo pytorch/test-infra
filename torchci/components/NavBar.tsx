416fe3ea9a31a6b6604cd1af5cc3341259d3c278
Catherine Lee <csl@fb.com>
2023-07-31 13:04:29 -0700

    update

--- torchci/components/NavBar.tsx
+++ torchci/components/NavBar.tsx
@@ -2,126 +2,155 @@ import styles from "components/NavBar.module.css";
 import Link from "next/link";
 import { AiFillGithub } from "react-icons/ai";
 import LoginSection from "./LoginSection";
-function NavBar() {
+import { useState } from "react";
+
+const NavBarDropdown = ({
+  title,
+  items,
+}: {
+  title: string;
+  items: any;
+}): JSX.Element => {
+  const [dropdown, setDropdown] = useState(false);
+  const dropdownStyle = dropdown ? { display: "block" } : {};
+
   return (
-    <div className={styles.linksContainer}>
-      <div className={styles.links}>
-        <div>
-          <ul className={styles.menu}>
-            <span className={styles.homeLink}>
-              <Link prefetch={false} href="/">
-                PyTorch CI HUD
-              </Link>
-            </span>
-            <li>
-              <Link prefetch={false} href="/minihud">
-                MiniHUD
-              </Link>
-            </li>
-            <li>
-              <Link prefetch={false} href="/hud/pytorch/pytorch/main">
-                Main
-              </Link>
-            </li>
-            <li>
-              <Link prefetch={false} href="/hud/pytorch/pytorch/nightly">
-                Nightly
-              </Link>
-            </li>
-            <li>
-              <Link prefetch={false} href="/hud/pytorch/vision/main">
-                TorchVision
-              </Link>
-            </li>
-            <li>
-              <Link prefetch={false} href="/hud/pytorch/audio/main">
-                TorchAudio
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div
-          style={{
-            display: "inline",
-            marginLeft: "auto",
-            marginRight: "0px",
-            whiteSpace: "nowrap",
-          }}
-        >
-          <ul style={{ marginBottom: "0" }} className={styles.menu}>
-            <li>
-              <Link href="https://github.com/pytorch/pytorch/wiki/Using-hud.pytorch.org">
-                Help
-              </Link>
-            </li>
-            <li>
-              <Link href="https://github.com/pytorch/test-infra/issues/new?assignees=&labels=&template=feature_request.yaml&title=%5Bfeature%5D%3A+">
-                Requests
-              </Link>
-            </li>
-            <li>
-              <Link prefetch={false} href="/metrics">
-                Metrics
-              </Link>
-            </li>
-            <li>
-              <Link prefetch={false} href="/kpis">
-                KPIs
-              </Link>
-            </li>
-            <li>
-              <Link prefetch={false} href="/sli">
-                SLIs
-              </Link>
-            </li>
-            <li>
-              <Link prefetch={false} href="/tts">
-                TTS
-              </Link>
-            </li>
-            <li>
-              <Link prefetch={false} href="/nightlies">
-                Nightly
-              </Link>
-            </li>
-            <li>
-              <Link prefetch={false} href="/reliability">
-                Failures Metric
-              </Link>
-            </li>
-            {/* uncomment after some eyeballs are on this */}
-            {/* <li>
-              <Link prefetch={false} href="/testing_overhead">
-                Testing Overhead
-              </Link>
-            </li> */}
-            <li>
-              <Link prefetch={false} href="/failedjobs/pytorch/pytorch/main">
-                Failures Classifier
-              </Link>
-            </li>
-            <li>
-              <Link prefetch={false} href="/benchmark/compilers">
-                TorchInductor
-              </Link>
-            </li>
-            <li>
-              <span style={{ cursor: "pointer" }}>
-                <Link
-                  href="https://github.com/pytorch/test-infra/tree/main/torchci"
-                  passHref
-                >
-                  <a style={{ color: "black" }}>
-                    <AiFillGithub />
-                  </a>
-                </Link>
-              </span>
-            </li>
-            <li>
-              <LoginSection />
-            </li>
-          </ul>
-        </div>
+    <li
+      onMouseEnter={() => setDropdown(true)}
+      onMouseLeave={() => setDropdown(false)}
+      style={{ padding: 0 }}
+    >
+      <div className={styles.dropdowntitle}>{title} ▾</div>
+      <ul className={styles.dropdown} style={dropdownStyle}>
+        {items.map((submenu: any) => (
+          <li>
+            <Link href={submenu.href} prefetch={false}>
+              {submenu.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </li>
+  );
+};
+
+function NavBar() {
+  const devInfraDropdown = [
+    {
+      name: "KPIs",
+      href: "/kpis",
+    },
+    {
+      name: "SLIs",
+      href: "/sli",
+    },
+    {
+      name: "TTS",
+      href: "/tts",
+    },
+    {
+      name: "Nightlies",
+      href: "/nightlies",
+    },
+    {
+      name: "Failures Metric",
+      href: "/reliability",
+    },
+    {
+      name: "Failures Classifier",
+      href: "/failedjobs/pytorch/pytorch/main",
+    },
+    // uncomment after some eyeballs are on this
+    // {
+    //   name: "Testing Overhead",
+    //   href: "/testingoverhead"
+    // }
+  ];
+
+  const otherReposDropdown = [
+    {
+      name: "TorchVision",
+      href: "/hud/pytorch/vision/main"
+    },
+    {
+      name: "TorchAudio",
+      href: "/hud/pytorch/audio/main"
+    }
+  ]
+
+  return (
+    <div className={styles.navbar}>
+      <div>
+        <ul className={styles.navbarlinkslist}>
+          <li className={styles.homeLink}>
+            <Link prefetch={false} href="/">
+              PyTorch CI HUD
+            </Link>
+          </li>
+          <li>
+            <Link prefetch={false} href="/minihud">
+              MiniHUD
+            </Link>
+          </li>
+          <li>
+            <Link prefetch={false} href="/hud/pytorch/pytorch/nightly">
+              Nightly
+            </Link>
+          </li>
+          <li>
+            <Link prefetch={false} href="/hud/pytorch/vision/main">
+              TorchVision
+            </Link>
+          </li>
+          <li>
+            <Link prefetch={false} href="/hud/pytorch/audio/main">
+              TorchAudio
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div
+        style={{
+          marginLeft: "auto",
+          marginRight: "0px",
+        }}
+      >
+        <ul className={styles.navbarlinkslist}>
+          <li>
+            <Link href="https://github.com/pytorch/pytorch/wiki/Using-hud.pytorch.org">
+              Help
+            </Link>
+          </li>
+          <li>
+            <Link href="https://github.com/pytorch/test-infra/issues/new?assignees=&labels=&template=feature_request.yaml&title=%5Bfeature%5D%3A+">
+              Requests
+            </Link>
+          </li>
+          <li>
+            <Link prefetch={false} href="/metrics">
+              Metrics
+            </Link>
+          </li>
+          <li>
+            <Link prefetch={false} href="/benchmark/compilers">
+              TorchInductor
+            </Link>
+          </li>
+          <NavBarDropdown title="Dev Infra" items={devInfraDropdown} />
+          <li style={{ cursor: "pointer" }}>
+            <Link
+              href="https://github.com/pytorch/test-infra/tree/main/torchci"
+              passHref
+            >
+              <a style={{ color: "black" }}>
+                <AiFillGithub />
+              </a>
+            </Link>
+          </li>
+          <li style={{ padding: "0 1rem" }}>
+            <LoginSection></LoginSection>
+          </li>
+        </ul>
       </div>
     </div>
   );
