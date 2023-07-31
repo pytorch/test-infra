ce49cbaf526430e91b1c75b3c447d069b6f1caa8
Catherine Lee <csl@fb.com>
2023-07-31 14:38:15 -0700

    udpate

--- .github/scripts/get_merge_base_info.py
+++ .github/scripts/get_merge_base_info.py
@@ -2,11 +2,11 @@ from typing import Dict, Any, List
 import datetime
 import subprocess
 from pathlib import Path
-from utils import query_rockset, upload_to_rockset
 import sys
 
 REPO_ROOT = Path(__file__).resolve().parent.parent.parent
 sys.path.insert(0, REPO_ROOT)
+from utils import query_rockset, upload_to_rockset
 
 FAILED_TEST_SHAS_QUERY = """
 SELECT
