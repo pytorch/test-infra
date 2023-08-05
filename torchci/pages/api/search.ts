bb4956f899f299db84c294a9be721036c8033fe0
Huy Do <huydhn@gmail.com>
2023-08-04 23:36:19 -0700

    Add an API endpoint to search for failed jobs with the similar failure (#4450)
    The failures are indexed, which allows fast full test search. The
    initial use case is to find failed jobs with the similar failure across
    the board during a period of time.
    
    ### Testing
    
    *
    https://torchci-git-fork-huydhn-search-similar-failures-fbopensource.vercel.app/api/search?failure=torchbenchmark.models.dlrm
    *
    https://torchci-git-fork-huydhn-search-similar-failures-fbopensource.vercel.app/api/search?failure=torchbenchmark.models.dlrm&startDate=2023-08-02T00:00:00Z&endDate=2023-08-03T00:00:00Z

---
+++ torchci/pages/api/search.ts
@@ -0,0 +1,50 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Client } from "@opensearch-project/opensearch";
+import { AwsSigv4Signer } from "@opensearch-project/opensearch/aws";
+import { defaultProvider } from "@aws-sdk/credential-provider-node";
+import { JobData } from "lib/types";
+import { searchSimilarFailures } from "lib/searchUtils";
+import { Credentials } from "@aws-sdk/types";
+
+const WORKFLOW_JOB_INDEX = "torchci-workflow-job";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<{ jobs: JobData[] }>
+) {
+  const failure = req.query.failure as string;
+  const index = (req.query.index as string) ?? WORKFLOW_JOB_INDEX;
+  const startDate = req.query.startDate as string;
+  const endDate = req.query.endDate as string;
+  // This is a weird way to satisfy TS2352
+  const minScore = (req.query.minScore as unknown as number) ?? 1.0;
+
+  // https://opensearch.org/docs/latest/clients/javascript/index
+  const client = new Client({
+    ...AwsSigv4Signer({
+      region: process.env.OPENSEARCH_REGION as string,
+      service: "es",
+      getCredentials: () => {
+        const credentials: Credentials = {
+          accessKeyId: process.env.OUR_AWS_ACCESS_KEY_ID as string,
+          secretAccessKey: process.env.OUR_AWS_SECRET_ACCESS_KEY as string,
+        };
+        return Promise.resolve(credentials);
+      },
+    }),
+    node: process.env.OPENSEARCH_ENDPOINT,
+  });
+
+  res
+    .status(200)
+    .json(
+      await searchSimilarFailures(
+        client,
+        failure,
+        index,
+        startDate,
+        endDate,
+        minScore
+      )
+    );
+}
