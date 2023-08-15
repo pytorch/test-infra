b64f1b9e20e5a20b72ee255a8af827df74e1dff9
Huy Do <huydhn@gmail.com>
2023-08-14 20:16:51 -0700

    Debug deployment

--- torchci/jest.config.js
+++ torchci/jest.config.js
@@ -7,11 +7,11 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/"],
   reporters: ["<rootDir>/test/reporter.js"],
-  moduleNameMapper: {
-    // A bug in jest config https://github.com/opensearch-project/opensearch-js/issues/410
-    "@opensearch-project/opensearch/aws":
-      "@opensearch-project/opensearch/lib/aws/index.js",
-  },
+  //moduleNameMapper: {
+  //  // A bug in jest config https://github.com/opensearch-project/opensearch-js/issues/410
+  //  "@opensearch-project/opensearch/aws":
+  //    "@opensearch-project/opensearch/lib/aws/index.js",
+  //},
 };
 
 module.exports = createJestConfig(customJestConfig);
