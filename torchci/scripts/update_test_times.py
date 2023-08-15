d0d78d5aefc05b5e36c59d7c2fa8b5f11ffb1e50
Catherine Lee <csl@fb.com>
2023-08-15 10:03:27 -0700

    add tests

--- torchci/scripts/update_test_times.py
+++ torchci/scripts/update_test_times.py
@@ -9,7 +9,7 @@ REPO_ROOT = Path(__file__).resolve().parent.parent.parent
 PROD_VERSIONS_FILE = REPO_ROOT / "torchci" / "rockset" / "prodVersions.json"
 
 
-def main() -> None:
+def get_data_from_rockset():
     rockset_client = rockset.RocksetClient(
         host="api.usw2a1.rockset.com", api_key=os.environ["ROCKSET_API_KEY"]
     )
@@ -26,11 +26,14 @@ def main() -> None:
         version=prod_versions["commons"]["test_time_per_file_periodic_jobs"],
         workspace="commons",
     ).results
+    return rockset_result + periodic_rockset_result
 
+
+def gen_test_times(rockset_results):
     test_times = defaultdict(lambda: defaultdict(lambda: defaultdict(float)))
     test_times_no_build_env = defaultdict(lambda: defaultdict(list))
     test_times_no_test_config = defaultdict(list)
-    for row in rockset_result + periodic_rockset_result:
+    for row in rockset_results:
         test_times[row["base_name"]][row["test_config"]][row["file"]] = row["time"]
         test_times_no_build_env[row["test_config"]][row["file"]].append(row["time"])
         test_times_no_test_config[row["file"]].append(row["time"])
@@ -45,7 +48,12 @@ def main() -> None:
     if "default" not in test_times:
         test_times["default"] = test_times_no_build_env
     if "default" not in test_times["default"]:
-        test_times["default"] = test_times_no_test_config
+        test_times["default"]["default"] = test_times_no_test_config
+    return test_times
+
+
+def main() -> None:
+    test_times = gen_test_times(get_data_from_rockset())
 
     with open("test-times.json", "w") as f:
         f.write(json.dumps(test_times, indent=2, sort_keys=True))
