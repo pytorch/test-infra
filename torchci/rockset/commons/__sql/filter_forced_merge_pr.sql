79959476c7c278ce1c83aad54415925466cd3522
Huy Do <huydhn@gmail.com>
2023-08-16 10:35:51 -0700

    Update filter_forced_merge_pr to be in sync with weekly_force_merge_stats

--- torchci/rockset/commons/__sql/filter_forced_merge_pr.sql
+++ torchci/rockset/commons/__sql/filter_forced_merge_pr.sql
@@ -1,7 +1,12 @@
+-- This query is used by fetchHud to get the force merge status of the pull requests so that
+-- they can be highlighted on HUD. Specifically, force merges with failures are highlighted
+-- with a darker shade of orange while regular force merges due to impatience are marked with
+--- yellow. The logic needs to be in sync with weekly_force_merge_stats query.
 WITH all_merges AS (
   SELECT
     skip_mandatory_checks,
     LENGTH(failed_checks) AS failed_checks_count,
+    LENGTH(ignore_current_checks) AS ignored_checks_count,
     ignore_current,
     is_failed,
     pr_num,
@@ -23,11 +28,13 @@ force_merges_with_failed_checks AS (
       OR (
         ignore_current = true
         AND is_failed = false
-      ),
+        AND ignored_checks_count > 0 -- if no checks were ignored, it's not a force merge
+        ),
       1,
       0
     ) AS force_merge,
     failed_checks_count,
+    ignored_checks_count,
     pr_num,
     merge_commit_sha,
   FROM
@@ -40,7 +47,10 @@ SELECT
   IF(
     (
       force_merge = 1
-      AND failed_checks_count > 0
+      AND (
+        failed_checks_count > 0
+        OR ignored_checks_count > 0
+      )
     ),
     1,
     0
