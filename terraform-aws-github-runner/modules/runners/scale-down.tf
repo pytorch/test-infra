16426d0c40472b43b0f7ca221e9c1774b37f9b46
Jean Schmidt <contato@jschmidt.me>
2023-08-17 15:39:25 +0200

    Sort and distinct entries for automatically discovered environment variables

--- terraform-aws-github-runner/modules/runners/scale-down.tf
+++ terraform-aws-github-runner/modules/runners/scale-down.tf
@@ -44,16 +44,16 @@ resource "aws_lambda_function" "scale_down" {
       REDIS_LOGIN                     = var.redis_login
       SCALE_DOWN_CONFIG               = jsonencode(var.idle_config)
       SECRETSMANAGER_SECRETS_ID       = var.secretsmanager_secrets_id
-      AWS_REGIONS_TO_VPC_IDS = sort(distinct(join(
+      AWS_REGIONS_TO_VPC_IDS = join(
         ",",
-        [
+        sort(distinct([
           for region_vpc in var.vpc_ids :
           format("%s|%s", region_vpc.region, region_vpc.vpc)
-        ]
-      )))
-      VPC_ID_TO_SECURITY_GROUP_IDS = sort(distinct(join(
+        ]))
+      )
+      VPC_ID_TO_SECURITY_GROUP_IDS = join(
         ",",
-        concat(
+        sort(distinct(concat(
           [
             for vpc in var.vpc_ids :
             format(
@@ -66,22 +66,22 @@ resource "aws_lambda_function" "scale_down" {
             for vpc_subnet in var.vpc_sgs :
             format("%s|%s", vpc_subnet.vpc, vpc_subnet.sg)
           ]
-        )
-      )))
-      VPC_ID_TO_SUBNET_IDS = sort(distinct(join(
+        )))
+      )
+      VPC_ID_TO_SUBNET_IDS = join(
         ",",
-        [
+        sort(distinct([
           for vpc_subnet in var.subnet_vpc_ids :
           format("%s|%s", vpc_subnet.vpc, vpc_subnet.subnet)
-        ]
-      )))
-      SUBNET_ID_TO_AZ = sort(distinct(join(
+        ]))
+      )
+      SUBNET_ID_TO_AZ = join(
         ",",
-        [
+        sort(distinct([
           for subnet_az in var.subnet_azs :
           format("%s|%s", subnet_az.subnet, subnet_az.az)
-        ]
-      )))
+        ]))
+      )
     }
   }
 
