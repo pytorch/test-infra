49b4a5bda41d374bfb949aa85ad050095a5de167
PaliC <sahancpal@gmail.com>
2023-08-23 10:17:15 -0700

    add paths to lintrunner ci

--- tools/lintrunner/do_release.sh
+++ tools/lintrunner/do_release.sh
@@ -2,6 +2,6 @@ set -eux
 git cliff --tag "$1" > CHANGELOG.md
 sed -i "s/^version.*/version = \"$1\"/" Cargo.toml
 git commit -am "chore(release): prep for $1"
-git tag "v$1"
+git tag "lintrunner/v$1"
 git push
-git push origin "v$1"
+git push origin "lintrunner/v$1"
