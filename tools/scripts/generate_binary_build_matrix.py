3d1d3c80774e7f38dd15affe79393a9b537b0f70
atalman <atalman@fb.com>
2023-09-20 14:30:34 -0700

    test

--- tools/scripts/generate_binary_build_matrix.py
+++ tools/scripts/generate_binary_build_matrix.py
@@ -131,7 +131,7 @@ def initialize_globals(channel: str):
             gpu_arch: f"pytorch/conda-builder:cuda{gpu_arch}"
             for gpu_arch in mod.CUDA_ARCHES
         },
-        "cpu": "pytorch/conda-builder:cpu:6f4536ed4a8e901e611b9276866f4af369e8781ef0b0b9611a13c8586c5bee5e",
+        "cpu": "pytorch/conda-builder:cpu-941be28cb5c686dc41b7ea8681701e64192c3002",
     }
     mod.LIBTORCH_CONTAINER_IMAGES: Dict[Tuple[str, str], str] = {
         **{
