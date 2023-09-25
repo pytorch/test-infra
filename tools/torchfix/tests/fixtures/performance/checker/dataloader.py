33816bc988354b3c9e31c1b2e7393a14fba67e86
Sergii Dymchenko <sdym@fb.com>
2023-09-25 15:32:30 -0600

    [TorchFix] Add test for non-static SynchronizedDataLoader case (#4586)
    Verify that TorchSynchronizedDataLoaderVisitor doesn't create noise when
    it's not possible to determine statically.
    
    Also don't run Test tools / linux-job and lintrunner on TorchFix PRs.

--- tools/torchfix/tests/fixtures/performance/checker/dataloader.py
+++ tools/torchfix/tests/fixtures/performance/checker/dataloader.py
@@ -5,3 +5,10 @@ async_dataloader = torch.utils.data.DataLoader(dataset, batch_size=10, num_worke
 sync_again_dataloader = torch.utils.data.DataLoader(dataset, batch_size=10, num_workers=0)
 sync3_dataloader = torch.utils.data.DataLoader(dataset, 10, False, None, None, 0)
 async2_dataloader = torch.utils.data.DataLoader(dataset, 10, False, None, None, 2)
+
+# For these we can't determine statically.
+# The checker should be silent on these to reduce noise.
+num_workers = 0
+sync_dyn_dataloader = torch.utils.data.DataLoader(dataset, batch_size=10, num_workers=num_workers)
+num_workers = 4
+async_dyn_dataloader = torch.utils.data.DataLoader(dataset, batch_size=10, num_workers=num_workers)
