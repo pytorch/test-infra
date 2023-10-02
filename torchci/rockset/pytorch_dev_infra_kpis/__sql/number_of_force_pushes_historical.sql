e70e652719b26af0a29c364115f6d62b75fba2de
Zain Rizvi <ZainR@meta.com>
2023-10-02 11:53:11 -0500

    Update kpi queries to use created time instead of rockset injestion time

--- torchci/rockset/pytorch_dev_infra_kpis/__sql/number_of_force_pushes_historical.sql
+++ torchci/rockset/pytorch_dev_infra_kpis/__sql/number_of_force_pushes_historical.sql
@@ -5,8 +5,8 @@ FROM
     commons.issue_comment
 WHERE
     issue_comment.body LIKE '%@pytorchbot merge -f%'
-    AND _event_time >= PARSE_DATETIME_ISO8601(:startTime)
-    AND _event_time < PARSE_DATETIME_ISO8601(:stopTime)
+    AND created >= PARSE_DATETIME_ISO8601(:startTime)
+    AND created < PARSE_DATETIME_ISO8601(:stopTime)
     AND issue_comment.user.login NOT LIKE '%pytorch-bot%'
     AND issue_comment.user.login NOT LIKE '%facebook-github-bot%'
     AND issue_comment.user.login NOT LIKE '%pytorchmergebot%'
