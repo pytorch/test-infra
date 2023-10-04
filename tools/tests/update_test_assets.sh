3c4ae41a214fb0898abc4846556d57681429d37f
atalman <atalman@fb.com>
2023-10-04 12:26:23 -0700

    test

--- tools/tests/update_test_assets.sh
+++ tools/tests/update_test_assets.sh
@@ -4,7 +4,7 @@ python ../scripts/generate_binary_build_matrix.py --with-rocm disable > ../tests
 python ../scripts/generate_binary_build_matrix.py --with-cpu disable > ../tests/assets/build_matrix_linux_wheel_nocpu.json
 python ../scripts/generate_binary_build_matrix.py --package-type conda > ../tests/assets/build_matrix_linux_conda_cuda.json
 python ../scripts/generate_binary_build_matrix.py --package-type conda > ../tests/assets/build_matrix_linux_conda_cuda.json
-python ../scripts/generate_binary_build_matrix.py --operating-system="macos" > ../tests/assets/build_matrix_macos_wheel.json
+python ../scripts/generate_binary_build_matrix.py --operating-system="macos" --with-cuda disable --with-rocm disable > ../tests/assets/build_matrix_macos_wheel.json
 python ../scripts/generate_binary_build_matrix.py --operating-system="macos" --package-type conda  > ../tests/assets/build_matrix_macos_conda.json
 python ../scripts/generate_binary_build_matrix.py --operating-system="windows" --package-type conda > ../tests/assets/build_matrix_windows_conda_cuda.json
 python ../scripts/generate_binary_build_matrix.py --operating-system="windows" > ../tests/assets/build_matrix_windows_wheel_cuda.json
