3584db0f48bc629f631888566dfd7a8144a87fbf
Sergii Dymchenko <sdym@fb.com>
2023-10-03 17:37:29 -0700

    Make pretrained rule work when a model imported from a submodule

--- tools/torchfix/torchfix/visitors/vision/__init__.py
+++ tools/torchfix/torchfix/visitors/vision/__init__.py
@@ -150,6 +150,27 @@ class TorchVisionDeprecatedPretrainedVisitor(TorchVisitor):
     }
     # fmt: on
 
+    # The same model can be imported from torchvision.models directly,
+    # or from a submodule like torchvision.models.resnet.
+    MODEL_SUBMODULES = (
+        "alexnet",
+        "convnext",
+        "densenet",
+        "efficientnet",
+        "googlenet",
+        "inception",
+        "mnasnet",
+        "mobilenet",
+        "regnet",
+        "resnet",
+        "shufflenetv2",
+        "squeezenet",
+        "vgg",
+        "vision_transformer",
+        "swin_transformer",
+        "maxvit",
+    )
+
     def visit_Call(self, node):
         def _new_arg_and_import(
             old_arg: cst.Arg, is_backbone: bool
@@ -185,6 +206,9 @@ class TorchVisionDeprecatedPretrainedVisitor(TorchVisitor):
             return
         if qualified_name.startswith("torchvision.models"):
             model_name = qualified_name[len("torchvision.models") + 1 :]
+            for submodule in self.MODEL_SUBMODULES:
+                if model_name.startswith(submodule + "."):
+                    model_name = model_name[len(submodule) + 1 :]
             message = None
 
             pretrained_arg = self.get_specific_arg(node, "pretrained", 0)
