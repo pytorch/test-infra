81b4259f3bbcf4e9b67d80b1f4d534e15c18a29d
Zain Rizvi <ZainR@meta.com>
2023-10-18 19:56:21 -0500

    [BE] HUD can show jobs run on a PR against commits before it was rebased

--- torchci/lib/fetchPR.ts
+++ torchci/lib/fetchPR.ts
@@ -1,5 +1,6 @@
 import { getOctokit } from "./github";
 import getRocksetClient from "./rockset";
+import rocksetVersions from "rockset/prodVersions.json";
 import { PRData } from "./types";
 
 export default async function fetchPR(
@@ -7,8 +8,15 @@ export default async function fetchPR(
   repo: string,
   prNumber: string
 ): Promise<PRData> {
+  // First pull data from Rockset to get everything including commits that have been force merged past.
+  // Then pull data from GitHub to get anything newer that was missed.
+
+  const rocksetClient = getRocksetClient();
+
+
+
   const octokit = await getOctokit(owner, repo);
-  const [pull, commits] = await Promise.all([
+  const [pull, commits, historicalCommits] = await Promise.all([
     octokit.rest.pulls.get({
       owner,
       repo,
@@ -20,11 +28,46 @@ export default async function fetchPR(
       pull_number: parseInt(prNumber),
       per_page: 100,
     }),
+    await rocksetClient.queryLambdas.executeQueryLambda(
+      "commons",
+      "pr_commits",
+      rocksetVersions.commons.pr_commits as string,
+      {
+        parameters: [
+          {
+            name: "pr_number",
+            type: "int",
+            value: prNumber,
+          },
+          {
+            name: "owner",
+            type: "string",
+            value: `${owner}`,
+          },
+          {
+            name: "repo",
+            type: "string",
+            value: `${repo}`,
+          },
+        ],
+      }
+    ),
   ]);
   const title = pull.data.title;
-  const shas = commits.map((commit) => {
-    return { sha: commit.sha, title: commit.commit.message.split("\n")[0] };
+
+  console.log("Fetcing PRS ZZZZZZZZZZ")
+  console.log(historicalCommits)
+  const shas = historicalCommits.results!.map((commit) => {
+    return { sha: commit.sha, title: commit.message.split("\n")[0] };
   });
+
+  // Ideally historicalCommits will be a superset of commits, but if there's a propagation delay with
+  // getting the data to rockset it may be missing recent commits for a bit.
+  // For the very last sha, checking to see if the shas themselves match offer a good proxy for detecting a missing commit.
+  const lastCommit = commits[commits.length - 1];
+  if (lastCommit.sha !== shas[shas.length - 1].sha) {
+    shas.push({ sha: lastCommit.sha, title: lastCommit.commit.message.split("\n")[0] });
+  }
 
   return { title, shas };
 }
