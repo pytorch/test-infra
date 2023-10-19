a80a046135aeea875e19f67eba756662c430caa3
Zain Rizvi <ZainRizvi@users.noreply.github.com>
2023-10-19 11:31:14 -0500

    [BE] HUD can show jobs run on a PR against commits before it was rebased (#4661)
    Makes it so that the HUD's commit dropdown shows all commits that have
    had CI run against them, _even if that commit has been rebased past_
    
    This makes it so that you don't loose CI data when you rebase your PR or
    amend and force push your latest commit.
    
    As a tradeoff, commits that were only committed locally but didn't have
    CI run against them (e.g. they were pushed to github as part of a set)
    will no longer show up in that dropdown, but they weren't useful anyways
    so it's fine
    
    ## Example
    Old list from https://hud.pytorch.org/pr/110578
    Hud lost all my commits before I squashed and rebased
    <img width="505" alt="image"
    src="https://github.com/pytorch/test-infra/assets/4468967/3d49d647-af9d-4ac1-a42e-e90364010119">
    
    New list, showing all my old commits. The commits with the same name are
    from amended commits that were force pushed.
    Source:
    https://torchci-git-zainr-rebase-proof-fbopensource.vercel.app/pr/110578
    <img width="605" alt="image"
    src="https://github.com/pytorch/test-infra/assets/4468967/8b4c2479-bf8b-4b85-b37e-adf6ec801b77">

---
+++ torchci/rockset/commons/__sql/pr_commits.sql
@@ -0,0 +1,44 @@
+-- This query is used by the HUD's /pull page to populate the list of historical commits
+-- made against a given PR.
+-- This improves upon the default github commits view because it allows HUD to show jobs 
+-- that ran on a PR before it was rebased
+
+WITH
+-- Get all PRs that were merged into master, and get all the SHAs for commits from that PR which CI jobs ran against
+-- We need the shas because some jobs (like trunk) don't have a PR they explicitly ran against, but they _were_ run against
+-- a commit from a PR
+pr_shas AS (
+  SELECT DISTINCT
+    FORMAT_ISO8601(
+        PARSE_TIMESTAMP_ISO8601(p.head_commit.timestamp),
+        'America/Los_Angeles'
+    ) as timestamp,
+    r.pull_requests[1].number AS pr_number,
+    p.head_commit.id AS sha,
+    p.head_commit.message,
+    CONCAT(
+      'https://github.com/',
+      :owner,
+      '/',
+      :repo,
+      '/pull/',
+      r.pull_requests[1].number
+    ) AS pr_url,
+    p.head_commit.url AS commit_url,
+  FROM
+    commons.workflow_job j
+    INNER JOIN commons.workflow_run r ON j.run_id = r.id
+    JOIN commons.push p ON p.head_commit.id = j.head_sha
+  WHERE
+    1 = 1
+    AND LENGTH(r.pull_requests) = 1
+    AND r.repository.owner.login = :owner
+    AND r.pull_requests[1].head.repo.name = :repo
+    AND r.pull_requests[1].number = :pr_num
+
+)
+SELECT
+  *
+FROM
+  pr_shas
+ORDER BY timestamp
