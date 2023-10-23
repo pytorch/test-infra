e02cc1b77b6f1ad990df4e96723a16bbab7b3375
Huy Do <huydhn@gmail.com>
2023-10-23 14:53:17 -0700

    Use OpenSearch cluster to power "more like this" failure search (#4668)
    Instead of using Rockset, we can now query the OpenSearch cluster for
    "more like this" failures. There are several benefits of doing so:
    
    * OpenSearch is indexed (like a search engine) and it returns the
    matches faster. Rockset query frequently hangs when there are too many
    results
    * Free-form text search is better. Fuzzy search results can also be
    used, but I haven't included them yet in this version to keep the
    behavior exactly the same. There are many examples for this if we just
    take any failures that are not a test failure. For examples:
    * [RuntimeError: CUDA unknown
    error](https://torchci-git-fork-huydhn-more-like-this-fbopensource.vercel.app/failure?name=periodic%20%2F%20win-vs2019-cuda11.8-py3%20%2F%20test%20(default%2C%201%2C%204%2C%20windows.g5.4xlarge.nvidia.gpu)&jobName=undefined&failureCaptures=%5B%22RuntimeError%3A%20CUDA%20unknown%20error%20-%20this%20may%20be%20due%20to%20an%20incorrectly%20set%20up%20environment%2C%20e.g.%20changing%20env%20variable%20CUDA_VISIBLE_DEVICES%20after%20program%20start.%20Setting%20the%20available%20devices%20to%20be%20zero.%22%5D)
    while [HUD link to the same
    failure](https://hud.pytorch.org/failure/RuntimeError%3A%20CUDA%20unknown%20error%20-%20this%20may%20be%20due%20to%20an%20incorrectly%20set%20up%20environment%2C%20e.g.%20changing%20env%20variable%20CUDA_VISIBLE_DEVICES%20after%20program%20start.%20Setting%20the%20available%20devices%20to%20be%20zero.)
    crashes
    * [The operation was
    canceled](https://torchci-git-fork-huydhn-more-like-this-fbopensource.vercel.app//failure?name=periodic%20%2F%20win-vs2019-cuda11.8-py3%20%2F%20test%20(force_on_cpu%2C%201%2C%201%2C%20windows.4xlarge.nonephemeral)&jobName=undefined&failureCaptures=%5B%22%23%23%5Berror%5DThe%20operation%20was%20canceled.%22%5D)
    while [HUD link to the same
    failure](https://hud.pytorch.org/failure/%23%23%5Berror%5DThe%20operation%20was%20canceled.)
    kind of hands because there are so many results
    * [Process completed with exit code
    1](https://torchci-git-fork-huydhn-more-like-this-fbopensource.vercel.app//failure?name=nightly%20%2F%20docs%20push%20%2F%20build-docs-python-true&jobName=docs%20push%20%2F%20build-docs-python-true&failureCaptures=%5B%22Process%20completed%20with%20exit%20code%201.%22%5D)
    while [HUD link to the same
    failure](https://hud.pytorch.org/failure/Process%20completed%20with%20exit%20code%201.)
    hangs
    
    * Same results in regular cases. They both come from
    `torchci-workflow-jobs` dynamo DB table after all.
    *
    [volo_d1_224](https://torchci-git-fork-huydhn-more-like-this-fbopensource.vercel.app//failure?name=inductor%20%2F%20cuda12.1-py3.10-gcc9-sm86%20%2F%20test%20(aot_inductor_timm%2C%202%2C%202%2C%20linux.g5.4xlarge.nvidia.gpu)&jobName=undefined&failureCaptures=%5B%22volo_d1_224%22%2C%22FAIL%3A%20%20%20%20%20accuracy%3Deager_two_runs_differ%2C%20expected%3Dpass%22%5D)
    v.s. [HUD
    link](https://hud.pytorch.org/failure/volo_d1_224%2CFAIL%3A%20%20%20%20%20accuracy%3Deager_two_runs_differ%2C%20expected%3Dpass)
    *
    [test_quantization.py::TestQuantizedOps::test_sigmoid](https://torchci-git-fork-huydhn-more-like-this-fbopensource.vercel.app//failure?name=periodic%20%2F%20macos-12-py3-x86-64%20%2F%20test%20(default%2C%201%2C%204%2C%20macos-12)&jobName=macos-12-py3-x86-64%20%2F%20test%20(default%2C%201%2C%204%2C%20macos-12)&failureCaptures=%5B%22test_quantization.py%3A%3ATestQuantizedOps%3A%3Atest_sigmoid%22%5D)
    v.s. [HUD
    link](https://hud.pytorch.org/failure/test_quantization.py%3A%3ATestQuantizedOps%3A%3Atest_sigmoid)

--- torchci/pages/failure.tsx
+++ torchci/pages/failure.tsx
@@ -14,6 +14,37 @@ import JobLinks from "components/JobLinks";
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 import { CSSProperties } from "react";
 
+import {
+  useGroupingPreference,
+  usePreference,
+} from "lib/useGroupingPreference";
+
+function FuzzySearchCheckBox({
+  useFuzzySearch,
+  setUseFuzzySearch,
+}: {
+  useFuzzySearch: boolean;
+  setUseFuzzySearch: any;
+}) {
+  return (
+    <>
+      <div
+        onClick={() => {
+          setUseFuzzySearch(!useFuzzySearch);
+        }}
+      >
+        <input
+          type="checkbox"
+          name="useFuzzySearch"
+          checked={useFuzzySearch}
+          onChange={() => {}}
+        />
+        <label htmlFor="useFuzzySearch"> Use fuzzy search</label>
+      </div>
+    </>
+  );
+}
+
 function FailureInfo({
   totalCount,
   jobCount,
@@ -219,21 +250,37 @@ function FailureInfo({
 
 export default function Page() {
   const router = useRouter();
-  const capture = router.query.capture;
+  const name = router.query.name as string;
+  const jobName = router.query.jobName as string;
+  const failureCaptures = router.query.failureCaptures as string;
 
+  const [useFuzzySearch, setUseFuzzySearch] = usePreference(
+    "useFuzzySearch",
+    false
+  );
   // `capture` is undefined pre-hydration, so we need to conditionally fetch in
   // `useSWR` to avoid sending a garbage request to the server.
   const swrKey =
-    capture !== undefined
-      ? `/api/failure/${encodeURIComponent(capture as string)}`
+    failureCaptures !== undefined
+      ? `/api/failure?name=${encodeURIComponent(
+          name
+        )}&jobName=${encodeURIComponent(
+          jobName
+        )}&failureCaptures=${encodeURIComponent(
+          failureCaptures
+        )}&useFuzzySearch=${useFuzzySearch}`
       : null;
   const { data } = useSWR(swrKey, fetcher);
   return (
     <div>
       <h1>PyTorch CI Failure Info</h1>
       <h2>
-        <code>{capture}</code>
+        <code>{failureCaptures}</code>
       </h2>
+      <FuzzySearchCheckBox
+        useFuzzySearch={useFuzzySearch}
+        setUseFuzzySearch={setUseFuzzySearch}
+      />
       <em>Showing last 14 days of data.</em>
       {data === undefined ? (
         <div>Loading...</div>
