266fc517f9499ba3f80856221e429af68a70ed00
Nikita Shulga <nshulga@meta.com>
2023-10-23 16:33:24 -0700

    Fixed Object is of type unknown violation
    By using `instanceof`
    
    ```
    src/main.ts:96:20 - error TS2571: Object is of type 'unknown'.
    
    96     core.setFailed(error.message)
                          ~~~~~
    
    ```

--- setup-ssh/src/main.ts
+++ setup-ssh/src/main.ts
@@ -93,7 +93,11 @@ async function run(): Promise<void> {
       return
     }
   } catch (error) {
-    core.setFailed(error.message)
+    if (error instanceof Error) {
+      core.setFailed(error.message)
+    } else {
+      core.setFailed(`Failed due to unexpected error ${error}`)
+    }
   }
 }
 
