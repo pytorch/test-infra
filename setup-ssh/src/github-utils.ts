c925407bb83b8de5fd9788e5283f2af093d2f757
Nikita Shulga <2453524+malfet@users.noreply.github.com>
2023-10-23 18:07:53 -0700

    [BE] Make add-github-ssh-key tolerant to errors (#4674)
    It should never fail, if for some reason keys could not be fetched, just
    print an error.

--- setup-ssh/src/github-utils.ts
+++ setup-ssh/src/github-utils.ts
@@ -6,12 +6,23 @@ export async function getPRAuthor(
   octokit: Octokit,
   prNumber: number
 ): Promise<string> {
-  const prInfo = await octokit.pulls.get({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    pull_number: prNumber
-  })
-  return prInfo.data.user?.login
+  const owner = github.context.repo.owner
+  const repo = github.context.repo.repo
+  try {
+    const prInfo = await octokit.pulls.get({
+      owner,
+      repo,
+      pull_number: prNumber
+    })
+    return prInfo.data.user?.login
+  } catch (error) {
+    const actor = github.context.actor
+    core.warning(
+      `Failed to get author of PR https://github.com/${owner}/${repo}/pull/${prNumber}: ${error}`
+    )
+    core.warning(`Returning ${actor} instead`)
+    return actor
+  }
 }
 
 export function extractCiFlowPrNumber(reference: string): number {
