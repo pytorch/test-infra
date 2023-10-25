6877eaf5af016c7bbdd8ccc84a11b8835a9c2f38
Nikita Shulga <nshulga@meta.com>
2023-10-24 17:31:30 -0700

    Extend the test

--- tools/torchfix/tests/fixtures/deprecated_symbols/codemod/aliased_import.py
+++ tools/torchfix/tests/fixtures/deprecated_symbols/codemod/aliased_import.py
@@ -1,5 +1,10 @@
-import torch as troch
+import torch
+import torch as foo
+import torch as bar
+import torch as baz
 
-A = troch.arange(9).reshape(3, 3)
-A3 = troch.chain_matmul(A, A, A)
+A = torch.arange(9.0).reshape(3, 3)
+A3 = foo.chain_matmul(A, A, A)
+rc1 = bar.cholesky(torch.mm(A3.t(), A3))
+rc2 = baz.qr(torch.mm(A.t(), A))
 
