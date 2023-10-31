b3f43794a64473bc93a8eaee36967ad9f12bf69c
clee2000 <44682903+clee2000@users.noreply.github.com>
2023-10-31 13:03:52 -0700

    [bot] Cancel pending workflows when PR is closed by non mergebot (#4686)
    Maybe controversial 
    
    Generally for when someone accidentally makes a ton of PRs through
    ghstack

---
+++ torchci/lib/bot/cancelWorkflowsOnCloseBot.ts
@@ -0,0 +1,49 @@
+import { Probot } from "probot";
+
+function cancelWorkflowsOnCloseBot(app: Probot): void {
+  app.on("pull_request.closed", async (ctx) => {
+    const owner = ctx.payload.repository.owner.login;
+    const repo = ctx.payload.repository.name;
+    const senderLogin = ctx.payload.sender.login;
+    const senderId = ctx.payload.sender.id;
+    const headSha = ctx.payload.pull_request.head.sha;
+
+    if (
+      senderLogin == "pytorchmergebot" ||
+      senderLogin == "pytorchbot" ||
+      senderLogin == "pytorch-bot[bot]" ||
+      senderId == 97764156 || // pytorchmergebot's id
+      senderId == 54816060 || // pytorch-bot's id
+      senderId == 21957446 // pytorchbot's id
+    ) {
+      return;
+    }
+
+    const workflowRuns = await ctx.octokit.paginate(
+      ctx.octokit.actions.listWorkflowRunsForRepo,
+      {
+        owner,
+        repo,
+        head_sha: headSha,
+        per_page: 30,
+      },
+      (o) => o.data.workflow_runs
+    );
+
+    await Promise.all(
+      workflowRuns
+        .filter((workflowRun) => workflowRun.status != "completed")
+        .map(
+          async (workflowRun) =>
+            await ctx.octokit.rest.actions.cancelWorkflowRun({
+              owner,
+              repo,
+              run_id: workflowRun.id,
+            })
+        )
+    );
+    return;
+  });
+}
+
+export default cancelWorkflowsOnCloseBot;
---
+++ torchci/test/cancelWorkflowsOnCloseBot.test.ts
@@ -0,0 +1,45 @@
+import nock from "nock";
+import { Probot } from "probot";
+import myProbotApp from "../lib/bot/cancelWorkflowsOnCloseBot";
+import { handleScope, requireDeepCopy } from "./common";
+import * as utils from "./utils";
+nock.disableNetConnect();
+
+describe("accept bot", () => {
+  let probot: Probot;
+
+  beforeEach(() => {
+    probot = utils.testProbot();
+    probot.load(myProbotApp);
+  });
+
+  test("Cancel in progress workflows when not pytorchmergebot closes thr PR", async () => {
+    const event = requireDeepCopy("./fixtures/pull_request.closed.json");
+    const scope = nock("https://api.github.com")
+      .get(
+        "/repos/clee2000/random-testing/actions/runs?head_sha=381ace654ad6474357cedad09418340896d16d90&per_page=30"
+      )
+      .reply(200, {
+        workflow_runs: [
+          { id: 6647495490, status: "in_progress" },
+          { id: 6647495495, status: "in_progress" },
+          { id: 6647495497, status: "completed" },
+        ],
+      })
+      .post(`/repos/clee2000/random-testing/actions/runs/6647495490/cancel`)
+      .reply(200, {})
+      .post(`/repos/clee2000/random-testing/actions/runs/6647495495/cancel`)
+      .reply(200, {});
+    await probot.receive(event);
+
+    handleScope(scope);
+  });
+
+  test("Do not cancel in progress workflows when pytorchmergebot closes the PR", async () => {
+    const event = requireDeepCopy("./fixtures/pull_request.closed.json");
+    event.payload.sender.login = "pytorchmergebot";
+    const scope = nock("https://api.github.com");
+    await probot.receive(event);
+    handleScope(scope);
+  });
+});
