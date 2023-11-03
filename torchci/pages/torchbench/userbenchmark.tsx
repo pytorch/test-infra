70d77c03025cd983d2f57ad566c5b7c29ead91f7
Xu Zhao <xzhao9@meta.com>
2023-11-03 14:44:03 -0400

    Getting torchbench queries to work.

--- torchci/pages/torchbench/userbenchmark.tsx
+++ torchci/pages/torchbench/userbenchmark.tsx
@@ -2,10 +2,22 @@ import { Grid } from "@mui/material";
 import TimeSeriesPanel from "components/metrics/panels/TimeSeriesPanel";
 import dayjs from "dayjs";
 import { RocksetParam } from "lib/rockset";
+import useSWR from "swr";
+import { fetcher } from "lib/GeneralUtils";
 import React, { useState } from "react";
-import styles from "components/hud.module.css";
+import styles from "components/metrics.module.css";
+import {
+  Stack,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  SelectChangeEvent,
+} from "@mui/material";
 
 const MAX_BRIGHTNESS: number = 255;
+const queryCollection = "torchbench";
 
 type UserbenchmarkRow = {
   metric_name: string;
@@ -14,135 +26,85 @@ type UserbenchmarkRow = {
   delta: string | number | null;
 };
 
-export const USERBENCHMARKS: { [k: string]: string } = {
-  torch_trt: "Torch TensorRT",
-};
+export function UserbenchmarkPicker({
+  userbenchmark,
+  setUserbenchmark
+}: {
+    userbenchmark: string;
+    setUserbenchmark: any;
+  }) {
+  function handleChange(e: SelectChangeEvent<string>) {
+    setUserbenchmark(e.target.value);
+  }
+  const queryName = "torchbench_list_userbenchmarks";
+  const queryParams: any[] = [];
+  const list_userbenchmark_url = `/api/query/${queryCollection}/${queryName}?parameters=${encodeURIComponent(
+    JSON.stringify(queryParams)
+  )}`;
 
-type UserbenchmarkState = { url: string | null; content: any };
-
-class UserbenchmarkReport extends React.Component<
-  UserbenchmarkState
-> {
-
-  // returns a list[UserbenchmarkRow]
-  // The first element is headers, remaining elements are rows.
-  // 1 header row is expected, followed by data rows.
-
-  getHeader(row: UserbenchmarkRow) {
-    const headers = [
-      row.metric_name as string,
-      row.control_value as string,
-      row.treatment_value as string,
-      row.delta as string,
-    ];
+  let { data, error } = useSWR(list_userbenchmark_url, fetcher, {
+    refreshInterval: 60 * 60 * 1000, // refresh every hour
+  });
+  console.log(data);
+  if (data === undefined || data.length === 0) {
     return (
-      <thead>
-        <tr className={styles.userbenchmarkTable}>
-          {headers.map((val: string, idx: number) => (
-            <th className={styles.userbenchmarkTable} key={idx}>
-              {val}
-            </th>
+      <>
+        <FormControl>
+            <InputLabel id="ub-picker-input-label">Userbenchmark</InputLabel>
+            <Select
+              value={userbenchmark}
+              label="Userbenchmark"
+              labelId="ub-picker-select-label"
+              id="ub-picker-select"
+              onChange={handleChange}
+            >
+              <MenuItem key="Error" value="Error">
+                  Error
+              </MenuItem>
+            </Select>
+        </FormControl>
+      </>
+      );
+  }
+  return (
+    <>
+      <FormControl>
+        <InputLabel id="ub-picker-input-label">Userbenchmark</InputLabel>
+        <Select
+          value="Userbenchmark"
+          label="Userbenchmark"
+          labelId="ub-picker-select-label"
+          id="ub-picker-select"
+          onChange={handleChange}
+        >
+          {Object.keys(data).map((ub) => (
+            <MenuItem key={data[ub].name} value={data[ub].name}>
+              {data[ub].name}
+            </MenuItem>
           ))}
-        </tr>
-      </thead>
-    );
-  }
-
-  getRow(row: UserbenchmarkRow, minSpeedup: number, maxSpeedup: number) {
-    const convertSpeedup = (speedupRaw: number) => {
-      if (typeof speedupRaw === "number") {
-        const text = (speedupRaw * 100).toFixed(2) + "%";
-        // assign a color
-        let color = `rgb(${MAX_BRIGHTNESS}, ${MAX_BRIGHTNESS}, ${MAX_BRIGHTNESS})`;
-        if (speedupRaw >= 0) {
-          const ratio = speedupRaw / maxSpeedup;
-          const otherColor = Math.floor(
-            MAX_BRIGHTNESS * (1 - ratio)
-          ).toString();
-          color = `rgb(${otherColor}, ${MAX_BRIGHTNESS}, ${otherColor})`;
-        } else {
-          const ratio = speedupRaw / minSpeedup;
-          const otherColor = Math.floor(
-            MAX_BRIGHTNESS * (1 - ratio)
-          ).toString();
-          color = `rgb(${MAX_BRIGHTNESS}, ${otherColor}, ${otherColor})`;
-        }
-        return (
-          <td
-            style={{ backgroundColor: color }}
-            className={styles.userbenchmarkTable}
-          >
-            {text}
-          </td>
-        );
-      } else {
-        return <td className={styles.userbenchmarkTable}>{speedupRaw}</td>;
-      }
-    };
-    return (
-      <tbody>
-        <tr className={styles.userbenchmarkTable}>
-          {[row.metric_name, row.control_value, row.treatment_value].map(
-            (val: any, idx: number) => (
-              <td className={styles.userbenchmarkTable} key={idx}>
-                {val}
-              </td>
-            )
-          )}
-          {convertSpeedup(row.delta as number)}
-        </tr>
-      </tbody>
-    );
-  }
-
-  jsonToTable(csvString: string) {
-  }
-
-  componentDidMount() {
-    let url: string | null = null;
-    if (typeof document !== "undefined") {
-      const searchParams = new URL(document.location.toString()).searchParams;
-      if (searchParams.has("url")) {
-        url = searchParams.get("url");
-      }
-    }
-
-    if (typeof url === "string") {
-      fetch(url)
-        .then((response) => {
-          const text = response.text();
-          return text;
-        })
-        .then((response) => {
-          this.setState({
-            content: "abc",
-            url: url,
-          });
-        });
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>
-          {" "}
-          TorchBench Userbenchmark results from <span>{this.state.url}</span>{" "}
-        </h1>
-        <p>
-          TorchBench Userbenchmarks can be run in the CI deployed in the pytorch/benchmark
-          repo. The CI job will generate a JSON of the results, showing result times
-          from the control revision as well as the treatment revision.
-          This page displays the metrics delta by comparing the revision results.
-        </p>
-        <div> {this.state.content} </div>
-      </div>
-    );
-  }
+        </Select>
+      </FormControl>
+    </>
+  );
 }
 
 export default function Page() {
+  const defaultUB = "torch_trt"
+  const [userbenchmark, setUserbenchmark] = useState(defaultUB);
   return <div>
-    <UserbenchmarkReport />
+    <Typography fontSize={"2rem"} fontWeight={"bold"}>
+      TorchBench Userbenchmark Dashboard
+    </Typography>
+    <p>
+      TorchBench Userbenchmarks can be run in the CI deployed in the pytorch/benchmark
+      repo. <br/> The CI job will generate a JSON of the results, showing result times
+      from the control revision as well as the treatment revision.
+    </p>
+    <Stack direction="row" spacing={2} sx={{ mb: 2 }}>
+      <UserbenchmarkPicker
+        userbenchmark={userbenchmark}
+        setUserbenchmark={setUserbenchmark}
+      />
+    </Stack>
   </div>;
 }
