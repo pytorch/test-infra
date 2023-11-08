c21470703ddd8148a77fb93ed50a19827340149d
Sergii Dymchenko <sdym@fb.com>
2023-11-07 16:57:06 -0800

    More assertive require_grad check (#4709)
    Fixes https://github.com/pytorch/test-infra/issues/4687

--- tools/torchfix/tests/fixtures/misc/checker/require_grad.py
+++ tools/torchfix/tests/fixtures/misc/checker/require_grad.py
@@ -1,9 +1,11 @@
+import random
 import torch
 x = torch.zeros(1)
 x.require_grad = False
 x.require_grad = True
+grad = random.choice([False, True])
+x.require_grad = grad
 
 # Don't trigger
 x.requires_grad = False
 require_grad = False
-x.require_grad = 10
--- tools/torchfix/tests/fixtures/misc/codemod/require_grad.py
+++ tools/torchfix/tests/fixtures/misc/codemod/require_grad.py
@@ -2,3 +2,9 @@ import torch
 x = torch.zeros(1)
 x.require_grad = False
 x.require_grad = True
+
+# from https://github.com/pytorch/test-infra/issues/4687
+import torch.nn as nn
+model = nn.Module()
+for name, param in model.named_parameters():
+    param.require_grad = 'specific_layer' in name
