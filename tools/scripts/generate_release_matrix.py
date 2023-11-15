d2feb90d03eb3c6607183818c9d3f51d41f01f6c
Andrey Talman <atalman@fb.com>
2023-11-14 19:10:12 -0500

    Add release matrix. Make sure domain releases versions are included (#4736)
    0. Fix test assets broken by
    https://github.com/pytorch/test-infra/pull/4731
    1. Add release matrix. Test:
    ```
    [atalman@devvm18003.prn0 /data/users/atalman/test-infra (release_versions_matrix)]$ python3 tools/scripts/generate_release_matrix.py --version 2.1.0
    {"torch": "2.1.0", "torchvision": "0.16.0", "torchaudio": "2.1.0", "torchtext": "0.16.0", "tochdata": "0.7.0"}
    [atalman@devvm18003.prn0 /data/users/atalman/test-infra (release_versions_matrix)]$ python3 tools/scripts/generate_release_matrix.py --version 2.1.2
    Traceback (most recent call last):
      File "tools/scripts/generate_release_matrix.py", line 47, in <module>
        main(sys.argv[1:])
      File "tools/scripts/generate_release_matrix.py", line 41, in main
        raise ValueError(f"{options.version} is not a valid release")
    ValueError: 2.1.2 is not a valid release
    ```
    
    
    Intent to add domain version validations here:
    https://github.com/pytorch/builder/blob/main/test/smoke_test/smoke_test.py#L61

---
+++ tools/scripts/generate_release_matrix.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+"""Generates a release matrix to be utilized through github actions
+
+Will output a JSON representing PyTorch Release versions. To be used with validation framework.
+
+"""
+
+
+import argparse
+import json
+import sys
+import os
+from typing import Dict
+
+mod = sys.modules[__name__]
+
+RELEASE_DICT = {
+    "2.1.0": { 'torch': '2.1.0', 'torchvision': '0.16.0', 'torchaudio': '2.1.0', 'torchtext': '0.16.0', 'tochdata': '0.7.0'},
+    "2.1.1": { 'torch': '2.1.1', 'torchvision': '0.16.1', 'torchaudio': '2.1.1', 'torchtext': '0.16.1', 'tochdata': '0.7.1'}
+}
+
+
+def main(args) -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--version",
+        help="PyTorch Release version",
+        type=str,
+        default=os.getenv("VERSION", "2.1.1"),
+    )
+
+    options = parser.parse_args(args)
+
+    if options.version not in RELEASE_DICT.keys():
+        raise ValueError(f"{options.version} is not a valid release")
+
+    print(json.dumps(RELEASE_DICT[options.version]))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
