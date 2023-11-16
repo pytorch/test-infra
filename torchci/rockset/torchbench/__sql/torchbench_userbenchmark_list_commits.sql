7e9a5c31cff8a849c14228fce1ded26cf1d49fef
Xu Zhao <xzhao9@meta.com>
2023-11-16 17:49:06 -0500

    Update list cocmmits

--- torchci/rockset/torchbench/__sql/torchbench_userbenchmark_list_commits.sql
+++ torchci/rockset/torchbench/__sql/torchbench_userbenchmark_list_commits.sql
@@ -1,4 +1,10 @@
-SELECT ARBITRARY(name) AS name, "torchbench-userbenchmark".environ.pytorch_git_version as pytorch_git_version
+WITH w AS (
+  SELECT ARBITRARY(name) AS name, "torchbench-userbenchmark".environ.pytorch_git_version as pytorch_git_version,
+  ARBITRARY("torchbench-userbenchmark".environ.pytorch_version) as pytorch_version,
   FROM torchbench."torchbench-userbenchmark"
   WHERE name = :userbenchmark
-  GROUP BY "torchbench-userbenchmark".environ.pytorch_git_version;
+  AND "torchbench-userbenchmark".environ.pytorch_version IS NOT NULL
+  GROUP BY "torchbench-userbenchmark".environ.pytorch_git_version
+)
+SELECT name, pytorch_git_version, pytorch_version, REGEXP_EXTRACT(pytorch_version, 'dev([0-9]+)', 1) AS pytorch_nightly_date FROM w
+  ORDER BY pytorch_nightly_date DESC;
