9909d1c7c756e7509a868570d80f289c60bd7482
Catherine Lee <csl@fb.com>
2023-12-11 14:25:29 -0800

    appease lint

--- torchci/pages/_app.tsx
+++ torchci/pages/_app.tsx
@@ -5,7 +5,7 @@ import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import "styles/globals.css";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
