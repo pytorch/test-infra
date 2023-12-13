980184fc2435dae6225cdd82533ec4304a369e10
clee2000 <44682903+clee2000@users.noreply.github.com>
2023-12-13 14:58:29 -0800

    [hud] Tracking experiment: add a session id (#4812)

--- torchci/lib/track.ts
+++ torchci/lib/track.ts
@@ -1,4 +1,14 @@
+import { randomBytes } from "crypto";
 export function track(router: any, type: string, info: any) {
+  // Gets cleared when local storage is cleared but I think that's ok.
+  if (window.localStorage.getItem("session_tracking_id") === null) {
+    localStorage.setItem(
+      "session_tracking_id",
+      `${randomBytes(20).toString("hex")}-${Date.now()}`
+    );
+  }
+  const sessionIDFromStorage = localStorage.getItem("session_tracking_id");
+
   fetch(`/api/track`, {
     method: "POST",
     body: JSON.stringify({
@@ -8,6 +18,7 @@ export function track(router: any, type: string, info: any) {
       windowPathname: window.location.pathname,
       routerPathname: router.pathname,
       routerPath: router.asPath,
+      sessionID: sessionIDFromStorage,
     }),
   });
 }
