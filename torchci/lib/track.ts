17a4f5c1a93b36afec59f0a18a82345d0f9dc0df
Catherine Lee <csl@fb.com>
2023-12-18 09:28:48 -0800

    update

--- torchci/lib/track.ts
+++ torchci/lib/track.ts
@@ -1,6 +1,7 @@
 import ReactGA from "react-ga4";
 
 export function track(router: any, type: string, info: any) {
+  if (window.location.href.startsWith("https://hud.pytorch.org")) {
     // TODO: I think there better ways to make sure it doesn't send dev data but
     // for now this is easy to read.
     ReactGA.event(type, {
@@ -10,4 +11,5 @@ export function track(router: any, type: string, info: any) {
       routerPathname: router.pathname,
       routerPath: router.asPath,
     });
+  }
 }
