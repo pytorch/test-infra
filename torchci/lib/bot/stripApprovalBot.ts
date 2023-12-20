638a0102f7cc789e17e3a3365b8f306759528b67
PaliC <>
2023-12-20 14:54:17 -0800

    Rerequest review on pr closure
    This PR addresses https://github.com/pytorch/test-infra/pull/4782
    
    Dismisses all reviews once a pr is closed.
    
    We should do some extra testing using malfet/deleteme.

---
+++ torchci/lib/bot/stripApprovalBot.ts
@@ -0,0 +1,46 @@
+import { Context, Probot } from "probot";
+import { hasWritePermissions } from "./utils";
+import { exit } from "process";
+
+export default function stripApprovalBot(app: Probot): void {
+  app.on(
+    ["pull_request.reopened"],
+    async (ctx: Context<"pull_request.reopened">) => {
+      const pr_author = ctx.payload.pull_request.user.login;
+      console.log("pr_author: ", pr_author);
+      // cause error
+      if (await hasWritePermissions(ctx, pr_author)) {
+        // if the user has write permissions, we don't need to strip approvals
+        return;
+      }
+
+      const owner = ctx.payload.repository.owner.login;
+      const repo = ctx.payload.repository.name;
+      const issue_number = ctx.payload.pull_request.number;
+
+      const reviews = await ctx.octokit.pulls.listReviews({
+        owner,
+        repo,
+        pull_number: issue_number,
+      });
+
+      const approved_reviews = reviews.data.filter((review) => {
+        return review.state === "APPROVED";
+      });
+
+      for (const review of approved_reviews) {
+        await ctx.octokit.pulls
+          .dismissReview({
+            owner,
+            repo,
+            pull_number: issue_number,
+            review_id: review.id,
+            message:
+              "This PR was reopened (likely due to being reverted), so your approval was removed. Please request another review.",
+          })
+          .catch((error) => console.error(error));
+      }
+      return;
+    }
+  );
+}
