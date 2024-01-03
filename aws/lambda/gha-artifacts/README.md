561002429e86d39006641b51425b61a2d47e4fd5
Huy Do <huydhn@gmail.com>
2024-01-02 20:11:25 -0800

    Add a new gha-artifacts lambda to list GH artifacts

---
+++ aws/lambda/gha-artifacts/README.md
@@ -0,0 +1,7 @@
+This lambda is used to list all artifacts from a specific GitHub
+workflows
+
+### Deployment
+
+A new version of the lambda can be deployed using `make deploy` and it
+is done so automatically as part of the CI.
---
+++ aws/lambda/gha-artifacts/lambda_function.py
@@ -0,0 +1,69 @@
+import asyncio
+import json
+import os
+import warnings
+from json.decoder import JSONDecodeError
+from typing import Any, List
+from urllib.parse import quote
+
+import aioboto3
+
+URL_PREFIX = "https://gha-artifacts.s3.amazonaws.com"
+GHA_ARTIFACTS_BUCKET = "gha-artifacts"
+DEFAULT_REPO = "pytorch/pytorch"
+SESSION = aioboto3.Session()
+
+
+async def list_objects(repo: str, workflow_id: str) -> List[str]:
+    async with SESSION.resource("s3") as s3:
+        bucket = await s3.Bucket(GHA_ARTIFACTS_BUCKET)
+
+        artifacts = []
+        async for s3_object in bucket.objects.filter(Prefix=f"{repo}/{workflow_id}"):
+            if not s3_object.key:
+                continue
+
+            artifact = "/".join([quote(v) for v in s3_object.key.split("/")])
+            artifacts.append(f"{URL_PREFIX}/{artifact}")
+
+        return artifacts
+
+
+def lambda_handler(event: Any, context: Any) -> None:
+    """
+    The input of the lambda is the ID of the workflow where we want to list its
+    artifacts, i.e. {"workflow_id": "123456"}
+
+    The repository is optional and default to pytorch/pytorch, i.e.
+    {
+        "workflow_id": "123456",
+        "repo": "pytorch/pytorch",
+    }
+    """
+    body = event.get("body", "")
+    if not body:
+        return []
+
+    try:
+        params = json.loads(body)
+    except JSONDecodeError as error:
+        warnings.warn(f"Failed to parse request body {body}: {error}")
+        return []
+
+    repo = params.get("repo", DEFAULT_REPO)
+    workflow_id = params.get("workflow_id", "")
+    if not workflow_id:
+        return []
+
+    print(f"Listing artifact for workflow {workflow_id} from {repo}")
+    return asyncio.run(list_objects(repo, workflow_id))
+
+
+if os.getenv("DEBUG", "0") == "1":
+    mock_body = {"workflow_id": "7392058557"}
+    print(
+        lambda_handler(
+            {"body": json.dumps(mock_body)},
+            None,
+        )
+    )
