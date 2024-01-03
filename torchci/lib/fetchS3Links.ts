51225f51d17b95279e4f2a6c02bfb065ffcecba0
Huy Do <huydhn@gmail.com>
2024-01-02 20:37:31 -0800

    Clean up the usage of AWS S3 credentials on HUD

--- torchci/lib/fetchS3Links.ts
+++ torchci/lib/fetchS3Links.ts
@@ -1,29 +1,37 @@
 import { Artifact } from "./types";
-import { ListObjectsV2Command, S3Client } from "@aws-sdk/client-s3";
-import s3client from "./s3";
+
+const GHA_ARTIFACTS_LAMBDA =
+  "https://np6xty2nm6jifkuuyb6wllx6ha0qtthb.lambda-url.us-east-1.on.aws";
+
 export default async function fetchS3Links(
   suiteId: string
 ): Promise<Artifact[]> {
-  const command = new ListObjectsV2Command({
-    Bucket: "gha-artifacts",
-    Prefix: `pytorch/pytorch/${suiteId}`,
+  const response = await fetch(GHA_ARTIFACTS_LAMBDA, {
+    method: "POST",
+    body: JSON.stringify({
+      workflow_id: suiteId,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
   });
-  const response = await s3client.send(command);
 
+  const results = await response.json();
   const artifacts =
-    response.Contents?.map((jobs) => {
-      const basename = jobs.Key?.split("/").slice(-1)[0];
+    results.map((artifact: any) => {
+      const url = artifact.url;
+      const size = artifact.size;
+
+      const basename = artifact.split("/").slice(-1)[0];
       const name =
         basename !== "artifacts.zip"
           ? basename
-          : jobs.Key?.split("/").slice(-2).join("/");
+          : artifact.split("/").slice(-2).join("/");
       return {
         kind: "s3",
         name: name ?? "",
-        sizeInBytes: jobs.Size ?? 0,
-        url: `https://gha-artifacts.s3.amazonaws.com/${jobs.Key?.split("/")
-          .map((x) => encodeURIComponent(x))
-          .join("/")}`,
+        sizeInBytes: size ?? 0,
+        url: url,
         expired: false,
       };
     }) ?? [];
