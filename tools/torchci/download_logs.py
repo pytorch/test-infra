1cdfb6258b6b475b59db09c502c2188271c0a62e
Catherine Lee <csl@fb.com>
2024-01-23 15:42:08 -0800

    update

--- tools/torchci/download_logs.py
+++ tools/torchci/download_logs.py
@@ -2,11 +2,23 @@ import argparse
 import multiprocessing as mp
 import os
 from pathlib import Path
+import glob
+import zipfile
 
 import requests
 from torchci.rockset_utils import query_rockset
 
 REPO_ROOT = Path(__file__).resolve().parent.parent.parent
+
+
+def get_s3():
+    import boto3
+    return boto3.resource("s3")
+
+
+def unzip(from_file, to_folder):
+    with zipfile.ZipFile(from_file, 'r') as zip_ref:
+        zip_ref.extractall(to_folder)
 
 
 def download_log(id):
@@ -47,15 +59,43 @@ def download_logs_to_dir(commit):
     return folder
 
 
+def download_artifacts_from_sha(commit):
+    s3 = get_s3()
+    bucket = s3.Bucket("gha-artifacts")
+    folder = REPO_ROOT / "_logs" / "artifacts" / commit
+    workflow_ids = query_rockset(f"select id from workflow_run where head_sha = '{commit}'")
+
+    zipped_path = folder / "zipped"
+    os.makedirs(zipped_path, exist_ok=True)
+
+    for row in workflow_ids:
+        workflow_id = row['id']
+        for obj in bucket.objects.filter(Prefix=f"pytorch/pytorch/{workflow_id}"):
+            if "test-reports" not in obj.key:
+                continue
+            s3.Object(bucket.name, obj.key).download_file(str(zipped_path / obj.key.replace("/", "_")))
+
+    unzipped_path = REPO_ROOT / "_logs" / "artifacts" / commit / "unzipped"
+    os.makedirs(unzipped_path, exist_ok=True)
+
+    for file in glob.glob(str(zipped_path) + '/**/*.zip', recursive=True):
+        unzip(file, unzipped_path / Path(file).stem)
+
+
 def get_parser():
     parser = argparse.ArgumentParser(
         description="Download TEST logs for a particular commit"
     )
     parser.add_argument("commit", type=str)
+    parser.add_argument("--artifacts", action="store_true")
     return parser
 
 
 if __name__ == "__main__":
     args = get_parser().parse_args()
-    download_logs_to_dir(args.commit)
-    print(f"Saved to {REPO_ROOT / '_logs' / 'ci_logs' / args.commit}")
+    if args.artifacts:
+        download_artifacts_from_sha(args.commit)
+        print(f"Saved to {REPO_ROOT / '_logs' / 'artifacts' / args.commit}")
+    else:
+        download_log(args.commit)
+        print(f"Saved to {REPO_ROOT / '_logs' / 'ci_logs' / args.commit}")
