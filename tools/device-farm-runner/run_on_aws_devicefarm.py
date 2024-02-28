e64e7d9341df33af609d9ed06f80314bb9112aae
Huy Do <huydhn@gmail.com>
2024-02-27 17:33:35 -0800

    Update the script to print test report

--- tools/device-farm-runner/run_on_aws_devicefarm.py
+++ tools/device-farm-runner/run_on_aws_devicefarm.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 
 import datetime
+import logging
 import os
 import random
 import string
 import sys
 import time
-import warnings
-from typing import Any
+from enum import Enum
+from logging import info
+from typing import Any, Dict, List, Optional
+from warnings import warn
 
 import boto3
 import requests
+
 
 POLLING_DELAY_IN_SECOND = 5
 MAX_UPLOAD_WAIT_IN_SECOND = 600
@@ -20,6 +24,19 @@ MAX_UPLOAD_WAIT_IN_SECOND = 600
 DEFAULT_DEVICE_POOL_ARN = (
     "arn:aws:devicefarm:us-west-2::devicepool:082d10e5-d7d7-48a5-ba5c-b33d66efa1f5"
 )
+
+
+# Device Farm report type
+class ReportType(Enum):
+    RUN = "run"
+    JOB = "job"
+    SUITE = "suite"
+    TEST = "test"
+
+
+DEVICE_FARM_BUCKET = "gha-artifacts"
+
+logging.basicConfig(level=logging.INFO)
 
 
 def parse_args() -> Any:
@@ -50,6 +67,18 @@ def parse_args() -> Any:
         default=DEFAULT_DEVICE_POOL_ARN,
         help="the name of the device pool to test on",
     )
+    parser.add_argument(
+        "--workflow-id",
+        type=str,
+        default="invalid-workflow-id",
+        help="the workflow run ID",
+    )
+    parser.add_argument(
+        "--workflow-attempt",
+        type=int,
+        default=0,
+        help="the workflow run attempt",
+    )
 
     return parser.parse_args()
 
@@ -61,7 +90,7 @@ def upload_file(
     filename: str,
     filetype: str,
     mime: str = "application/octet-stream",
-):
+) -> str:
     """
     Upload the app file and XCTest suite to AWS
     """
@@ -72,11 +101,11 @@ def upload_file(
         contentType=mime,
     )
     upload_name = r["upload"]["name"]
-    upload_arn = r["upload"]["arn"]
+    upload_arn: str = r["upload"]["arn"]
     upload_url = r["upload"]["url"]
 
     with open(filename, "rb") as file_stream:
-        print(f"Uploading {filename} to Device Farm as {upload_name}...")
+        info(f"Uploading {filename} to Device Farm as {upload_name}...")
         r = requests.put(upload_url, data=file_stream, headers={"content-type": mime})
         if not r.ok:
             raise Exception(f"Couldn't upload {filename}: {r.reason}")
@@ -93,7 +122,7 @@ def upload_file(
         r = client.get_upload(arn=upload_arn)
         status = r["upload"].get("status", "")
 
-        print(f"{filename} is in state {status} after {waiting_time}")
+        info(f"{filename} is in state {status} after {waiting_time}")
 
         if status == "FAILED":
             raise Exception(f"Couldn't upload {filename}: {r}")
@@ -105,13 +134,145 @@ def upload_file(
     return upload_arn
 
 
+def is_success(result: Optional[str]) -> bool:
+    return bool(result and result == "PASSED")
+
+
+def download_artifact(artifact_url: str, local_filename: str) -> str:
+    """
+    Download an artifact to local
+    """
+    response = requests.get(artifact_url)
+    with open(local_filename, "wb") as f:
+        f.write(response.content)
+    return local_filename
+
+
+def upload_file_to_s3(
+    file_name: str,
+    bucket: str,
+    key: str,
+) -> None:
+    """
+    Upload a local file to S3
+    """
+    boto3.client("s3").upload_file(
+        file_name,
+        bucket,
+        key,
+    )
+
+
+def print_test_artifacts(
+    client: Any, test_arn: str, workflow_id: str, workflow_attempt: int, indent: int = 0
+) -> List[Dict[str, str]]:
+    """
+    Return all artifacts from this specific test. There are 3 types of artifacts
+    from Device Farm including FILE, LOG, and SCREENSHOT
+    """
+    gathered_artifacts = []
+
+    for artifact_type in ["FILE", "LOG", "SCREENSHOT"]:
+        r = client.list_artifacts(arn=test_arn, type=artifact_type)
+        for artifact in r.get("artifacts", []):
+            filetype = artifact["type"]
+            filename = artifact["name"].replace(" ", "_")
+            extension = artifact["extension"].replace(" ", "_")
+
+            local_filename = (
+                artifact["arn"].replace(":", "_").replace("/", "_")
+                + f"_{filename}.{extension}"
+            )
+            s3_key = f"device_farm/{workflow_id}/{workflow_attempt}/{local_filename}"
+            # Download the artifact locally
+            upload_file_to_s3(
+                download_artifact(artifact["url"], local_filename),
+                DEVICE_FARM_BUCKET,
+                s3_key,
+            )
+
+            info(
+                f"{' ' * indent}Saving {artifact_type} {filename}.{extension} ({filetype}) "
+                + f"at https://{DEVICE_FARM_BUCKET}.s3.amazonaws.com/{s3_key}"
+            )
+            gathered_artifacts.append(artifact)
+
+    return gathered_artifacts
+
+
+def print_report(
+    client: Any,
+    report: Dict[str, Any],
+    rtype: ReportType,
+    workflow_id: str,
+    workflow_attempt: int,
+    indent: int = 0,
+) -> List[Dict[str, str]]:
+    """
+    Print the test report from Device Farm in a friendly way and return the list
+    of any notable artifacts from the test run, i.e. logs and screenshots
+    """
+    if not report:
+        warn("Missing report, returning...")
+        return []
+
+    name = report["name"]
+    result = report["result"]
+
+    extra_msg = ""
+    if rtype == ReportType.SUITE or is_success(result):
+        counters = report["counters"]
+        extra_msg = f"with stats {counters}"
+
+    info(f"{' ' * indent}{name} {result} {extra_msg}")
+
+    if is_success(result):
+        return []
+
+    arn = report["arn"]
+    if rtype == ReportType.RUN:
+        more_reports = client.list_jobs(arn=arn)
+        next_rtype = ReportType.JOB
+    elif rtype == ReportType.JOB:
+        more_reports = client.list_suites(arn=arn)
+        next_rtype = ReportType.SUITE
+    elif rtype == ReportType.SUITE:
+        more_reports = client.list_tests(arn=arn)
+        next_rtype = ReportType.TEST
+    elif rtype == ReportType.TEST:
+        return print_test_artifacts(
+            client, arn, workflow_id, workflow_attempt, indent + 2
+        )
+
+    artifacts = []
+    for more_report in more_reports.get(f"{next_rtype.value}s", []):
+        artifacts.extend(
+            print_report(
+                client,
+                more_report,
+                next_rtype,
+                workflow_id,
+                workflow_attempt,
+                indent + 2,
+            )
+        )
+    return artifacts
+
+
 # TODO(huydhn): Extend this to support Android
 def main() -> None:
     args = parse_args()
 
+    name_prefix = args.name_prefix
+    workflow_id = args.workflow_id
+    workflow_attempt = args.workflow_attempt
+
     # NB: Device Farm is only available in us-west-2 region atm
     client = boto3.client("devicefarm", region_name="us-west-2")
-    unique_prefix = f"{args.name_prefix}-{datetime.date.today().isoformat()}-{''.join(random.sample(string.ascii_letters, 8))}"
+    unique_prefix = (
+        f"{name_prefix}-{workflow_id}-{workflow_attempt}-"
+        + f"{datetime.date.today().isoformat()}-{''.join(random.sample(string.ascii_letters, 8))}"
+    )
 
     # Upload the test app
     appfile_arn = upload_file(
@@ -121,7 +282,7 @@ def main() -> None:
         filename=args.app_file,
         filetype="IOS_APP",
     )
-    print(f"Uploaded app: {appfile_arn}")
+    info(f"Uploaded app: {appfile_arn}")
     # Upload the XCTest suite
     xctest_arn = upload_file(
         client=client,
@@ -130,7 +291,7 @@ def main() -> None:
         filename=args.xctest_file,
         filetype="XCTEST_TEST_PACKAGE",
     )
-    print(f"Uploaded XCTest: {xctest_arn}")
+    info(f"Uploaded XCTest: {xctest_arn}")
 
     # Schedule the test
     r = client.schedule_run(
@@ -143,7 +304,7 @@ def main() -> None:
     run_arn = r["run"]["arn"]
 
     start_time = datetime.datetime.now()
-    print(f"Run {unique_prefix} is scheduled as {run_arn}:")
+    info(f"Run {unique_prefix} is scheduled as {run_arn}")
 
     state = "UNKNOWN"
     result = ""
@@ -157,16 +318,17 @@ def main() -> None:
                 break
 
             waiting_time = datetime.datetime.now() - start_time
-            print(
-                f"Run {unique_prefix} in state {state} after {datetime.datetime.now() - start_time}"
-            )
+            info(f"Run {unique_prefix} in state {state} after {waiting_time}")
             time.sleep(30)
     except Exception as error:
-        warnings.warn(f"Failed to run {unique_prefix}: {error}")
+        warn(f"Failed to run {unique_prefix}: {error}")
         sys.exit(1)
+    finally:
+        print_report(
+            client, r.get("run"), ReportType.RUN, workflow_id, workflow_attempt
+        )
 
     if not result or result == "FAILED":
-        print(f"Run {unique_prefix} failed, exiting...")
         sys.exit(1)
 
 
