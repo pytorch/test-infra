pytorch/test-infra

commit 8f11dcd8c75ff288804305bcda42d8804c337c1e

[drci] Surround with try catch (#5005)
Surround certain things with try catch.

One PR failing to update/retrieve information should not prevent other
PRs from also not being able to update

There are some commits that don't have a merge base 
ex
https://github.com/pytorch/pytorch/commit/a67cd8f0c436cbe0fa3634b759bc14d6589879d1
has no grandparents

Failure listing is too long sometimes
```
Failed to update comment for PR 121810 RequestError [HttpError]: Invalid request.

Only 65535 characters are allowed; 134370 were supplied.
```
Need to trim that in a future PR

torchci/pages/api/drci/drci.ts
@@ -100,106 +100,117 @@ export async function updateDrciComments(
   const failures: { [pr: number]: { [cat: string]: RecentWorkflowsData[] } } =
     {};
 
-  await forAllPRs(workflowsByPR, async (pr_info: PRandJobs) => {
-    const labels = await fetchIssueLabels(
-      octokit,
-      pr_info.owner,
-      pr_info.repo,
-      pr_info.pr_number
-    );
-
-    const { pending, failedJobs, flakyJobs, brokenTrunkJobs, unstableJobs } =
-      await getWorkflowJobsStatuses(
-        pr_info,
-        flakyRules,
-        baseCommitJobs.get(pr_info.merge_base) || new Map(),
-        labels || []
+  await forAllPRs(
+    workflowsByPR,
+    async (pr_info: PRandJobs) => {
+      const labels = await fetchIssueLabels(
+        octokit,
+        pr_info.owner,
+        pr_info.repo,
+        pr_info.pr_number
       );
 
-    failures[pr_info.pr_number] = {
-      FAILED: failedJobs,
-      FLAKY: flakyJobs,
-      BROKEN_TRUNK: brokenTrunkJobs,
-      UNSTABLE: unstableJobs,
-    };
+      const { pending, failedJobs, flakyJobs, brokenTrunkJobs, unstableJobs } =
+        await getWorkflowJobsStatuses(
+          pr_info,
+          flakyRules,
+          baseCommitJobs.get(pr_info.merge_base) || new Map(),
+          labels || []
+        );
 
-    const failureInfo = constructResultsComment(
-      pending,
-      failedJobs,
-      flakyJobs,
-      brokenTrunkJobs,
-      unstableJobs,
-      pr_info.head_sha,
-      pr_info.merge_base,
-      pr_info.merge_base_date,
-      `${HUD_URL}${OWNER}/${repo}/${pr_info.pr_number}`
-    );
+      failures[pr_info.pr_number] = {
+        FAILED: failedJobs,
+        FLAKY: flakyJobs,
+        BROKEN_TRUNK: brokenTrunkJobs,
+        UNSTABLE: unstableJobs,
+      };
 
-    const comment = formDrciComment(
-      pr_info.pr_number,
-      OWNER,
-      repo,
-      failureInfo,
-      formDrciSevBody(sevs)
-    );
+      const failureInfo = constructResultsComment(
+        pending,
+        failedJobs,
+        flakyJobs,
+        brokenTrunkJobs,
+        unstableJobs,
+        pr_info.head_sha,
+        pr_info.merge_base,
+        pr_info.merge_base_date,
+        `${HUD_URL}${OWNER}/${repo}/${pr_info.pr_number}`
+      );
 
-    const { id, body } =
-      existingDrCiComments.get(pr_info.pr_number) ||
-      (await getDrciComment(octokit, OWNER, repo, pr_info.pr_number));
+      const comment = formDrciComment(
+        pr_info.pr_number,
+        OWNER,
+        repo,
+        failureInfo,
+        formDrciSevBody(sevs)
+      );
 
-    // The comment is there and remains unchanged, so there is no need to do anything
-    if (body === comment) {
-      return;
-    }
+      const { id, body } =
+        existingDrCiComments.get(pr_info.pr_number) ||
+        (await getDrciComment(octokit, OWNER, repo, pr_info.pr_number));
 
-    // If the id is 0, it means that the bot has failed to create the comment, so we
-    // are free to create a new one here
-    if (id === 0) {
-      await octokit.rest.issues.createComment({
-        body: comment,
+      // The comment is there and remains unchanged, so there is no need to do anything
+      if (body === comment) {
+        return;
+      }
+
+      // If the id is 0, it means that the bot has failed to create the comment, so we
+      // are free to create a new one here
+      if (id === 0) {
+        await octokit.rest.issues.createComment({
+          body: comment,
+          owner: OWNER,
+          repo: repo,
+          issue_number: pr_info.pr_number,
+        });
+      }
+      // Otherwise, update the existing comment
+      else {
+        await octokit.rest.issues.updateComment({
+          body: comment,
+          owner: OWNER,
+          repo: repo,
+          comment_id: id,
+        });
+      }
+
+      // Also update the check run status. As this is run under pytorch-bot,
+      // the check run will show up under that GitHub app
+      await octokit.rest.checks.create({
         owner: OWNER,
         repo: repo,
-        issue_number: pr_info.pr_number,
+        name: "Dr.CI",
+        head_sha: pr_info.head_sha,
+        status: "completed",
+        conclusion: "neutral",
+        output: {
+          title: "Dr.CI classification results",
+          // NB: the summary contains the classification result from Dr.CI,
+          // so that it can be queried elsewhere
+          summary: JSON.stringify(failures[pr_info.pr_number]),
+        },
       });
+    },
+    async (pr_info: PRandJobs, e: Error) => {
+      console.log("Failed to update PR", pr_info.pr_number, e);
     }
-    // Otherwise, update the existing comment
-    else {
-      await octokit.rest.issues.updateComment({
-        body: comment,
-        owner: OWNER,
-        repo: repo,
-        comment_id: id,
-      });
-    }
-
-    // Also update the check run status. As this is run under pytorch-bot,
-    // the check run will show up under that GitHub app
-    await octokit.rest.checks.create({
-      owner: OWNER,
-      repo: repo,
-      name: "Dr.CI",
-      head_sha: pr_info.head_sha,
-      status: "completed",
-      conclusion: "neutral",
-      output: {
-        title: "Dr.CI classification results",
-        // NB: the summary contains the classification result from Dr.CI,
-        // so that it can be queried elsewhere
-        summary: JSON.stringify(failures[pr_info.pr_number]),
-      },
-    });
-  });
+  );
 
   return failures;
 }
 
 async function forAllPRs(
   workflowsByPR: Map<number, PRandJobs>,
-  func: CallableFunction
+  func: CallableFunction,
+  errorFunc: CallableFunction
 ) {
   await Promise.all(
     Array.from(workflowsByPR.values()).map(async (pr_info) => {
-      await func(pr_info);
+      try {
+        await func(pr_info);
+      } catch (e) {
+        await errorFunc(pr_info, e);
+      }
     })
   );
 }
@@ -253,32 +264,42 @@ where
   );
   const newData: any[] = [];
 
-  await forAllPRs(workflowsByPR, async (pr_info: PRandJobs) => {
-    const rocksetMergeBase = rocksetMergeBases.get(pr_info.head_sha);
-    if (rocksetMergeBase === undefined) {
-      // Not found in rockset, ask github instead, then put into rockset
-      const diff = await octokit.rest.repos.compareCommits({
-        owner: OWNER,
-        repo: repo,
-        base: pr_info.head_sha,
-        head: head,
-      });
-      pr_info.merge_base = diff.data.merge_base_commit.sha;
-      pr_info.merge_base_date =
-        diff.data.merge_base_commit.commit.committer?.date ?? "";
+  await forAllPRs(
+    workflowsByPR,
+    async (pr_info: PRandJobs) => {
+      const rocksetMergeBase = rocksetMergeBases.get(pr_info.head_sha);
+      if (rocksetMergeBase === undefined) {
+        // Not found in rockset, ask github instead, then put into rockset
+        const diff = await octokit.rest.repos.compareCommits({
+          owner: OWNER,
+          repo: repo,
+          base: pr_info.head_sha,
+          head: head,
+        });
+        pr_info.merge_base = diff.data.merge_base_commit.sha;
+        pr_info.merge_base_date =
+          diff.data.merge_base_commit.commit.committer?.date ?? "";
 
-      newData.push({
-        sha: pr_info.head_sha,
-        merge_base: pr_info.merge_base,
-        changed_files: diff.data.files?.map((e) => e.filename),
-        merge_base_commit_date: pr_info.merge_base_date ?? "",
-        repo: `${OWNER}/${repo}`,
-      });
-    } else {
-      pr_info.merge_base = rocksetMergeBase.merge_base;
-      pr_info.merge_base_date = rocksetMergeBase.merge_base_commit_date;
+        newData.push({
+          sha: pr_info.head_sha,
+          merge_base: pr_info.merge_base,
+          changed_files: diff.data.files?.map((e) => e.filename),
+          merge_base_commit_date: pr_info.merge_base_date ?? "",
+          repo: `${OWNER}/${repo}`,
+        });
+      } else {
+        pr_info.merge_base = rocksetMergeBase.merge_base;
+        pr_info.merge_base_date = rocksetMergeBase.merge_base_commit_date;
+      }
+    },
+    async (pr_info: PRandJobs, e: Error) => {
+      console.log("Failed to retrieve merge base for PR", pr_info.pr_number, e);
+      // Insert dummy values if merge base can't be found
+      pr_info.merge_base =
+        "failed to retrieve merge base, please contact dev infra";
+      pr_info.merge_base_date = "0";
     }
-  });
+  );
   rocksetClient.documents.addDocuments("commons", "merge_bases", {
     data: newData,
   });

commit fb422931836f8051b908170ebfea38c36785d920

Do not set the merge base date to the mock value of 0 (#5034)
Fixes https://github.com/pytorch/test-infra/issues/5028

This is a regression after
https://github.com/pytorch/test-infra/pull/5005 and I should have
document the way Dr.CI checks for similar failures better. Here is what
happens:

1. A known fact is that Dr.CI won't be able to find the merge base for
ghstack PRs and https://github.com/pytorch/test-infra/pull/5005 fixes
the way this is handled
2. The issue, however, comes from the fact that
`pr_info.merge_base_date` is default to `0` instead of an empty string
or an undefined value
3. The date of the merge base is used to limit the range of the similar
search query here
https://github.com/pytorch/test-infra/blob/main/torchci/lib/drciUtils.ts#L263-L264.
This is used exactly to avoid FP. If the merge base date is not set, it
will fall back to the job timestamp.
4. The gotcha is when `merge_base_date` is set to 0 and `dayjs` is
"smart" enough to take this as a timestamp meaning searching for
everything since "the beginning of time". This query is bound to return
one match or another causing the FP in
https://github.com/pytorch/pytorch/pull/122273 where all failures are
marked as flaky.

### Testing

Rerun https://github.com/pytorch/pytorch/pull/122273 with the fix and
failures are shown up correctly.

<!-- drci-comment-start -->

## :link: Helpful Links
### :test_tube: See artifacts and rendered test results at
[hud.pytorch.org/pr/122273](https://hud.pytorch.org/pr/122273)
* :page_facing_up: Preview [Python docs built from this
PR](https://docs-preview.pytorch.org/pytorch/pytorch/122273/index.html)
* :page_facing_up: Preview [C++ docs built from this
PR](https://docs-preview.pytorch.org/pytorch/pytorch/122273/cppdocs/index.html)
* :question: Need help or want to give feedback on the CI? Visit the
[bot commands
wiki](https://github.com/pytorch/pytorch/wiki/Bot-commands) or our
[office

torchci/lib/drciUtils.ts
@@ -261,7 +261,9 @@ export async function querySimilarFailures(
   const failure = job.failure_captures.join(" ");
   const endDate = dayjs(job.completed_at);
   const startDate = dayjs(
-    baseCommitDate !== "" ? baseCommitDate : job.completed_at
+    baseCommitDate !== "" && baseCommitDate !== "0"
+      ? baseCommitDate
+      : job.completed_at
   ).subtract(lookbackPeriodInHours, "hour");
 
   // Get the workflow name if possible

torchci/pages/api/drci/drci.ts
@@ -292,12 +292,16 @@ where
         pr_info.merge_base_date = rocksetMergeBase.merge_base_commit_date;
       }
     },
+    // NB (huydhn): This function couldn't find merge base for ghstack PR and
+    // always throw an error in that case, so I decide to not print anything
+    // here to void confusion when seeing this error in the log
     async (pr_info: PRandJobs, e: Error) => {
-      console.log("Failed to retrieve merge base for PR", pr_info.pr_number, e);
       // Insert dummy values if merge base can't be found
       pr_info.merge_base =
         "failed to retrieve merge base, please contact dev infra";
-      pr_info.merge_base_date = "0";
+      // NB: Leave the merge base date empty or undefined here, any mock value
+      // like 0 is treated as a timestamp to use when quering similar failures
+      pr_info.merge_base_date = "";
     }
   );
   rocksetClient.documents.addDocuments("commons", "merge_bases", {