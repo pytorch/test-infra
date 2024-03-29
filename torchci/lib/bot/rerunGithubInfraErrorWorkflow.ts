19ae9fdd430880414bc04bdc341a8450ae0e5ded
clee2000 <44682903+clee2000@users.noreply.github.com>
2024-03-29 15:43:47 -0700

    [bots] Continuation of #5052 ( adding logging ) (#5053)
    https://github.com/pytorch/test-infra/pull/5052
    
    I can't even find the relevant logs on vercel, so remove the restriction
    on the type of event its run on

--- torchci/lib/bot/rerunGithubInfraErrorWorkflow.ts
+++ torchci/lib/bot/rerunGithubInfraErrorWorkflow.ts
@@ -3,12 +3,17 @@ import { Probot } from "probot";
 function rerunGithubInfraErrorWorkflow(app: Probot): void {
   // This bot is used to rerun failed workflows on pytorch/pytorch that look
   // like https://github.com/pytorch/pytorch/actions/runs/8454565307
-  app.on("workflow_run.completed", async (ctx) => {
+  app.on("workflow_run", async (ctx) => {
     const tagPrefix = "rerunGithubInfraFailure/";
     // Only run this if pytorch/pytorch, failed, is a weird infra error, and is
     // not a previous run of this bot
     ctx.log(
-      `Failed workflow_id :${ctx.payload.workflow_run.id} with conclusion: ${ctx.payload.workflow_run.conclusion} and head_branch: ${ctx.payload.workflow_run.head_branch}`
+      `Failed workflow_id: ${ctx.payload.workflow_run.id} ` +
+        `with conclusion: ${ctx.payload.workflow_run.conclusion} and ` +
+        `head_branch: ${ctx.payload.workflow_run.head_branch} and ` +
+        `name: ${ctx.payload.workflow_run.name} and ` +
+        `repository: ${ctx.payload.repository.full_name} and ` +
+        `event: ${ctx.payload.action}`
     );
     if (
       ctx.payload.repository.full_name !== "pytorch/pytorch" ||
