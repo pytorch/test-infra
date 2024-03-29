02a3be510c4f4e50e4adbbaf2dba6f986e713343
clee2000 <44682903+clee2000@users.noreply.github.com>
2024-03-29 11:52:59 -0700

    [bots] Add logging to rerunGithubInfraErrorWorkflow (#5052)
    It's not working but I can't tell why
    
    It's probably the refs thing

--- torchci/lib/bot/rerunGithubInfraErrorWorkflow.ts
+++ torchci/lib/bot/rerunGithubInfraErrorWorkflow.ts
@@ -1,14 +1,17 @@
 import { Probot } from "probot";
 
-function acceptBot(app: Probot): void {
+function rerunGithubInfraErrorWorkflow(app: Probot): void {
   // This bot is used to rerun failed workflows on pytorch/pytorch that look
   // like https://github.com/pytorch/pytorch/actions/runs/8454565307
   app.on("workflow_run.completed", async (ctx) => {
     const tagPrefix = "rerunGithubInfraFailure/";
     // Only run this if pytorch/pytorch, failed, is a weird infra error, and is
     // not a previous run of this bot
+    ctx.log(
+      `Failed workflow_id :${ctx.payload.workflow_run.id} with conclusion: ${ctx.payload.workflow_run.conclusion} and head_branch: ${ctx.payload.workflow_run.head_branch}`
+    );
     if (
-      ctx.payload.workflow_run.repository.full_name !== "pytorch/pytorch" ||
+      ctx.payload.repository.full_name !== "pytorch/pytorch" ||
       ctx.payload.workflow_run.conclusion !== "failure" ||
       !ctx.payload.workflow_run.name.startsWith(".github/workflows") ||
       ctx.payload.workflow_run.head_branch.startsWith(tagPrefix)
@@ -19,12 +22,16 @@ function acceptBot(app: Probot): void {
     // workflows might have been successfully triggered and we don't want to
     // rerun those
     const tagName = `${tagPrefix}${ctx.payload.workflow_run.id}`;
+    ctx.log(`Creating new tag: ${tagName}`);
     await ctx.octokit.git.createRef({
       owner: ctx.payload.repository.owner.login,
       repo: ctx.payload.repository.name,
-      ref: tagName,
+      ref: `refs/tags/${tagName}`,
       sha: ctx.payload.workflow_run.head_sha,
     });
+    ctx.log(
+      `Triggering workflow for ${ctx.payload.repository.owner.login}/${ctx.payload.repository.name} with ref ${tagName} and workflow_id ${ctx.payload.workflow_run.workflow_id}`
+    );
     await ctx.octokit.actions.createWorkflowDispatch({
       owner: ctx.payload.repository.owner.login,
       repo: ctx.payload.repository.name,
@@ -34,4 +41,4 @@ function acceptBot(app: Probot): void {
   });
 }
 
-export default acceptBot;
+export default rerunGithubInfraErrorWorkflow;
