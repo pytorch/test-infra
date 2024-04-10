95048475516c09f909101791fdbfd3846f99f71e
atalman <atalman@fb.com>
2024-04-10 08:27:02 -0700

    Add docker image to docker release matrix

--- tools/scripts/generate_docker_release_matrix.py
+++ tools/scripts/generate_docker_release_matrix.py
@@ -16,6 +16,7 @@ import os
 import sys
 import argparse
 from typing import Dict, List
+from datetime import datetime
 
 import generate_binary_build_matrix
 
@@ -27,6 +28,16 @@ def generate_docker_matrix(channel: str) -> Dict[str, List[Dict[str, str]]]:
     ret: List[Dict[str, str]] = []
     for cuda in generate_binary_build_matrix.CUDA_ARCHES_DICT[channel]:
         version = generate_binary_build_matrix.CUDA_CUDDN_VERSIONS[cuda]
+
+        prefix = "ghcr.io/pytorch/pytorch"
+        docker_image_version = ""
+        if channel == "release":
+            docker_image_version = f"{prefix}:{generate_binary_build_matrix.CURRENT_STABLE_VERSION}"
+        elif channel == "test":
+            docker_image_version = f"{prefix}-test:{generate_binary_build_matrix.CURRENT_CANDIDATE_VERSION}"
+        else:
+            docker_image_version = f"{prefix}-nightly:{generate_binary_build_matrix.CURRENT_NIGHTLY_VERSION}.dev{datetime.today().strftime('%Y%m%d')}"
+
         for image in DOCKER_IMAGE_TYPES:
             ret.append(
                 {
@@ -34,6 +45,7 @@ def generate_docker_matrix(channel: str) -> Dict[str, List[Dict[str, str]]]:
                     "cuda_full_version": version["cuda"],
                     "cudnn_version": version["cudnn"],
                     "image_type": image,
+                    "docker": f"{docker_image_version}-cuda{cuda}-cudnn{version['cudnn']}-{image}",
                     "platform": "linux/arm64,linux/amd64",
                 }
             )
