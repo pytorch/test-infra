20c071fcae6983e9c2c3b35fee3129e0113f6f77
atalman <atalman@fb.com>
2024-04-25 12:08:18 -0700

    test

--- aws/websites/download.pytorch.org/meta_cdn_whl_redirect.js
+++ aws/websites/download.pytorch.org/meta_cdn_whl_redirect.js
@@ -4,11 +4,19 @@ function handler(event) {
     var CDN_TEST_PATH = '/whl/cdntest/';
 
     // if we are requesting for whl files in cdntest path - perform the translation to meta CDN
-    if(uri.startsWith(CDN_TEST_PATH) && uri.endsWith('.whl')) {
+    if( uri.startsWith(CDN_TEST_PATH) && uri.endsWith('.whl') ) {
         var uri_suffix = uri.slice(CDN_TEST_PATH.length).split('/').join('%2F');
         var meta_cdn_path = 'https://scontent.xx.fbcdn.net/mci_ab/uap/?ab_b=m&amp;ab_page=PyTorchBinary&amp;ab_entry=tree%2Fwhl%2Ftest%2F'
-        request.uri=meta_cdn_path+uri_suffix
-    }
 
+        const response = {
+                statusCode: 302,
+                statusDescription: 'Found',
+                headers:
+                    { "location": { "value": meta_cdn_path+uri_suffix } }
+                }
+
+        return response;
+
+    }
     return request;
 }
