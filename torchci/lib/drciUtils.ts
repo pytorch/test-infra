81dc0d0cafa84524242b62dd6a0f542a3193a426
Huy Do <huydhn@gmail.com>
2024-04-25 17:48:17 -0700

    Fix typo

--- torchci/lib/drciUtils.ts
+++ torchci/lib/drciUtils.ts
@@ -41,7 +41,6 @@ export const BOT_COMMANDS_WIKI_URL =
   "https://github.com/pytorch/pytorch/wiki/Bot-commands";
 export const FLAKY_RULES_JSON =
   "https://raw.githubusercontent.com/pytorch/test-infra/generated-stats/stats/flaky-rules.json";
-
 export const EXCLUDED_FROM_FLAKINESS = [
   "lint",
   "linux-docs",
