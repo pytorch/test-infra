a7c25f05e8d47b763aae8002da0e55bdc42af5a4
Andrey Talman <atalman@fb.com>
2024-04-29 18:40:24 -0400

    Update manage.py

--- s3_management/manage.py
+++ s3_management/manage.py
@@ -46,7 +46,7 @@ PACKAGE_ALLOW_LIST = {
     "aiohttp",
     "aiosignal",
     "antlr4_python3_runtime",
-    "antlr4-python3-runtime",
+    "antlr4",
     "attrs",
     "blobfile",
     "datasets",
