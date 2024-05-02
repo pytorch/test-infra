958794428d855138ba2de791caea1bb568efcc7f
clee2000 <44682903+clee2000@users.noreply.github.com>
2024-05-02 10:43:01 -0700

    [TDB] Reruns + TD exclusions (#5157)
    Adds small button to toggle additional test info, which can show test
    reruns and TD exclusions
    
    When toggled, the workflow box becomes wide, meaning it takes up the
    entire row
    
    Only looks at first run attempt because test stat uploader doesnt really
    work with it right now and HUD isn't really configured to work with it
    either
    
    https://torchci-git-csl-tda1-fbopensource.vercel.app/pr/125246

---
+++ torchci/components/additionalTestInfo/TestInfo.tsx
@@ -0,0 +1,305 @@
+import { JobData } from "lib/types";
+import { useState } from "react";
+import _ from "lodash";
+import styles from "./TestInfo.module.css";
+import useSWR from "swr";
+import { fetcher } from "lib/GeneralUtils";
+
+function RecursiveDetailsSummary({
+  info,
+  level,
+  summaryFunction = (name: any, info: any) => <>{name}</>,
+  children,
+}: {
+  info: any;
+  level: number;
+  summaryFunction?: (name: any, info: any) => JSX.Element;
+  children: (name: any, info: any, numSiblings: number) => JSX.Element;
+}) {
+  const keysInInfo = Object.keys(info);
+  if (level == 0) {
+    return (
+      <ul style={{ paddingLeft: "1em" }}>
+        {keysInInfo.map((config) => (
+          <li style={{ listStyleType: "none" }} key={config}>
+            {children(config, info[config], keysInInfo.length)}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+  return (
+    <ul style={{ paddingLeft: "1em" }}>
+      {keysInInfo.map((config) => (
+        <li style={{ listStyleType: "none" }} key={config}>
+          <details open={keysInInfo.length == 1}>
+            <summary>{summaryFunction(config, info[config])}</summary>
+            <RecursiveDetailsSummary
+              key={config}
+              summaryFunction={summaryFunction}
+              info={info[config]}
+              level={level - 1}
+            >
+              {children}
+            </RecursiveDetailsSummary>
+          </details>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function TestRerunsInfoIndividiual({
+  info,
+  name,
+  numSiblings,
+}: {
+  info: any;
+  name: any;
+  numSiblings: number;
+}) {
+  const [failures, successes] = _.partition(
+    Array.from(info),
+    (i: any) => i.failure
+  );
+
+  const [trackbacksToShow, setTrackbacksToShow] = useState(
+    new Set<number>(failures.length == 1 ? [0] : [])
+  );
+
+  return (
+    <details open={numSiblings == 1}>
+      <summary>
+        {name} ({failures.length} reruns)
+      </summary>
+      {failures.map((i: any, ind: number) => {
+        return (
+          <div key={ind}>
+            <span
+              onClick={() => {
+                if (trackbacksToShow.has(ind)) {
+                  const newSet = new Set(trackbacksToShow);
+                  newSet.delete(ind);
+                  setTrackbacksToShow(newSet);
+                } else {
+                  setTrackbacksToShow(new Set(trackbacksToShow).add(ind));
+                }
+              }}
+            >
+              Show Trackback #{ind + 1} on
+            </span>
+            <a href={`#${i.job_id}-box`}> job {i.job_id}</a>
+            {trackbacksToShow.has(ind) && (
+              <div
+                style={{
+                  overflowY: "auto",
+                  maxHeight: "50vh",
+                  borderColor: "black",
+                  borderStyle: "solid",
+                  borderWidth: "1px",
+                  padding: "0em 0.5em",
+                }}
+              >
+                <pre>{i.failure.text}</pre>
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </details>
+  );
+}
+
+function TestRerunsInfo({
+  workflowId,
+  jobs,
+  runAttempt,
+}: {
+  workflowId: string;
+  jobs: JobData[];
+  runAttempt: string;
+}) {
+  const shouldShow =
+    jobs.every((job) => job.conclusion !== "pending") &&
+    jobs.some((job) => job.name!.includes("/ test "));
+
+  const { data: info, error } = useSWR(
+    shouldShow
+      ? `https://ossci-raw-job-status.s3.amazonaws.com/additional_info/reruns/${workflowId}/${runAttempt}`
+      : null,
+    fetcher
+  );
+
+  if (!shouldShow) {
+    return <div>Workflow is still pending or there are no test jobs</div>;
+  }
+
+  if (error) {
+    return <div>Error retrieving data {`${error}`}</div>;
+  }
+
+  if (!info) {
+    return <div>Loading...</div>;
+  }
+  if (Object.keys(info).length == 0) {
+    return <div>No tests were rerun or there was trouble parsing data</div>;
+  }
+
+  return (
+    <>
+      <div
+        style={{ fontSize: "1.17em", fontWeight: "bold", paddingTop: "1em" }}
+      >
+        Info about tests that got rerun
+      </div>
+      <div>
+        <RecursiveDetailsSummary info={info} level={4}>
+          {(name: any, info: any, numSiblings: number) => (
+            <TestRerunsInfoIndividiual
+              key={name}
+              info={info}
+              name={name}
+              numSiblings={numSiblings}
+            />
+          )}
+        </RecursiveDetailsSummary>
+      </div>
+    </>
+  );
+}
+
+function TDInfo({
+  workflowId,
+  jobs,
+  runAttempt,
+}: {
+  workflowId: string;
+  jobs: JobData[];
+  runAttempt: string;
+}) {
+  const shouldShow =
+    jobs.every((job) => job.conclusion !== "pending") &&
+    jobs.some((job) => job.name!.includes("/ test "));
+  const { data: info, error } = useSWR(
+    shouldShow
+      ? `https://ossci-raw-job-status.s3.amazonaws.com/additional_info/td_exclusions/${workflowId}/${runAttempt}`
+      : null,
+    fetcher
+  );
+
+  if (!shouldShow) {
+    return <div>Workflow is still pending or there are no test jobs</div>;
+  }
+
+  if (error) {
+    return <div>Error retrieving data {`${error}`}</div>;
+  }
+
+  if (!info) {
+    return <div>Loading...</div>;
+  }
+
+  if (Object.keys(info).length == 0) {
+    return (
+      <div>No test files were excluded or there was trouble parsing data</div>
+    );
+  }
+  return (
+    <div>
+      <div
+        style={{ fontSize: "1.17em", fontWeight: "bold", paddingTop: "1em" }}
+      >
+        Excluded test files by job
+      </div>
+      <RecursiveDetailsSummary
+        info={info}
+        level={1}
+        summaryFunction={(name: any, info: any) => {
+          const count = Object.values(info).reduce(
+            (prev: number, curr: any) => curr.length + prev,
+            0
+          );
+          return (
+            <>
+              {name} ({count})
+            </>
+          );
+        }}
+      >
+        {(config: any, configInfo: any, numSiblings: number) => (
+          <details open={numSiblings == 1} key={config}>
+            <summary>
+              {config} ({configInfo.length})
+            </summary>
+            <div
+              style={{
+                overflowY: "auto",
+                maxHeight: "50vh",
+                borderColor: "black",
+                borderStyle: "solid",
+                borderWidth: "1px",
+                padding: "0em 0.5em",
+              }}
+            >
+              {Array.from(configInfo).map((file: any) => {
+                return <div key={file}>{file}</div>;
+              })}
+            </div>
+          </details>
+        )}
+      </RecursiveDetailsSummary>
+    </div>
+  );
+}
+
+export function TestInfo({
+  workflowId,
+  runAttempt,
+  jobs,
+}: {
+  workflowId: string;
+  runAttempt: string;
+  jobs: JobData[];
+}) {
+  const [showInfo, setShowInfo] = useState("Reruns Info");
+
+  function setShowInfoHelper(info: string) {
+    if (showInfo === info) {
+      setShowInfo("none");
+    } else {
+      setShowInfo(info);
+    }
+  }
+
+  function ButtonSelector({ name }: { name: string }) {
+    return (
+      <button
+        onClick={() => setShowInfoHelper(name)}
+        className={showInfo === name ? styles.active : ""}
+      >
+        {name}
+      </button>
+    );
+  }
+
+  return (
+    <>
+      <div className={styles.tab}>
+        <ButtonSelector name="Reruns Info" />
+        <ButtonSelector name="TD Info" />
+      </div>
+      <div className={styles.tabcontent}>
+        {showInfo == "TD Info" && (
+          <TDInfo workflowId={workflowId} jobs={jobs} runAttempt={runAttempt} />
+        )}
+        {showInfo == "Reruns Info" && (
+          <TestRerunsInfo
+            workflowId={workflowId}
+            jobs={jobs}
+            runAttempt={runAttempt}
+          />
+        )}
+      </div>
+    </>
+  );
+}
