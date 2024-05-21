3be34c099d0d5c4905496bd88ee9be8806488ebe
Zain Rizvi <ZainR@meta.com>
2024-05-21 14:08:14 -0500

    lint

--- torchci/pages/hud/[repoOwner]/[repoName]/[branch]/[[...page]].tsx
+++ torchci/pages/hud/[repoOwner]/[repoName]/[branch]/[[...page]].tsx
@@ -29,7 +29,13 @@ import useHudData from "lib/useHudData";
 import useTableFilter from "lib/useTableFilter";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import React, { createContext, useContext, useEffect, useRef, useState } from "react";
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import PageSelector from "components/PageSelector";
 import {
   isFailedJob,
