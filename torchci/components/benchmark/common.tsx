79dca728a04f592550b872c32ece8fb99eec5398
Huy Do <huydhn@gmail.com>
2024-06-04 18:56:22 -0700

    Fix typo

--- torchci/components/benchmark/common.tsx
+++ torchci/components/benchmark/common.tsx
@@ -2,6 +2,6 @@ export const SHA_DISPLAY_LENGTH = 10;
 export const MAIN_BRANCH = "main";
 export const LOG_PREFIX = "https://ossci-raw-job-status.s3.amazonaws.com/log";
 export const DEFAULT_REPO_NAME = "pytorch/pytorch";
-export const HUD_PREFIX = `${DEFAULT_REPO_NAME}/commit`;
+export const HUD_PREFIX = `/${DEFAULT_REPO_NAME}/commit`;
 export const LAST_N_DAYS = 7;
 export const TIME_FIELD_NAME = "granularity_bucket";
