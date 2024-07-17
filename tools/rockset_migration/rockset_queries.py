f6379292dfe0500102ea8df6b38deb56dc8e2a11
Huy Do <huydhn@gmail.com>
2024-07-17 11:26:05 -0700

    [BE] Apply lintrunner on rockset_queries.py (#5409)
    Run `lintrunner -a` on the script and fix all lint issues. As this is
    exported from the notebook, I apply the same changes to the notebook
    too.

--- tools/rockset_migration/rockset_queries.py
+++ tools/rockset_migration/rockset_queries.py
@@ -2,25 +2,29 @@
 # coding: utf-8
 
 # # Parse and cleanup Rockset query lambdas and collections
-# This code is for analyzing Rockset query lambdas and collections to help determine how critical each one is to our infrastructure
-# 
+# This code is for analyzing Rockset query lambdas and collections to help determine
+# how critical each one is to our infrastructure
+#
 # We'll also use this to delete some query lambdas that are clearly not used
-# 
+#
 # Note: rockset_queries.py is generated from rockset_queries.ipynb with the command:
-# `jupyter nbconvert --to script rockset_queries.ipynb`.  It is there just for ease of code reviews, any edits/execution is intended to occur in rockset_queries.ipynb (which is the source of truth)
+# `jupyter nbconvert --to script rockset_queries.ipynb`.  It is there just
+# for ease of code reviews, any edits/execution is intended to occur in
+# rockset_queries.ipynb (which is the source of truth)
 
 # In[ ]:
 
 
-import requests
-import json
 import datetime
+import json
 import os
 from pathlib import Path
 
-from typing import NamedTuple, List, Dict, Any
+from typing import Any, Dict, List, NamedTuple
 
-ROCKSET_API_KEY = os.environ.get('ROCKSET_API_KEY')
+import requests
+
+ROCKSET_API_KEY = os.environ.get("ROCKSET_API_KEY")
 
 # In[ ]:
 
@@ -41,10 +45,11 @@ class LambdaQuery(NamedTuple):
     sql: Dict[str, Any]
     raw_response: Dict[str, Any]
 
-    def printfields(self, fields: List[str]):
+    def printfields(self, fields: List[str]) -> None:
         print(f"Query: {self.workspace}.{self.name}")
         for field in fields:
             print(f"  {field}: {getattr(self, field)}")
+
 
 def get_description_from_query_sql(sql: str) -> str:
     """
@@ -66,12 +71,13 @@ def get_description_from_query_sql(sql: str) -> str:
             break
     return description.strip()
 
-def get_query_lambdas():
+
+def get_query_lambdas() -> Dict[str, LambdaQuery]:
     url = "https://api.usw2a1.rockset.com/v1/orgs/self/lambdas"
 
     headers = {
         "accept": "application/json",
-        "Authorization": f"ApiKey {ROCKSET_API_KEY}"
+        "Authorization": f"ApiKey {ROCKSET_API_KEY}",
     }
 
     response = requests.get(url, headers=headers)
@@ -89,14 +95,19 @@ def get_query_lambdas():
             version_count=lambdaquery["version_count"],
             collections=lambdaquery["latest_version"]["collections"],
             last_executed=lambdaquery["latest_version"]["stats"]["last_executed"],
-            last_execution_error=lambdaquery["latest_version"]["stats"]["last_execution_error"],
+            last_execution_error=lambdaquery["latest_version"]["stats"][
+                "last_execution_error"
+            ],
             description=lambdaquery["latest_version"]["description"],
-            human_description=get_description_from_query_sql(lambdaquery["latest_version"]["sql"]["query"]),
+            human_description=get_description_from_query_sql(
+                lambdaquery["latest_version"]["sql"]["query"]
+            ),
             sql=lambdaquery["latest_version"]["sql"],
             raw_response=lambdaquery,
         )
 
     return queries
+
 
 # In[ ]:
 
@@ -114,15 +125,16 @@ class Collections(NamedTuple):
     size: int
     raw_response: Dict[str, Any]
 
-def get_collections() -> Dict:
+
+def get_collections() -> Dict[str, Collections]:
     url = "https://api.usw2a1.rockset.com/v1/orgs/self/collections"
     headers = {
         "accept": "application/json",
-        "Authorization": f"ApiKey {ROCKSET_API_KEY}"
+        "Authorization": f"ApiKey {ROCKSET_API_KEY}",
     }
     response = requests.get(url, headers=headers)
     data = json.loads(response.text)
-    print (json.dumps(data, indent=2))
+    print(json.dumps(data, indent=2))
 
     collections = {}
     for collection in data["data"]:
@@ -135,7 +147,11 @@ def get_collections() -> Dict:
             workspace=collection["workspace"],
             created_at=collection["created_at"],
             created_by=collection["created_by"],
-            last_queried=datetime.datetime.fromtimestamp(collection["stats"]["last_queried_ms"] / 1000),
+            last_queried=str(
+                datetime.datetime.fromtimestamp(
+                    collection["stats"]["last_queried_ms"] / 1000
+                )
+            ),
             last_queried_raw=collection["stats"]["last_queried_ms"],
             size=collection["stats"]["total_size"],
             doc_count=collection["stats"]["doc_count"],
@@ -148,43 +164,59 @@ def get_collections() -> Dict:
     collections = dict(sorted(collections.items()))
     return collections
 
-# 
+
+#
 
 # In[ ]:
 
 
-def print_human_descriptions(queries: Dict[str, LambdaQuery]):
+def print_human_descriptions(queries: Dict[str, LambdaQuery]) -> None:
     for k, query in queries.items():
         if query.human_description:
             print(f"{k}:\n {query.human_description}")
 
-def have_human_descriptions(queries: Dict[str, LambdaQuery]):
+
+def have_human_descriptions(queries: Dict[str, LambdaQuery]) -> Dict[str, LambdaQuery]:
     """Returns a dict of queries that have human descriptions"""
     return {k: v for k, v in queries.items() if v.human_description}
 
-def inactive_queries(queries: Dict[str, LambdaQuery]) -> Dict:
+
+def inactive_queries(queries: Dict[str, LambdaQuery]) -> Dict[str, LambdaQuery]:
     return {k: v for k, v in queries.items() if v.state != "ACTIVE"}
 
 
-def not_run(info: Dict[str, LambdaQuery]) -> Dict:
+def not_run(info: Dict[str, LambdaQuery]) -> Dict[str, LambdaQuery]:
     """Queries that have never been run."""
     return {k: v for k, v in info.items() if v.last_executed is None}
 
-def not_recently_run(info: Dict[str, LambdaQuery], days: int) -> Dict:
+
+def not_recently_run(info: Dict[str, LambdaQuery], days: int) -> Dict[str, LambdaQuery]:
     """Queries that have not been run in the last `days` days."""
     cutoff = datetime.datetime.now() - datetime.timedelta(days=days)
     return {
-        k: v for k, v in info.items()
-        if v.last_executed is not None and datetime.datetime.strptime(v.last_executed, "%Y-%m-%dT%H:%M:%SZ") < cutoff}
+        k: v
+        for k, v in info.items()
+        if v.last_executed is not None
+        and datetime.datetime.strptime(v.last_executed, "%Y-%m-%dT%H:%M:%SZ") < cutoff
+    }
 
-def queries_run_recently(queries: Dict[str, LambdaQuery], days: int) -> Dict:
+
+def queries_run_recently(
+    queries: Dict[str, LambdaQuery], days: int
+) -> Dict[str, LambdaQuery]:
     """Queries that have been run in the last `days` days."""
     cutoff = datetime.datetime.now() - datetime.timedelta(days=days)
     return {
-        k: v for k, v in queries.items()
-        if v.last_executed is not None and datetime.datetime.strptime(v.last_executed, "%Y-%m-%dT%H:%M:%SZ") > cutoff}
+        k: v
+        for k, v in queries.items()
+        if v.last_executed is not None
+        and datetime.datetime.strptime(v.last_executed, "%Y-%m-%dT%H:%M:%SZ") > cutoff
+    }
 
-def not_in(queries: Dict[str, LambdaQuery], excepting: Dict[str, LambdaQuery]) -> Dict:
+
+def not_in(
+    queries: Dict[str, LambdaQuery], excepting: Dict[str, LambdaQuery]
+) -> Dict[str, LambdaQuery]:
     """Returns LambdaQueries that are not in the excepting dictionary."""
     return {k: v for k, v in queries.items() if k not in excepting}
 
@@ -197,11 +229,12 @@ def delete_lambda(query: LambdaQuery) -> None:
 
     headers = {
         "accept": "application/json",
-        "Authorization": f"ApiKey {ROCKSET_API_KEY}"
+        "Authorization": f"ApiKey {ROCKSET_API_KEY}",
     }
 
     response = requests.delete(url, headers=headers)
     print(response.text)
+
 
 def backup_lambdas(queries: Dict[str, LambdaQuery], dir: Path) -> None:
     # Create dir if it doesn't exist
@@ -211,6 +244,7 @@ def backup_lambdas(queries: Dict[str, LambdaQuery], dir: Path) -> None:
             f.write(json.dumps(query.sql, indent=2))
         with open(dir / f"{query.workspace}.{query.name}.raw.json", "w") as f:
             f.write(json.dumps(query.raw_response, indent=2))
+
 
 # In[ ]:
 
@@ -256,10 +290,11 @@ len(have_human_descriptions(important_queries))
 # In[ ]:
 
 
-def printq(queries: Dict[str, LambdaQuery], fields) -> None:
+def printq(queries: Dict[str, LambdaQuery], fields: List[str]) -> None:
     for query in queries.values():
         query.printfields(fields)
         print()
+
 
 def print_query_descriptions(queries: Dict[str, LambdaQuery]) -> None:
     for query in queries.values():
@@ -270,6 +305,7 @@ def print_query_descriptions(queries: Dict[str, LambdaQuery]) -> None:
             print(f" - {query.description}")
         else:
             print()
+
 
 occasionally_run = not_in(important_queries, queries_run_recently(queries, 7))
 len(occasionally_run)
@@ -288,17 +324,23 @@ len(collections)
 
 # List all collections in collections that are not in any collection
 # used by the important_queries
-def unused_collections(collections: Dict[str, Collections], queries: Dict[str, LambdaQuery]) -> Dict:
+def unused_collections(
+    collections: Dict[str, Collections], queries: Dict[str, LambdaQuery]
+) -> Dict[str, Collections]:
     used_collections = set()
     for query in queries.values():
         used_collections.update(query.collections)
     return {k: v for k, v in collections.items() if k not in used_collections}
 
-def used_collections(collections: Dict[str, Collections], queries: Dict[str, LambdaQuery]) -> Dict:
+
+def used_collections(
+    collections: Dict[str, Collections], queries: Dict[str, LambdaQuery]
+) -> Dict[str, Collections]:
     used_collections = set()
     for query in queries.values():
         used_collections.update(query.collections)
     return {k: v for k, v in collections.items() if k in used_collections}
+
 
 print("Used collections:")
 for collection in used_collections(collections, important_queries).values():
