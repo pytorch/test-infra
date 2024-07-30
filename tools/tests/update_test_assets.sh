e712f00444c3be241c7a013ef07f154ce35d2a61
atalman <atalman@fb.com>
2024-07-30 12:17:24 -0700

    test

--- tools/tests/update_test_assets.sh
+++ tools/tests/update_test_assets.sh
@@ -3,7 +3,6 @@ python ../scripts/generate_binary_build_matrix.py --build-python-only disable --
 python ../scripts/generate_binary_build_matrix.py --build-python-only disable --with-rocm disable --with-xpu disable > ../tests/assets/build_matrix_linux_wheel_cuda_norocm.json
 python ../scripts/generate_binary_build_matrix.py --build-python-only disable --with-cpu disable --with-xpu disable > ../tests/assets/build_matrix_linux_wheel_nocpu.json
 python ../scripts/generate_binary_build_matrix.py --build-python-only disable --with-cpu disable --with-rocm disable  --with-xpu enable > ../tests/assets/build_matrix_linux_wheel_xpu.json
-python ../scripts/generate_binary_build_matrix.py --build-python-only disable --with-xpu enable > ../tests/assets/build_matrix_linux_wheel_cuda.json
 python ../scripts/generate_binary_build_matrix.py --build-python-only disable --package-type conda > ../tests/assets/build_matrix_linux_conda_cuda.json
 python ../scripts/generate_binary_build_matrix.py --build-python-only disable --package-type conda > ../tests/assets/build_matrix_linux_conda_cuda.json
 python ../scripts/generate_binary_build_matrix.py --build-python-only disable --operating-system="macos" --with-cuda disable --with-rocm disable > ../tests/assets/build_matrix_macos_wheel.json
