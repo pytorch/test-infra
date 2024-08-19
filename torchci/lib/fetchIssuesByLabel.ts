0cd0f6c9e5aa803533c203bdd87a3039a6839a85
Ivan Zaitsev <ivanzaitsev@fb.com>
2024-08-19 15:49:50 -0700

    migrate issue_query to Clickhouse

--- torchci/lib/fetchIssuesByLabel.ts
+++ torchci/lib/fetchIssuesByLabel.ts
@@ -1,24 +1,36 @@
 import rocksetVersions from "rockset/prodVersions.json";
+import { enableClickhouse, queryClickhouseSaved } from "./clickhouse";
 import getRocksetClient from "./rockset";
 import { IssueData } from "./types";
 
 export default async function fetchIssuesByLabel(
-  label: string
+  label: string,
+  useClickhouse?: boolean
 ): Promise<IssueData[]> {
-  const rocksetClient = getRocksetClient();
-  const query = await rocksetClient.queryLambdas.executeQueryLambda(
-    "commons",
-    "issue_query",
-    rocksetVersions.commons.issue_query,
-    {
-      parameters: [
-        {
-          name: "label",
-          type: "string",
-          value: label as string,
-        },
-      ],
-    }
-  );
-  return query.results!;
+  if (useClickhouse === undefined) {
+    useClickhouse = enableClickhouse();
+  }
+
+  if (!useClickhouse) {
+    const rocksetClient = getRocksetClient();
+    const query = await rocksetClient.queryLambdas.executeQueryLambda(
+      "commons",
+      "issue_query",
+      rocksetVersions.commons.issue_query,
+      {
+        parameters: [
+          {
+            name: "label",
+            type: "string",
+            value: label as string,
+          },
+        ],
+      }
+    );
+    return query.results!;
+  } else {
+    return queryClickhouseSaved("issue_query", {
+      label,
+    }) as Promise<IssueData[]>;
+  }
 }
