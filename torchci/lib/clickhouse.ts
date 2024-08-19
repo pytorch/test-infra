6d4015749cc32ee502f7ddf1480dc5a685848607
clee2000 <44682903+clee2000@users.noreply.github.com>
2024-08-18 20:33:41 -0700

    [HUD] Add clickhouse client and some useful functions and an example of a query (#5563)
    Adds various functions to query clickhouse
    * `queryClickhouse` for adhoc queries with a string
    * `queryClickhouseSaved` for a less sophisticated version of Rockset's
    query lambdas + prodVersions (see
    https://github.com/pytorch/test-infra/pull/5563#discussion_r1720420875.
    This is the best solution I have for now that will enable version
    control)
    * `enableClickhouse` to check if clickhouse is enabled for HUD in prod
    
    Adds clickhouse query for `commit_jobs_query` as an example of structure
    (It does return data in a list of dictionaries format, but it is not
    functional due to downstream issues after query, I believe it's
    something to do with typing)
    * Gates based on `enableClickhouse` so that it can be switched easily at
    will
    * Note the file structure for the `clickhouse_queries` folder

---
+++ torchci/lib/clickhouse.ts
@@ -0,0 +1,57 @@
+import { createClient } from "@clickhouse/client";
+import { readFileSync } from "fs";
+
+export function getClickhouseClient() {
+  return createClient({
+    host: process.env.CLICKHOUSE_HUD_USER_URL ?? "http://localhost:8123",
+    username: process.env.CLICKHOUSE_HUD_USER_USERNAME ?? "default",
+    password: process.env.CLICKHOUSE_HUD_USER_PASSWORD ?? "",
+  });
+}
+
+export async function queryClickhouse(
+  query: string,
+  params: Record<string, unknown>
+) {
+  /**
+   * queryClickhouse
+   * @param query: string, the sql query
+   * @param params: Record<string, unknown>, the parameters to the query ex { sha: "abcd" }
+   */
+  const clickhouseClient = getClickhouseClient();
+  const res = await clickhouseClient.query({
+    query,
+    format: "JSONEachRow",
+    query_params: params,
+  });
+
+  return await res.json();
+}
+
+export async function queryClickhouseSaved(
+  queryName: string,
+  inputParams: Record<string, unknown>
+) {
+  /**
+   * queryClickhouseSaved
+   * @param queryName: string, the name of the query, which is the name of the folder in clickhouse_queries
+   * @param inputParams: Record<string, unknown>, the parameters to the query, an object where keys are the parameter names
+   *
+   * This function will filter the inputParams to only include the parameters that are in the query params json file
+   */
+  const query = readFileSync(
+    `clickhouse_queries/${queryName}/query.sql`,
+    "utf8"
+  );
+  const paramsText = require(`clickhouse_queries/${queryName}/params.json`);
+
+  const queryParams = new Map(
+    Object.entries(paramsText).map(([key, _]) => [key, inputParams[key]])
+  );
+  return await queryClickhouse(query, Object.fromEntries(queryParams));
+}
+
+export function enableClickhouse() {
+  // Use this to quickly toggle between clickhouse and rockset
+  return process.env.USE_CLICKHOUSE == "true";
+}
