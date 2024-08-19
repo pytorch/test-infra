7dc1e4827195aef7c0f4320c4a7c2c5d3eb7e492
Zain Rizvi <ZainR@meta.com>
2024-08-19 17:49:40 -0500

    Add clickhouse version of last_branch_push query Tested in ClickHouse Cloud UI

---
+++ torchci/clickhouse_queries/last_branch_push/query.sql
@@ -0,0 +1,15 @@
+-- Elapsed seconds since the last commit was pushed to pytorch/pytorch's main branch
+
+select
+    DATE_DIFF('second', head_commit.timestamp, CURRENT_TIMESTAMP()) as push_seconds_ago
+from
+    push
+where
+    push.ref = { branch : String }
+    AND push.repository.owner.name = 'pytorch'
+    AND push.repository.name = 'pytorch'
+    AND push.head_commit is not null
+order by
+    push.head_commit.timestamp desc
+limit
+    1
