abfa304db450c4d22ae98517651bbb6f67ac661f
Sergii Dymchenko <sdym@meta.com>
2024-08-23 14:38:17 -0700

    Update torchao.tsx

--- torchci/pages/benchmark/torchao.tsx
+++ torchci/pages/benchmark/torchao.tsx
@@ -3,8 +3,6 @@ import { BranchAndCommitPicker } from "components/benchmark/BranchAndCommitPicke
 import { CommitPanel } from "components/benchmark/CommitPanel";
 import { LAST_N_DAYS, MAIN_BRANCH } from "components/benchmark/common";
 import { BenchmarkLogs } from "components/benchmark/compilers/BenchmarkLogs";
-#import { GraphPanel } from "components/benchmark/compilers/SummaryGraphPanel";
-#import { SummaryPanel } from "components/benchmark/compilers/SummaryPanel";
 import {
   DTypePicker,
   ModePicker,
