88507ba7e0bcc6e4e819ac2de4e8949cc2f40c66
Catherine Lee <csl@fb.com>
2024-08-28 13:55:17 -0700

    tc

--- torchci/clickhouse_queries/strict_lag_sec/query.sql
+++ torchci/clickhouse_queries/strict_lag_sec/query.sql
@@ -1,30 +1,29 @@
--- !!! Query is not converted to CH syntax yet.  Delete this line when it gets converted
 WITH master as (
     SELECT
-        PARSE_TIMESTAMP_ISO8601(push.head_commit.timestamp) as master
+        push.head_commit.timestamp as master
     FROM
         push
     WHERE
-        push.ref = :head
+        push.ref = {head: String }
         AND push.repository.owner.name = 'pytorch'
-        AND push.repository.name = :repo
-        AND push.head_commit is not null
+        AND push.repository.name = {repo: String }
+        AND push.head_commit.id != ''
     ORDER BY
-        push._event_time desc
+        push.head_commit.timestamp desc
     LIMIT
         1
 ), strict as (
     SELECT
-        PARSE_TIMESTAMP_ISO8601(push.head_commit.timestamp) as strict
+        push.head_commit.timestamp as strict
     FROM
         push
     WHERE
         push.ref = 'refs/heads/viable/strict'
         AND push.repository.owner.name = 'pytorch'
-        AND push.repository.name = :repo
-        AND push.head_commit is not null
+        AND push.repository.name = {repo: String }
+        AND push.head_commit.id != ''
     ORDER BY
-        push._event_time desc
+        push.head_commit.timestamp desc
     LIMIT
         1
 )
