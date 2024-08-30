e172534fbe0f8a4801875d7fb48285948f79be0b
Wouter Devriendt <wouterdevriendt@meta.com>
2024-08-30 15:46:10 -0700

    add dynamo tables to terraform (prep for backup)

---
+++ terraform-aws-github-runner/dynamodb.tf
@@ -0,0 +1,183 @@
+resource "aws_dynamodb_table" "pytorchbot-logs" {
+    name           = "pytorchbot-logs"
+    hash_key = "dynamoKey"
+    billing_mode   = "PROVISIONED"
+    read_capacity = 1
+    write_capacity = 1
+    stream_enabled = true
+    stream_view_type = "NEW_AND_OLD_IMAGES"
+    attribute {
+        name = "dynamoKey"
+        type = "S"
+    }
+}
+
+resource "aws_dynamodb_table" "torchci-dynamo-perf-stats" {
+    name           = "torchci-dynamo-perf-stats"
+    hash_key = "dynamoKey"
+    billing_mode   = "PAY_PER_REQUEST"
+    stream_enabled = true
+    stream_view_type = "NEW_AND_OLD_IMAGES"
+    attribute {
+        name = "dynamoKey"
+        type = "S"
+    }
+}
+
+resource "aws_dynamodb_table" "torchci-issue-comment" {
+    name           = "torchci-issue-comment"
+    hash_key = "dynamoKey"
+    billing_mode   = "PAY_PER_REQUEST"
+    stream_enabled = true
+    stream_view_type = "NEW_AND_OLD_IMAGES"
+    attribute {
+        name = "dynamoKey"
+        type = "S"
+    }
+}
+
+resource "aws_dynamodb_table" "torchci-issues" {
+    name           = "torchci-issues"
+    hash_key = "dynamoKey"
+    billing_mode   = "PAY_PER_REQUEST"
+    stream_enabled = true
+    stream_view_type = "NEW_AND_OLD_IMAGES"
+    attribute {
+        name = "dynamoKey"
+        type = "S"
+    }
+}
+
+resource "aws_dynamodb_table" "torchci-job-annotation" {
+    name           = "torchci-job-annotation"
+    hash_key = "dynamoKey"
+    billing_mode   = "PAY_PER_REQUEST"
+    stream_enabled = true
+    stream_view_type = "NEW_AND_OLD_IMAGES"
+    attribute {
+        name = "dynamoKey"
+        type = "S"
+    }
+}
+
+resource "aws_dynamodb_table" "torchci-metrics" {
+    name           = "torchci-metrics"
+    hash_key = "dynamo_key"
+    range_key = "metrics_name"
+    billing_mode   = "PAY_PER_REQUEST"
+    stream_enabled = true
+    stream_view_type = "NEW_AND_OLD_IMAGES"
+    attribute {
+        name = "dynamo_key"
+        type = "S"
+    }
+    attribute {
+        name = "metrics_name"
+        type = "S"
+    }
+}
+
+resource "aws_dynamodb_table" "torchci-metrics-ci-wait-time" {
+    name           = "torchci-metrics-ci-wait-time"
+    hash_key = "dynamoKey"
+    billing_mode   = "PROVISIONED"
+    read_capacity = 1
+    write_capacity = 1
+    stream_enabled = true
+    stream_view_type = "NEW_AND_OLD_IMAGES"
+    attribute {
+        name = "dynamoKey"
+        type = "S"
+    }
+}
+
+resource "aws_dynamodb_table" "torchci-oss-ci-benchmark" {
+    name           = "torchci-oss-ci-benchmark"
+    hash_key = "dynamoKey"
+    billing_mode   = "PAY_PER_REQUEST"
+    stream_enabled = true
+    stream_view_type = "NEW_AND_OLD_IMAGES"
+    attribute {
+        name = "dynamoKey"
+        type = "S"
+    }
+}
+
+resource "aws_dynamodb_table" "torchci-pull-request" {
+    name           = "torchci-pull-request"
+    hash_key = "dynamoKey"
+    billing_mode   = "PAY_PER_REQUEST"
+    stream_enabled = true
+    stream_view_type = "NEW_AND_OLD_IMAGES"
+    attribute {
+        name = "dynamoKey"
+        type = "S"
+    }
+}
+
+resource "aws_dynamodb_table" "torchci-pull-request-review" {
+    name           = "torchci-pull-request-review"
+    hash_key = "dynamoKey"
+    billing_mode   = "PAY_PER_REQUEST"
+    stream_enabled = true
+    stream_view_type = "NEW_AND_OLD_IMAGES"
+    attribute {
+        name = "dynamoKey"
+        type = "S"
+    }
+}
+
+resource "aws_dynamodb_table" "torchci-pull-request-review-comment" {
+    name           = "torchci-pull-request-review-comment"
+    hash_key = "dynamoKey"
+    billing_mode   = "PAY_PER_REQUEST"
+    stream_enabled = true
+    stream_view_type = "NEW_AND_OLD_IMAGES"
+    attribute {
+        name = "dynamoKey"
+        type = "S"
+    }
+}
+
+resource "aws_dynamodb_table" "torchci-push" {
+    name           = "torchci-push"
+    hash_key = "dynamoKey"
+    billing_mode   = "PAY_PER_REQUEST"
+    stream_enabled = true
+    stream_view_type = "NEW_AND_OLD_IMAGES"
+    attribute {
+        name = "dynamoKey"
+        type = "S"
+    }
+}
+
+resource "aws_dynamodb_table" "torchci-retry-bot" {
+    name           = "torchci-retry-bot"
+    hash_key = "dynamoKey"
+    billing_mode   = "PAY_PER_REQUEST"
+    stream_enabled = true
+    stream_view_type = "NEW_AND_OLD_IMAGES"
+    attribute {
+        name = "dynamoKey"
+        type = "S"
+    }
+}
+
+resource "aws_dynamodb_table" "torchci-tutorial-filenames" {
+    name           = "torchci-tutorial-filenames"
+    hash_key = "commit_id"
+    range_key = "filename"
+    billing_mode   = "PROVISIONED"
+    read_capacity = 10
+    write_capacity = 10
+    stream_enabled = true
+    stream_view_type = "NEW_AND_OLD_IMAGES"
+    attribute {
+        name = "commit_id"
+        type = "S"
+    }
+    attribute {
+        name = "filename"
+        type = "S"
+    }
+}
