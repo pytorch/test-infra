44ae89ff8b1557fe8737a9f7f8a47ac5a7b6a403
Sahan Paliskara <sahanp@meta.com>
2024-09-11 16:42:44 -0700

    fix rebase

--- torchci/pages/metrics.tsx
+++ torchci/pages/metrics.tsx
@@ -957,25 +957,30 @@ export default function Page() {
           <TimeSeriesPanel
             title={"Workflow load per Day"}
             queryName={"workflow_load"}
-            queryParams={[
-              {
-                name: "timezone",
-                type: "string",
-                value: Intl.DateTimeFormat().resolvedOptions().timeZone,
-              },
-              {
-                name: "repo",
-                type: "string",
-                value: "pytorch/pytorch",
-              },
-              ...timeParams,
-            ]}
+            queryParams={
+              useClickHouse
+                ? { ...timeParamsClickHouse, repo: "pytorch/pytorch" }
+                : [
+                    {
+                      name: "timezone",
+                      type: "string",
+                      value: Intl.DateTimeFormat().resolvedOptions().timeZone,
+                    },
+                    {
+                      name: "repo",
+                      type: "string",
+                      value: "pytorch/pytorch",
+                    },
+                    ...timeParams,
+                  ]
+            }
             granularity={"hour"}
             groupByFieldName={"name"}
             timeFieldName={"granularity_bucket"}
             yAxisFieldName={"count"}
             yAxisLabel={"workflows started"}
             yAxisRenderer={(value) => value}
+            useClickHouse={useClickHouse}
           />
         </Grid>
 
@@ -1080,7 +1085,7 @@ export default function Page() {
             yAxisLabel={"rate delta"}
             yAxisFieldName={"success_rate_delta"}
             yAxisRenderer={(value) => value}
-            // groupByFieldName={"job_name"}
+            groupByFieldName={"job_name"}
           />
         </Grid>
 
