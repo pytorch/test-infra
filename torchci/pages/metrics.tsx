943c5274e4a6136c7ffac65599b45a5e36bb22ee
Sahan Paliskara <sahancpal@gmail.com>
2024-09-12 14:28:43 -0700

    fix format of percentage of jobs rolled over chart (#5658)

--- torchci/pages/metrics.tsx
+++ torchci/pages/metrics.tsx
@@ -1079,7 +1079,13 @@ export default function Page() {
             title={"LF vs Meta: Success rate delta"}
             queryName={"lf_rollover_health"}
             queryCollection={"metrics"}
-            queryParams={[]}
+            queryParams={[
+              {
+                name: "days_ago",
+                type: "int",
+                value: timeRange,
+              },
+            ]}
             granularity={"day"}
             timeFieldName={"bucket"}
             yAxisLabel={"rate delta"}
@@ -1094,7 +1100,13 @@ export default function Page() {
             title={"LF vs Meta: Cancelled rate delta"}
             queryName={"lf_rollover_health"}
             queryCollection={"metrics"}
-            queryParams={[]}
+            queryParams={[
+              {
+                name: "days_ago",
+                type: "int",
+                value: timeRange,
+              },
+            ]}
             granularity={"day"}
             timeFieldName={"bucket"}
             yAxisLabel={"rate delta"}
@@ -1109,7 +1121,13 @@ export default function Page() {
             title={"LF vs Meta: Duration increase ratio"}
             queryName={"lf_rollover_health"}
             queryCollection={"metrics"}
-            queryParams={[]}
+            queryParams={[
+              {
+                name: "days_ago",
+                type: "int",
+                value: timeRange,
+              },
+            ]}
             granularity={"day"}
             timeFieldName={"bucket"}
             yAxisLabel="increase ratio"
@@ -1118,12 +1136,18 @@ export default function Page() {
             groupByFieldName={"job_name"}
           />
         </Grid>
-        <Grid item xs={6} height={ROW_HEIGHT}>
+        <Grid item xs={12} height={ROW_HEIGHT}>
           <TimeSeriesPanel
             title={"Percentage of jobs rolled over to Linux Foundation"}
             queryName={"lf_rollover_percentage"}
             queryCollection={"metrics"}
-            queryParams={[]}
+            queryParams={[
+              {
+                name: "days_ago",
+                type: "int",
+                value: timeRange,
+              },
+            ]}
             granularity={"hour"}
             timeFieldName={"bucket"}
             yAxisFieldName={"percentage"}
