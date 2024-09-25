bbf20b76c4381784b742e5116f42a79167183c15
Catherine Lee <csl@fb.com>
2024-09-25 13:16:00 -0700

    tc

--- aws/lambda/clickhouse-replicator-dynamo/test_lambda_function.py
+++ aws/lambda/clickhouse-replicator-dynamo/test_lambda_function.py
@@ -3,6 +3,9 @@ from pathlib import Path
 from lambda_function import handle_event
 
 if __name__ == "__main__":
+    # Uses the sample.json file to test the lambda function.  Does not perform
+    # the insert.  Does not go through error path.  Prints the query so you can
+    # try it out in the console.  Change the database to fortesting to check.
     with open(Path(__file__).parent / "sample.json") as f:
         event = json.load(f)
-    handle_event(event, dry_run=True)
+    handle_event(event, dry_run=False)
