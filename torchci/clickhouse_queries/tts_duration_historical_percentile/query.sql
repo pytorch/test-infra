dac76faa8345baf292babcd80708491b44d13c65
Huy Do <huydhn@gmail.com>
2024-10-18 13:47:49 -0700

    Switch to quantile instead of percent_rank

--- torchci/clickhouse_queries/tts_duration_historical_percentile/query.sql
+++ torchci/clickhouse_queries/tts_duration_historical_percentile/query.sql
@@ -20,54 +20,14 @@ WITH tts_duration AS (
         AND job.name NOT LIKE '%mem_leak_check%'
         AND job.name NOT LIKE '%rerun_disabled_tests%'
         AND toUnixTimestamp(job.completed_at) != 0 -- To remove jobs that are still running
-),
-p AS (
-    SELECT
-        granularity_bucket,
-        tts_sec,
-        -- Switch this to percent_rank once it's available on ClickHouse Cloud https://github.com/ClickHouse/ClickHouse/issues/46300
-        ifNull(
-            (
-                rank() OVER(
-                    PARTITION BY full_name
-                    ORDER BY
-                        tts_sec DESC
-                ) - 1
-            ) / nullif(count(1) OVER(PARTITION BY full_name) -1, 0),
-            0
-        ) AS tts_percentile,
-        duration_sec,
-        -- Switch this to percent_rank once it's available on ClickHouse Cloud https://github.com/ClickHouse/ClickHouse/issues/46300
-        ifNull(
-            (
-                rank() OVER(
-                    PARTITION BY full_name
-                    ORDER BY
-                        duration_sec DESC
-                ) - 1
-            ) / nullif(count(1) OVER(PARTITION BY full_name) -1, 0),
-            0
-        ) AS duration_percentile,
-        full_name,
-    FROM
-        tts_duration
-),
-filtered_p AS (
-    SELECT
-        *
-    FROM
-        p
-    WHERE
-        p.tts_percentile >= (1.0 - {percentile: Float32})
-        OR p.duration_percentile >= (1.0 - {percentile: Float32})
 )
 SELECT
     granularity_bucket,
-    MAX(tts_sec) AS tts_percentile_sec,
-    MAX(duration_sec) AS duration_percentile_sec,
+    quantile({percentile: Float32})(tts_sec) AS tts_percentile_sec,
+    quantile({percentile: Float32})(duration_sec) AS duration_percentile_sec,
     full_name
 FROM
-    filtered_p
+    tts_duration
 GROUP BY
     granularity_bucket,
     full_name
