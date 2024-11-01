d137d3a05ff56eaba1e96c02eb2db97e6bbf5f12
Wouter Devriendt <wouterdevriendt@meta.com>
2024-11-01 15:14:42 -0700

    fix for the typescript code after upgrading to IMDSv2 (#5848)
    this is a follow up on PR #5783 that enabled IMDSv2. We're about to
    remove support for IMDSv1 on our AWS account globally
    
    This is part of an effort to move all our calls across all pytorch repos
    to v2.
    
    See also:
    https://github.com/pytorch/FBGEMM/pull/3297
    https://github.com/pytorch/pytorch/pull/138286
    https://github.com/pytorch/torchsnapshot/pull/180
    https://github.com/pytorch/torchrec/pull/2532
    https://github.com/pytorch/torcheval/pull/210

--- setup-ssh/src/ec2-utils.ts
+++ setup-ssh/src/ec2-utils.ts
@@ -1,18 +1,15 @@
-import {HttpClient} from '@actions/http-client'
-
+import {HttpClient, HttpClientResponse} from '@actions/http-client'
 export async function getEC2Metadata(category: string): Promise<string> {
   const maxRetries = 10
   const http = new HttpClient('seemethere/add-github-ssh-key', undefined, {
     allowRetries: true,
     maxRetries
   })
-  // convert these two curls:
-  // curl -H "X-aws-ec2-metadata-token: $(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 30")" -fsSL "http://169.254.169.254/latest/meta-data/${category}"
-    const tokenResponse = await http.put(
-    `http://169.254.169.254/latest/api/token`, undefined, {
-      headers: {
-        'X-aws-ec2-metadata-token-ttl-seconds': '30'
-      }
+  const tokenResponse: HttpClientResponse = await http.put(
+    `http://169.254.169.254/latest/api/token`,
+    '',
+    {
+      'X-aws-ec2-metadata-token-ttl-seconds': '30'
     }
   )
 
@@ -21,13 +18,12 @@ export async function getEC2Metadata(category: string): Promise<string> {
   }
 
   const resp = await http.get(
-    `http://169.254.169.254/latest/meta-data/${category}`, {
-      headers: {
-        'X-aws-ec2-metadata-token': tokenResponse.result
-      }
+    `http://169.254.169.254/latest/meta-data/${category}`,
+    {
+      'X-aws-ec2-metadata-token': await tokenResponse.readBody()
     }
   )
-  
+
   if (resp.message.statusCode !== 200) {
     return ''
   }
