4abe5c1881a9fb28d9e686dafdf5807928be2f29
Yang Wang <elainewy@meta.com>
2024-11-05 15:38:55 -0800

    test

--- torchci/pages/api/log_annotation/[repoOwner]/[repoName]/[annotation].ts
+++ torchci/pages/api/log_annotation/[repoOwner]/[repoName]/[annotation].ts
@@ -19,8 +19,9 @@ export default async function handler(
   }
 
   const { repoOwner, repoName, annotation } = req.query;
-  const repoOwnerStr = Array.isArray(repoOwner) ? repoOwner[0] : repoOwner;
-  const repoNameStr = Array.isArray(repoName) ? repoName[0] : repoName;
+  // Argument of type 'string | undefined' is not assignable to parameter of type 'string'.
+  const repoOwnerStr = Array.isArray(repoOwner) ? repoOwner[0] : (repoOwner || '');
+  const repoNameStr = Array.isArray(repoName) ? repoName[0] : (repoName || '');
   const octokit = await getOctokit(repoOwnerStr, repoNameStr);
   const user = await octokit.rest.users.getAuthenticated();
   const hasPermission = hasWritePermissionsUsingOctokit(
