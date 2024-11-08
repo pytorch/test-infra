86228c003d2bd0d6d4be3102b86eb0741ee2c8b1
clee2000 <44682903+clee2000@users.noreply.github.com>
2024-11-08 08:56:18 -0800

    [CH] Move external contrib queries on KPIs page to ClickHouse (#5878)
    The returned data looks different between CH and RS because there are
    some errors in the RS table (dup rows) that I removed in CH. This is
    also why I'm switching entirely to CH
    
    Also removes old time params for the query since it says to remove them
    (I believe it's because contrb stats were only available after that
    time)

--- torchci/clickhouse_queries/external_contribution_stats/query.sql
+++ torchci/clickhouse_queries/external_contribution_stats/query.sql
@@ -1,26 +1,46 @@
--- !!! Query is not converted to CH syntax yet.  Delete this line when it gets converted
 WITH rolling_average_table as (
-  SELECT
-    FORMAT_ISO8601(
-        CAST(date as date)
-    ) AS granularity_bucket,
-    -- weekly granularity with a 4 week rolling average
-    TRUNC(SUM(pr_count)
-           OVER(ORDER BY date ROWS 27 PRECEDING),1)/4
-           AS weekly_pr_count_rolling_average,
-  TRUNC(LENGTH(ARRAY_DISTINCT(ARRAY_FLATTEN(ARRAY_AGG(users)
-  OVER(ORDER BY date ROWS 27 PRECEDING)))),1)/4 as weekly_user_count_rolling_average,
-FROM
-    metrics.external_contribution_stats
-    WHERE CAST(date as date) >= PARSE_DATETIME_ISO8601(:startTime) - DAYS(28)
-    AND CAST(date as date) < PARSE_DATETIME_ISO8601(:stopTime)
+    SELECT
+        date AS granularity_bucket,
+        -- weekly granularity with a 4 week rolling average
+        TRUNC(
+            SUM(pr_count) OVER(
+                ORDER BY
+                    date ROWS 27 PRECEDING
+            ),
+            1
+        ) / 4 AS weekly_pr_count_rolling_average,
+        TRUNC(
+            LENGTH(
+                arrayDistinct(
+                    arrayFlatten(
+                        ARRAY_AGG(users) OVER(
+                            ORDER BY
+                                date ROWS 27 PRECEDING
+                        )
+                    )
+                )
+            ),
+            1
+        ) / 4 as weekly_user_count_rolling_average
+    FROM
+        misc.external_contribution_stats
+    WHERE
+        date >= {startTime: DateTime64(9) } - interval 28 day
+        AND date < {stopTime: DateTime64(9) }
 )
 SELECT
-granularity_bucket,
-weekly_pr_count_rolling_average AS pr_count,
-weekly_user_count_rolling_average AS user_count,
+    granularity_bucket,
+    weekly_pr_count_rolling_average AS pr_count,
+    weekly_user_count_rolling_average AS user_count
 FROM
-rolling_average_table
-WHERE CAST(granularity_bucket as date) >= PARSE_DATETIME_ISO8601(:startTime)
-    AND CAST(granularity_bucket as date) < PARSE_DATETIME_ISO8601(:stopTime)
-    AND (DATE_DIFF('DAY', CAST(granularity_bucket as date), CAST(PARSE_DATETIME_ISO8601(:startTime) as date)) % 7) = 0
+    rolling_average_table
+WHERE
+    granularity_bucket >= {startTime: DateTime64(9) }
+    AND granularity_bucket < {stopTime: DateTime64(9) }
+    AND (
+        DATE_DIFF(
+            'DAY',
+            granularity_bucket,
+            {startTime: DateTime64(9) }
+        ) % 7
+    ) = 0
