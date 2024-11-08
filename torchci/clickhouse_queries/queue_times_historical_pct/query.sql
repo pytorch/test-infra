18c1e2d9d4d9ba318933f150a11694d65b230b4f
Jean Schmidt <4520845+jeanschmidt@users.noreply.github.com>
2024-11-08 19:50:45 +0100

    Migrate hud /sli page queries to clickhouse (#5882)
    Co-authored-by: clee2000 <44682903+clee2000@users.noreply.github.com>

--- torchci/clickhouse_queries/queue_times_historical_pct/query.sql
+++ torchci/clickhouse_queries/queue_times_historical_pct/query.sql
@@ -1,10 +1,7 @@
 -- !!! Query is not converted to CH syntax yet.  Delete this line when it gets converted
 SELECT
-    FORMAT_ISO8601(
-        DATE_TRUNC(
-            'hour',
-            q._event_time AT TIME ZONE :timezone
-        )
+    toStartOfHour(
+        toDateTime(q.time, {timezone: String})
     ) AS granularity_bucket,
     q.queue_s_max,
     q.queue_s_p99,
@@ -15,10 +12,10 @@ SELECT
     q.queue_s_avg,
     q.machine_type
 FROM
-    metrics.queue_times_24h_stats q
+    misc.queue_times_24h_stats q
 WHERE
-    q._event_time >= DATE_TRUNC('hour', PARSE_DATETIME_ISO8601(:startTime) AT TIME ZONE :timezone)
-    AND q._event_time < DATE_TRUNC('hour', PARSE_DATETIME_ISO8601(:stopTime) AT TIME ZONE :timezone)
-    AND ARRAY_CONTAINS(SPLIT(:workersTypes, ','), q.machine_type)
+    q.time >= toStartOfHour(toDateTime({startTime: DateTime64(3)}, {timezone: String}))
+    AND q.time < toStartOfHour(toDateTime({stopTime: DateTime64(3)}, {timezone: String}))
+    AND has({workersTypes: Array(String)}, q.machine_type)
 ORDER BY
     granularity_bucket, machine_type ASC
