2c602ac6d8f098cc4f53b9f209ed925003791c85
Yang Wang <elainewy@meta.com>
2024-11-08 08:47:54 -0800

    [BE] Add rendering loading page in HUD TABLE view (#5881)
    # Description
    adding LaodingPage to Hub table during data fetching
    
    # UI snapshot
    <img width="1554" alt="image"
    src="https://github.com/user-attachments/assets/c05e4991-d0b2-43fa-a88c-4f8e43186ad2">
    
    # License
    MUI: MIT LICENSE (Pre-approved oss license)

--- torchci/pages/hud/[repoOwner]/[repoName]/[branch]/[[...page]].tsx
+++ torchci/pages/hud/[repoOwner]/[repoName]/[branch]/[[...page]].tsx
@@ -9,6 +9,7 @@ import styles from "components/hud.module.css";
 import JobConclusion from "components/JobConclusion";
 import JobFilterInput from "components/JobFilterInput";
 import JobTooltip from "components/JobTooltip";
+import LoadingPage from "components/LoadingPage";
 import PageSelector from "components/PageSelector";
 import { LocalTimeHuman } from "components/TimeUtils";
 import TooltipTarget from "components/TooltipTarget";
@@ -383,7 +384,15 @@ export function MonsterFailuresCheckbox() {
 }
 
 function HudTable({ params }: { params: HudParams }) {
-  return <GroupedView params={params} />;
+  const data = useHudData(params);
+  if (data === undefined) {
+    return <LoadingPage />;
+  }
+  return (
+    <>
+      <GroupedHudTable params={params} data={data} />
+    </>
+  );
 }
 
 function HudHeader({ params }: { params: HudParams }) {
@@ -519,15 +528,6 @@ function CopyPermanentLink({
   const path = formatHudUrlForRoute("hud", permaParams);
   const url = `${domain}${path}`;
   return <CopyLink textToCopy={url} compressed={false} style={style} />;
-}
-
-function GroupedView({ params }: { params: HudParams }) {
-  const data = useHudData(params);
-  if (data === undefined) {
-    return <div>Loading...</div>;
-  }
-
-  return <GroupedHudTable params={params} data={data} />;
 }
 
 function GroupedHudTable({
