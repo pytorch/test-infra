4a84e9c75bfb981bc90fdf8f3b2974c61b1fc8f4
Yang Wang <elainewy@meta.com>
2024-11-22 04:17:38 -0500

    test

--- torchci/lib/bot/autoLabelBot.ts
+++ torchci/lib/bot/autoLabelBot.ts
@@ -12,7 +12,7 @@ import {
   LabelToLabelConfigTracker,
 } from "./utils";
 
-// List of Regex Patterns for Assigning Labels to Pull Requests
+// List of Regex Patterns For Assigning Labels to Pull Requests
 const PrTitleRegexToLabel: [RegExp, string][] = [
   [/reland/gi, "ci-no-td"],
   [/rocm/gi, "module: rocm"],
@@ -24,7 +24,7 @@ const PrTitleRegexToLabel: [RegExp, string][] = [
   [/UNSTABLE\s+.*\s+\/\s+.*/g, "module: ci"],
 ];
 
-//List of Regex Patterns for Assigning Labels to Issues
+// List of Regex Patterns For Assigning Labels to Issues
 const IssueTitleRegexToLabel: [RegExp, string][] = [
   [/rocm/gi, "module: rocm"],
   [/rocm/gi, "ciflow/rocm"],
