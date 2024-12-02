8f2c3ce6d70d683a2ef40e3e48080579f8edaf4b
atalman <atalman@fb.com>
2024-12-02 14:30:11 -0800

    fix

--- tools/scripts/generate_binary_build_matrix.py
+++ tools/scripts/generate_binary_build_matrix.py
@@ -17,7 +17,7 @@ import json
 import os
 import sys
 
-from typing import Any, Callable, Dict, List, Optional, Tuple
+from typing import Dict, List, Optional, Tuple, Any, Callable
 
 PYTHON_ARCHES_DICT = {
     "nightly": ["3.9", "3.10", "3.11", "3.12"],
@@ -148,10 +148,10 @@ def initialize_globals(channel: str, build_python_only: bool) -> None:
     else:
         PYTHON_ARCHES = PYTHON_ARCHES_DICT[channel]
     WHEEL_CONTAINER_IMAGES = {
-        "11.8": "pytorch/manylinux-builder:cuda11.8",
-        "12.1": "pytorch/manylinux-builder:cuda12.1",
-        "12.4": "pytorch/manylinux-builder:cuda12.4",
-        "12.6": "pytorch/manylinux2_28-builder:cuda12.6",
+       "11.8": "pytorch/manylinux-builder:cuda11.8",
+       "12.1": "pytorch/manylinux-builder:cuda12.1",
+       "12.4": "pytorch/manylinux-builder:cuda12.4",
+       "12.6": "pytorch/manylinux2_28-builder:cuda12.6",
         **{
             gpu_arch: f"pytorch/manylinux-builder:rocm{gpu_arch}"
             for gpu_arch in ROCM_ARCHES
