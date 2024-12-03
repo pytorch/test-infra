dce97369cb057f1ea6278ee4d3b75af05758565d
amdfaa <107946068+amdfaa@users.noreply.github.com>
2024-12-03 12:02:52 -0600

    Update run_with_env_secrets.py

--- .github/scripts/run_with_env_secrets.py
+++ .github/scripts/run_with_env_secrets.py
@@ -68,7 +68,7 @@ def main():
             -e RUNNER_ARTIFACT_DIR=/artifacts \
             -e RUNNER_DOCS_DIR=/docs \
             -e RUNNER_TEST_RESULTS_DIR=/test-results \
-            --env-file="{ os.environ.get('RUNNER_TEMP', '') }/github_env_{ os.environ.get('GITHUB_RUN_ID', '') }" \
+            --env-file="/tmp/github_env_{ os.environ.get('GITHUB_RUN_ID', '') }" \
             `# It is unknown why the container sees a different value for this.` \
             -e GITHUB_STEP_SUMMARY \
             { ' '.join([ f'-e {v}' for v in secrets_u_names ]) } \
