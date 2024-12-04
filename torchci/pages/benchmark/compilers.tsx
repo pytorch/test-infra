2afdb9b9bded6c68d0c7fb66fac0a608841e4985
Yang Wang <elainewy@meta.com>
2024-12-04 16:18:53 -0500

    test

--- torchci/pages/benchmark/compilers.tsx
+++ torchci/pages/benchmark/compilers.tsx
@@ -133,7 +133,6 @@ function Report({
       >
         <BenchmarkLogs workflowId={lData[0].workflow_id} />
       </CommitPanel>
-      <div>I'm here</div>
       <SummaryPanel
         dashboard={"torchinductor"}
         startTime={startTime}
