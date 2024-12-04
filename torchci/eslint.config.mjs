3ba9b597bfbfbc56ab7cd5b4ed1cc3936c536fd5
Catherine Lee <csl@fb.com>
2024-12-04 10:10:41 -0800

    tc

---
+++ torchci/eslint.config.mjs
@@ -0,0 +1,36 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import unusedImports from "eslint-plugin-unused-imports";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    plugins: {
+      "unused-imports": unusedImports,
+    },
+
+    rules: {
+      "no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+];
