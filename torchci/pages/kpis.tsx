31d4e6e954d01e684f6249153f2316afa46ce258
Catherine Lee <csl@fb.com>
2024-12-11 10:14:35 -0800

    tc

--- torchci/pages/kpis.tsx
+++ torchci/pages/kpis.tsx
@@ -10,7 +10,7 @@ export default function Kpis() {
   const [startTime, _setStartTime] = useState(dayjs().subtract(6, "month"));
   const [stopTime, _setStopTime] = useState(dayjs());
 
-  const clickhouseTimeParams = {
+  const timeParams = {
     startTime: startTime.utc().format("YYYY-MM-DDTHH:mm:ss.SSS"),
     stopTime: stopTime.utc().format("YYYY-MM-DDTHH:mm:ss.SSS"),
   };
@@ -22,7 +22,7 @@ export default function Kpis() {
           title={"% of commits red on trunk (Weekly)"}
           queryName={"master_commit_red_percent"}
           queryParams={{
-            ...clickhouseTimeParams,
+            ...timeParams,
             granularity: "week",
             workflowNames: ["lint", "pull", "trunk"],
           }}
@@ -40,7 +40,7 @@ export default function Kpis() {
         <TimeSeriesPanel
           title={"# of force merges (Weekly)"}
           queryName={"number_of_force_pushes_historical"}
-          queryParams={clickhouseTimeParams}
+          queryParams={timeParams}
           granularity={"week"}
           timeFieldName={"bucket"}
           yAxisFieldName={"count"}
@@ -53,7 +53,7 @@ export default function Kpis() {
           title={"Time to Red Signal - (Weekly, pull workflow)"}
           queryName={"ttrs_percentiles"}
           queryParams={{
-            ...clickhouseTimeParams,
+            ...timeParams,
             one_bucket: false,
             percentile_to_get: 0,
             workflow: "pull",
@@ -88,7 +88,7 @@ export default function Kpis() {
           title={"% of force merges (Weekly, 2 week rolling avg)"}
           queryName={"weekly_force_merge_stats"}
           queryParams={{
-            ...clickhouseTimeParams,
+            ...timeParams,
             one_bucket: false,
             merge_type: "",
             granularity: "week",
@@ -107,7 +107,7 @@ export default function Kpis() {
         <TimeSeriesPanel
           title={"Avg time-to-signal - E2E (Weekly)"}
           queryName={"time_to_signal"}
-          queryParams={clickhouseTimeParams}
+          queryParams={timeParams}
           granularity={"week"}
           timeFieldName={"week_bucket"}
           yAxisFieldName={"avg_tts"}
@@ -121,7 +121,7 @@ export default function Kpis() {
         <TimeSeriesPanel
           title={"# of reverts (2 week moving avg)"}
           queryName={"num_reverts"}
-          queryParams={clickhouseTimeParams}
+          queryParams={timeParams}
           granularity={"week"}
           timeFieldName={"bucket"}
           yAxisFieldName={"num"}
@@ -135,7 +135,7 @@ export default function Kpis() {
           title={"viable/strict lag (Daily)"}
           queryName={"strict_lag_historical"}
           queryParams={{
-            ...clickhouseTimeParams,
+            ...timeParams,
             // Missing data prior to 2024-10-01 due to migration to ClickHouse
             ...(startTime < dayjs("2024-10-01") && {
               startTime: dayjs("2024-10-01")
@@ -158,7 +158,7 @@ export default function Kpis() {
         <TimeSeriesPanel
           title={"Weekly external PR count (4 week moving average)"}
           queryName={"external_contribution_stats"}
-          queryParams={clickhouseTimeParams}
+          queryParams={timeParams}
           granularity={"week"}
           timeFieldName={"granularity_bucket"}
           yAxisFieldName={"pr_count"}
@@ -171,7 +171,7 @@ export default function Kpis() {
         <TimeSeriesPanel
           title={"Monthly external PR count"}
           queryName={"monthly_contribution_stats"}
-          queryParams={clickhouseTimeParams}
+          queryParams={timeParams}
           granularity={"month"}
           timeFieldName={"year_and_month"}
           timeFieldDisplayFormat={"MMMM YYYY"}
@@ -185,7 +185,7 @@ export default function Kpis() {
         <TimeSeriesPanel
           title={"Total number of open disabled tests (Daily)"}
           queryName={"disabled_test_historical"}
-          queryParams={{ ...clickhouseTimeParams, repo: "pytorch/pytorch" }}
+          queryParams={{ ...timeParams, repo: "pytorch/pytorch" }}
           granularity={"day"}
           timeFieldName={"granularity_bucket"}
           yAxisFieldName={"number_of_open_disabled_tests"}
