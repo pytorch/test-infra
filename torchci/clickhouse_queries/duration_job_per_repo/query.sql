595e4d54449d41a3461863916060e9bd3af88ab4
Wouter Devriendt <wouterdevriendt@meta.com>
2024-12-17 00:01:31 -0800

    allow  cost dashboard to be grouped by repo

---
+++ torchci/clickhouse_queries/duration_job_per_repo/query.sql
@@ -0,0 +1,24 @@
+select
+    DATE_TRUNC(
+        {granularity: String},
+        rc.date
+    ) as granularity_bucket,
+    rc.group_repo as repo,
+    sum(rc.duration) as total_duration
+from
+    misc.runner_cost rc final
+where
+    rc.date > {startTime: DateTime64(9)}
+    and rc.date < {stopTime: DateTime64(9)}
+    and rc.duration > 0
+    and rc.group_repo in {selectedRepos: Array(String)}
+    and rc.gpu in {selectedGPU: Array(UInt8)}
+    and rc.os in {selectedPlatforms: Array(String)}
+    and rc.provider in {selectedProviders: Array(String)}
+    and rc.owning_account in {selectedOwners: Array(String)}
+group by
+    granularity_bucket,
+    repo
+order by
+    granularity_bucket asc
+
