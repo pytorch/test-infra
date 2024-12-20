47960688d160683d014930c12296b04510fcacb7
Andrey Talman <atalman@fb.com>
2024-12-20 15:01:58 +0000

    Nightlies page use clickhouse, deprecate rockset (#6090)
    Rewrite Nightlies page using clickhouse

--- torchci/pages/nightlies.tsx
+++ torchci/pages/nightlies.tsx
@@ -1,10 +1,9 @@
 import { Grid, Paper, Skeleton, Stack, Typography } from "@mui/material";
-import TablePanel from "components/metrics/panels/TablePanelRS";
+import TablePanel from "components/metrics/panels/TablePanel";
 import dayjs from "dayjs";
 import { EChartsOption } from "echarts";
 import ReactECharts from "echarts-for-react";
 import { fetcher } from "lib/GeneralUtils";
-import { RocksetParam } from "lib/rockset";
 import { TimeRangePicker } from "pages/metrics";
 import { useState } from "react";
 import useSWR from "swr";
@@ -13,14 +12,16 @@ function NightlyJobsRedPanel({
   params,
   repo,
 }: {
-  params: RocksetParam[];
+  params: { [key: string]: any };
   repo: string;
 }) {
-  let repo_p = params.find(({ name }) => name == "repo");
-  if (repo_p && repo) repo_p.value = repo;
-
-  const url = `/api/query/nightlies/nightly_jobs_red?parameters=${encodeURIComponent(
-    JSON.stringify(params)
+  const queryParams: { [key: string]: any } = {
+    ...params,
+    repo: repo,
+    granularity: "day",
+  };
+  const url = `/api/clickhouse/nightly_jobs_red?parameters=${encodeURIComponent(
+    JSON.stringify(queryParams)
   )}`;
 
   const { data } = useSWR(url, fetcher, {
@@ -71,134 +72,17 @@ function NightlyJobsRedPanel({
   );
 }
 
-function ValidationRedPanel({
-  params,
-  channel,
-  query_type,
-}: {
-  params: RocksetParam[];
-  channel: string;
-  query_type: string;
-}) {
-  const url =
-    `/api/query/nightlies/` +
-    query_type +
-    `_jobs_red?parameters=${encodeURIComponent(
-      JSON.stringify([
-        ...params,
-        {
-          name: "timezone",
-          type: "string",
-          value: Intl.DateTimeFormat().resolvedOptions().timeZone,
-        },
-        {
-          name: "channel",
-          type: "string",
-          value: channel,
-        },
-      ])
-    )}`;
-
-  const { data } = useSWR(url, fetcher, {
-    refreshInterval: 5 * 60 * 1000, // refresh every 5 minutes
-  });
-
-  if (data === undefined) {
-    return <Skeleton variant={"rectangular"} height={"100%"} />;
-  }
-
-  const options: EChartsOption = {
-    title: {
-      text:
-        channel.charAt(0).toUpperCase() +
-        channel.slice(1) +
-        " " +
-        query_type.charAt(0).toUpperCase() +
-        query_type.slice(1) +
-        " workflows failures",
-    },
-    grid: { top: 60, right: 8, bottom: 24, left: 36 },
-    dataset: { source: data },
-    xAxis: { type: "category" },
-    yAxis: {
-      type: "value",
-    },
-    series: [
-      {
-        type: "bar",
-        stack: "all",
-        encode: {
-          x: "granularity_bucket",
-          y: "green",
-        },
-      },
-      {
-        type: "bar",
-        stack: "all",
-        encode: {
-          x: "granularity_bucket",
-          y: "red",
-        },
-      },
-      {
-        type: "bar",
-        stack: "all",
-        encode: {
-          x: "granularity_bucket",
-          y: "pending",
-        },
-      },
-    ],
-    color: ["#3ba272", "#ee6666", "#f2d643"],
-    tooltip: {
-      trigger: "axis",
-      formatter: (params: any) => {
-        const red = params[0].data.red;
-        const green = params[0].data.green;
-        const pending = params[0].data.pending;
-        const total = params[0].data.total;
-
-        const redPct = ((red / total) * 100).toFixed(2) + "%";
-        const greenPct = ((green / total) * 100).toFixed(2) + "%";
-        const pendingPct = ((pending / total) * 100).toFixed(2) + "%";
-        return `Red: ${red} (${redPct})<br/>Green: ${green} (${greenPct})<br/>Pending: ${pending} (${pendingPct})<br/>Total: ${total}`;
-      },
-    },
-  };
-
-  return (
-    <Paper sx={{ p: 2, height: "100%" }} elevation={3}>
-      <ReactECharts
-        style={{ height: "100%", width: "100%" }}
-        option={options}
-      />
-    </Paper>
-  );
-}
-
 const ROW_HEIGHT = 340;
 export default function Page() {
   const [startTime, setStartTime] = useState(dayjs().subtract(1, "week"));
   const [stopTime, setStopTime] = useState(dayjs());
   const [timeRange, setTimeRange] = useState<number>(7);
 
-  const timeParams: RocksetParam[] = [
-    {
-      name: "startTime",
-      type: "string",
-      value: startTime,
-    },
-    {
-      name: "stopTime",
-      type: "string",
-      value: stopTime,
-    },
-    {
-      name: "repo",
-      type: "string",
-      value: "pytorch",
-    },
-  ];
+  const timeParams = {
+    startTime: dayjs(startTime).utc().format("YYYY-MM-DDTHH:mm:ss.SSS"),
+    stopTime: dayjs(stopTime).utc().format("YYYY-MM-DDTHH:mm:ss.SSS"),
+    repo: "pytorch",
+  };
 
   return (
     <div>
@@ -225,19 +109,12 @@ export default function Page() {
           <TablePanel
             title={"Nightly PyTorch build jobs for past 24hrs"}
             queryName={"nightly_jobs_red_past_day"}
-            queryParams={[
-              {
-                name: "repo",
-                type: "string",
-                value: "pytorch",
-              },
-            ]}
-            queryCollection="nightlies"
+            queryParams={{ repo: "pytorch" }}
             columns={[
               { field: "COUNT", headerName: "Count", flex: 1 },
               { field: "name", headerName: "Name", flex: 4 },
             ]}
-            dataGridProps={{ getRowId: (el: any) => el.name }}
+            dataGridProps={{ getRowId: (el: any) => el["name"] }}
           />
         </Grid>
 
@@ -249,19 +126,12 @@ export default function Page() {
           <TablePanel
             title={"Nightly Vision build jobs for past 24hrs"}
             queryName={"nightly_jobs_red_past_day"}
-            queryParams={[
-              {
-                name: "repo",
-                type: "string",
-                value: "vision",
-              },
-            ]}
-            queryCollection="nightlies"
+            queryParams={{ repo: "vision" }}
             columns={[
               { field: "COUNT", headerName: "Count", flex: 1 },
               { field: "name", headerName: "Name", flex: 4 },
             ]}
-            dataGridProps={{ getRowId: (el: any) => el.name }}
+            dataGridProps={{ getRowId: (el: any) => el["name"] }}
           />
         </Grid>
 
@@ -273,51 +143,12 @@ export default function Page() {
           <TablePanel
             title={"Nightly Audio build jobs for past 24hrs"}
             queryName={"nightly_jobs_red_past_day"}
-            queryParams={[
-              {
-                name: "repo",
-                type: "string",
-                value: "audio",
-              },
-            ]}
-            queryCollection="nightlies"
+            queryParams={{ repo: "audio" }}
             columns={[
               { field: "COUNT", headerName: "Count", flex: 1 },
               { field: "name", headerName: "Name", flex: 4 },
             ]}
-            dataGridProps={{ getRowId: (el: any) => el.name }}
-          />
-        </Grid>
-
-        <Grid item xs={6} height={ROW_HEIGHT}>
-          <NightlyJobsRedPanel params={timeParams} repo={"text"} />
-        </Grid>
-
-        <Grid item xs={6} height={ROW_HEIGHT}>
-          <TablePanel
-            title={"Nightly Text build jobs for past 24hrs"}
-            queryName={"nightly_jobs_red_past_day"}
-            queryParams={[
-              {
-                name: "repo",
-                type: "string",
-                value: "text",
-              },
-            ]}
-            queryCollection="nightlies"
-            columns={[
-              { field: "COUNT", headerName: "Count", flex: 1 },
-              { field: "name", headerName: "Name", flex: 4 },
-            ]}
-            dataGridProps={{ getRowId: (el: any) => el.name }}
-          />
-        </Grid>
-
-        <Grid item xs={6} height={ROW_HEIGHT}>
-          <ValidationRedPanel
-            params={timeParams}
-            channel={"release"}
-            query_type={"validation"}
+            dataGridProps={{ getRowId: (el: any) => el["name"] }}
           />
         </Grid>
 
@@ -325,27 +156,12 @@ export default function Page() {
           <TablePanel
             title={"Release failed validation jobs for past 24hrs"}
             queryName={"validation_jobs_red_past_day"}
-            queryParams={[
-              {
-                name: "channel",
-                type: "string",
-                value: "release",
-              },
-            ]}
-            queryCollection="nightlies"
+            queryParams={{ channel: "release" }}
             columns={[
               { field: "COUNT", headerName: "Count", flex: 1 },
               { field: "name", headerName: "Name", flex: 4 },
             ]}
-            dataGridProps={{ getRowId: (el: any) => el.name }}
-          />
-        </Grid>
-
-        <Grid item xs={6} height={ROW_HEIGHT}>
-          <ValidationRedPanel
-            params={timeParams}
-            channel={"nightly"}
-            query_type={"validation"}
+            dataGridProps={{ getRowId: (el: any) => el["name"] }}
           />
         </Grid>
 
@@ -353,27 +169,12 @@ export default function Page() {
           <TablePanel
             title={"Nightly failed validation jobs for past 24hrs"}
             queryName={"validation_jobs_red_past_day"}
-            queryParams={[
-              {
-                name: "channel",
-                type: "string",
-                value: "nightly",
-              },
-            ]}
-            queryCollection="nightlies"
+            queryParams={{ channel: "nightly" }}
             columns={[
               { field: "COUNT", headerName: "Count", flex: 1 },
               { field: "name", headerName: "Name", flex: 4 },
             ]}
-            dataGridProps={{ getRowId: (el: any) => el.name }}
-          />
-        </Grid>
-
-        <Grid item xs={6} height={ROW_HEIGHT}>
-          <ValidationRedPanel
-            params={timeParams}
-            channel={""}
-            query_type={"docker"}
+            dataGridProps={{ getRowId: (el: any) => el["name"] }}
           />
         </Grid>
 
@@ -384,12 +185,11 @@ export default function Page() {
             }
             queryName={"nightly_jobs_red_by_name"}
             queryParams={timeParams}
-            queryCollection="nightlies"
             columns={[
               { field: "COUNT", headerName: "Count", flex: 1 },
               { field: "name", headerName: "Name", flex: 4 },
             ]}
-            dataGridProps={{ getRowId: (el: any) => el.name }}
+            dataGridProps={{ getRowId: (el: any) => el["name"] }}
           />
         </Grid>
 
@@ -400,7 +200,6 @@ export default function Page() {
             }
             queryName={"nightly_jobs_red_by_platform"}
             queryParams={timeParams}
-            queryCollection="nightlies"
             columns={[
               { field: "Count", headerName: "Count", flex: 1 },
               { field: "Platform", headerName: "Platform", flex: 4 },
