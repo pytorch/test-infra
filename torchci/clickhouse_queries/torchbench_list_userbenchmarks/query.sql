5c8b064d2704a9b2d07790e808b42d628ed6de9d
Sergii Dymchenko <sdym@meta.com>
2024-12-19 17:07:21 -0800

    Convert torchbench_list_userbenchmarks to CH (#6073)

--- torchci/clickhouse_queries/torchbench_list_userbenchmarks/query.sql
+++ torchci/clickhouse_queries/torchbench_list_userbenchmarks/query.sql
@@ -1,6 +1,4 @@
--- !!! Query is not converted to CH syntax yet.  Delete this line when it gets converted
 SELECT DISTINCT
     name
 FROM
-    torchbench."torchbench-userbenchmark"
-WHERE name is not NULL
+    benchmark.torchbench_userbenchmark
