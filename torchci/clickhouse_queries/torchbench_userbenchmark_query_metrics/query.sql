382d7d4aaec1cdb58042576401711c20312cc468
Sergii Dymchenko <sdym@meta.com>
2024-12-30 21:24:49 -0800

    Convert torchbench_userbenchmark_query_metrics to CH (#6122)

--- torchci/clickhouse_queries/torchbench_userbenchmark_query_metrics/query.sql
+++ torchci/clickhouse_queries/torchbench_userbenchmark_query_metrics/query.sql
@@ -1,4 +1,10 @@
--- !!! Query is not converted to CH syntax yet.  Delete this line when it gets converted
-SELECT * FROM torchbench."torchbench-userbenchmark"
-  WHERE name = :userbenchmark 
-  AND REGEXP_LIKE("torchbench-userbenchmark".environ.pytorch_git_version, :commit);
+SELECT
+    *
+FROM
+    benchmark.torchbench_userbenchmark
+WHERE
+    name = {userbenchmark: String }
+    AND match(
+        JSONExtractString(environ, 'pytorch_git_version') as pytorch_git_version,
+        {commit: String }
+    )
