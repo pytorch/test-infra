pytorch/test-infra

commit dd19e346ad18b0b3716d41a854a8ebd801fe05a0

Display MPS benchmark results in eager mode (#6006)
This goes together with https://github.com/pytorch/pytorch/pull/142034.
MPS benchmark is running in eager mode at the moment.

torchci/clickhouse_queries/compilers_benchmark_performance/query.sql
@@ -88,33 +88,33 @@ accuracy_results AS (
 ),
 results AS (
     SELECT
-        accuracy_results.workflow_id AS workflow_id,
-        accuracy_results.job_id AS job_id,
+        performance_results.workflow_id AS workflow_id,
+        performance_results.job_id AS job_id,
         CASE
-            WHEN accuracy_results.replaced_filename LIKE '%_torchbench' THEN 'torchbench'
-            WHEN accuracy_results.replaced_filename LIKE '%_timm_models' THEN 'timm_models'
-            WHEN accuracy_results.replaced_filename LIKE '%_huggingface' THEN 'huggingface'
+            WHEN performance_results.replaced_filename LIKE '%_torchbench' THEN 'torchbench'
+            WHEN performance_results.replaced_filename LIKE '%_timm_models' THEN 'timm_models'
+            WHEN performance_results.replaced_filename LIKE '%_huggingface' THEN 'huggingface'
             ELSE ''
         END AS suite,
         CASE
-            WHEN accuracy_results.replaced_filename LIKE '%_torchbench' THEN REPLACE(
-                accuracy_results.replaced_filename,
+            WHEN performance_results.replaced_filename LIKE '%_torchbench' THEN REPLACE(
+                performance_results.replaced_filename,
                 '_torchbench',
                 ''
             )
-            WHEN accuracy_results.replaced_filename LIKE '%_timm_models' THEN REPLACE(
-                accuracy_results.replaced_filename,
+            WHEN performance_results.replaced_filename LIKE '%_timm_models' THEN REPLACE(
+                performance_results.replaced_filename,
                 '_timm_models',
                 ''
             )
-            WHEN accuracy_results.replaced_filename LIKE '%_huggingface' THEN REPLACE(
-                accuracy_results.replaced_filename,
+            WHEN performance_results.replaced_filename LIKE '%_huggingface' THEN REPLACE(
+                performance_results.replaced_filename,
                 '_huggingface',
                 ''
             )
             ELSE ''
         END AS compiler,
-        accuracy_results.name,
+        performance_results.name,
         IF(speedup != '', toFloat32(speedup), 0.0) AS speedup,
         accuracy,
         IF(
@@ -140,8 +140,8 @@ results AS (
             accuracy_results.timestamp
         ) AS timestamp
     FROM
-        accuracy_results
-        LEFT JOIN performance_results ON performance_results.name = accuracy_results.name
+        performance_results
+        LEFT JOIN accuracy_results ON performance_results.name = accuracy_results.name
         AND performance_results.replaced_filename = accuracy_results.replaced_filename
         AND performance_results.workflow_id = accuracy_results.workflow_id
 )

torchci/components/benchmark/compilers/common.tsx
@@ -27,6 +27,7 @@ export const COMPILER_NAMES_TO_DISPLAY_NAMES: { [k: string]: string } = {
   inductor_with_cudagraphs_freezing: "cudagraphs_freezing",
   inductor_cudagraphs_low_precision: "cudagraphs_low_precision",
   inductor_export: "export",
+  inductor_eager: "eager",
 };
 export const DISPLAY_NAMES_TO_COMPILER_NAMES: { [k: string]: string } = {
   inductor_default: "inductor_no_cudagraphs",
@@ -38,8 +39,9 @@ export const DISPLAY_NAMES_TO_COMPILER_NAMES: { [k: string]: string } = {
   cudagraphs_freezing: "inductor_with_cudagraphs_freezing",
   cudagraphs_low_precision: "inductor_cudagraphs_low_precision",
   export: "inductor_export",
+  eager: "inductor_eager",
 };
-export const BLOCKLIST_COMPILERS = ["aot_eager", "eager"];
+export const BLOCKLIST_COMPILERS = ["aot_eager"];
 export const PASSING_ACCURACY = ["pass", "pass_due_to_skip", "eager_variation"];
 
 // The number of digit after decimal to display on the summary page
@@ -53,7 +55,7 @@ export interface BranchAndCommitPerfData extends BranchAndCommit {
 export const HELP_LINK =
   "https://pytorch.org/docs/main/torch.compiler_performance_dashboard.html";
 
-export const DTYPES = ["amp", "float16", "bfloat16", "quant"];
+export const DTYPES = ["amp", "float16", "bfloat16", "quant", "notset"];
 
 export const DEFAULT_DEVICE_NAME = "cuda (a100)";
 // TODO (huydhn): there is a way to avoid hard-coding dtypes and devices like how
@@ -63,10 +65,12 @@ export const DISPLAY_NAMES_TO_DEVICE_NAMES: { [k: string]: string } = {
   "cuda (a10g)": "cuda_a10g",
   "cpu (x86)": "cpu_x86",
   "cpu (aarch64)": "cpu_aarch64",
+  mps: "mps",
 };
 export const DISPLAY_NAMES_TO_WORKFLOW_NAMES: { [k: string]: string } = {
   "cuda (a100)": "inductor-A100-perf-nightly",
   "cuda (a10g)": "inductor-perf-nightly-A10g",
   "cpu (x86)": "inductor-perf-nightly-x86",
   "cpu (aarch64)": "inductor-perf-nightly-aarch64",
+  mps: "inductor-perf-nightly-macos",
 };

torchci/lib/benchmark/compilerUtils.ts
@@ -39,7 +39,7 @@ export function getPassingModels(data: CompilerPerformanceData[]) {
       models[bucket][workflowId][suite][compiler] = new Set<string>();
     }
 
-    if (PASSING_ACCURACY.includes(accuracy)) {
+    if (PASSING_ACCURACY.includes(accuracy) || compiler === "eager") {
       models[bucket][workflowId][suite][compiler].add(model);
     }
   });

commit 21cb495479c0984fdef731c5e6103b7dd0441412

Fix pt2 dashboard passrate calculation regression (#6170)
The regression comes from
https://github.com/pytorch/test-infra/pull/6006 where I changed
`accuracy_results LEFT JOIN performance_results` to `performance_results
LEFT JOIN accuracy_results` to accommodate Apple MPS eager benchmark.
The swapped join wrongly returned `model_fail_to_load` and
`eager_fail_to_run` models that shouldn't be included in the pass rate
calculation because they are not something wrong with pt2.

### Testing


https://torchci-git-fork-huydhn-fix-pt2-dashboard-p-e92d66-fbopensource.vercel.app/benchmark/compilers?dashboard=torchinductor&startTime=Fri%2C%2019%20Jul%202024%2000%3A29%3A48%20GMT&stopTime=Wed%2C%2015%20Jan%202025%2001%3A29%3A48%20GMT&granularity=week&mode=inference&dtype=bfloat16&deviceName=cuda%20(a100)&lBranch=main&lCommit=1dab79470dbecef79ba4c7d4308d8a181091e58e&rBranch=main&rCommit=b732b52f1e4378f8486ceb5e7026be3321c2651c

* Before
https://hud.pytorch.org/benchmark/torchbench/inductor_with_cudagraphs?dashboard=torchinductor&startTime=Thu%2C%2018%20Jul%202024%2023%3A36%3A11%20GMT&stopTime=Wed%2C%2015%20Jan%202025%2000%3A36%3A11%20GMT&granularity=week&mode=inference&dtype=bfloat16&deviceName=cuda%20(a100)&lBranch=main&lCommit=b732b52f1e4378f8486ceb5e7026be3321c2651c&rBranch=main&rCommit=b732b52f1e4378f8486ceb5e7026be3321c2651c

* After
https://torchci-git-fork-huydhn-fix-pt2-dashboard-p-e92d66-fbopensource.vercel.app/benchmark/torchbench/inductor_with_cudagraphs?dashboard=torchinductor&startTime=Thu%2C%2018%20Jul%202024%2023%3A36%3A11%20GMT&stopTime=Wed%2C%2015%20Jan%202025%2000%3A36%3A11%20GMT&granularity=week&mode=inference&dtype=bfloat16&deviceName=cuda%20(a100)&lBranch=main&lCommit=b732b52f1e4378f8486ceb5e7026be3321c2651c&rBranch=main&rCommit=b732b52f1e4378f8486ceb5e7026be3321c2651chttps%3A%2F%2Ftorchci-git-fork-huydhn-fix-pt2-dashboard-p-e92d66-fbopensource.vercel.app%2F

torchci/clickhouse_queries/compilers_benchmark_performance/query.sql
@@ -83,10 +83,8 @@ accuracy_results AS (
             workflow_id = { workflowId: Int64 }
             OR { workflowId: Int64 } = 0
         )
-        AND accuracy != 'model_fail_to_load'
-        AND accuracy != 'eager_fail_to_run'
 ),
-results AS (
+performance_join_accuracy_results AS (
     SELECT
         performance_results.workflow_id AS workflow_id,
         performance_results.job_id AS job_id,
@@ -144,6 +142,62 @@ results AS (
         LEFT JOIN accuracy_results ON performance_results.name = accuracy_results.name
         AND performance_results.replaced_filename = accuracy_results.replaced_filename
         AND performance_results.workflow_id = accuracy_results.workflow_id
+    WHERE
+        accuracy != 'model_fail_to_load'
+        AND accuracy != 'eager_fail_to_run'
+),
+-- This is to accommodate cases where only accuracy results are available, i.e. export
+accuracy_join_performance_results AS (
+    SELECT
+        accuracy_results.workflow_id AS workflow_id,
+        accuracy_results.job_id AS job_id,
+        CASE
+            WHEN accuracy_results.replaced_filename LIKE '%_torchbench' THEN 'torchbench'
+            WHEN accuracy_results.replaced_filename LIKE '%_timm_models' THEN 'timm_models'
+            WHEN accuracy_results.replaced_filename LIKE '%_huggingface' THEN 'huggingface'
+            ELSE ''
+        END AS suite,
+        CASE
+            WHEN accuracy_results.replaced_filename LIKE '%_torchbench' THEN REPLACE(
+                accuracy_results.replaced_filename,
+                '_torchbench',
+                ''
+            )
+            WHEN accuracy_results.replaced_filename LIKE '%_timm_models' THEN REPLACE(
+                accuracy_results.replaced_filename,
+                '_timm_models',
+                ''
+            )
+            WHEN accuracy_results.replaced_filename LIKE '%_huggingface' THEN REPLACE(
+                accuracy_results.replaced_filename,
+                '_huggingface',
+                ''
+            )
+            ELSE ''
+        END AS compiler,
+        accuracy_results.name,
+        0.0 AS speedup,
+        accuracy,
+        0.0 AS compilation_latency,
+        0.0 AS compression_ratio,
+        0.0 AS abs_latency,
+        0.0 AS dynamo_peak_mem,
+        0.0 AS eager_peak_mem,
+        accuracy_results.timestamp AS timestamp
+    FROM
+        accuracy_results
+        LEFT JOIN performance_results ON performance_results.name = accuracy_results.name
+        AND performance_results.replaced_filename = accuracy_results.replaced_filename
+        AND performance_results.workflow_id = accuracy_results.workflow_id
+    WHERE
+        performance_results.name = ''
+        AND accuracy != 'model_fail_to_load'
+        AND accuracy != 'eager_fail_to_run'
+),
+results AS (
+    SELECT * FROM performance_join_accuracy_results
+    UNION ALL
+    SELECT * FROM accuracy_join_performance_results
 )
 SELECT
     DISTINCT results.workflow_id,