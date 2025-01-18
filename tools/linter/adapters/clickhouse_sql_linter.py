bf6a18c5d9d496835c88a2cf48d805bc5e95dd3c
Camyll Harajli <camyllh@meta.com>
2025-01-17 16:18:10 -0800

    add clickhouse binaries and use s3 init

--- tools/linter/adapters/clickhouse_sql_linter.py
+++ tools/linter/adapters/clickhouse_sql_linter.py
@@ -1,263 +1,138 @@
-from __future__ import annotations
-
 import argparse
 import concurrent.futures
-import dataclasses
-import enum
 import json
 import logging
 import os
+import re
 import subprocess
-import sys
 import time
-from typing import Any, BinaryIO
+from enum import Enum
+from typing import List, NamedTuple, Optional, Pattern
 
 
 LINTER_CODE = "CLICKHOUSE"
-IS_WINDOWS: bool = os.name == "nt"
 
 
-def eprint(*args: Any, **kwargs: Any) -> None:
-    """Print to stderr."""
-    print(*args, file=sys.stderr, flush=True, **kwargs)
+class LintSeverity(str, Enum):
+    ERROR = "error"
+    WARNING = "warning"
+    ADVICE = "advice"
+    DISABLED = "disabled"
 
 
-
-@dataclasses.dataclass(frozen=True)
-class LintMessage:
-    """A lint message defined by https://docs.rs/lintrunner/latest/lintrunner/lint_message/struct.LintMessage.html."""
-
-    path: str | None
-    line: int | None
-    char: int | None
+class LintMessage(NamedTuple):
+    path: Optional[str]
+    line: Optional[int]
+    char: Optional[int]
     code: str
+    severity: LintSeverity
     name: str
-    original: str | None
-    replacement: str | None
-    description: str | None
-
-    def asdict(self) -> dict[str, Any]:
-        return dataclasses.asdict(self)
-
-    def display(self) -> None:
-        """Print to stdout for lintrunner to consume."""
-        print(json.dumps(self.asdict()), flush=True)
+    original: Optional[str]
+    replacement: Optional[str]
+    description: Optional[str]
 
 
-def as_posix(name: str) -> str:
-    return name.replace("\\", "/") if IS_WINDOWS else name
+RESULTS_RE: Pattern[str] = re.compile(
+    r"""(?mx)
+    ^
+    (?P<file>.*?):
+    (?P<line>\d+):
+    (?P<char>\d+):
+    \s(?P<message>.*)
+    \s(?P<code>\[.*\])
+    $
+    """
+)
 
 
-def _run_command(
-    args: list[str],
-    *,
-    timeout: int | None,
-    stdin: BinaryIO | None,
-    input: bytes | None,
-    check: bool,
-    cwd: os.PathLike[Any] | None,
-) -> subprocess.CompletedProcess[bytes]:
+def run_command(
+    args: List[str],
+) -> "subprocess.CompletedProcess[bytes]":
     logging.debug("$ %s", " ".join(args))
     start_time = time.monotonic()
     try:
-        if input is not None:
-            return subprocess.run(
-                args,
-                capture_output=True,
-                shell=False,
-                input=input,
-                timeout=timeout,
-                check=check,
-                cwd=cwd,
-            )
-
         return subprocess.run(
             args,
-            stdin=stdin,
             capture_output=True,
-            shell=False,
-            timeout=timeout,
-            check=check,
-            cwd=cwd,
         )
     finally:
         end_time = time.monotonic()
         logging.debug("took %dms", (end_time - start_time) * 1000)
 
 
-def run_command(
-    args: list[str],
-    *,
-    retries: int = 0,
-    timeout: int | None = None,
-    stdin: BinaryIO | None = None,
-    input: bytes | None = None,
-    check: bool = False,
-    cwd: os.PathLike[Any] | None = None,
-) -> subprocess.CompletedProcess[bytes]:
-    remaining_retries = retries
-    while True:
-        try:
-            return _run_command(
-                args, timeout=timeout, stdin=stdin, input=input, check=check, cwd=cwd
-            )
-        except subprocess.TimeoutExpired as err:
-            if remaining_retries == 0:
-                raise err
-            remaining_retries -= 1
-            logging.warning(
-                "(%s/%s) Retrying because command failed with: %r",
-                retries - remaining_retries,
-                retries,
-                err,
-            )
-            time.sleep(1)
-
-
-def add_default_options(parser: argparse.ArgumentParser) -> None:
-    """Add default options to a parser.
-
-    This should be called the last in the chain of add_argument calls.
-    """
-    parser.add_argument(
-        "--retries",
-        type=int,
-        default=3,
-        help="number of times to retry if the linter times out.",
-    )
-    parser.add_argument(
-        "--verbose",
-        action="store_true",
-        help="verbose logging",
-    )
-    parser.add_argument(
-        "filenames",
-        nargs="+",
-        help="paths to lint",
-    )
-
-
-# def explain_rule(code: str) -> str:
-#     proc = run_command(
-#         ["clickhouse", "rule", "--output-format=json", code],
-#         check=True,
-#     )
-#     rule = json.loads(str(proc.stdout, "utf-8").strip())
-#     return f"\n{rule['linter']}: {rule['summary']}"
-
-
-def format_lint_message(
-    message: str, code: str, rules: dict[str, str], show_disable: bool
-) -> str:
-    if rules:
-        message += f".\n{rules.get(code) or ''}"
-    message += (
-        ".\nSee https://clickhouse.com/docs/en/operations/utilities/clickhouse-format"
-    )
-    if show_disable:
-        message += f".\n\nTo disable, use `  # noqa: {code}`"
-    return message
-
-
-def check_file_for_fixes(
-    filename: str,
-    *,
-    config: str | None,
-    retries: int,
-    timeout: int,
-) -> list[LintMessage]:
+def check_file(
+    binary: str,
+    file: str,
+) -> List[LintMessage]:
     try:
-        with open(filename, "rb") as f:
-            original = f.read()
-        with open(filename, "rb") as f:
-            proc_fix = run_command(
-                [
-                    sys.executable,
-                    "clickhouse-format",
-                    "--query",
-                    '"',
-                    filename,
-                    '"',
-                ],
-                stdin=f,
-                retries=retries,
-                timeout=timeout,
-                check=True,
-            )
-    except (OSError, subprocess.CalledProcessError) as err:
+        proc = run_command([binary, '--format','--query', file])
+    except OSError as err:
         return [
             LintMessage(
                 path=None,
                 line=None,
                 char=None,
                 code=LINTER_CODE,
+                severity=LintSeverity.ERROR,
                 name="command-failed",
                 original=None,
                 replacement=None,
-                description=(
-                    f"Failed due to {err.__class__.__name__}:\n{err}"
-                    if not isinstance(err, subprocess.CalledProcessError)
-                    else (
-                        f"COMMAND (exit code {err.returncode})\n"
-                        f"{' '.join(as_posix(x) for x in err.cmd)}\n\n"
-                        f"STDERR\n{err.stderr.decode('utf-8').strip() or '(empty)'}\n\n"
-                        f"STDOUT\n{err.stdout.decode('utf-8').strip() or '(empty)'}"
-                    )
-                ),
+                description=(f"Failed due to {err.__class__.__name__}:\n{err}"),
             )
         ]
-
-    replacement = proc_fix.stdout
-    if original == replacement:
-        return []
-
+    stdout = str(proc.stdout, "utf-8").strip()
     return [
         LintMessage(
-            path=filename,
-            name="format",
-            description="Run `lintrunner -a` to apply this patch.",
-            line=None,
-            char=None,
+            path=match["file"],
+            name=match["code"],
+            description=match["message"],
+            line=int(match["line"]),
+            char=int(match["char"]),
             code=LINTER_CODE,
-            original=original.decode("utf-8"),
-            replacement=replacement.decode("utf-8"),
+            severity=LintSeverity.ERROR,
+            original=None,
+            replacement='CAMYLL',
         )
+        for match in RESULTS_RE.finditer(stdout)
     ]
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(
-        description=f"Clickhouse format linter. Linter code: {LINTER_CODE}. Use with CLICKHOUSE-FIX to auto-fix issues.",
+        description=f"Clickhouse format linter for sql queries.",
         fromfile_prefix_chars="@",
     )
-
-    add_default_options(parser)
-    args = parser.parse_args()
-
-    logging.basicConfig(
-        format="<%(threadName)s:%(levelname)s> %(message)s",
-        level=(
-            logging.NOTSET
-            if args.verbose
-            else logging.DEBUG if len(args.filenames) < 1000 else logging.INFO
-        ),
-        stream=sys.stderr,
+    parser.add_argument(
+        "filenames",
+        nargs="+",
+        help="paths to lint",
+    )
+    parser.add_argument(
+        "--binary",
+        required=True,
+        help="clickhouse binary path",
     )
 
-    #trying this here since having issues with init_command'
-    run_command(
-                [
-                    sys.executable,
-                    '-m',
-                    'pip',
-                    'install',
-                    'clickhouse',
-                ],
-                retries=0,
-                timeout=0,
-                check=True,
-            )
-    # run_command(["python3 -m pip install clickhouse"])             
+    args = parser.parse_args()
+
+
+    if not os.path.exists(args.binary):
+        err_msg = LintMessage(
+            path="<none>",
+            line=None,
+            char=None,
+            code=LINTER_CODE,
+            severity=LintSeverity.ERROR,
+            name="command-failed",
+            original=None,
+            replacement=None,
+            description=(
+                f"Could not find clickhouse binary at {args.binary},"
+                " you may need to run `lintrunner init`."
+            ),
+        )
+        print(json.dumps(err_msg._asdict()), flush=True)
+        exit(0)
 
     with concurrent.futures.ThreadPoolExecutor(
         max_workers=os.cpu_count(),
@@ -265,22 +140,19 @@ def main() -> None:
     ) as executor:
         futures = {
             executor.submit(
-                check_file_for_fixes,
-                path,
-                config='',
-                retries=0,
-                timeout=90,
-            ): path
-            for path in args.filenames
+                check_file,
+                args.binary,
+                filename,
+            ): filename
+            for filename in args.filenames
         }
         for future in concurrent.futures.as_completed(futures):
             try:
                 for lint_message in future.result():
-                    lint_message.display()
-            except Exception:  # Catch all exceptions for lintrunner
+                    print(json.dumps(lint_message._asdict()), flush=True)
+            except Exception:
                 logging.critical('Failed at "%s".', futures[future])
                 raise
-
 
 if __name__ == "__main__":
     main()
