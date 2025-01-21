eaed0e0dd4efbfc39301e0ef28f360f5478e0a89
Yang Wang <elainewy@meta.com>
2025-01-21 10:17:02 -0800

    test

--- clickhouse_db_schema/oss_ci_utilization/oss_ci_utilization_metadata_schema.sql
+++ clickhouse_db_schema/oss_ci_utilization/oss_ci_utilization_metadata_schema.sql
@@ -1,4 +1,4 @@
--- This query creates the oss_ci_benchmark_v3 table on ClickHouse
+-- This query creates the oss_ci_utilization_metadata table on ClickHouse
 CREATE TABLE misc.oss_ci_utilization_metadata
 (
     -- time stamp info
@@ -28,36 +28,6 @@ CREATE TABLE misc.oss_ci_utilization_metadata
 ENGINE = SharedMergeTree('/clickhouse/tables/{uuid}/{shard}', '{replica}')
 PARTITION BY toYYYYMM(started_at)
 ORDER BY (workflow_id, job_id, started_at)
--- data exists in the db for a year.
--- time to live is based on created_at which is when the record is inserted in db.
-TTL toDate(created_at) + toIntervalYear(1)
-SETTINGS index_granularity = 8192
-
-CREATE TABLE misc.oss_ci_time_series(
-     -- created_at DateTime when the record is processed in db.
-    `created_at` DateTime64(0,'UTC'),
-    `time_stamp` DateTime64(0,'UTC'),
-    `workflow_id` UInt64,
-    `job_id` UInt64,
-    `run_attempt` UInt32,
-    -- type of time series, for instance, utilization log data is 'utilization'.
-    `type` String,
-    `workflow_template_id` UInt64,
-    `job_name` String,
-    -- the data stored as raw json string.
-    -- Notice in clickhouse the length of string type is not limited.
-    `json_data` String,
-    -- The raw records on S3, this is populated by the s3 replicator
-    `_meta` Tuple(bucket String, key String),
-)ENGINE = SharedMergeTree('/clickhouse/tables/{uuid}/{shard}', '{replica}')
-PARTITION BY toYYYYMM(time_stamp)
-ORDER BY
-    (
-        workflow_id,
-        job_id,
-        type,
-        time_stamp,
-    )
 -- data exists in the db for a year.
 -- time to live is based on created_at which is when the record is inserted in db.
 TTL toDate(created_at) + toIntervalYear(1)
