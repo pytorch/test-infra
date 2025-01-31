f168a6e2351ae7e4406e21c9c759f95fe9f88d78
Yang Wang <elainewy@meta.com>
2025-01-31 10:16:28 -0800

    test

--- torchci/lib/utilization/fetchUtilization.ts
+++ torchci/lib/utilization/fetchUtilization.ts
@@ -36,6 +36,8 @@ export default async function fetchUtilization(
     return null;
   }
 
+  const test: UtilizationMetadata = metadata;
+
   const resp: TimeSeriesDbData[] = await getUtilTimesSeries(
     params.workflow_id,
     params.job_id,
