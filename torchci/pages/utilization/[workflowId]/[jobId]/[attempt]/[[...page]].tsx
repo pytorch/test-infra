2af8a270848fdc5ce7293e0ca07a2550c3f884b0
Yang Wang <elainewy@meta.com>
2025-01-30 20:29:19 -0800

    test

--- torchci/pages/utilization/[workflowId]/[jobId]/[attempt]/[[...page]].tsx
+++ torchci/pages/utilization/[workflowId]/[jobId]/[attempt]/[[...page]].tsx
@@ -1,41 +1,27 @@
-import { fetcherHandleError } from "lib/GeneralUtils";
-import { useRouter } from "next/router";
-import useSWRImmutable from "swr";
+import { fetcher } from 'lib/GeneralUtils';
+import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
+import useSWR from 'swr';
 
-const Utilization = () => {
+const ApiData = () => {
   const router = useRouter();
-  const { workflowId, jobId, attempt } = router.query;
+  const { workflowId, jobId, attempt} = router.query;
+  let { data, error } = useSWR(`/api/utilization/${workflowId}/${jobId}/${attempt}`,fetcher,{
+    refreshInterval: 12 * 60 * 60 * 1000, // refresh every 12 hours
+  })
 
-  let { data, error } = useSWRImmutable(
-    `/api/utilization/${workflowId}/${jobId}/${attempt}`,
-    fetcherHandleError
-  );
-
-  if (error) {
-    return (
-      <div>
-        error: {error.message}, StatusCode: {error.status}, info: {error.info}
-      </div>
-    );
-  }
-
-  if (!data) {
-    return <div>loading...</div>;
+  if(!data){
+    return <div>loading...</div>
   }
 
   return (
     <div>
       <h1>API Data</h1>
-      <div>
-        workflowId:{workflowId}, JobId: {jobId}, attempt: {attempt}, job_name:{" "}
-        {data.metadata?.job_name}, workflow_name: {data.metadata?.workflow_name}
-      </div>
-      <div
-        style={{ maxWidth: "800px", whiteSpace: "pre-wrap", overflowX: "auto" }}
-      >
+      <div>workflowId:{workflowId}, JobId: {jobId}, attempt: {attempt}, job_name: {data.metadata.job_name}, workflow_name: {data.metadata.workflow_name}</div>
+      <div style={{ maxWidth: '800px', whiteSpace: 'pre-wrap', overflowX: 'auto'}}>
         <pre>{JSON.stringify(data.metadata, null, 2)}</pre>
-      </div>
+      </div>∂
     </div>
   );
 };
-export default Utilization;
+export default ApiData;
