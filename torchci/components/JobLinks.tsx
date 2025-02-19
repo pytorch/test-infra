0a8a3d2d272f3beab42e062e76a1494f36690dc1
Catherine Lee <csl@fb.com>
2025-02-19 14:56:42 -0800

    tc

--- torchci/components/JobLinks.tsx
+++ torchci/components/JobLinks.tsx
@@ -24,7 +24,7 @@ export default function JobLinks({
       <a
         target="_blank"
         rel="noreferrer"
-        href={`/pytorch/pytorch/commit/${job.sha}#${job.id}-box`}
+        href={`/${job.repo}/commit/${job.sha}#${job.id}-box`}
       >
         Commit
       </a>
