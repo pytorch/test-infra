aa125bfa11a2b73e3157b44584e9126b921d20f6
Ting Lu <92425201+tinglvv@users.noreply.github.com>
2025-02-24 08:18:30 -0800

    Add Cu128 Windows Torchaudio and Torchvision Builds (#6308)
    CUDA 12.8 windows nightlies are available, adding the domain builds too
    
    cc @atalman @ptrblck

--- tools/scripts/generate_binary_build_matrix.py
+++ tools/scripts/generate_binary_build_matrix.py
@@ -462,9 +462,6 @@ def generate_wheels_matrix(
             upload_to_base_bucket = "no"
             if os in (LINUX, WINDOWS):
                 arches += CUDA_ARCHES
-            # todo: remove once windows cuda 12.8 binaries are available
-            if channel == NIGHTLY and os == WINDOWS:
-                arches.remove("12.8")
 
         if with_rocm == ENABLE and os == LINUX:
             arches += ROCM_ARCHES
