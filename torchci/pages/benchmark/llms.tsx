ee6fa7e0c20e3d33c3b0a9b837fece7bb361d4a6
Yang Wang <elainewy@meta.com>
2025-02-27 09:58:18 -0800

    test

--- torchci/pages/benchmark/llms.tsx
+++ torchci/pages/benchmark/llms.tsx
@@ -1,4 +1,5 @@
-import LlmsPage from "components/benchmark/llms/LLsPage";
+import LlmsPage from "components/benchmark/llms/LlmsPage";
+
 /**
  *
  * API routing endpoint for the LLMs pages. Main code is in components/benchmark/llms/LlmsPage.tsx
