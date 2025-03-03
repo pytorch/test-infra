3eaec6e8adbd766f5f8f7c9426984cf978146dd0
Yang Wang <elainewy@meta.com>
2025-03-04 08:48:12 +0900

    remove log

--- clickhouse_db_schema/oss_ci_queue_time_histogram/schema.sql
+++ clickhouse_db_schema/oss_ci_queue_time_histogram/schema.sql
@@ -1,29 +1,23 @@
-CREATE TABLE misc.oss_ci_query_histogram(
-    `type` String,
-    `created_at` DateTime64(0,'UTC'),
-    `time_stamp` DateTime64(0,'UTC'),
+CREATE TABLE misc.oss_ci_queue_time_histogram(
+    `created_at` DateTime64(0, 'UTC'),
+    `time_stamp` DateTime64(0, 'UTC'),
     `repo` String DEFAULT 'pytorch/pytorch',
-    `workflow_id` UInt64,
-    `run_attempt` UInt32,
-    `job_id` UInt64,
     `workflow_name` String,
     `job_name` String,
     `machine_type` String,
     `data_version` String,
     `histogram` Array(UInt64),
     `max_queue_time` UInt64,
-)ENGINE = SharedMergeTree('/clickhouse/tables/{uuid}/{shard}', '{replica}')
+    `total_count` UInt64
+)
+ENGINE = SharedMergeTree('/clickhouse/tables/{uuid}/{shard}', '{replica}')
 PARTITION BY toYYYYMM(created_at)
-ORDER BY
-    (
-        workflow_id,
-        job_id,
-        repo,
-        workflow_name,
-        job_name,
-        machine_type,
-        time_stamp,
-    )
--- data exists in the db for a year.
+ORDER BY (
+    repo,
+    workflow_name,
+    machine_type
+    job_name,
+    time_stamp
+)
 TTL toDate(time_stamp) + toIntervalYear(2)
 SETTINGS index_granularity = 8192
