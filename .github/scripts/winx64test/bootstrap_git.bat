c5222207ffe9492b6cd51c0d2c3903b1b2c076f3
alinpahontu2912 <alin_pahontu@yahoo.com>
2025-03-04 15:20:37 +0100

    update path test

--- .github/scripts/winx64test/bootstrap_git.bat
+++ .github/scripts/winx64test/bootstrap_git.bat
@@ -1,37 +1,25 @@
 :: we need to install newer version of Git manually as "-submodules" function is not supported in the default version of runner.
 
 @echo off
-setlocal enabledelayedexpansion
 
 echo Dependency Git installation started.
 
 :: Pre-check for downloads and dependencies folders
-set "DOWNLOADS_DIR=c:\temp\downloads"
-set "DEPENDENCIES_DIR=c:\temp\dependencies"
-
-if not exist "%DOWNLOADS_DIR%" mkdir "%DOWNLOADS_DIR%"
-if not exist "%DEPENDENCIES_DIR%" mkdir "%DEPENDENCIES_DIR%"
+if not exist "%DOWNLOADS_DIR%" mkdir %DOWNLOADS_DIR%
+if not exist "%DEPENDENCIES_DIR%" mkdir %DEPENDENCIES_DIR%
 
 :: Set download URL for the Git
-set "DOWNLOAD_URL=https://github.com/git-for-windows/git/releases/download/v2.46.0.windows.1/Git-2.46.0-64-bit.exe"
-set "INSTALLER_FILE=%DOWNLOADS_DIR%\Git-2.46.0-64-bit.exe"
+set DOWNLOAD_URL="https://github.com/git-for-windows/git/releases/download/v2.46.0.windows.1/Git-2.46.0-64-bit.exe"
+set INSTALLER_FILE=%DOWNLOADS_DIR%\Git-2.46.0-64-bit.exe
 
 :: Download installer
 echo Downloading Git...
-curl -L -o "%INSTALLER_FILE%" "%DOWNLOAD_URL%"
-
-:: Verify download success
-if not exist "%INSTALLER_FILE%" (
-    echo "Failed to download Git!"
-    exit /b 1
-)
+curl -L -o "%INSTALLER_FILE%" %DOWNLOAD_URL%
 
 :: Install Git
 echo Installing Git...
-
-echo %INSTALLER_FILE%
-
-%INSTALLER_FILE% /VERYSILENT /NORESTART /DIR="%DEPENDENCIES_DIR%\git"
+"%INSTALLER_FILE%" /VERYSILENT /NORESTART /DIR="%DEPENDENCIES_DIR%\git"
+echo Git installed to %DEPENDENCIES_DIR%\git
 
 dir %DEPENDENCIES_DIR%\git
 
@@ -44,9 +32,7 @@ if %errorlevel% neq 0 (
 :: Enable long paths
 call "%DEPENDENCIES_DIR%\git\cmd\git.exe" config --system core.longpaths true
 
-:: Add Git to PATH
-echo %DEPENDENCIES_DIR%\git\cmd\; >> "%GITHUB_PATH%"
-echo %DEPENDENCIES_DIR%\git\bin\; >> "%GITHUB_PATH%"
+:: Add to PATH
+echo %DEPENDENCIES_DIR%\git\cmd\;%DEPENDENCIES_DIR%\git\bin\>> %GITHUB_PATH%
 
 echo Dependency Git installation finished.
-exit /b 0
