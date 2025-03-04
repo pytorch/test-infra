cf2e6e30cf13ff147b75b4f838d2ab8c544342c2
Yang Wang <elainewy@meta.com>
2025-03-05 06:13:20 +0900

    remove log

--- clickhouse_db_schema/oss_ci_queue_time_histogram/schema.sql
+++ clickhouse_db_schema/oss_ci_queue_time_histogram/schema.sql
@@ -1,6 +1,7 @@
 CREATE TABLE misc.oss_ci_queue_time_histogram(
     `created_at` DateTime64(0, 'UTC'),
     `time_stamp` DateTime64(0, 'UTC'),
+    `type` String,
     `repo` String DEFAULT 'pytorch/pytorch',
     `workflow_name` String,
     `job_name` String,
@@ -8,16 +9,18 @@ CREATE TABLE misc.oss_ci_queue_time_histogram(
     `histogram_version` String,
     `histogram` Array(UInt64),
     `max_queue_time` UInt64,
-    `total_count` UInt64
+    `total_count` UInt64,
 )
 ENGINE = SharedMergeTree('/clickhouse/tables/{uuid}/{shard}', '{replica}')
 PARTITION BY toYYYYMM(created_at)
 ORDER BY (
-    repo,
-    workflow_name,
-    machine_type
     job_name,
-    time_stamp
+    workflow_name,
+    machine_type,
+    job_name,
+    time_stamp,
+    repo,
+    type,
 )
 TTL toDate(time_stamp) + toIntervalYear(5)
 SETTINGS index_granularity = 8192
