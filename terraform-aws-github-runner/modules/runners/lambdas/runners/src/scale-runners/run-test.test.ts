8becf7f626e6d25c88caab3fdb86351159ecad31
Camyll Harajli <camyllh@meta.com>
2025-03-05 17:43:58 -0800

    add tests

--- terraform-aws-github-runner/modules/runners/lambdas/runners/src/scale-runners/run-test.test.ts
+++
@@ -1,9 +0,0 @@
-// Import the required modules
-import { getQueuedJobs } from './scale-up-chron';
-
-
-test('getQueuedRunners should fetch queued runners', async () => {
-  const runners = await getQueuedJobs();
-  console.log('Queued Runners:', runners);
-  expect(runners).toBeDefined();
-});
