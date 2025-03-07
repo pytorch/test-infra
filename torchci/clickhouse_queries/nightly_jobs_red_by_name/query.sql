54e045de58047621beb024aca06837b02e0ab64d
Camyll Harajli <camyllh@meta.com>
2025-03-07 10:27:20 -0800

    Update sqlfluff linter capitalization rules and lint more files (#6373)
    Updating capitalization rules to: 
    1. exclude identifiers from being changed in capitalization 
    2. only capitalize keywords, never lowercase to match
    
    Also linting more existing files
    
    addresses issue #6311

--- torchci/clickhouse_queries/nightly_jobs_red_by_name/query.sql
+++ torchci/clickhouse_queries/nightly_jobs_red_by_name/query.sql
@@ -1,19 +1,19 @@
-  SELECT
-            COUNT(*) AS COUNT,
-            workflow.name as name
-        FROM
-            workflow_job job
-            JOIN workflow_run workflow on workflow.id = job.run_id
-            JOIN push ON workflow.head_commit.'id' = push.head_commit.'id'
-        WHERE
-            job.name NOT LIKE '%generate-matrix%'
-            AND job.name NOT LIKE '%unittests%'
-            AND workflow.name NOT IN ('cron', 'Bandit', 'tests', 'Lint')
-            AND push.ref = 'refs/heads/nightly'
-            AND push.repository.'owner'.'name' = 'pytorch'
-            AND push.repository.'name' IN ('pytorch', 'vision', 'audio')
-            AND job.created_at >= {startTime: DateTime64(3)}
-            AND job.created_at < {stopTime: DateTime64(3)}
-            AND job.conclusion IN ('failure', 'timed_out', 'cancelled')
-  GROUP BY
-  workflow.name
+SELECT
+    COUNT(*) AS COUNT,
+    workflow.name AS name
+FROM
+    workflow_job job
+JOIN workflow_run workflow ON workflow.id = job.run_id
+JOIN push ON workflow.head_commit.'id' = push.head_commit.'id'
+WHERE
+    job.name NOT LIKE '%generate-matrix%'
+    AND job.name NOT LIKE '%unittests%'
+    AND workflow.name NOT IN ('cron', 'Bandit', 'tests', 'Lint')
+    AND push.ref = 'refs/heads/nightly'
+    AND push.repository.'owner'.'name' = 'pytorch'
+    AND push.repository.'name' IN ('pytorch', 'vision', 'audio')
+    AND job.created_at >= {startTime: DateTime64(3)}
+    AND job.created_at < {stopTime: DateTime64(3)}
+    AND job.conclusion IN ('failure', 'timed_out', 'cancelled')
+GROUP BY
+    workflow.name
--- torchci/clickhouse_queries/nightly_jobs_red_past_day/query.sql
+++ torchci/clickhouse_queries/nightly_jobs_red_past_day/query.sql
@@ -1,18 +1,18 @@
 SELECT
-  COUNT(*) AS COUNT,
-  job.name as name
+    COUNT(*) AS COUNT,
+    job.name AS name
 FROM
-  workflow_job job
-  JOIN workflow_run workflow ON workflow.id = job.run_id
-  join push on push.head_commit.'id' = workflow.head_commit.'id'
+    workflow_job job
+JOIN workflow_run workflow ON workflow.id = job.run_id
+JOIN push ON push.head_commit.'id' = workflow.head_commit.'id'
 WHERE
-  job.name NOT LIKE '%generate-matrix%'
-  AND job.name NOT LIKE '%unittests%'
-  AND workflow.name NOT IN ('cron', 'Bandit', 'tests')
-  AND push.ref = 'refs/heads/nightly'
-  AND push.repository.'owner'.'name' = 'pytorch'
-  AND push.repository.'name' = {repo: String }
-  AND job.conclusion IN ('failure', 'timed_out', 'cancelled')
-  AND job.completed_at >= today() - 1
+    job.name NOT LIKE '%generate-matrix%'
+    AND job.name NOT LIKE '%unittests%'
+    AND workflow.name NOT IN ('cron', 'Bandit', 'tests')
+    AND push.ref = 'refs/heads/nightly'
+    AND push.repository.'owner'.'name' = 'pytorch'
+    AND push.repository.'name' = {repo: String }
+    AND job.conclusion IN ('failure', 'timed_out', 'cancelled')
+    AND job.completed_at >= today() - 1
 GROUP BY job.name
 ORDER BY COUNT;
