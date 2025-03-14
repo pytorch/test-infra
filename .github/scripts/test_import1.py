2ce12cc50bf704eb76e680c5abf3d7b1507f556c
Andrey Talman <atalman@fb.com>
2025-03-14 18:34:32 -0400

    Update test_import1.py

--- .github/scripts/test_import1.py
+++ .github/scripts/test_import1.py
@@ -4,7 +4,7 @@ SEGFAULT_PROCESS_RETURNCODE = -11
 
 
 try:
-    subprocess.run(["python3", "-c", "import torch"],
+    subprocess.run(["python3", "-m", "run_torch.py"],
                    check=True)
 except subprocess.CalledProcessError as err:
     if err.returncode == SEGFAULT_PROCESS_RETURNCODE:
