38618c591d10cae5f727623aa177eeb0a431bc2f
Yang Wang <elainewy@meta.com>
2025-03-14 18:35:39 -0700

    add time

--- clickhouse_db_schema/oss_ci_job_queue_time_historical/schema.sql
+++ clickhouse_db_schema/oss_ci_job_queue_time_historical/schema.sql
@@ -1,26 +1,22 @@
 CREATE TABLE misc.oss_ci_job_queue_time_historical(
-    `time` DateTime64(9),
-    `type` String,
+    `queue_s` UInt64,
     `repo` String DEFAULT 'pytorch/pytorch',
     `workflow_name` String,
     `job_name` String,
-    `machine_type` String,
-    `histogram_version` String,
-    `histogram` Array(UInt64),
-    `max_queue_time` UInt64,
-    `total_count` UInt64,
-    `extra_info` Map(String,String)
+    `machine_type` String
+    `time` DateTime64(9),
+    -- The raw records on S3, this is populated by the s3 replicator
+    `_meta` Tuple(bucket String, key String)
 )
 ENGINE = SharedMergeTree('/clickhouse/tables/{uuid}/{shard}', '{replica}')
-PARTITION BY toYYYYMM(created_at)
+PARTITION BY toYYYYMM(time)
 ORDER BY (
-    job_name,
-    workflow_name,
-    machine_type,
-    job_name,
-    time,
     repo,
-    type,
+    workflow_name,
+    job_name,
+    machine_type,
+    queue_s,
+    time,
 )
 TTL toDate(time) + toIntervalYear(5)
 SETTINGS index_granularity = 8192
--- clickhouse_db_schema/oss_ci_queue_time_histogram/schema.sql
+++ clickhouse_db_schema/oss_ci_queue_time_histogram/schema.sql
@@ -1,6 +1,4 @@
 CREATE TABLE misc.oss_ci_queue_time_histogram(
-    `created_at` DateTime64(0, 'UTC'),
-    `time_stamp` DateTime64(0, 'UTC'),
     `type` String,
     `repo` String DEFAULT 'pytorch/pytorch',
     `workflow_name` String,
@@ -9,19 +7,20 @@ CREATE TABLE misc.oss_ci_queue_time_histogram(
     `histogram_version` String,
     `histogram` Array(UInt64),
     `max_queue_time` UInt64,
+    `sum_queue_time` UInt64,
     `total_count` UInt64,
+    `time` DateTime64(9),
     `extra_info` Map(String,String)
 )
 ENGINE = SharedMergeTree('/clickhouse/tables/{uuid}/{shard}', '{replica}')
-PARTITION BY toYYYYMM(created_at)
+PARTITION BY toYYYYMM(time)
 ORDER BY (
-    job_name,
-    workflow_name,
-    machine_type,
-    job_name,
-    time_stamp,
-    repo,
     type,
+    workflow_name,
+    job_name,
+    machine_type,
+    repo,
+    time,
 )
-TTL toDate(time_stamp) + toIntervalYear(5)
+TTL toDate(time) + toIntervalYear(5)
 SETTINGS index_granularity = 8192
