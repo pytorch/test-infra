aee09c899836ae47e7cd9ea8296885e45b36d1b4
Wouter Devriendt <wouterdevriendt@meta.com>
2025-03-17 13:48:58 -0700

    lint

--- torchci/pages/_document.tsx
+++ torchci/pages/_document.tsx
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
@@ -50,5 +50,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
