408865d669601ecd74430f1fc9bb5ae76963a55c
Catherine Lee <csl@fb.com>
2025-03-18 14:18:27 -0700

    tc

--- torchci/components/LoginSection.tsx
+++ torchci/components/LoginSection.tsx
@@ -1,56 +1,57 @@
-import { signIn, useSession } from "next-auth/react";
-import { useState } from "react";
-import LoggedInMenu from "./LoggedInMenu";
+import { Button, Menu, MenuItem } from "@mui/material";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import React from "react";
 import styles from "./LoginSection.module.css";
 
 export default function LoginSection() {
   const { data: session, status } = useSession();
-  const loading = status === "loading";
-  const [showLoginSection, setShowLoginSection] = useState(false);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const onClick = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const onClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <>
-      <span
-        className={`nojs-show ${
-          !session && loading ? styles.loading : styles.loaded
-        }`}
-      >
-        <span className={`nojs-show`}>
-          {!session && (
-            <>
-              <a
-                href={`/api/auth/signin`}
-                className={styles.buttonPrimary}
-                onClick={(e) => {
-                  e.preventDefault();
-                  signIn();
-                }}
-              >
-                Sign in
-              </a>
-            </>
-          )}
-          {session?.user && (
-            <span>
-              {session.user.image && (
-                <img
-                  onClick={() => {
-                    console.log("HELLO");
-                    setShowLoginSection(!showLoginSection);
-                  }}
-                  style={{
-                    backgroundImage: `url('${session.user.image}')`,
-                    display: "inline",
-                    cursor: "pointer",
-                  }}
-                  className={styles.avatar}
-                />
-              )}
-              {showLoginSection && <LoggedInMenu />}
-            </span>
-          )}
-        </span>
-      </span>
+      {status == "loading" && <Button disabled></Button>}
+      {status != "loading" && !session && (
+        <Link
+          href={`/api/auth/signin`}
+          onClick={(e) => {
+            e.preventDefault();
+            signIn();
+          }}
+        >
+          <Button>Sign in</Button>
+        </Link>
+      )}
+      {session && (
+        <>
+          <Button style={{ minWidth: "0px" }} onClick={onClick}>
+            <img
+              style={{
+                backgroundImage: `url('${session?.user?.image}')`,
+              }}
+              className={styles.avatar}
+            />
+          </Button>
+          <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={onClose}>
+            <MenuItem>Signed in as {session?.user?.name}</MenuItem>
+            <Link
+              href={`/api/auth/signout`}
+              onClick={(e) => {
+                e.preventDefault();
+                signOut();
+              }}
+            >
+              <MenuItem>Sign out</MenuItem>
+            </Link>
+          </Menu>
+        </>
+      )}
     </>
   );
 }
