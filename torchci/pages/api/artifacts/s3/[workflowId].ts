cbe27788ddc294821d37b669a84e1fbafc90655c
Catherine Lee <csl@fb.com>
2025-03-20 10:04:02 -0700

    tc

--- torchci/pages/api/artifacts/s3/[workflowId].ts
+++ torchci/pages/api/artifacts/s3/[workflowId].ts
@@ -1,13 +1,12 @@
 import fetchS3Links from "lib/fetchS3Links";
 import { Artifact } from "lib/types";
+import _ from "lodash";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Artifact[]>
 ) {
-  const workflowIds = (req.query.workflowId as string)
-    .split(",")
-    .map((id) => id.trim());
-  res.status(200).json(await fetchS3Links(workflowIds));
+
+  res.status(200).json(await fetchS3Links(req.query.workflowId as string));
 }
