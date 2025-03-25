377fc70be56479d09e32c38beeb1c570ebdb7605
Camyll Harajli <camyllh@meta.com>
2025-03-25 16:48:41 -0700

    linted

--- tools/device-farm-runner/run_perf_workflow.py
+++ tools/device-farm-runner/run_perf_workflow.py
@@ -6,27 +6,31 @@ import json
 import logging
 import os
 import random
-from re import A
 import string
 import sys
 import time
 from argparse import ArgumentParser
 from logging import info
+from re import A
 from typing import Any
 
 import requests
 
+
 GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
 logging.basicConfig(level=logging.INFO)
 
+
 def parse_args() -> Any:
-    parser = ArgumentParser("Run Android and iOS tests on AWS Device Farm via github actions workflow run")
+    parser = ArgumentParser(
+        "Run Android and iOS tests on AWS Device Farm via github actions workflow run"
+    )
     parser.add_argument(
-        "--branch", 
-        type=str, 
-        default="main", 
-        required=False, 
-        help="what gh branch to use in pytorch/executorch"
+        "--branch",
+        type=str,
+        default="main",
+        required=False,
+        help="what gh branch to use in pytorch/executorch",
     )
 
     app_type = parser.add_mutually_exclusive_group(required=True)
@@ -48,21 +52,21 @@ def parse_args() -> Any:
         type=str,
         required=False,
         default="llama",
-        help="the model to run on. Default is llama."
+        help="the model to run on. Default is llama.",
     )
     parser.add_argument(
         "--devices",
         type=str,
         required=False,
         default="",
-        help="specific devices to run on. Default is s22 for android and iphone 15 for ios."
+        help="specific devices to run on. Default is s22 for android and iphone 15 for ios.",
     )
     parser.add_argument(
         "--benchmark_configs",
         type=str,
         required=False,
         default="",
-        help="The list of configs used in the benchmark"
+        help="The list of configs used in the benchmark",
     )
 
     parser.add_argument(
@@ -71,7 +75,6 @@ def parse_args() -> Any:
         help="debug mode, the artifacts won't be uploaded to s3, it should mainly used in local env",
     )
 
-
     # in case when removing the flag, the mobile jobs does not failed due to unrecognized flag.
     args, unknown = parser.parse_known_args()
     if len(unknown) > 0:
@@ -79,41 +82,57 @@ def parse_args() -> Any:
     return args
 
 
-
 def run_workflow(app_type, branch, models, devices, benchmark_configs):
     dispatch_hook = "/dispatches"
     if app_type == "android":
         url = f"https://api.github.com/repos/pytorch/executorch/actions/workflows/android-perf.yml"
-    else: 
+    else:
         url = f"https://api.github.com/repos/pytorch/executorch/actions/workflows/apple-perf.yml"
-    
+
     headers = {
         "Accept": "application/vnd.github.v3+json",
         "Authorization": f"Bearer {GITHUB_TOKEN}",
         "X-GitHub-Api-Version": "2022-11-28",
     }
 
-    data = {"ref":f"{branch}", "inputs": {"models": f"{models}", "devices": f"{devices}", "benchmark_configs": f"{benchmark_configs}"}}
-   
+    data = {
+        "ref": f"{branch}",
+        "inputs": {
+            "models": f"{models}",
+            "devices": f"{devices}",
+            "benchmark_configs": f"{benchmark_configs}",
+        },
+    }
+
     resp = requests.post(url + dispatch_hook, headers=headers, data=json.dumps(data))
     if resp.status_code != 204:
         raise Exception(f"Failed to start workflow: {resp.text}")
     else:
         print("Workflow started successfully.")
         if app_type == "android":
-            print("Find your workflow run here: https://github.com/pytorch/executorch/actions/workflows/android-perf.yml")
+            print(
+                "Find your workflow run here: https://github.com/pytorch/executorch/actions/workflows/android-perf.yml"
+            )
         else:
-            print("Find your workflow run here: https://github.com/pytorch/executorch/actions/workflows/apple-perf.yml")
-  
+            print(
+                "Find your workflow run here: https://github.com/pytorch/executorch/actions/workflows/apple-perf.yml"
+            )
+
 
 def main() -> None:
     args = parse_args()
     if args.android:
-        resp = run_workflow("android", args.branch, args.models, args.devices, args.benchmark_configs)
+        resp = run_workflow(
+            "android", args.branch, args.models, args.devices, args.benchmark_configs
+        )
     elif args.ios:
-        resp = run_workflow("ios", args.branch, args.models, args.devices, args.benchmark_configs)
+        resp = run_workflow(
+            "ios", args.branch, args.models, args.devices, args.benchmark_configs
+        )
     else:
-        raise Exception("No app type specified. Please specify either --android or --ios.")
+        raise Exception(
+            "No app type specified. Please specify either --android or --ios."
+        )
 
 
 if __name__ == "__main__":
