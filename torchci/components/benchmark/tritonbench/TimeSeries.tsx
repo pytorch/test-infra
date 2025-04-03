fdadb4e7ba3056fabbc23652190ea7407308e026
Xu Zhao <xzhao9@meta.com>
2025-04-03 17:38:03 -0400

    add triton compile time

---
+++ torchci/components/benchmark/tritonbench/TimeSeries.tsx
@@ -0,0 +1,199 @@
+import { Grid2, Skeleton } from "@mui/material";
+import { Card, CardContent, CardHeader } from "@mui/material";
+import dayjs from "dayjs";
+import { COMMIT_TO_WORKFLOW_ID } from "components/benchmark/BranchAndCommitPicker";
+import { TIME_FIELD_NAME } from "components/benchmark/common";
+import { styled } from "@mui/material/styles";
+import {
+  Granularity,
+  seriesWithInterpolatedTimes,
+  TimeSeriesPanelWithData,
+} from "components/metrics/panels/TimeSeriesPanel";
+import { BranchAndCommit } from "lib/types";
+import { computeCompileTime } from "lib/benchmark/tritonbench/compileTimeUtils"
+import { fetcher } from "lib/GeneralUtils";
+import useSWR from "swr";
+
+
+/** Mui Styles */
+const GraphCardGroup = styled(Card)({
+    margin: "5px",
+  });
+/** Mui Styles */
+
+
+function TimeSeriesGraphPanel({
+  queryName,
+  queryParams,
+  granularity,
+  suite,
+  branch,
+  lCommit,
+  rCommit,
+}:{
+  queryName: string;
+  queryParams: { [key: string]: any };
+  granularity: Granularity;
+  suite: string;
+  branch: string;
+  lCommit: string;
+  rCommit: string;
+}) {
+    return (
+      <>
+      <GraphCardGroup>
+        <CardHeader title={`Suite: TritonBench`} />
+        <CardContent>
+          <SingleGraphPanel
+            queryName={queryName}
+            queryParams={queryParams}
+            granularity={granularity}
+            suite={suite}
+            branch={branch}
+            lCommit={lCommit}
+            rCommit={rCommit}
+          />
+        </CardContent>
+      </GraphCardGroup>
+      </>
+    );
+};
+
+const GRAPH_ROW_HEIGHT = 245;
+
+function SingleGraphPanel({
+    queryName,
+    queryParams,
+    granularity,
+    suite,
+    branch,
+    lCommit,
+    rCommit,
+}:{
+    queryName: string;
+    queryParams: { [key: string]: any };
+    granularity: Granularity;
+    suite: string;
+    branch: string;
+    lCommit: string;
+    rCommit: string;
+}) {
+    const queryParamsWithSuite: { [key: string]: any } = {
+      ...queryParams,
+      branches: [branch],
+      suites: [suite],
+    };
+    // NB: Querying data for all the suites blows up the response from the database
+    // over the lambda reponse body limit of 6MB. So I need to split up the query
+    // here into multiple smaller ones to keep them under the limit
+    //
+    // See more:
+    // * https://nextjs.org/docs/messages/api-routes-body-size-limit
+    // * https://docs.aws.amazon.com/lambda/latest/dg/gettingstarted-limits.html
+    const url = `/api/clickhouse/${queryName}?parameters=${encodeURIComponent(
+      JSON.stringify(queryParamsWithSuite)
+    )}`;
+
+    let { data, error } = useSWR(url, fetcher, {
+        refreshInterval: 60 * 60 * 1000, // refresh every hour
+      });
+    
+      if (error !== undefined) {
+        return (
+          <div>
+            An error occurred while fetching data, perhaps there are too many
+            results with your choice of time range and granularity?
+          </div>
+        );
+      }
+    
+      if (data === undefined || data.length === 0) {
+        return <Skeleton variant={"rectangular"} height={"100%"} />;
+      }
+
+    const groupByFieldName = undefined;
+    // Only show records between these twos
+    const lWorkflowId = COMMIT_TO_WORKFLOW_ID[lCommit];
+    const rWorkflowId = COMMIT_TO_WORKFLOW_ID[rCommit];
+    
+    // const compileTime = computeCompileTime(data).filter((r: any) => {
+    //   const id = r.workflow_id;
+    //   return (
+    //     (id >= lWorkflowId && id <= rWorkflowId) ||
+    //     (id <= lWorkflowId && id >= rWorkflowId)
+    //   );
+    // });
+    // // Clamp to the nearest granularity (e.g. nearest hour) so that the times will
+    // // align with the data we get from the database
+    // const startTime = dayjs(queryParams["startTime"]).startOf(granularity);
+    // const stopTime = dayjs(queryParams["stopTime"]).startOf(granularity);
+
+    // const compileTimeSeries = seriesWithInterpolatedTimes(
+    //     compileTime,
+    //     startTime,
+    //     stopTime,
+    //     granularity,
+    //     groupByFieldName,
+    //     TIME_FIELD_NAME,
+    //     "compile_time",
+    //     false
+    // );
+
+  return (
+    <>
+    {/* <Grid2 container spacing={2}>
+      <Grid2 size={{ xs: 12, lg: 6 }} height={GRAPH_ROW_HEIGHT}>
+        <TimeSeriesPanelWithData
+          data={compileTime}
+          series={compileTimeSeries}
+          title={`Average Compile Time}`}
+          groupByFieldName={groupByFieldName}
+          yAxisRenderer={(unit) => {
+            return `${(unit * 100).toFixed(0)} ms`;
+          }}
+          yAxisLabel={"ms"}
+          additionalOptions={{
+            yAxis: {
+              scale: true,
+            },
+            label: {
+              show: true,
+              align: "left",
+              formatter: (r: any) => {
+                return (r.value[1] * 100).toFixed(0);
+              },
+            },
+          }}
+          legendPadding={310}
+        />
+      </Grid2>
+      </Grid2> */}
+    </>
+    )
+}
+
+export function TimeSeriesGraphReport({
+  queryParams,
+  granularity,
+  lBranchAndCommit,
+  rBranchAndCommit,
+}:{
+  queryParams: { [key: string]: any };
+  granularity: Granularity;
+  lBranchAndCommit: BranchAndCommit;
+  rBranchAndCommit: BranchAndCommit;
+}) {
+  return (
+    <>
+    <TimeSeriesGraphPanel
+      queryName="tritonbench_compile_time"
+      queryParams={queryParams}
+      granularity={granularity}
+      suite={"tritonbench"}
+      branch={lBranchAndCommit.branch}
+      lCommit={lBranchAndCommit.commit}
+      rCommit={rBranchAndCommit.commit}
+    />
+    </>
+  );
+}
---
+++ torchci/pages/tritonbench/compile_time.tsx
@@ -0,0 +1,154 @@
+import { Divider, Stack, Typography } from "@mui/material";
+import {
+  LAST_N_DAYS,
+  MAIN_BRANCH,
+} from "components/benchmark/common";
+import {
+  DEFAULT_HIGHLIGHT_KEY,
+} from "components/benchmark/compilers/common";
+import { TimeRangePicker } from "../metrics";
+import GranularityPicker from "components/GranularityPicker";
+import { Granularity } from "components/metrics/panels/TimeSeriesPanel";
+import dayjs from "dayjs";
+import CopyLink from "components/CopyLink";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { SUITES } from "components/benchmark/tritonbench/SuitePicker";
+import { BranchAndCommitPicker } from "components/benchmark/BranchAndCommitPicker";
+import { TimeSeriesGraphReport } from "components/benchmark/tritonbench/TimeSeries";
+
+export default function Page() {
+  const router = useRouter();
+
+  const defaultStartTime = dayjs().subtract(LAST_N_DAYS, "day");
+  const [startTime, setStartTime] = useState(defaultStartTime);
+  const defaultStopTime = dayjs();
+  const [stopTime, setStopTime] = useState(defaultStopTime);
+  const [timeRange, setTimeRange] = useState<number>(LAST_N_DAYS);
+  const [baseUrl, setBaseUrl] = useState<string>("");
+  const [granularity, setGranularity] = useState<Granularity>("hour");
+  const [lBranch, setLBranch] = useState<string>(MAIN_BRANCH);
+  const [lCommit, setLCommit] = useState<string>("");
+  const [rBranch, setRBranch] = useState<string>(MAIN_BRANCH);
+  const [rCommit, setRCommit] = useState<string>("");
+  const [highlightKey, setHighlightKey] = useState<string>(
+    DEFAULT_HIGHLIGHT_KEY
+  );
+
+  // Set the dropdown value what is in the param
+  useEffect(() => {
+    const startTime: string = (router.query.startTime as string) ?? undefined;
+    if (startTime !== undefined) {
+      setStartTime(dayjs(startTime));
+
+      if (dayjs(startTime).valueOf() !== defaultStartTime.valueOf()) {
+        setTimeRange(-1);
+      }
+    }
+
+    const stopTime: string = (router.query.stopTime as string) ?? undefined;
+    if (stopTime !== undefined) {
+      setStopTime(dayjs(stopTime));
+
+      if (dayjs(stopTime).valueOf() !== defaultStopTime.valueOf()) {
+        setTimeRange(-1);
+      }
+    }
+
+    const granularity: Granularity =
+      (router.query.granularity as Granularity) ?? undefined;
+    if (granularity !== undefined) {
+      setGranularity(granularity);
+    }
+
+    const suite: string = (router.query.suite as string) ?? undefined;
+
+    setBaseUrl(
+      `${window.location.protocol}//${
+        window.location.host
+      }${router.asPath.replace(/\?.+/, "")}`
+    );
+  }, [router.query]);
+
+  const queryParams: { [key: string]: any } = {
+      commits: [],
+      compilers: [],
+      getJobId: false,
+      granularity: granularity,
+      repo: "pytorch-labs/tritonbench",
+      benchmark_name: "compile_time",
+      startTime: dayjs(startTime).utc().format("YYYY-MM-DDTHH:mm:ss.SSS"),
+      stopTime: dayjs(stopTime).utc().format("YYYY-MM-DDTHH:mm:ss.SSS"),
+      suites: Object.keys(SUITES),
+      workflowId: 0,
+  };
+
+  return (
+  <>
+    <div>
+      <Stack direction="row" spacing={2} sx={{ mb: 2 }}>
+        <Typography fontSize={"2rem"} fontWeight={"bold"}>
+          Triton Compile Time DashBoard
+        </Typography>
+        <CopyLink
+          textToCopy={`${baseUrl}`}
+        />
+      </Stack>
+      <Stack direction="row" spacing={2} sx={{ mb: 2 }}>
+        <TimeRangePicker
+          startTime={startTime}
+          setStartTime={setStartTime}
+          stopTime={stopTime}
+          setStopTime={setStopTime}
+          timeRange={timeRange}
+          setTimeRange={setTimeRange}
+          setGranularity={setGranularity}
+        />
+        <GranularityPicker
+          granularity={granularity}
+          setGranularity={setGranularity}
+        />
+        <BranchAndCommitPicker
+          queryName={"tritonbench_benchmark_branches"}
+          queryParams={queryParams}
+          branch={rBranch}
+          setBranch={setRBranch}
+          commit={rCommit}
+          setCommit={setRCommit}
+          titlePrefix={"Base"}
+          fallbackIndex={-1} // Default to the next to latest in the window
+          timeRange={timeRange}
+          highlightConfig={{
+            keys: highlightKey === DEFAULT_HIGHLIGHT_KEY ? [] : [highlightKey],
+            highlightColor: "yellow",
+          }}
+        />
+        <Divider orientation="vertical" flexItem>
+          &mdash;Diff→
+        </Divider>
+        <BranchAndCommitPicker
+          queryName={"tritonbench_benchmark_branches"}
+          queryParams={queryParams}
+          branch={lBranch}
+          setBranch={setLBranch}
+          commit={lCommit}
+          setCommit={setLCommit}
+          titlePrefix={"New"}
+          fallbackIndex={0} // Default to the latest commit
+          timeRange={timeRange}
+          highlightConfig={{
+            keys: highlightKey === DEFAULT_HIGHLIGHT_KEY ? [] : [highlightKey],
+            highlightColor: "yellow",
+          }}
+        />
+      </Stack>
+      <TimeSeriesGraphReport
+        queryParams={queryParams}
+        granularity={granularity}
+        lBranchAndCommit={{ branch: lBranch, commit: lCommit }}
+        rBranchAndCommit={{ branch: rBranch, commit: rCommit }}
+      />
+    </div>
+  </>
+  );
+}
