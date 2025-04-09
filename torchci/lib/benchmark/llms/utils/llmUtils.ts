e409582a91f09c1c07dcc105945bded5d496c253
Yang Wang <elainewy@meta.com>
2025-04-09 11:41:02 -0700

    Add device-level failure (#6514)
    add device level failure to indicate failure of a commit
    
    vercel:
    [demo](https://torchci-479yg882c-fbopensource.vercel.app/benchmark/llms?startTime=Tue%2C%2001%20Apr%202025%2020%3A26%3A59%20GMT&stopTime=Tue%2C%2008%20Apr%202025%2020%3A26%3A59%20GMT&granularity=day&lBranch=main&lCommit=b7eee0cab6057399d20774bdfe5a13a0a9a27179&rBranch=main&rCommit=56c8dc23da92eb1ab0ebf3057a8fd9f4827c88f3&repoName=pytorch%2Fexecutorch&benchmarkName=&modelName=All%20Models&backendName=All%20Backends&modeName=All%20Modes&dtypeName=All%20DType&deviceName=All%20Devices&archName=All%20Platforms)
    
    if base commit has failures:
       - failure_repot column: indicate the failure base commit
       - for models: redender Failure -> RightSight Value
    if new commit has failure:
        - failure_repot column: indicate the failure on new commit
        - for models: redender leftSide value -> rightside
     if lcommit === rcommit:
    - failure_repot column: indicate the failure
     - for models: redender Failure 
    
    UI Screenshot:
    
    ![image](https://github.com/user-attachments/assets/e91cd430-7fd2-40a7-9bb4-bddd8f82bf62)
    
    UI demo with tooltip
    <img width="1569" alt="image"
    src="https://github.com/user-attachments/assets/dbd176ea-106e-402d-b8f0-ad04844f5088"
    />
    
    next step:
    add job-level failure
    
    ---------
    
    Co-authored-by: Huy Do <huydhn@gmail.com>

--- torchci/lib/benchmark/llms/utils/llmUtils.ts
+++ torchci/lib/benchmark/llms/utils/llmUtils.ts
@@ -95,6 +95,7 @@ export function combineLeftAndRight(
   const rData = rPerfData.data;
 
   const dataGroupedByModel: { [k: string]: any } = {};
+
   rData.forEach((record: LLMsBenchmarkData) => {
     const model = record.model;
     const backend = record.backend;
@@ -144,33 +145,6 @@ export function combineLeftAndRight(
     });
   }
 
-  // NB: This is a hack to keep track of valid devices. The problem is that the records
-  // in the benchmark database alone don't have the information to differentiate between
-  // benchmarks that are failed to run and benchmarks that are not run. Both show up as
-  // 0 on the dashboard. Note that we can do a join with workflow_job table to get this
-  // information, but it's a rather slow and expensive route
-  const validDevices = new Set<string>();
-  const validBackends = new Set<string>();
-  // First round to get all the valid devices
-  Object.keys(dataGroupedByModel).forEach((key: string) => {
-    const [model, backend, mode, dtype, device, arch, extra] = key.split(";");
-    const row: { [k: string]: any } = {
-      // Keep the name as as the row ID as DataGrid requires it
-      name: `${model} ${backend} (${mode} / ${dtype} / ${device} / ${arch})`,
-    };
-
-    for (const metric in dataGroupedByModel[key]) {
-      const record = dataGroupedByModel[key][metric];
-      const hasL = "l" in record;
-      const hasR = "r" in record;
-
-      if (hasL && hasR) {
-        validDevices.add(device);
-        validBackends.add(`${model} ${backend}`);
-      }
-    }
-  });
-
   // Transform the data into a displayable format
   const data: { [k: string]: any }[] = [];
   Object.keys(dataGroupedByModel).forEach((key: string) => {
@@ -182,27 +156,14 @@ export function combineLeftAndRight(
 
     for (const metric in dataGroupedByModel[key]) {
       const record = dataGroupedByModel[key][metric];
+
       const hasL = "l" in record;
       const hasR = "r" in record;
-
-      // Skip devices and models that weren't run in this commit
-      if (
-        (validDevices.size !== 0 && !validDevices.has(device)) ||
-        (validBackends.size !== 0 && !validBackends.has(`${model} ${backend}`))
-      ) {
-        continue;
-      }
-
-      // No overlapping between left and right commits, just show what it's on the
-      // right commit instead of showing a blank page
-      if (!hasR) {
-        continue;
-      }
 
       if (!("metadata" in row)) {
         row["metadata"] = {
           model: model,
-          origins: record["r"].origins,
+          origins: hasR ? record["r"].origins : [],
           backend: backend,
           mode: mode,
           dtype: dtype,
@@ -280,32 +241,51 @@ export function combineLeftAndRight(
         const extraInfo = JSON.parse(extra);
         row["is_dynamic"] = extraInfo["is_dynamic"];
       }
-
-      row[metric] = {
-        l: hasL
-          ? {
-              actual: record["l"].actual,
-              target: record["l"].target,
-            }
-          : {
-              actual: 0,
-              target: 0,
-            },
-        r: hasR
-          ? {
-              actual: record["r"].actual,
-              target: record["r"].target,
-            }
-          : {
-              actual: 0,
-              target: 0,
-            },
-        highlight:
-          validDevices.size !== 0 &&
-          validBackends.has(`${model} ${backend}`) &&
-          hasL &&
-          hasR,
-      };
+      if (metric == "FAILURE_REPORT") {
+        row[metric] = {
+          l: hasL
+            ? {
+                actual: Number.MAX_SAFE_INTEGER, // indicate the failure on left side
+                target: 0,
+              }
+            : {
+                actual: 0,
+                target: 0,
+              },
+          r: hasR
+            ? {
+                actual: Number.MAX_SAFE_INTEGER, // indicate the failure on right side
+                target: 0,
+              }
+            : {
+                actual: 0,
+                target: 0,
+              },
+          highlight: hasL && hasR,
+        };
+      } else {
+        row[metric] = {
+          l: hasL
+            ? {
+                actual: record["l"].actual,
+                target: record["l"].target,
+              }
+            : {
+                actual: 0,
+                target: 0,
+              },
+          r: hasR
+            ? {
+                actual: record["r"].actual,
+                target: record["r"].target,
+              }
+            : {
+                actual: 0,
+                target: 0,
+              },
+          highlight: hasL && hasR,
+        };
+      }
     }
 
     if ("metadata" in row) {
