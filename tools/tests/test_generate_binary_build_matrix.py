31c20c2b56204d8231a6d3ac1cd623c8631e267e
Camyll Harajli <camyllh@meta.com>
2025-04-29 10:53:42 -0700

    update tests

--- tools/tests/test_generate_binary_build_matrix.py
+++ tools/tests/test_generate_binary_build_matrix.py
@@ -60,17 +60,6 @@ class GenerateBuildMatrixTest(TestCase):
             reference_output_file="build_matrix_linux_wheel_cuda.json",
         )
 
-    def test_linux_conda_cuda(self):
-        self.matrix_compare_helper(
-            package_type="conda",
-            operating_system="linux",
-            cuda=True,
-            rocm=True,
-            cpu=True,
-            xpu=False,
-            reference_output_file="build_matrix_linux_conda_cuda.json",
-        )
-
     def test_macos_wheel(self):
         self.matrix_compare_helper(
             package_type="wheel",
@@ -82,16 +71,6 @@ class GenerateBuildMatrixTest(TestCase):
             reference_output_file="build_matrix_macos_wheel.json",
         )
 
-    def test_macos_conda(self):
-        self.matrix_compare_helper(
-            package_type="conda",
-            operating_system="macos",
-            cuda=False,
-            rocm=False,
-            cpu=True,
-            xpu=False,
-            reference_output_file="build_matrix_macos_conda.json",
-        )
 
     def test_windows_wheel_cuda(self):
         self.matrix_compare_helper(
@@ -104,16 +83,6 @@ class GenerateBuildMatrixTest(TestCase):
             reference_output_file="build_matrix_windows_wheel_cuda.json",
         )
 
-    def test_windows_conda_cuda(self):
-        self.matrix_compare_helper(
-            package_type="conda",
-            operating_system="windows",
-            cuda=True,
-            rocm=True,
-            cpu=True,
-            xpu=True,
-            reference_output_file="build_matrix_windows_conda_cuda.json",
-        )
 
     def test_windows_wheel_xpu(self):
         self.matrix_compare_helper(
