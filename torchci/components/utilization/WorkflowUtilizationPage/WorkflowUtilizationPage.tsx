1f573519d277d021fa9c22a62a9919385d9e1031
Yang Wang <elainewy@meta.com>
2025-05-06 13:52:54 -0700

    add model test1
    Signed-off-by: Yang Wang <elainewy@meta.com>

--- torchci/components/utilization/WorkflowUtilizationPage/WorkflowUtilizationPage.tsx
+++ torchci/components/utilization/WorkflowUtilizationPage/WorkflowUtilizationPage.tsx
@@ -140,7 +140,7 @@ function useUtilMetadata(workflowId: string | undefined): {
     `/api/list_utilization_metadata_info/${workflowId}?includes_stats=true`,
     fetcher,
     {
-      refreshInterval: 20* 60 * 1000, // refresh every 20 minuts
+      refreshInterval: 20 * 60 * 1000, // refresh every 20 minuts
       // Refresh even when the user isn't looking, so that switching to the tab
       // will always have fresh info.
       refreshWhenHidden: true,
