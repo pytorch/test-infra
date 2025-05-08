c739f89ef4c1dd1df24282a579878b0568abf70e
Yang Wang <elainewy@meta.com>
2025-05-08 12:41:33 -0700

    add model test1
    Signed-off-by: Yang Wang <elainewy@meta.com>

--- torchci/components/benchmark/llms/components/LLMsGraphPanel.tsx
+++ torchci/components/benchmark/llms/components/LLMsGraphPanel.tsx
@@ -419,7 +419,9 @@ function formGraphItem(data: any[]) {
     const deviceId = item?.metadata_info?.device_id;
     const displayName = item.display;
     const group_key =
-      deviceId && deviceId!=="" ? `${displayName} (${deviceId})` : displayName;
+      deviceId && deviceId !== ""
+        ? `${displayName} (${deviceId})`
+        : displayName;
     const seriesData = deepClone(item);
     seriesData.group_key = group_key;
     res.push(seriesData);
