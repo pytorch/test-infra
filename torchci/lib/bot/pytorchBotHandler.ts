e3ae6bbe8061af6b063acc7ae5eb7e86c57fa263
Camyll Harajli <camyllh@meta.com>
2025-05-14 10:22:29 -0700

    [Mergebot] Adding ciflow/pull in PR without pull and lint workflows (#6610)
    fixes on top of reverted PR
    https://github.com/pytorch/test-infra/pull/6604
    
    Addresses issue https://github.com/pytorch/pytorch/issues/152718
    
    ---------
    
    Co-authored-by: Huy Do <huydhn@gmail.com>

--- torchci/lib/bot/pytorchBotHandler.ts
+++ torchci/lib/bot/pytorchBotHandler.ts
@@ -2,6 +2,7 @@ import { PullRequestReview } from "@octokit/webhooks-types";
 import _ from "lodash";
 import { updateDrciComments } from "pages/api/drci/drci";
 import shlex from "shlex";
+import { queryClickhouseSaved } from "../clickhouse";
 import { getHelp, getParser } from "./cliParser";
 import { cherryPickClassifications } from "./Constants";
 import PytorchBotLogger from "./pytorchbotLogger";
@@ -17,6 +18,7 @@ import {
 } from "./utils";
 
 export const CIFLOW_TRUNK_LABEL = "ciflow/trunk";
+export const CIFLOW_PULL_LABEL = "ciflow/pull";
 
 export interface PytorchbotParams {
   owner: string;
@@ -344,6 +346,9 @@ The explanation needs to be clear on why this is needed. Here are some good exam
         }
         await addLabels(this.ctx, [CIFLOW_TRUNK_LABEL]);
       }
+      if (!(await this.hasCiFlowPull())) {
+        await addLabels(this.ctx, [CIFLOW_PULL_LABEL]);
+      }
     }
 
     await this.dispatchEvent("try-merge", {
@@ -609,6 +614,29 @@ The explanation needs to be clear on why this is needed. Here are some good exam
       default:
         return await this.handleConfused(false);
     }
+  }
+
+  async hasCiFlowPull(): Promise<boolean> {
+    try {
+      const workflowNames = await this.getWorkflowsLatest();
+      return (
+        workflowNames?.some(
+          (workflow: any) => workflow.workflow_name === "pull"
+        ) ?? false
+      );
+    } catch (error: any) {
+      // Return true if we cannot read workflow data so that we don't unneccisarily tag the PR
+      await this.logger.log("workflow-pull-error", error);
+      return true;
+    }
+  }
+
+  // Returns the workflows attached to the PR only for the latest commit
+  async getWorkflowsLatest(): Promise<any> {
+    return await queryClickhouseSaved("get_workflows_for_commit", {
+      prNumber: this.prNum,
+      headSha: this.headSha,
+    });
   }
 }
 
