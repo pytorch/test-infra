c922e81f0783bcac4d1b1778fab0979d13113561
Wouter Devriendt <wouterdevriendt@meta.com>
2025-05-27 13:53:50 -0700

    allow users to export cost table as csv (#6672)
    <img width="835" alt="image"
    src="https://github.com/user-attachments/assets/ef761184-23f1-422c-8420-1b1866b032d2"
    />

--- torchci/pages/cost_analysis.tsx
+++ torchci/pages/cost_analysis.tsx
@@ -854,7 +854,12 @@ export default function Page() {
               timeFieldDisplayFormat="M/D (UTC)"
               sort_by="total"
               auto_refresh={false}
-              max_items_in_series={30}
+              defaultOptions={{
+                platform: OS_OPTIONS,
+                gpu: GPU_OPTIONS,
+                provider: PROVIDER_OPTIONS,
+                owner: OWNER_OPTIONS,
+              }}
             />
           )}
           {isLoading && <div>Loading...</div>}
