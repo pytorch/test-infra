f4671a86b089a31f84485e8f1262755ff82e7164
Klaus Zimmermann <klaus.zimmermann@quansight.com>
2025-05-29 11:47:04 +0200

    Add test infrastructure

--- .github/actions/upload-artifact-s3/jest.config.js
+++ .github/actions/upload-artifact-s3/jest.config.js
@@ -5,8 +5,17 @@ module.exports = {
     testEnvironment: 'node',
     testMatch: ['**/*.test.ts'],
     testRunner: 'jest-circus/runner',
+    extensionsToTreatAsEsm: ['.ts'],
     transform: {
-      '^.+\\.ts$': 'ts-jest'
+      '^.+\\.js?$': 'babel-jest',
+      '^.+\\.ts$': [
+        'ts-jest',
+        {
+          useESM: true,
+          tsconfig: 'tsconfig.json'
+        }
+      ]
     },
+    transformIgnorePatterns: ['/node_modules/(?!mime)'],
     verbose: true
   }
