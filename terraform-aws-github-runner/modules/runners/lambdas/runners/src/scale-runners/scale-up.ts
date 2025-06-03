e65e511793f964bdff3d883747c90bd2e4b150c3
Yang Wang <elainewy@meta.com>
2025-06-03 16:49:51 -0700

    add model test1
    Signed-off-by: Yang Wang <elainewy@meta.com>

--- terraform-aws-github-runner/modules/runners/lambdas/runners/src/scale-runners/scale-up.ts
+++ terraform-aws-github-runner/modules/runners/lambdas/runners/src/scale-runners/scale-up.ts
@@ -165,7 +165,7 @@ export async function scaleUp(
   }
 }
 
-export async function createRunnerConfigArgument(
+async function createRunnerConfigArgument(
   runnerType: RunnerType,
   repo: Repo,
   installationId: number | undefined,
