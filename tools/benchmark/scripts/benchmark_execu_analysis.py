713b909704956ff6ee4c9a5005bb9f21b9e17e3e
Yang Wang <elainewy@meta.com>
2025-06-12 11:27:02 -0700

    final
    Signed-off-by: Yang Wang <elainewy@meta.com>

--- tools/benchmark/scripts/benchmark_execu_analysis.py
+++ tools/benchmark/scripts/benchmark_execu_analysis.py
@@ -4,6 +4,8 @@ import sys
 import os
 from datetime import datetime
 from data_models.benchmark_query_group_data_model import BenchmarkQueryGroupDataParams
+from pydantic import ValidationError
+
 
 def validate_iso8601_no_ms(value):
     try:
@@ -16,7 +18,7 @@ def validate_iso8601_no_ms(value):
 
 def argparser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--env', choices=['local', 'prod'], default='local', help='Choose environment')
+    parser.add_argument('--env', choices=['local', 'prod'], default='prod', help='Choose environment')
     parser.add_argument('--startTime', type=validate_iso8601_no_ms, required=True, help='Start time in ISO format (e.g. 2025-06-01T00:00:00)')
     parser.add_argument('--endTime', type=validate_iso8601_no_ms, required=True, help='End time in ISO format (e.g. 2025-06-06T00:00:00)')
     return  parser.parse_args()
@@ -34,17 +36,22 @@ def main():
     start_time_str = args.startTime.strftime("%Y-%m-%dT%H:%M:%S")
     end_time_str = args.endTime.strftime("%Y-%m-%dT%H:%M:%S")
 
-    paramsObject = BenchmarkQueryGroupDataParams(
-        repo = "pytorch/executorch",
-        benchmark_name="ExecuTorch",
-        start_time = start_time_str,
-        end_time = end_time_str,
-        group_table_by_fields= ["device", "backend", "model"],
-        group_row_by_fields= ["workflow_id", "job_id", "granularity_bucket"]
-    )
 
-    params = paramsObject.model_dump()
-    print(params)
+    try:
+        paramsObject = BenchmarkQueryGroupDataParams(
+            repo = "pytorch/executorch",
+            benchmark_name="ExecuTorch",
+            start_time = start_time_str,
+            end_time = end_time_str,
+            group_table_by_fields= ["device", "backend", "model"],
+            group_row_by_fields= ["workflow_id", "job_id", "granularity_bucket"]
+        )
+        params = paramsObject.model_dump()
+        print(f"preparing request paranns: {params}")
+    except ValidationError as e:
+        print(f"Validation failed, {e}")
+        raise
+
     response = requests.get(url, params=params)
     if response.status_code == 200:
         print("Successfully fetched benchmark data")
