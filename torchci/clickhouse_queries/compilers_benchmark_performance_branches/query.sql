ecc36931bae3a875cc2ba235592e29147f826484
Huy Do <huydhn@gmail.com>
2025-06-18 17:19:01 -0700

    Fix lint
    Signed-off-by: Huy Do <huydhn@gmail.com>

--- torchci/clickhouse_queries/compilers_benchmark_performance_branches/query.sql
+++ torchci/clickhouse_queries/compilers_benchmark_performance_branches/query.sql
@@ -1,7 +1,7 @@
 -- This query is used to get the list of branches and commits used by different
 -- OSS CI benchmark experiments. This powers HUD TorchInductor benchmarks dashboards
-SELECT
-    DISTINCT replaceOne(head_branch, 'refs/heads/', '') AS head_branch,
+SELECT DISTINCT
+    replaceOne(head_branch, 'refs/heads/', '') AS head_branch,
     head_sha,
     workflow_id AS id,
     toStartOfDay(fromUnixTimestamp(timestamp)) AS event_time
