e5a921f6d6f5280c9e7c7a128a690c8a845e77e4
Huy Do <huydhn@gmail.com>
2025-06-19 12:50:27 -0700

    Fix materialized view
    Signed-off-by: Huy Do <huydhn@gmail.com>

--- clickhouse_db_schema/oss_ci_benchmark_torchinductor_materialized_views/schema.sql
+++ clickhouse_db_schema/oss_ci_benchmark_torchinductor_materialized_views/schema.sql
@@ -35,31 +35,31 @@ ORDER BY
 CREATE MATERIALIZED VIEW benchmark.oss_ci_benchmark_torchinductor_mv TO benchmark.oss_ci_benchmark_torchinductor AS
 SELECT
     IF(
-        empty(tupleElement(runners [ 1 ], 'name')),
+        empty(tupleElement(runners[1], 'name')),
         IF(
-            empty(tupleElement(benchmark, 'extra_info') [ 'device' ]),
+            empty(tupleElement(benchmark, 'extra_info')['device']),
             'cpu',
-            tupleElement(benchmark, 'extra_info') [ 'device' ]
+            tupleElement(benchmark, 'extra_info')['device']
         ),
-        tupleElement(runners [ 1 ], 'name')
+        tupleElement(runners[1], 'name')
     ) AS device,
     IF(
-        empty(tupleElement(runners [ 1 ], 'type')),
+        empty(tupleElement(runners[1], 'type')),
         IF(
-            empty(tupleElement(benchmark, 'extra_info') [ 'arch' ]),
-            tupleElement(runners [ 1 ], 'cpu_info'),
-            tupleElement(benchmark, 'extra_info') [ 'arch' ]
+            empty(tupleElement(benchmark, 'extra_info')['arch']),
+            tupleElement(runners[1], 'cpu_info'),
+            tupleElement(benchmark, 'extra_info')['arch']
         ),
-        tupleElement(runners [ 1 ], 'type')
+        tupleElement(runners[1], 'type')
     ) AS arch,
-    model.name AS model_name,
-    model.origins [ 1 ] AS suite,
-    metric.name AS metric_name,
-    floor(arrayAvg(metric.benchmark_values), 4) AS value,
-    metric.extra_info AS metric_extra_info,
+    tupleElement(model, 'name') AS model_name,
+    tupleElement(model, 'origins')[1] AS suite,
+    tupleElement(metric, 'name') AS metric_name,
+    floor(arrayAvg(tupleElement(metric, 'benchmark_values')), 4) AS value,
+    tupleElement(metric, 'extra_info') AS metric_extra_info,
     tupleElement(benchmark, 'dtype') AS benchmark_dtype,
     tupleElement(benchmark, 'mode') AS benchmark_mode,
-    benchmark.extra_info AS benchmark_extra_info,
+    tupleElement(benchmark, 'extra_info') AS benchmark_extra_info,
     head_branch AS head_branch,
     head_sha AS head_sha,
     workflow_id AS workflow_id,
@@ -72,7 +72,7 @@ WHERE
     AND tupleElement(benchmark, 'name') = 'TorchInductor'
     AND repo = 'pytorch/pytorch'
     AND (
-        metric.name in [ 'accuracy',
+        tupleElement(metric, 'name') in [ 'accuracy',
         'speedup',
         'compilation_latency',
         'compression_ratio',
@@ -87,31 +87,31 @@ INSERT INTO
     benchmark.oss_ci_benchmark_torchinductor
 SELECT
     IF(
-        empty(tupleElement(runners [ 1 ], 'name')),
+        empty(tupleElement(runners[1], 'name')),
         IF(
-            empty(tupleElement(benchmark, 'extra_info') [ 'device' ]),
+            empty(tupleElement(benchmark, 'extra_info')['device']),
             'cpu',
-            tupleElement(benchmark, 'extra_info') [ 'device' ]
+            tupleElement(benchmark, 'extra_info')['device']
         ),
-        tupleElement(runners [ 1 ], 'name')
+        tupleElement(runners[1], 'name')
     ) AS device,
     IF(
-        empty(tupleElement(runners [ 1 ], 'type')),
+        empty(tupleElement(runners[1], 'type')),
         IF(
-            empty(tupleElement(benchmark, 'extra_info') [ 'arch' ]),
-            tupleElement(runners [ 1 ], 'cpu_info'),
-            tupleElement(benchmark, 'extra_info') [ 'arch' ]
+            empty(tupleElement(benchmark, 'extra_info')['arch']),
+            tupleElement(runners[1], 'cpu_info'),
+            tupleElement(benchmark, 'extra_info')['arch']
         ),
-        tupleElement(runners [ 1 ], 'type')
+        tupleElement(runners[1], 'type')
     ) AS arch,
-    model.name AS model_name,
-    model.origins [ 1 ] AS suite,
-    metric.name AS metric_name,
-    floor(arrayAvg(metric.benchmark_values), 4) AS value,
-    metric.extra_info AS metric_extra_info,
+    tupleElement(model, 'name') AS model_name,
+    tupleElement(model, 'origins')[1] AS suite,
+    tupleElement(metric, 'name') AS metric_name,
+    floor(arrayAvg(tupleElement(metric, 'benchmark_values')), 4) AS value,
+    tupleElement(metric, 'extra_info') AS metric_extra_info,
     tupleElement(benchmark, 'dtype') AS benchmark_dtype,
     tupleElement(benchmark, 'mode') AS benchmark_mode,
-    benchmark.extra_info AS benchmark_extra_info,
+    tupleElement(benchmark, 'extra_info') AS benchmark_extra_info,
     head_branch AS head_branch,
     head_sha AS head_sha,
     workflow_id AS workflow_id,
@@ -123,7 +123,7 @@ WHERE
     tupleElement(benchmark, 'name') = 'TorchInductor'
     AND repo = 'pytorch/pytorch'
     AND (
-        metric.name in [ 'accuracy',
+        tupleElement(metric, 'name') in [ 'accuracy',
         'speedup',
         'compilation_latency',
         'compression_ratio',
@@ -134,4 +134,4 @@ WHERE
         'eager_peak_mem' ]
     )
     AND timestamp >= toUnixTimestamp(toDateTime('2025-01-01 00:00:00'))
-    AND timestamp < toUnixTimestamp(toDateTime('2025-06-20 00:00:00'));
+    AND timestamp < toUnixTimestamp(toDateTime('2025-06-17 00:00:00'));
