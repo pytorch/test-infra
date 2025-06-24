c4a850d5bbc46c6c1f21b347861d57a3f3c13f08
Camyll Harajli <camyllh@meta.com>
2025-06-24 12:51:59 -0700

    add check for release version

--- release/cut-release-branch.sh
+++ release/cut-release-branch.sh
@@ -6,19 +6,26 @@ to the right script.
 
 This script can be used to cut any branch on any repository
 
-For `pytorch/pytorch` usage would be like:
-> DRY_RUN=disabled cut-release-branch.sh
-
 For `pytorch/test-infra` or domains usage would be like:
 > DRY_RUN=disabled GIT_BRANCH_TO_CUT_FROM=main RELEASE_VERSION={version} ./release/cut-release-branch.sh
 '
 
 set -eou pipefail
-
+RELEASE_VERSION=${RELEASE_VERSION:-""}
+if [[ ! RELEASE_VERSION ]]; then
+    echo "RELEASE_VERSION is not set"
+    exit 1
+fi
 GIT_TOP_DIR=$(git rev-parse --show-toplevel)
 GIT_REMOTE=${GIT_REMOTE:-origin}
-GIT_BRANCH_TO_CUT_FROM=${GIT_BRANCH_TO_CUT_FROM:-viable/strict}
+GIT_BRANCH_TO_CUT_FROM=${GIT_BRANCH_TO_CUT_FROM:-main}
 
+
+RELEASE_VERSION=${RELEASE_VERSION:-""}
+if [[ ! RELEASE_VERSION ]]; then
+    echo "RELEASE_VERSION is not set"
+    exit 1
+fi
 TEST_INFRA_BRANCH=${TEST_INFRA_BRANCH:-"release/${RELEASE_VERSION}"}
 
 DRY_RUN_FLAG="--dry-run"
