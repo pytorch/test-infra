2316c59276b7aa9d43f4ade641f0212909ce21f3
Catherine Lee <csl@fb.com>
2025-07-11 10:48:51 -0700

    tc
    ghstack-source-id: 5c6037427d20aa7e151eed703999b60eb7e48c36
    ghstack-comment-id: 3063204502
    Pull-Request: https://github.com/pytorch/test-infra/pull/6918

--- torchci/pages/api/[repoOwner]/[repoName]/commit/[sha].ts
+++ torchci/pages/api/[repoOwner]/[repoName]/commit/[sha].ts
@@ -5,19 +5,22 @@ import type { NextApiRequest, NextApiResponse } from "next";
 export type CommitApiResponse = {
   commit: CommitData;
   jobs: JobData[];
+  workflowIdsByName: Record<string, number[]>;
 };
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<CommitApiResponse>
 ) {
+  const workflowId = parseInt(req.query.workflowId as string, 10) || 0;
   res
     .status(200)
     .json(
       await fetchCommit(
         req.query.repoOwner as string,
         req.query.repoName as string,
-        req.query.sha as string
+        req.query.sha as string,
+        workflowId
       )
     );
 }
