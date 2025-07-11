6efb71bbeafde7f470fadd99d4e16cc155c294a3
Catherine Lee <csl@fb.com>
2025-07-11 09:21:05 -0700

    Update
    [ghstack-poisoned]

---
+++ torchci/components/commit/CommitInfo.tsx
@@ -0,0 +1,55 @@
+import CommitStatus from "components/commit/CommitStatus";
+import { fetcher } from "lib/GeneralUtils";
+import { CommitApiResponse } from "pages/api/[repoOwner]/[repoName]/commit/[sha]";
+import { IssueLabelApiResponse } from "pages/api/issue/[label]";
+import useSWR from "swr";
+
+export default function CommitInfo({
+  repoOwner,
+  repoName,
+  sha,
+}: {
+  repoOwner: string;
+  repoName: string;
+  sha: string;
+}) {
+  const { data: commitData, error } = useSWR<CommitApiResponse>(
+    sha != null ? `/api/${repoOwner}/${repoName}/commit/${sha}` : null,
+    fetcher,
+    {
+      refreshInterval: 60 * 1000, // refresh every minute
+      // Refresh even when the user isn't looking, so that switching to the tab
+      // will always have fresh info.
+      refreshWhenHidden: true,
+    }
+  );
+
+  const { data: unstableIssuesData } = useSWR<IssueLabelApiResponse>(
+    `/api/issue/unstable`,
+    fetcher,
+    {
+      dedupingInterval: 300 * 1000,
+      refreshInterval: 300 * 1000, // refresh every 5 minutes
+    }
+  );
+
+  if (error != null) {
+    return <div>Error occurred</div>;
+  }
+
+  if (commitData === undefined) {
+    return <div>Loading...</div>;
+  }
+  const { commit, jobs } = commitData;
+
+  return (
+    <CommitStatus
+      repoOwner={repoOwner}
+      repoName={repoName}
+      commit={commit}
+      jobs={jobs}
+      isCommitPage={false}
+      unstableIssues={unstableIssuesData ?? []}
+    />
+  );
+}
