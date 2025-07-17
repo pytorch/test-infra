31c97859db034e3b59dcfe2511580ba6870d7672
clee2000 <44682903+clee2000@users.noreply.github.com>
2025-07-17 09:50:12 -0700

    [BE][HUD] Create unified CommitInfo component (#6920)
    There's one for commit and one for PR, but they're mostly the same
    
    Only difference I found:
    * Commit has commit title - so I added an `isCommitPage` param to
    CommitInfo (there's already one in the CommitStatus component so I
    figured why not)
    * data fetch in PR checks if sha is null - changed to sha && url to get
    the same functionality
    
    Should have no UI changes

--- torchci/pages/[repoOwner]/[repoName]/commit/[sha].tsx
+++ torchci/pages/[repoOwner]/[repoName]/commit/[sha].tsx
@@ -1,64 +1,6 @@
-import CommitStatus from "components/commit/CommitStatus";
+import { CommitInfo } from "components/commit/CommitInfo";
 import { useSetTitle } from "components/layout/DynamicTitle";
-import { fetcher } from "lib/GeneralUtils";
 import { useRouter } from "next/router";
-import { CommitApiResponse } from "pages/api/[repoOwner]/[repoName]/commit/[sha]";
-import { IssueLabelApiResponse } from "pages/api/issue/[label]";
-import useSWR from "swr";
-
-export function CommitInfo({
-  repoOwner,
-  repoName,
-  sha,
-}: {
-  repoOwner: string;
-  repoName: string;
-  sha: string;
-}) {
-  const { data: commitData, error } = useSWR<CommitApiResponse>(
-    `/api/${repoOwner}/${repoName}/commit/${sha}`,
-    fetcher,
-    {
-      refreshInterval: 60 * 1000, // refresh every minute
-      // Refresh even when the user isn't looking, so that switching to the tab
-      // will always have fresh info.
-      refreshWhenHidden: true,
-    }
-  );
-
-  const { data: unstableIssuesData } = useSWR<IssueLabelApiResponse>(
-    `/api/issue/unstable`,
-    fetcher,
-    {
-      dedupingInterval: 300 * 1000,
-      refreshInterval: 300 * 1000, // refresh every 5 minutes
-    }
-  );
-
-  if (error != null) {
-    return <div>Error occured</div>;
-  }
-
-  if (commitData === undefined) {
-    return <div>Loading...</div>;
-  }
-
-  const { commit, jobs } = commitData;
-
-  return (
-    <div>
-      <h2>{commit.commitTitle}</h2>
-      <CommitStatus
-        repoOwner={repoOwner}
-        repoName={repoName}
-        commit={commit}
-        jobs={jobs}
-        isCommitPage={true}
-        unstableIssues={unstableIssuesData ?? []}
-      />
-    </div>
-  );
-}
 
 export default function Page() {
   const router = useRouter();
@@ -86,6 +28,7 @@ export default function Page() {
           repoOwner={repoOwner as string}
           repoName={repoName as string}
           sha={sha as string}
+          isCommitPage={true}
         />
       )}
     </div>
