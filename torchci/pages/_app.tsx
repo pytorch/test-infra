2baa712dc58591cb67622d5cc727fb300ab9a9d1
Yang Wang <elainewy@meta.com>
2025-07-22 12:07:31 -0700

    Add event tracking use GA (#6940)
    # Desription
    Add google analytics utils to track UI usage. 
    
    
    By default, each page will have a sessionId to track the user
    interaction flow for targeting UI components (such as click button,
    scroll etc). If user navigate to another page in a new tab, Or close the
    page and reopen, it will create a new session id,
    
    This pr provides two ways to target ui components:
    1. add method 
    2. add attribute
    
    Please see the pr for more details.
    
    ---------
    
    Signed-off-by: Yang Wang <elainewy@meta.com>
    Co-authored-by: clee2000 <44682903+clee2000@users.noreply.github.com>

--- torchci/pages/_app.tsx
+++ torchci/pages/_app.tsx
@@ -7,25 +7,41 @@ import TitleProvider from "components/layout/DynamicTitle";
 import NavBar from "components/layout/NavBar";
 import SevReport from "components/sevReport/SevReport";
 import { DarkModeProvider } from "lib/DarkModeContext";
-import { track } from "lib/track";
+import { setupGAAttributeEventTracking } from "lib/tracking/eventTrackingHandler";
+import {
+  initGaAnalytics,
+  isGaInitialized,
+  trackRouteEvent,
+} from "lib/tracking/track";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import ReactGA from "react-ga4";
 import { useAppTheme } from "styles/MuiThemeOverrides";
 import "styles/globals.css";
 import("lib/chartTheme");
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
-  useEffect(() => {
-    // GA records page views on its own, but I want to see how it differs with
-    // this one.
-    track(router, "pageview", {});
-  }, [router, router.pathname]);
 
-  ReactGA.initialize("G-HZEXJ323ZF");
+  useEffect(() => {
+    let cleanup: (() => void) | undefined;
+    initGaAnalytics(true);
+    if (isGaInitialized()) {
+      cleanup = setupGAAttributeEventTracking(["click"]);
+    } else {
+      console.warn(
+        "GA not initialized, skipping attribute event tracking setup."
+      );
+    }
+    return () => {
+      if (cleanup) cleanup();
+    };
+  }, []);
+
+  useEffect(() => {
+    trackRouteEvent(router, "pageview", {});
+  }, [router.asPath]);
 
   // Wrap everything in DarkModeProvider
   return (
