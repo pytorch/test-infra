df9b329a6521260bc1ad51d50baca241286382df
Catherine Lee <csl@fb.com>
2025-07-31 14:47:58 -0700

    tc

--- torchci/clickhouse_queries/test_times/job_names_past_month/query.sql
+++ torchci/clickhouse_queries/test_times/job_names_past_month/query.sql
@@ -1,5 +1,4 @@
-SELECT DISTINCT
-    REGEXP_EXTRACT(j.name, '^(.*) /', 1) AS base_name
+SELECT DISTINCT REGEXP_EXTRACT(j.name, '^(.*) /', 1) AS base_name
 FROM
     default.workflow_job j
 WHERE j.created_at > now() - INTERVAL 1 MONTH
