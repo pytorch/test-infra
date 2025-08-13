5382f4db611d5ab74d002b2f61a2a0cb30f86433
Zhewen Li <zhewenli@meta.com>
2025-08-13 16:36:14 -0700

    [RFC] add class support in bc-linter (#6953)
    as discussed with @izaitsevfb, we want to also include linter checks on
    classes in addition to public functions; but prior to landing this PR,
    we need to create some rules template for linters, where we can define
    what to check(or not).
    Like:
    ```
    pytorch:
    - Files: global
    - Include: functions
    
    vLLM:
    - Files: vllm/v1/attrition, vllm/v1/core
    - Include, classes, functions
    ```
    
    
    cc @huydhn @yangw-dev @houseroad
    
    ---------
    
    Co-authored-by: Ivan Zaitsev <ivanzaitsev@fb.com>

--- tools/stronghold/tests/api/test_ast.py
+++ tools/stronghold/tests/api/test_ast.py
@@ -1,5 +1,6 @@
 """Tests the api.ast module."""
 
+import dataclasses
 import pathlib
 
 import api
@@ -12,7 +13,7 @@ def test_extract_empty(tmp_path: pathlib.Path) -> None:
     def func() -> None:
         pass  # pragma: no cover
 
-    funcs = api.ast.extract(source.make_file(tmp_path, func))
+    funcs = api.ast.extract(source.make_file(tmp_path, func)).functions
     assert funcs == {
         "func": api.Parameters(
             parameters=[], variadic_args=False, variadic_kwargs=False, line=1
@@ -24,7 +25,7 @@ def test_extract_positional(tmp_path: pathlib.Path) -> None:
     def func(x: int, /) -> None:
         pass  # pragma: no cover
 
-    funcs = api.ast.extract(source.make_file(tmp_path, func))
+    funcs = api.ast.extract(source.make_file(tmp_path, func)).functions
     assert funcs == {
         "func": api.Parameters(
             parameters=[
@@ -48,7 +49,7 @@ def test_extract_positional_with_default(tmp_path: pathlib.Path) -> None:
     def func(x: int = 0, /) -> None:
         pass  # pragma: no cover
 
-    funcs = api.ast.extract(source.make_file(tmp_path, func))
+    funcs = api.ast.extract(source.make_file(tmp_path, func)).functions
     assert funcs == {
         "func": api.Parameters(
             parameters=[
@@ -72,7 +73,7 @@ def test_extract_flexible(tmp_path: pathlib.Path) -> None:
     def func(x: int) -> None:
         pass  # pragma: no cover
 
-    funcs = api.ast.extract(source.make_file(tmp_path, func))
+    funcs = api.ast.extract(source.make_file(tmp_path, func)).functions
     assert funcs == {
         "func": api.Parameters(
             parameters=[
@@ -96,7 +97,7 @@ def test_extract_flexible_with_default(tmp_path: pathlib.Path) -> None:
     def func(x: int = 0) -> None:
         pass  # pragma: no cover
 
-    funcs = api.ast.extract(source.make_file(tmp_path, func))
+    funcs = api.ast.extract(source.make_file(tmp_path, func)).functions
     assert funcs == {
         "func": api.Parameters(
             parameters=[
@@ -120,7 +121,7 @@ def test_extract_keyword(tmp_path: pathlib.Path) -> None:
     def func(*, x: int) -> None:
         pass  # pragma: no cover
 
-    funcs = api.ast.extract(source.make_file(tmp_path, func))
+    funcs = api.ast.extract(source.make_file(tmp_path, func)).functions
     assert funcs == {
         "func": api.Parameters(
             parameters=[
@@ -144,7 +145,7 @@ def test_extract_keyword_with_default(tmp_path: pathlib.Path) -> None:
     def func(*, x: int = 0) -> None:
         pass  # pragma: no cover
 
-    funcs = api.ast.extract(source.make_file(tmp_path, func))
+    funcs = api.ast.extract(source.make_file(tmp_path, func)).functions
     assert funcs == {
         "func": api.Parameters(
             parameters=[
@@ -168,7 +169,7 @@ def test_extract_variadic_args(tmp_path: pathlib.Path) -> None:
     def func(*args: int) -> None:
         pass  # pragma: no cover
 
-    funcs = api.ast.extract(source.make_file(tmp_path, func))
+    funcs = api.ast.extract(source.make_file(tmp_path, func)).functions
     assert funcs == {
         "func": api.Parameters(
             parameters=[], variadic_args=True, variadic_kwargs=False, line=1
@@ -180,7 +181,7 @@ def test_extract_variadic_kwargs(tmp_path: pathlib.Path) -> None:
     def func(**kwargs: int) -> None:
         pass  # pragma: no cover
 
-    funcs = api.ast.extract(source.make_file(tmp_path, func))
+    funcs = api.ast.extract(source.make_file(tmp_path, func)).functions
     assert funcs == {
         "func": api.Parameters(
             parameters=[], variadic_args=False, variadic_kwargs=True, line=1
@@ -193,7 +194,7 @@ def test_extract_class_method(tmp_path: pathlib.Path) -> None:
         def func(self, /) -> None:
             pass  # pragma: no cover
 
-    funcs = api.ast.extract(source.make_file(tmp_path, Class))
+    funcs = api.ast.extract(source.make_file(tmp_path, Class)).functions
     assert funcs == {
         "Class.func": api.Parameters(
             parameters=[
@@ -212,14 +213,74 @@ def test_extract_class_method(tmp_path: pathlib.Path) -> None:
     }
 
 
+def test_extract_dataclass(tmp_path: pathlib.Path) -> None:
+    @dataclasses.dataclass
+    class Class:
+        a: int
+        b: int = 1
+
+    classes = api.ast.extract(
+        source.make_file(tmp_path, Class), include_classes=True
+    ).classes
+    assert classes == {
+        "Class": api.Class(
+            fields=[
+                api.Field(
+                    name="a",
+                    required=True,
+                    line=3,
+                    type_annotation=api.types.TypeName("int"),
+                ),
+                api.Field(
+                    name="b",
+                    required=False,
+                    line=4,
+                    type_annotation=api.types.TypeName("int"),
+                ),
+            ],
+            line=2,
+            dataclass=True,
+        )
+    }
+
+
 def test_extract_comprehensive(tmp_path: pathlib.Path) -> None:
     class Class:
+        a: int
+        b: float = 1.0
+
         def func(
             self, a: int, /, b: float = 2, *args: int, c: int, **kwargs: int
         ) -> None:
             pass  # pragma: no cover
 
-    funcs = api.ast.extract(source.make_file(tmp_path, Class))
+    extract_api = api.ast.extract(
+        source.make_file(tmp_path, Class), include_classes=True
+    )
+    funcs = extract_api.functions
+    classes = extract_api.classes
+
+    assert classes == {
+        "Class": api.Class(
+            fields=[
+                api.Field(
+                    name="a",
+                    required=True,
+                    line=2,
+                    type_annotation=api.types.TypeName("int"),
+                ),
+                api.Field(
+                    name="b",
+                    required=False,
+                    line=3,
+                    type_annotation=api.types.TypeName("float"),
+                ),
+            ],
+            line=1,
+            dataclass=False,
+        )
+    }
+
     assert funcs == {
         "Class.func": api.Parameters(
             parameters=[
@@ -228,14 +289,14 @@ def test_extract_comprehensive(tmp_path: pathlib.Path) -> None:
                     positional=True,
                     keyword=False,
                     required=True,
-                    line=3,
+                    line=6,
                 ),
                 api.Parameter(
                     name="a",
                     positional=True,
                     keyword=False,
                     required=True,
-                    line=3,
+                    line=6,
                     type_annotation=api.types.TypeName("int"),
                 ),
                 api.Parameter(
@@ -243,7 +304,7 @@ def test_extract_comprehensive(tmp_path: pathlib.Path) -> None:
                     positional=True,
                     keyword=True,
                     required=False,
-                    line=3,
+                    line=6,
                     type_annotation=api.types.TypeName("float"),
                 ),
                 api.Parameter(
@@ -251,12 +312,12 @@ def test_extract_comprehensive(tmp_path: pathlib.Path) -> None:
                     positional=False,
                     keyword=True,
                     required=True,
-                    line=3,
+                    line=6,
                     type_annotation=api.types.TypeName("int"),
                 ),
             ],
             variadic_args=True,
             variadic_kwargs=True,
-            line=2,
+            line=5,
         )
     }
