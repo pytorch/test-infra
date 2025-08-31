ee4732790c86cafce88ec002a3f2d782c169ccdc
Yang Wang <elainewy@meta.com>
2025-08-31 11:15:09 -0700

    addid

--- torchci/components/metrics/panels/TablePanel.tsx
+++ torchci/components/metrics/panels/TablePanel.tsx
@@ -7,6 +7,7 @@ import useSWR from "swr";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
+
 export default function TablePanel({
   // Human-readable title for this panel.
   title,
