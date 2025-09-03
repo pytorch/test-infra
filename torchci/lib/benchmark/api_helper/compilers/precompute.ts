a21f91adfe4ab7e6e265222651b81a888c14f194
Yang Wang <elainewy@meta.com>
2025-09-03 10:40:55 -0700

    addid

--- torchci/lib/benchmark/api_helper/compilers/precompute.ts
+++ torchci/lib/benchmark/api_helper/compilers/precompute.ts
@@ -163,7 +163,7 @@ function toPrecomputeCompiler(
   }
 
   const response: BenchmarkTimeSeriesResponse = {
-    time_series: res,
+    time_series: rawData,
     time_range: {
       start: new Date(earliest_timestamp).toISOString(),
       end: new Date(latest_timestamp).toISOString(),
