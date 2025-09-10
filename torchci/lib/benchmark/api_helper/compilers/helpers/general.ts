c5df95416862347490934086e1e8b84199c10f3e
Yang Wang <elainewy@meta.com>
2025-09-09 19:32:35 -0700

    improve api

--- torchci/lib/benchmark/api_helper/compilers/helpers/general.ts
+++ torchci/lib/benchmark/api_helper/compilers/helpers/general.ts
@@ -19,6 +19,7 @@ export function toGeneralCompilerData(
   const end_ts = new Date(
     rawData[rawData.length - 1].granularity_bucket
   ).getTime();
+
   rawData = rawData.map((data) => {
     return {
       ...data,
@@ -26,7 +27,6 @@ export function toGeneralCompilerData(
       arch: toApiArch(data.device, data.arch),
     };
   });
-
   let res: any[] = [];
   switch (type) {
     case "time_series":
