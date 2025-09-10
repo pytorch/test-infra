fd121a5edefff3e072bf48ae1bfa618e2fbd572c
Catherine Lee <csl@fb.com>
2025-09-10 08:30:17 -0700

    tc

--- torchci/pages/metrics.tsx
+++ torchci/pages/metrics.tsx
@@ -24,7 +24,9 @@ import { EChartsOption } from "echarts";
 import ReactECharts from "echarts-for-react";
 import { useDarkMode } from "lib/DarkModeContext";
 import { fetcher } from "lib/GeneralUtils";
-import { useEffect, useState } from "react";
+import { useEffect, useState
+
+} from "react";
 import { default as useSWR, default as useSWRImmutable } from "swr";
 
 const DISABLED_TESTS_CONDENSED_URL =
