9f3cd5c5f8561be3efb697789fc9cf92e6d350b5
Yang Wang <elainewy@meta.com>
2025-09-12 00:58:55 -0400

    add old way

--- torchci/lib/benchmark/api_helper/compilers/helpers/general.ts
+++ torchci/lib/benchmark/api_helper/compilers/helpers/general.ts
@@ -35,25 +35,33 @@ const COMPILER_GENERAL_TABLE_SUB_GROUP_KEY = ["metric"];
  */
 export function toGeneralCompilerData(
   rawData: any[],
-  type: string = "time_series"
+  formats: string[] = ["time_series"]
 ) {
   const start_ts = new Date(rawData[0].granularity_bucket).getTime();
   const end_ts = new Date(
     rawData[rawData.length - 1].granularity_bucket
   ).getTime();
 
-  let res: any[] = [];
-  switch (type) {
+  let formats_result: any = {};
+
+  formats.forEach((format) => {
+    const data = getformat(rawData, format);
+    formats_result[format] = data;
+  });
+  return toTimeSeriesResponse(formats_result, rawData.length, start_ts, end_ts);
+}
+
+function getformat(data: any, format: string) {
+  switch (format) {
     case "time_series":
-      res = to_time_series_data(
-        rawData,
+      return to_time_series_data(
+        data,
         COMPILER_GENERAL_TS_GROUP_KEY,
         COMPILER_GENERAL_TS_SUB_GROUP_KEY
       );
-      break;
     case "table":
-      res = groupByBenchmarkData(
-        rawData,
+      return groupByBenchmarkData(
+        data,
         COMPILER_GENERAL_TABLE_GROUP_KEY,
         COMPILER_GENERAL_TABLE_SUB_GROUP_KEY
       );
@@ -61,5 +69,4 @@ export function toGeneralCompilerData(
     default:
       throw new Error("Invalid type");
   }
-  return toTimeSeriesResponse(res, rawData.length, start_ts, end_ts);
 }
