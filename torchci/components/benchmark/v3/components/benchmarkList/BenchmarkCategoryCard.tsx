cb4df87c8f47144572b37240b2cffb89670ad8a1
Yang Wang <elainewy@meta.com>
2025-10-02 11:10:23 -0700

    Add wrap so when user shrink the screen, all info and chip buttons are not out of card scope (#7278)
    https://torchci-git-fixcss-fbopensource.vercel.app/benchmark/benchmark_list

--- torchci/components/benchmark/v3/components/benchmarkList/BenchmarkCategoryCard.tsx
+++ torchci/components/benchmark/v3/components/benchmarkList/BenchmarkCategoryCard.tsx
@@ -126,6 +126,7 @@ function BenchmarkCardItem({
             sx={{ px: 2, pb: 1, pt: 0.5 }}
             direction="row"
             alignItems="center"
+            flexWrap="wrap"
           >
             <Typography variant="subtitle1">Details:</Typography>
             <Typography
@@ -144,7 +145,12 @@ function BenchmarkCardItem({
 
         {it.actions && (
           <Box sx={{ px: 2, pb: 1, pt: 0.5 }}>
-            <Stack direction="row" spacing={1} alignItems="center">
+            <Stack
+              direction="row"
+              spacing={1}
+              alignItems="center"
+              flexWrap="wrap"
+            >
               <Typography variant="subtitle1">Pages:</Typography>
               {it.actions.map((a, idx) => {
                 if (!a.href) {
