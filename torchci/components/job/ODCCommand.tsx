ebf10bd2594ad87e17c4e806a194787738bc261c
Catherine Lee <csl@fb.com>
2025-10-23 15:32:40 -0700

    tc

--- torchci/components/job/ODCCommand.tsx
+++ torchci/components/job/ODCCommand.tsx
@@ -1,5 +1,17 @@
-import { Typography } from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 import { Box, Stack } from "@mui/system";
+import { useState } from "react";
 import useSWR from "swr";
 
 const STARTING_INSTRUCTIONS =
@@ -16,15 +28,16 @@ const STARTING_INSTRUCTIONS =
  * - only tests
  * - no building
  */
-
-export function OSDCommandInstructions({
+export function ODCommandInstructions({
   jobId,
   workflowId,
   failureLineNum,
+  headSha,
 }: {
-  jobId: string;
+  jobId: number;
   workflowId: number;
   failureLineNum: number;
+  headSha: string;
 }) {
   /**
    * Steps:
@@ -35,7 +48,10 @@ export function OSDCommandInstructions({
    * 5. Set environment variables
    * 6. Run command
    *
-   * Example gpu-dev command: gpu-dev reserve --gpu-type t4 --dockerimage  ghcr.io/pytorch/ci-image:pytorch-linux-jammy-py3.10-clang12-3bdcaae7001ead0f64837461a6
+   * Example reserve gpu-dev command: gpu-dev reserve --gpu-type t4
+   * --dockerimage
+   * ghcr.io/pytorch/ci-image:pytorch-linux-jammy-py3.10-clang12-3bdcaae7001ead0f64837461a6
+   *
    * Search the job log for:
    *   binary info
    *   docker image
@@ -47,9 +63,232 @@ export function OSDCommandInstructions({
    * In the future, this should probably be an artifact of the job
    *
    */
-  const logURL = `https://ossci-raw-job-status.s3.amazonaws.com/log/${jobId}`;
 
-  const log = useSWR<string | undefined>(
+  const [open, setOpen] = useState(false);
+  const reproInfo = useInformationFromJobLog(workflowId, jobId, failureLineNum);
+
+  if (reproInfo === undefined) {
+    return null;
+  }
+
+  const { gpuType, dockerImage, binaryURL, envVars, command } = reproInfo;
+
+  function getInstructionsInsideMachine() {
+    // Helper function for the initial setup commands inside the reserved
+    // machine
+    const instructions = [
+      `# Use a tmp directory to avoid polluting your workspace`,
+      `rm -rf /tmp/odc-repro && mkdir -p /tmp/odc-repro && cd /tmp/odc-repro`,
+      `# Set up environment variables`,
+      `export PATH=${envVars["PATH"]}:$PATH`,
+      `# Clone pytorch repo`,
+      `git clone https://github.com/pytorch/pytorch.git && cd pytorch && git checkout ${headSha}`,
+      `# Download the binary artifacts`,
+      `curl ${binaryURL} -o /tmp/odc-repro/artifacts.zip`,
+      `unzip /tmp/odc-repro/artifacts.zip -d /tmp/odc-repro/artifacts`,
+      `# Install the wheel `,
+      `pip install $(echo /tmp/odc-repro/artifacts/dist/*.whl)[opt-einsum]`,
+      `# Set up python path to use the local checkout`,
+      `export PYTHONPATH=/tmp/odc-repro/pytorch:$PYTHONPATH`,
+    ];
+    return instructions.join("\n");
+  }
+
+  return (
+    <>
+      <Button
+        size="small"
+        onClick={() => setOpen(!open)}
+        sx={{
+          borderTop: 0,
+          borderBottom: 0,
+          mt: 0,
+          mb: 0,
+          pt: 0,
+          pb: 0,
+        }}
+      >
+        gpu-dev instructions
+      </Button>
+      <Dialog
+        open={open}
+        onClose={() => setOpen(false)}
+        maxWidth="md"
+        fullWidth
+      >
+        <DialogTitle>OSDC gpu-dev Reproduction Instructions</DialogTitle>
+        <DialogContent>
+          <Stack spacing={2}>
+            <Typography variant="body1">
+              This is only available to Meta employees. You can use the OSDC
+              gpu-dev CLI to reserve a machine similar to the one used in CI and
+              reproduce the failure. Follow the instructions below to set up the
+              gpu-dev CLI, reserve a machine, and run the command that failed in
+              the CI job.
+            </Typography>
+            <Typography variant="body1">
+              Currently this does not support rebuilding the binary, so
+              modifications to cpp and other files will not be reflected.
+            </Typography>
+
+            <InstructionItem
+              index={1}
+              title="Set up OSDC gpu-dev CLI"
+              component={
+                <Typography variant="body1">
+                  Follow the instructions{" "}
+                  <a
+                    href={STARTING_INSTRUCTIONS}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    here
+                  </a>{" "}
+                  to set up the OSDC gpu-dev CLI.
+                </Typography>
+              }
+            />
+
+            <InstructionItem
+              index={2}
+              title="Reserve a machine"
+              component={
+                <>
+                  <Typography variant="body1">
+                    Run the following command to reserve a machine similar to
+                    the one used in CI:
+                  </Typography>
+                  <TerminalCopyBox
+                    text={
+                      `gpu-dev reserve --gpu-type ${gpuType} ` +
+                      `--dockerimage ${dockerImage}`
+                    }
+                  />
+                </>
+              }
+            />
+
+            <InstructionItem
+              index={3}
+              title="SSH into the reserved machine"
+              component={
+                <Typography variant="body1">
+                  The gpu-dev command to reserve a machine will provide an ssh
+                  command.
+                </Typography>
+              }
+            />
+            <InstructionItem
+              index={4}
+              title="Download the binary"
+              component={
+                <>
+                  <Typography variant="body1">
+                    The following commands will clone pytorch, set up
+                    environment variables, and download the binary:
+                  </Typography>
+                  <TerminalCopyBox text={getInstructionsInsideMachine()} />
+                </>
+              }
+            />
+            <InstructionItem
+              index={5}
+              title="Download the binary"
+              component={
+                <>
+                  <Typography variant="body1">Run the test</Typography>
+                  <TerminalCopyBox text={command} />
+                </>
+              }
+            />
+          </Stack>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)}>Close</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
+
+function InstructionItem({
+  index,
+  title,
+  component,
+}: {
+  index: number;
+  title: string;
+  component: React.ReactNode;
+}) {
+  return (
+    <Box>
+      <Typography variant="subtitle1" fontWeight="bold">
+        Step {index}: {title}
+      </Typography>
+      <Box>{component}</Box>
+    </Box>
+  );
+}
+
+/**
+ * Component for displaying a box with monospace text that is copied on click.
+ * Good for terminal commands.
+ * @param param0
+ * @returns
+ */
+function TerminalCopyBox({ text }: { text: string }) {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(text);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
+  };
+  return (
+    <Box
+      sx={{
+        backgroundColor: "#f5f5f5",
+        padding: "10px",
+        borderRadius: "5px",
+        marginTop: "10px",
+        cursor: "pointer",
+        display: "flex",
+        alignItems: "center",
+      }}
+      onClick={handleCopy}
+      overflow={"auto"}
+    >
+      <Typography
+        sx={{
+          fontFamily: "monospace",
+          whiteSpace: "pre",
+          overflowX: "auto",
+          flexGrow: 1,
+        }}
+      >
+        {text}
+      </Typography>
+
+      <Tooltip title={copied ? "Copied!" : "Copy"}>
+        <IconButton onClick={handleCopy} size="small">
+          {copied ? (
+            <CheckIcon fontSize="small" />
+          ) : (
+            <ContentCopyIcon fontSize="small" />
+          )}
+        </IconButton>
+      </Tooltip>
+    </Box>
+  );
+}
+
+function useInformationFromJobLog(
+  workflowId: number,
+  jobId: number,
+  failureLineNum: number
+) {
+  const logURL = `https://ossci-raw-job-status.s3.amazonaws.com/log/${jobId}`;
+  const { data: log } = useSWR<string | undefined>(
     logURL,
     async (url) => {
       try {
@@ -74,131 +313,45 @@ export function OSDCommandInstructions({
     }
   );
 
-  if (log.data === undefined) {
-    return <div>Unable to fetch job log to generate repro instructions.</div>;
+  if (log === undefined) {
+    return undefined;
   }
 
-  const { gpuType, dockerImage, binaryURL, envVars, command } =
-    useInformationFromJobLog(workflowId, log.data, failureLineNum);
-
-  if (command === undefined || binaryURL === undefined || gpuType === undefined) {
-    return <div>Unable to fetch information about the job to create repro instructions</div>
-  }
-
-  return (
-    <Stack spacing={2}>
-      <Typography variant="h6">OSDC gpu-dev Repro Instructions</Typography>
-      <Typography variant="body1">
-        This is only available to Meta employees.
-        You can use the OSDC gpu-dev CLI to reserve a machine similar to the one used in CI and reproduce the
-        failure. Follow the instructions below to set up the gpu-dev CLI, reserve
-        a machine, and run the command that failed in the CI job.
-      </Typography>
-
-      <InstructionItem
-        index={1}
-        title="Set up OSDC gpu-dev CLI"
-        component={
-          <Typography variant="body1">
-            Follow the instructions{" "}
-            <a href={STARTING_INSTRUCTIONS} target="_blank" rel="noreferrer">
-              here
-            </a>{" "}
-            to set up the OSDC gpu-dev CLI.
-          </Typography>
-        }
-      />
-
-      <InstructionItem
-        index={2}
-        title="Reserve a machine"
-        component={
-          <Typography variant="body1">
-            Run the following command to reserve a machine similar to the one
-            used in CI:
-            <Box
-              component="pre"
-              sx={{
-                backgroundColor: "#f5f5f5",
-                padding: "10px",
-                borderRadius: "5px",
-                marginTop: "10px",
-              }}
-            >
-              gpu-dev reserve --gpu-type {gpuType.toLowerCase()}{" "}
-              {dockerImage
-                ? `--dockerimage ${dockerImage}`
-                : "--no-docker"}
-            </Box>
-          </Typography>
-        }
-      />
-
-      <InstructionItem
-        index={3}
-        title="SSH into the reserved machine"
-        component={
-          <Typography variant="body1">
-            After reserving, ssh into the machine using the command provided by
-            gpu-dev.
-          </Typography>
-        }
-      />
-
-
-    </Stack>
-  );
-}
-
-function InstructionItem({index, title, component}: {index: number, title: string, component: React.ReactNode}) {
-  return (
-    <Box>
-      <Typography variant="subtitle1" fontWeight="bold">
-        Step {index}: {title}
-      </Typography>
-      <Box mt={1} mb={2}>
-        {component}
-      </Box>
-    </Box>
-  );
-}
-
-function useInformationFromJobLog(
-  workflowId: number,
-  log: string,
-  failureLineNum: number
-) {
   const noTimeStampLogLines = log
     .split("\n")
     .map((line) => line.slice("2025-10-22T19:23:27.2253709Z ".length));
-
-  const noTimeStampLog = noTimeStampLogLines.join("\n");
 
   // Docker image
   const dockerImage = noTimeStampLogLines
     .find((line) => line.startsWith("docker pull ghcr.io/pytorch/ci-image:"))
     ?.slice("docker pull ".length);
 
-  // https://gha-artifacts.s3.amazonaws.com/pytorch/pytorch/18726871000/linux-jammy-py3.10-gcc11-pch/artifacts.zip
+  if (dockerImage === undefined) {
+    return undefined;
+  }
+
+  // Binary URL
   const buildEnvironment = noTimeStampLogLines
     .find((line) => line.startsWith("  build-environment: "))
     ?.slice("  build-environment: ".length);
-  const binaryURL = buildEnvironment
-    ? `https://gha-artifacts.s3.amazonaws.com/pytorch/pytorch/${workflowId}/${buildEnvironment}/artifacts.zip`
-    : undefined;
+  if (buildEnvironment === undefined) {
+    return undefined;
+  }
+  // https://gha-artifacts.s3.amazonaws.com/pytorch/pytorch/18726871000/linux-jammy-py3.10-gcc11-pch/artifacts.zip
+  const binaryURL = `https://gha-artifacts.s3.amazonaws.com/pytorch/pytorch/${workflowId}/${buildEnvironment}/artifacts.zip`;
 
   // Command
   const reproCommandLine = noTimeStampLogLines
-    .slice(failureLineNum - 1)
+    .slice(0, failureLineNum - 1)
     .findLastIndex((line) =>
       line.startsWith(
         "To execute this test, run the following from the base repo dir:"
       )
     );
-  const command =
-    reproCommandLine !== -1
-      ? noTimeStampLogLines[reproCommandLine + 1].trim()
-      : undefined;
+  if (reproCommandLine === -1) {
+    return undefined;
+  }
+  const command = noTimeStampLogLines[reproCommandLine + 1].trim();
 
   // Machine type
   const runnerType = noTimeStampLogLines
@@ -214,10 +367,14 @@ function useInformationFromJobLog(
   }
 
   // envVars
-  // Current just PATH
+  // Currently just PATH
   const path = noTimeStampLogLines
     .find((line) => line.startsWith("PATH="))
     ?.slice("PATH=".length);
+
+  if (path === undefined) {
+    return undefined;
+  }
 
   return {
     gpuType,
