ce582d00e3441182d753515a00511d8bf5dc333f
Yang Wang <elainewy@meta.com>
2025-11-20 17:31:11 -0700

    Not call list util metadata in workflow box (#7503)
    we run into some memory issues, so improve some utilization queries:
    
    1. list_util_metadata is called when a commit page is loaded, let's just
    show the button when a job is finished, if no util is record, let the
    page handle it.
    2. change the util api to job_utilization, and not call the api unless
    the workflowId and jobId is ready

--- torchci/pages/api/job_utilization/[workflowId]/[jobId]/[attempt].ts
+++ torchci/pages/api/job_utilization/[workflowId]/[jobId]/[attempt].ts
@@ -2,15 +2,30 @@ import { getErrorMessage } from "lib/error_utils";
 import fetchUtilization from "lib/utilization/fetchUtilization";
 import { UtilizationParams } from "lib/utilization/types";
 import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "pages/api/auth/[...nextauth]";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const { workflowId, jobId, attempt } = req.query;
-  if (workflowId === undefined || jobId === undefined || attempt == undefined) {
+
+  // @ts-ignore
+  const session = await getServerSession(req, res, authOptions);
+  if (!session?.user || !session?.accessToken) {
+    return res
+      .status(401)
+      .json({ error: "Authentication required to require utilization data" });
+  }
+
+  if (!workflowId || !jobId || !attempt) {
+    console.log(
+      "[api job_utilization][warning] No workflowId, jobId, or attempt provided"
+    );
     return res.status(200).json({});
   }
+
   const params: UtilizationParams = {
     workflow_id: workflowId as string,
     run_attempt: attempt as string,
