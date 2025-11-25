34bb47a97c2d2f22945ddf57596e6719e6f6f480
Yang Wang <elainewy@meta.com>
2025-11-25 14:32:32 -0700

    hidemetadata

--- torchci/pages/api/auth/[...nextauth].ts
+++ torchci/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GithubProvider from "next-auth/providers/github";
 
 const isPreview = process.env.VERCEL_ENV === "preview";
 
