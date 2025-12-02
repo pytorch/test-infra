488d7512c135ebd82f68bbf4326d86b2351733f1
Yang Wang <elainewy@meta.com>
2025-12-02 09:36:53 +0900

    hidemetadata

--- aws/lambda/benchmark_regression_summary_report/common/config.py
+++ aws/lambda/benchmark_regression_summary_report/common/config.py
@@ -190,8 +190,8 @@ COMPILER_BENCHMARK_CONFIG = BenchmarkConfig(
     source=BenchmarkApiSource(
         api_query_url="https://hud.pytorch.org/api/benchmark/get_time_series",
         type="benchmark_time_series_api",
-        # currently we only detect the regression for h100 with dtype bfloat16, and mode inference
-        # we can extend this to other devices, dtypes and mode in the future
+        # currently we only detect the regression for h100,b200 with dtype bfloat16,amp,
+        # and float16 with mode inference,training
         api_endpoint_params_template="""
                 {
                   "name": "compiler_precompute",
