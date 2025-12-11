e0448418ea2a2fa1c83c90e4b3e2132bae9cb10e
Jithun Nair <37884920+jithunnair-amd@users.noreply.github.com>
2025-12-11 12:47:40 -0600

    Use rocm-mi300 as default label for any ROCm PRs

--- torchci/lib/bot/autoLabelBot.ts
+++ torchci/lib/bot/autoLabelBot.ts
@@ -23,7 +23,7 @@ const IssueAndPRRegexToLabel: [RegExp, string][] = [
 const PrTitleRegexToLabel: [RegExp, string][] = [
   [/reland/gi, "ci-no-td"],
   [/revert/gi, "ci-no-td"],
-  [/rocm/gi, "ciflow/rocm"],
+  [/rocm/gi, "ciflow/rocm-mi300"],
   ...IssueAndPRRegexToLabel,
 ];
 
