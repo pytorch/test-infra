dc1a89217fe5a989cf4ade0c5ab9e713de553a29
atalman <atalman@fb.com>
2025-12-15 11:42:23 -0800

    lint

--- tools/pkg-helpers/pytorch_pkg_helpers/version.py
+++ tools/pkg-helpers/pytorch_pkg_helpers/version.py
@@ -104,9 +104,7 @@ def get_version_variables(
 ) -> List[str]:
     version = PytorchVersion(
         gpu_arch_version=gpu_arch_version,
-        no_build_suffix=(
-            platform == "darwin"
-        ),
+        no_build_suffix=(platform == "darwin"),
         base_build_version=base_build_version,
     )
     output_version = version.get_nightly_version()
