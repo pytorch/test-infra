87200453f92403e2e1fa6eec431c2d59411e7a25
Zain Rizvi <zainr@meta.com>
2026-04-20 11:47:37 -0700

    Revert "Update Windows AMI for 13.2.1 (#7971)"
    This reverts commit 385d3db2784bcf1d724b2f2fbd2dc5440d76d44f.

--- aws/ami/windows/scripts/Installers/Install-CUDA-Tools.ps1
+++ aws/ami/windows/scripts/Installers/Install-CUDA-Tools.ps1
@@ -35,7 +35,7 @@ Switch ($cudaVersion) {
   }
   "13.2" {
     $cudnn_subfolder="cudnn-windows-x86_64-9.20.0.48_cuda13-archive"
-    $toolkitInstaller = "cuda_13.2.1_windows.exe"
+    $toolkitInstaller = "cuda_13.2.0_windows.exe"
     $installerArgs = ""
   }
 }
